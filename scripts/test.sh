0cc659637d4f9c1d697426f27d15b40ef11dc85c
Ravi Rahman <ravi@mosaicml.com>
2021-11-18 10:24:47 -0800

    testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ JUNIT_PREFIX=${JUNIT_PREFIX:-'build/output/composer'}
 
 mkdir -p $(dirname $JUNIT_PREFIX)
 
-python -m composer -n 1 --master_port 26000 -m coverage run -m pytest --junitxml $JUNIT_PREFIX.n1.junit.xml $@
-python -m composer -n 2 --master_port 26000 -m coverage run -m pytest --junitxml $JUNIT_PREFIX.n2.junit.xml $@
+python -m composer.cli.launcher -n 1 --master_port 26000 -m coverage run -m pytest --junitxml $JUNIT_PREFIX.n1.junit.xml $@
+python -m composer.cli.launcher -n 2 --master_port 26000 -m coverage run -m pytest --junitxml $JUNIT_PREFIX.n2.junit.xml $@
