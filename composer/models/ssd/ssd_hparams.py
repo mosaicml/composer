a595aed08b3b9f4cf5dc0885e27565a4a067068e
Laura Florescu <lara.florescu@gmail.com>
2021-11-19 13:13:59 -0800

    add draft

---
+++ composer/models/ssd/ssd_hparams.py
@@ -0,0 +1,13 @@
+# Copyright 2021 MosaicML. All Rights Reserved.
+
+from dataclasses import dataclass
+
+from composer.models.model_hparams import ModelHparams
+
+
+@dataclass
+class SSDHparams(ModelHparams):
+
+    def initialize_object(self):
+        from composer.models.unet.unet import SSD
+        return SSD(self)
