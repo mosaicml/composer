3ebc2870bb7cd395256d228d4b7c03abcc51ddf7
Landan Seguin <landanjs@gmail.com>
2021-11-28 22:36:32 +0000

    Add option to sync bn

--- composer/models/deeplabv3/deeplabv3.py
+++ composer/models/deeplabv3/deeplabv3.py
@@ -6,10 +6,27 @@ from torchmetrics.collections import MetricCollection
 from torchvision.models import resnet50
 from torchvision.models.segmentation import deeplabv3_resnet50
 
+from composer.core.surgery import replace_module_classes
 from composer.core.types import BatchPair, Tensor, Tensors
 from composer.models.base import BaseMosaicModel
 from composer.models.deeplabv3.deeplabv3_hparams import DeepLabv3Hparams
 from composer.models.loss import CrossEntropyLoss, mIoU, soft_cross_entropy
+
+
+def bn_to_syncbn(module, module_index):
+    new_module = torch.nn.SyncBatchNorm(num_features=module.num_features,
+                                        eps=module.eps,
+                                        momentum=module.momentum,
+                                        affine=module.affine,
+                                        track_running_stats=module.track_running_stats)
+    new_module.running_mean = module.running_mean
+    new_module.running_var = module.running_var
+
+    if module.affine:
+        new_module.weight.data = module.weight.data.clone().detach()
+        new_module.bias.data = module.bias.data.clone().detach()
+
+    return new_module
 
 
 class DeepLabv3(BaseMosaicModel):
@@ -22,9 +39,12 @@ class DeepLabv3(BaseMosaicModel):
             backbone = resnet50(pretrained=True, progress=False)
             del backbone.fc
             self.model.backbone.load_state_dict(backbone.state_dict())
+        if self.hparams.sync_bn:
+            policy = {torch.nn.BatchNorm2d: bn_to_syncbn}
+            replace_module_classes(self.model, policy)
 
-    def forward(self, batch: BatchPair
-               ):  # Should the forward pass take a batch pair? We shouldn't expect the forward pass to have labels
+    # Should the forward pass take a batch pair? We shouldn't expect the forward pass to have labels
+    def forward(self, batch: BatchPair):
         x, _ = batch
 
         context = contextlib.nullcontext if self.training else torch.no_grad  # should this be added?
--- composer/models/deeplabv3/deeplabv3_hparams.py
+++ composer/models/deeplabv3/deeplabv3_hparams.py
@@ -10,6 +10,7 @@ class DeepLabv3Hparams(ModelHparams):
     num_classes: int = hp.required("Number of classes for classification")
     ignore_index: int = hp.optional("Class index to ignore for loss calculations", default=0)
     is_pretrained: bool = hp.optional("Whether or not to use a pretrained ImageNet backbone", default=False)
+    sync_bn: bool = hp.optional("Whether or not to sync the batch statistics across devices", default=False)
 
     def initialize_object(self):
         from composer.models.deeplabv3.deeplabv3 import DeepLabv3
