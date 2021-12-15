5d8e63c9e0a268682b8f2041cf0a6667beac1474
Davis Blalock <davis@mosaicml.com>
2021-12-15 15:21:55 -0800

    add missing license line

--- composer/algorithms/stratify_batches/__init__.py
+++ composer/algorithms/stratify_batches/__init__.py
@@ -1,3 +1,5 @@
+# Copyright 2021 MosaicML. All Rights Reserved.
+
 from composer.algorithms.stratify_batches.stratify import StratifyBatches as StratifyBatches
 from composer.algorithms.stratify_batches.stratify import StratifyBatchesHparams as StratifyBatchesHparams
 from composer.algorithms.stratify_batches.stratify_core import StratifiedBatchSampler as StratifiedBatchSampler
