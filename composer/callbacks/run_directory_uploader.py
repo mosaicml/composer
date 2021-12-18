mosaicml/composer

commit bc1c87b8b69574ddfe5e90db50c429f409f68d7c

WandB and run directory improvements (#162)
* Artifact uploading in the wandb and run directory uploader are guarded with ddp barriers. These barriers ensure that no other callbacks on other ranks are writing to the run directory.
* Per discussion with WandB, creating artifacts for only modified files, not all files.
* When running tests, setting the run directory to a subfolder named for the test. This replaces the `ddp_tmpdir`. This change ensures each test has a clean run directory.

composer/callbacks/run_directory_uploader.py
@@ -20,7 +20,7 @@ from composer.core.logging import Logger
 from composer.core.logging.logger import LogLevel
 from composer.core.state import State
 from composer.utils import ddp
-from composer.utils.run_directory import get_run_directory
+from composer.utils.run_directory import get_modified_files, get_run_directory
 
 log = logging.getLogger(__name__)
 
@@ -206,47 +206,43 @@ class RunDirectoryUploader(Callback):
         # Assuming only the main thread on each rank writes to the run directory, then the barrier here will ensure
         # that the run directory is not being modified after we pass this barrier
         ddp.barrier()
-        if not ddp.get_local_rank() == 0:
-            return
-        run_directory = get_run_directory()
-        assert run_directory is not None, "invariant error"
-        # the disk time can differ from system time, so going to touch a file and then read the timestamp from it to get the real time
-        python_time = time.time()
-        touch_file = (pathlib.Path(run_directory) / f".{python_time}")
-        touch_file.touch()
-        new_last_uploaded_timestamp = os.path.getmtime(str(touch_file))
+        if ddp.get_local_rank() == 0:
+            run_directory = get_run_directory()
+            assert run_directory is not None, "invariant error"
+            # the disk time can differ from system time, so going to touch a file and then read the timestamp from it to get the real time
+            python_time = time.time()
+            touch_file = (pathlib.Path(run_directory) / f".{python_time}")
+            touch_file.touch()
+            new_last_uploaded_timestamp = os.path.getmtime(str(touch_file))
 
-        # Now, for each file that was modified since self._last_upload_timestamp, copy it to the temporary directory
-        files_to_be_uploaded = []
+            # Now, for each file that was modified since self._last_upload_timestamp, copy it to the temporary directory
+            files_to_be_uploaded = []
 
-        # check if any upload threads have crashed. if so, then shutdown the training process
-        for worker in self._workers:
-            if not worker.is_alive():
-                assert self._finished is not None, "invariant error"
-                self._finished.set()
-                raise RuntimeError("Upload worker crashed unexpectedly")
-        for root, dirs, files in os.walk(run_directory):
-            del dirs  # unused
-            for file in files:
-                if any(x.startswith(".") for x in file.split(os.path.sep)):
-                    # skip hidden files and folders
-                    continue
-                filepath = os.path.join(root, file)
+            # check if any upload threads have crashed. if so, then shutdown the training process
+            for worker in self._workers:
+                if not worker.is_alive():
+                    assert self._finished is not None, "invariant error"
+                    self._finished.set()
+                    raise RuntimeError("Upload worker crashed unexpectedly")
+            modified_files = get_modified_files(self._last_upload_timestamp)
+            for filepath in modified_files:
                 relpath = os.path.relpath(filepath, run_directory)  # chop off the run directory
-                modified_time = os.path.getmtime(filepath)
-                if modified_time > self._last_upload_timestamp:
-                    copied_path = os.path.join(self._upload_staging_folder, str(new_last_uploaded_timestamp), relpath)
-                    files_to_be_uploaded.append(relpath)
-                    copied_path_dirname = os.path.dirname(copied_path)
-                    os.makedirs(copied_path_dirname, exist_ok=True)
-                    shutil.copy2(filepath, copied_path)
-                    self._file_upload_queue.put_nowait(copied_path)
-        self._last_upload_timestamp = new_last_uploaded_timestamp
-        if logger is not None and log_level is not None:
-            # now log which files are being uploaded. OK to do, since we're done reading the directory,
-            # and any logfiles will now have their last modified timestamp
-            # incremented past self._last_upload_timestamp
-            logger.metric(log_level, {"run_directory/uploaded_files": files_to_be_uploaded})
+                copied_path = os.path.join(self._upload_staging_folder, str(new_last_uploaded_timestamp), relpath)
+                files_to_be_uploaded.append(relpath)
+                copied_path_dirname = os.path.dirname(copied_path)
+                os.makedirs(copied_path_dirname, exist_ok=True)
+                shutil.copy2(filepath, copied_path)
+                self._file_upload_queue.put_nowait(copied_path)
+
+            self._last_upload_timestamp = new_last_uploaded_timestamp
+            if logger is not None and log_level is not None:
+                # now log which files are being uploaded. OK to do, since we're done reading the directory,
+                # and any logfiles will now have their last modified timestamp
+                # incremented past self._last_upload_timestamp
+                logger.metric(log_level, {"run_directory/uploaded_files": files_to_be_uploaded})
+
+        # Ensure that other callbacks do not start writing to the run directory until we copying everything
+        ddp.barrier()
 
 
 def _validate_credentials(

composer/utils/run_directory.py
@@ -2,6 +2,8 @@
 
 import logging
 import os
+import pathlib
+import time
 
 log = logging.getLogger(__name__)
 
@@ -12,8 +14,56 @@ def get_run_directory():
     return os.environ.get(_RUN_DIRECTORY_KEY)
 
 
-def get_relative_to_run_directory(path: str, base: str = ".") -> str:
+def get_relative_to_run_directory(*path: str, base: str = ".") -> str:
     run_directory = get_run_directory()
     if run_directory is None:
-        return os.path.join(base, path)
-    return os.path.join(run_directory, path)
+        raise ValueError("shouldn't be triggered")
+        return os.path.join(base, *path)
+    return os.path.join(run_directory, *path)
+
+
+def get_modified_files(modified_since_timestamp: float, *, ignore_hidden: bool = True):
+    """Returns a list of files (recursively) in the run directory that have been modified since
+    ``modified_since_timestamp``.
+
+    Args:
+        modified_since_timestamp (float): Minimum last modified timestamp(in seconds since EPOCH)
+            of files to include.
+        ignore_hidden (bool, optional): Whether to ignore hidden files and folders (default: ``True``)
+    Returns:
+        List[str]: List of filepaths that have been modified since ``modified_since_timestamp``
+    """
+    modified_files = []
+    run_directory = get_run_directory()
+    if run_directory is None:
+        raise RuntimeError("Run directory is not defined")
+    for root, dirs, files in os.walk(run_directory):
+        del dirs  # unused
+        for file in files:
+            if ignore_hidden and any(x.startswith(".") for x in file.split(os.path.sep)):
+                # skip hidden files and folders
+                continue
+            filepath = os.path.join(root, file)
+            modified_time = os.path.getmtime(filepath)
+            if modified_time >= modified_since_timestamp:
+                modified_files.append(filepath)
+    return modified_files
+
+
+def get_run_directory_timestamp() -> float:
+    """Returns the current timestamp on the run directory filesystem.
+    Note that the disk time can differ from system time (e.g. when using
+    network filesystems).
+
+    Returns:
+        float: the current timestamp on the run directory filesystem.
+    """
+    run_directory = get_run_directory()
+    if run_directory is None:
+        raise RuntimeError("Run directory is not defined")
+    python_time = time.time()
+    touch_file = (pathlib.Path(run_directory) / f".{python_time}")
+    touch_file.touch()
+    new_last_uploaded_timestamp = os.path.getmtime(str(touch_file))
+    os.remove(str(touch_file))
+    return new_last_uploaded_timestamp

composer/loggers/wandb_logger.py
@@ -6,14 +6,14 @@ import os
 import sys
 from typing import Any, Dict, Optional
 
-from composer.core.logging import LogLevel, RankZeroLoggerBackend, TLogData
+from composer.core.logging import BaseLoggerBackend, LogLevel, TLogData
 from composer.core.types import Logger, State, StateDict
-from composer.utils.run_directory import get_run_directory
+from composer.utils import ddp, run_directory
 
 import wandb  # isort: skip
 
 
-class WandBLoggerBackend(RankZeroLoggerBackend):
+class WandBLoggerBackend(BaseLoggerBackend):
     """Log to Weights and Biases (https://wandb.ai/)
 
     Args:
@@ -33,21 +33,26 @@ class WandBLoggerBackend(RankZeroLoggerBackend):
         super().__init__()
         self._log_artifacts = log_artifacts
         self._log_artifacts_every_n_batches = log_artifacts_every_n_batches
+        self._last_upload_timestamp = 0.0
         if init_params is None:
             init_params = {}
         self._init_params = init_params
 
     def _log_metric(self, epoch: int, step: int, log_level: LogLevel, data: TLogData):
         del epoch, log_level  # unused
-        wandb.log(data, step=step)
+        if ddp.get_local_rank() == 0:
+            wandb.log(data, step=step)
 
     def state_dict(self) -> StateDict:
         # Storing these fields in the state dict to support run resuming in the future.
+        if ddp.get_local_rank() != 0:
+            raise RuntimeError("WandB can only be checkpointed on rank 0")
         return {"name": wandb.run.name, "project": wandb.run.project, "entity": wandb.run.entity, "id": wandb.run.id}
 
     def init(self, state: State, logger: Logger) -> None:
         del state, logger  # unused
-        wandb.init(**self._init_params)
+        if ddp.get_local_rank() == 0:
+            wandb.init(**self._init_params)
 
     def batch_end(self, state: State, logger: Logger) -> None:
         del logger  # unused
@@ -72,16 +77,25 @@ class WandBLoggerBackend(RankZeroLoggerBackend):
         # so uploads will not block the training loop
         # slowdown is likely from extra I/O of scanning the directory and/or
         # scheduling uploads
-        run_directory = get_run_directory()
-        if run_directory is not None:
-            for subfile in os.listdir(run_directory):
-                artifact = wandb.Artifact(name=subfile, type=subfile)
-                full_path = os.path.join(run_directory, subfile)
-                if os.path.isdir(full_path):
-                    artifact.add_dir(full_path)
-                else:
-                    artifact.add_file(full_path)
+        run_directory_name = run_directory.get_run_directory()
+        if run_directory_name is None:
+            return
+        # barrier that every process has reached this point and that
+        # previous callbacks are finished writing to the run directory
+        ddp.barrier()
+        if ddp.get_local_rank() == 0:
+            modified_files = run_directory.get_modified_files(self._last_upload_timestamp)
+            for modified_file in modified_files:
+                file_type = modified_file.split(".")[-1]
+                relpath = os.path.relpath(modified_file, run_directory_name)
+                relpath = relpath.replace("/", "-")
+                artifact = wandb.Artifact(name=relpath, type=file_type)
+                artifact.add_file(os.path.abspath(modified_file))
                 wandb.log_artifact(artifact)
+            self._last_upload_timestamp = run_directory.get_run_directory_timestamp()
+        # barrier to ensure that other processes do not continue to other callbacks
+        # that could start writing to the run directory
+        ddp.barrier()
 
     def post_close(self) -> None:
         # Cleaning up on post_close so all artifacts are uploaded
@@ -90,8 +104,9 @@ class WandBLoggerBackend(RankZeroLoggerBackend):
 
         exc_tpe, exc_info, tb = sys.exc_info()
 
-        if (exc_tpe, exc_info, tb) == (None, None, None):
-            wandb.finish(0)
-        else:
-            # record there was an error
-            wandb.finish(1)
+        if ddp.get_local_rank() == 0:
+            if (exc_tpe, exc_info, tb) == (None, None, None):
+                wandb.finish(0)
+            else:
+                # record there was an error
+                wandb.finish(1)

tests/conftest.py
@@ -13,16 +13,16 @@ import _pytest.fixtures
 import _pytest.mark
 import pytest
 import torch.distributed
+from _pytest.monkeypatch import MonkeyPatch
 
 import composer
-from composer.utils.run_directory import get_relative_to_run_directory
+from composer.utils import run_directory
 
 # Allowed options for pytest.mark.world_size()
 # Important: when updating this list, make sure to also up scripts/test.sh
 # so tests of all world sizes will be executed
 WORLD_SIZE_OPTIONS = (1, 2)
 
-PYTEST_DDP_LOCKFILE_DIR = get_relative_to_run_directory(os.path.join("pytest_lockfiles"))
 DDP_TIMEOUT = datetime.timedelta(seconds=5)
 
 # Add the path of any pytest fixture files you want to make global
@@ -112,12 +112,13 @@ def set_loglevels():
     logging.getLogger(composer.__name__).setLevel(logging.DEBUG)
 
 
-@pytest.fixture
-def ddp_tmpdir(tmpdir: pathlib.Path) -> str:
+@pytest.fixture(autouse=True)
+def subfolder_run_directory(tmpdir: pathlib.Path, monkeypatch: MonkeyPatch) -> None:
     tmpdir_test_folder_name = os.path.basename(os.path.normpath(str(tmpdir)))
-    test_folder_tmpdir = get_relative_to_run_directory(tmpdir_test_folder_name, base=os.path.join(str(tmpdir), ".."))
+    test_folder_tmpdir = run_directory.get_relative_to_run_directory(tmpdir_test_folder_name,
+                                                                     base=os.path.join(str(tmpdir), ".."))
+    monkeypatch.setenv(run_directory._RUN_DIRECTORY_KEY, test_folder_tmpdir)
     os.makedirs(test_folder_tmpdir, exist_ok=True)
-    return os.path.abspath(test_folder_tmpdir)
 
 
 @pytest.fixture(autouse=True)
@@ -139,24 +140,24 @@ def configure_ddp(request: pytest.FixtureRequest):
 
 
 @pytest.fixture(autouse=True)
-def wait_for_all_procs(ddp_tmpdir: str):
+def wait_for_all_procs(subfolder_run_directory: None):
     yield
     if not 'RANK' in os.environ:
         # Not running in a DDP environment
         return
     global_rank = int(os.environ['RANK'])
     world_size = int(os.environ['WORLD_SIZE'])
-    proc_lockfile = os.path.join(ddp_tmpdir, f"{global_rank}_finished")
+    proc_lockfile = run_directory.get_relative_to_run_directory(f"{global_rank}_finished")
     pathlib.Path(proc_lockfile).touch(exist_ok=False)
     # other processes shouldn't be (too) far behind the current one
     end_time = datetime.datetime.now() + datetime.timedelta(seconds=15)
     for rank in range(world_size):
-        if not os.path.exists(os.path.join(ddp_tmpdir, f"{rank}_finished")):
+        if not os.path.exists(run_directory.get_relative_to_run_directory(f"{rank}_finished")):
             # sleep for the other procs to write their finished file
             if datetime.datetime.now() < end_time:
                 time.sleep(0.1)
             else:
-                test_name = os.path.basename(os.path.normpath(ddp_tmpdir))
+                test_name = os.path.basename(os.path.normpath(str(run_directory.get_run_directory())))
                 raise RuntimeError(f"Rank {rank} did not finish test {test_name}")
 
 

tests/test_amp.py
@@ -32,7 +32,7 @@ def run_and_measure_memory(precision: Precision) -> int:
 
 @pytest.mark.timeout(60)
 @pytest.mark.gpu
-def test_fp16_mixed(ddp_tmpdir: str):
+def test_fp16_mixed():
     memory_full = run_and_measure_memory(Precision.FP32)
     memory_amp = run_and_measure_memory(Precision.AMP)
     assert memory_amp < 0.7 * memory_full

tests/test_logger.py
@@ -13,7 +13,8 @@ from composer.core.event import Event
 from composer.core.logging import Logger, LogLevel
 from composer.core.state import State
 from composer.loggers.file_logger import FileLoggerBackend
-from composer.loggers.logger_hparams import FileLoggerBackendHparams, TQDMLoggerBackendHparams
+from composer.loggers.logger_hparams import (FileLoggerBackendHparams, TQDMLoggerBackendHparams,
+                                             WandBLoggerBackendHparams)
 from composer.trainer.trainer_hparams import TrainerHparams
 
 
@@ -90,3 +91,15 @@ def test_tqdm_logger(mosaic_trainer_hparams: TrainerHparams, monkeypatch: Monkey
     for mock_tqdm in is_train_to_mock_tqdms[False]:
         assert mock_tqdm.update.call_count == trainer._eval_subset_num_batches
         mock_tqdm.close.assert_called_once()
+
+
+@pytest.mark.parametrize("world_size", [
+    pytest.param(1),
+    pytest.param(2, marks=pytest.mark.world_size(2)),
+])
+@pytest.mark.timeout(10)
+def test_wandb_logger(mosaic_trainer_hparams: TrainerHparams, world_size: int, monkeypatch: MonkeyPatch):
+    del world_size  # unused. Set via launcher script
+    mosaic_trainer_hparams.loggers = [WandBLoggerBackendHparams(log_artifacts=True, log_artifacts_every_n_batches=1)]
+    trainer = mosaic_trainer_hparams.initialize_object()
+    trainer.fit()

tests/trainer/test_checkpoint.py
@@ -18,7 +18,7 @@ from composer.core.types import Logger, StateDict
 from composer.trainer.devices import CPUDeviceHparams, DeviceHparams, GPUDeviceHparams
 from composer.trainer.trainer import Trainer
 from composer.trainer.trainer_hparams import TrainerHparams, callback_registry
-from composer.utils import ddp
+from composer.utils import ddp, run_directory
 from tests.test_state import assert_state_equivalent
 from tests.utils.deep_compare import deep_compare
 
@@ -139,7 +139,6 @@ def test_checkpoint(
     checkpoint_filename: str,
     validate_every_n_batches: int,
     validate_every_n_epochs: int,
-    ddp_tmpdir: str,
 ):
     """strategy:
     - train two epochs. capture checkpoints after `checkpoint_interval` and ep2.
@@ -147,32 +146,28 @@ def test_checkpoint(
     - assert that the checkpoint from the new trainer at the end is the same as the checkpoint from the first trainer at the end.
     """
     del world_size  # unused. Read via env variable
-    checkpoint_a_folder = os.path.join(ddp_tmpdir, "first")
-    checkpointing_trainer_hparams.checkpoint_folder = checkpoint_a_folder
 
+    checkpointing_trainer_hparams.device = device_hparams
+
+    checkpoint_a_folder = "first"
+    checkpointing_trainer_hparams.checkpoint_folder = checkpoint_a_folder
     checkpointing_trainer_hparams.checkpoint_interval_unit = "ep" if checkpoint_filename.startswith("ep") else "it"
     checkpointing_trainer_hparams.validate_every_n_batches = validate_every_n_batches
     checkpointing_trainer_hparams.validate_every_n_epochs = validate_every_n_epochs
     final_checkpoint = "ep2.pt" if checkpoint_filename.startswith("ep") else "it8.pt"
-
-    checkpointing_trainer_hparams.device = device_hparams
-
     _test_checkpoint_trainer(checkpointing_trainer_hparams)
     checkpoint_a_file_path = os.path.join(checkpoint_a_folder, f"{checkpoint_filename}.pt")
-    checkpoint_b_file_path = os.path.join(checkpoint_a_folder, final_checkpoint)
-    trainer_1_hparams_filepath = os.path.join(checkpoint_a_folder, "hparams.yaml")
+    checkpoint_b_file_path = run_directory.get_relative_to_run_directory(checkpoint_a_folder, final_checkpoint)
+    trainer_1_hparams_filepath = run_directory.get_relative_to_run_directory(checkpoint_a_folder, "hparams.yaml")
 
     second_trainer_hparams = TrainerHparams.create(trainer_1_hparams_filepath, cli_args=False)
-    checkpoint_b_folder = os.path.join(ddp_tmpdir, "second")
+    checkpoint_b_folder = "second"
     second_trainer_hparams.checkpoint_folder = checkpoint_b_folder
-    second_trainer_hparams.checkpoint_filepath = checkpoint_a_file_path
+    second_trainer_hparams.checkpoint_filepath = run_directory.get_relative_to_run_directory(checkpoint_a_file_path)
     _test_checkpoint_trainer(second_trainer_hparams)
 
-    checkpoint_c_file_path = os.path.join(ddp_tmpdir, "checkpoint_c.pt")
-    trainer_2_hparams_filepath = os.path.join(ddp_tmpdir, "trainer_2_hparams.yaml")
-
-    checkpoint_c_file_path = os.path.join(checkpoint_b_folder, final_checkpoint)
-    trainer_2_hparams_filepath = os.path.join(checkpoint_b_folder, "hparams.yaml")
+    checkpoint_c_file_path = run_directory.get_relative_to_run_directory(checkpoint_b_folder, final_checkpoint)
+    trainer_2_hparams_filepath = run_directory.get_relative_to_run_directory(checkpoint_b_folder, "hparams.yaml")
 
     if ddp.get_global_rank() == 0:
 

commit 450e505b19dfbb4ca381afdbcce7bec2735baa3a

Fixed the run directory uploader test
The issue was introduced in #162 with how the run directory was monkeypatched for the tests. This monkeypatching did not apply to subprocessing. Changed uploading to use UUIDs when copying to the staging directory to avoid conflicts.

composer/callbacks/run_directory_uploader.py
@@ -12,8 +12,9 @@ import sys
 import tempfile
 import threading
 import time
+import uuid
 import warnings
-from typing import Any, Callable, Dict, Optional, Type, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
 
 from composer.core.callback import Callback
 from composer.core.logging import Logger
@@ -136,8 +137,8 @@ class RunDirectoryUploader(Callback):
 
         if use_procs:
             mp_ctx = multiprocessing.get_context('spawn')
-            self._file_upload_queue: Union[queue.Queue[str],
-                                           multiprocessing.JoinableQueue[str]] = mp_ctx.JoinableQueue()
+            self._file_upload_queue: Union[queue.Queue[Tuple[str, str]],
+                                           multiprocessing.JoinableQueue[Tuple[str, str]]] = mp_ctx.JoinableQueue()
             self._finished_cls: Union[Callable[[], multiprocessing._EventType], Type[threading.Event]] = mp_ctx.Event
             self._proc_class = mp_ctx.Process
         else:
@@ -163,7 +164,6 @@ class RunDirectoryUploader(Callback):
                              kwargs={
                                  "file_queue": self._file_upload_queue,
                                  "is_finished": self._finished,
-                                 "upload_staging_dir": self._upload_staging_folder,
                                  "provider_name": self._provider,
                                  "container_name": self._container,
                                  "object_name_prefix": self._object_name_prefix,
@@ -200,6 +200,8 @@ class RunDirectoryUploader(Callback):
             self._finished.set()
         for worker in self._workers:
             worker.join()
+        if self._tempdir is not None:
+            self._tempdir.cleanup()
 
     def _trigger_upload(self, logger: Optional[Logger], log_level: Optional[LogLevel]) -> None:
         # Ensure that every rank is at this point
@@ -227,12 +229,12 @@ class RunDirectoryUploader(Callback):
             modified_files = get_modified_files(self._last_upload_timestamp)
             for filepath in modified_files:
                 relpath = os.path.relpath(filepath, run_directory)  # chop off the run directory
-                copied_path = os.path.join(self._upload_staging_folder, str(new_last_uploaded_timestamp), relpath)
+                copied_path = os.path.join(self._upload_staging_folder, str(uuid.uuid4()))
                 files_to_be_uploaded.append(relpath)
                 copied_path_dirname = os.path.dirname(copied_path)
                 os.makedirs(copied_path_dirname, exist_ok=True)
                 shutil.copy2(filepath, copied_path)
-                self._file_upload_queue.put_nowait(copied_path)
+                self._file_upload_queue.put_nowait((copied_path, relpath))
 
             self._last_upload_timestamp = new_last_uploaded_timestamp
             if logger is not None and log_level is not None:
@@ -266,7 +268,6 @@ def _validate_credentials(
 def _upload_worker(
     file_queue: Union[queue.Queue[str], multiprocessing.JoinableQueue[str]],
     is_finished: Union[multiprocessing._EventType, threading.Event],
-    upload_staging_dir: str,
     provider_name: str,
     container_name: str,
     object_name_prefix: str,
@@ -281,7 +282,6 @@ def _upload_worker(
             set when training is finished and no new files will be added to the queue.
             The worker will continue to upload existing files that are in the queue.
             When the queue is empty, the worker will exit.
-        upload_staging_dir (str): The upload staging directory.
         provider_name (str): The cloud provider name.
         container_name (str): The container name (e.g. s3 bucket) for the provider
             where files will be uploaded.
@@ -297,19 +297,18 @@ def _upload_worker(
     container = provider.get_container(container_name)
     while True:
         try:
-            file_path_to_upload = file_queue.get(block=True, timeout=0.5)
+            file_path_to_upload, obj_name = file_queue.get(block=True, timeout=0.5)
         except queue.Empty:
             if is_finished.is_set():
                 break
             else:
                 continue
-        obj_name = os.path.sep.join(os.path.relpath(file_path_to_upload, upload_staging_dir).split(
-            os.path.sep)[1:])  # the first folder is the upload timestamp. Chop that off.
         log.info("Uploading file %s to %s://%s/%s%s", file_path_to_upload, provider_name, container_name,
                  object_name_prefix, obj_name)
         retry_counter = 0
         while True:
             try:
+                assert os.path.exists(file_path_to_upload)
                 provider.upload_object(
                     file_path=file_path_to_upload,
                     container=container,

composer/utils/run_directory.py
@@ -17,7 +17,6 @@ def get_run_directory():
 def get_relative_to_run_directory(*path: str, base: str = ".") -> str:
     run_directory = get_run_directory()
     if run_directory is None:
-        raise ValueError("shouldn't be triggered")
         return os.path.join(base, *path)
     return os.path.join(run_directory, *path)
 

tests/callbacks/test_run_directory_uploader.py
@@ -12,12 +12,7 @@ from composer.core.state import State
 from composer.utils.run_directory import get_run_directory
 
 
-# This test is flaky see: https://github.com/mosaicml/composer/issues/176
 @pytest.mark.parametrize("use_procs", [False, True])
-# TODO(ravi) -- remove the pytest.in #110. The TRAINING_END event is likely slow as it has to copy many
-# files created by the ddp test. #110 grately reduces the number of files from the DDP test.
-@pytest.mark.timeout(15)
-@pytest.mark.xfail
 def test_run_directory_uploader(tmpdir: pathlib.Path, use_procs: bool, dummy_state: State, dummy_logger: Logger):
     dummy_state.epoch = 0
     dummy_state.step = 0