f9478bc201efa7a02aa1506ac23510a7072e3788
Ravi Rahman <ravi@mosaicml.com>
2021-12-17 18:27:27 -0800

    Added imports to __init__.py

--- composer/core/__init__.py
+++ composer/core/__init__.py
@@ -8,3 +8,5 @@ from composer.core.engine import Trace as Trace
 from composer.core.event import Event as Event
 from composer.core.logging import Logger as Logger
 from composer.core.state import State as State
+from composer.core.time import Time as Time
+from composer.core.time import Timer as Timer
