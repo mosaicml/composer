c2fe018c12a2384b9fad9fc56c8150555a714643
Moin Nadeem <moin@mosaicml.com>
2021-12-30 01:08:37 +0000

    renaming Checkpointer -> CheckpointSaver

--- composer/trainer/checkpoint_hparams.py
+++ composer/trainer/checkpoint_hparams.py
@@ -2,11 +2,10 @@
 
 import logging
 from dataclasses import dataclass
-from typing import Optional
 
 import yahp as hp
 
-from composer.trainer.checkpoint import Checkpointer, CheckpointLoader
+from composer.trainer.checkpoint import CheckpointLoader, CheckpointSaver
 
 log = logging.getLogger(__name__)
 
@@ -18,13 +17,12 @@ class CheckpointLoaderHparams(hp.Hparams):
     See the documentation for the :class:`CheckpointLoader`.
     """
     filepath: str = hp.required(doc="Path to the serialized state_dict to recover state from.")
-    load_weights_only: Optional[bool] = hp.optional(doc="Whether to only load the weights from the model.",
-                                                    default=False)
-    strict: Optional[bool] = hp.optional(
-        doc="Ensure that the set of weights in the checkpoint and model must exactly match.", default=True)
+    load_weights_only: bool = hp.optional(doc="Whether to only load the weights from the model.", default=False)
+    strict_model_weights: bool = hp.optional(
+        doc="Ensure that the set of weights in the checkpoint and model must exactly match.", default=False)
 
     def validate(self):
-        if not self.load_weights_only and self.strict:
+        if not self.load_weights_only and self.strict_model_weights:
             raise ValueError(
                 "Strict cannot be used when load_weights_only is true. Restoring a checkpoint from previous state assumes that the checkpoint should perfectly match the model."
             )
@@ -32,14 +30,14 @@ class CheckpointLoaderHparams(hp.Hparams):
     def initialize_object(self) -> CheckpointLoader:
         return CheckpointLoader(checkpoint_filepath=self.filepath,
                                 load_weights_only=self.load_weights_only,
-                                strict=self.strict)
+                                strict_model_weights=self.strict_model_weights)
 
 
 @dataclass
-class CheckpointerHparams(hp.Hparams):
-    """Hparams for the :class:`Checkpointer`.
+class CheckpointSaverHparams(hp.Hparams):
+    """Hparams for the :class:`CheckpointSaver`.
 
-    See the documentation for the :class:`Checkpointer`.
+    See the documentation for the :class:`CheckpointSaver`.
     """
     interval_unit: str = hp.required(
         doc="Unit for the checkpoint save interval -- should be 'ep' for epochs; 'it' for iterations")
@@ -54,7 +52,7 @@ class CheckpointerHparams(hp.Hparams):
         if self.interval_unit not in ['ep', 'it']:
             raise ValueError("Checkpointing interval unit must be one of 'ep' for epochs, or 'it' for iterations.")
 
-    def initialize_object(self) -> Checkpointer:
-        return Checkpointer(checkpoint_interval_unit=self.interval_unit,
-                            checkpoint_interval=self.interval,
-                            checkpoint_folder=self.folder)
+    def initialize_object(self) -> CheckpointSaver:
+        return CheckpointSaver(checkpoint_interval_unit=self.interval_unit,
+                               checkpoint_interval=self.interval,
+                               checkpoint_folder=self.folder)
