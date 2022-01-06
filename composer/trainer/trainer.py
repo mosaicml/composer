1b6df4dea26374219edf5d6ffc9415ae0d6fef5d
Davis Blalock <davis@mosaicml.com>
2022-01-05 20:41:16 -0800

    fix broken filelogging with stratify batches from merge of dev

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -355,11 +355,6 @@ class Trainer:
 
         model = hparams.model.initialize_object()
 
-        # callbacks, loggers, and seed
-        callbacks = [x.initialize_object() for x in hparams.callbacks]
-        dict_config = hparams.to_dict()
-        log_destinations = [x.initialize_object(config=dict_config) for x in hparams.loggers]
-
         if hparams.datadir is not None:
             hparams.train_dataset.datadir = hparams.datadir
             hparams.val_dataset.datadir = hparams.datadir
@@ -379,6 +374,11 @@ class Trainer:
             (set to {hparams.eval_subset_num_batches}), val_dataset.shuffle should be set to False. Otherwise,
             each evaluation epoch may load a different subset of samples."""))
         eval_dataloader = hparams.val_dataset.initialize_object(eval_device_batch_size, hparams.dataloader)
+
+        # callbacks, loggers, and seed
+        callbacks = [x.initialize_object() for x in hparams.callbacks]
+        dict_config = hparams.to_dict()
+        log_destinations = [x.initialize_object(config=dict_config) for x in hparams.loggers]
 
         # Checkpoint loading hparams
         checkpoint_filepath = hparams.load_checkpoint.filepath if hparams.load_checkpoint is not None else None
