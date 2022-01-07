2f7762bbca25086637e5b2fc02c057632f4b3ab6
austin <jacobsonaustinj@gmail.com>
2022-01-06 19:00:31 -0800

    allow image size and dropout rates to be changed through yaml

--- composer/models/vit_small_patch16/hparams.py
+++ composer/models/vit_small_patch16/hparams.py
@@ -1,12 +1,25 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
 from dataclasses import asdict, dataclass
+import yahp as hp
+
 
 from composer.models.model_hparams import ModelHparams
 
 
 @dataclass
 class ViTSmallPatch16Hparams(ModelHparams):
+    
+    dropout: float = hp.optional("dropout rate", default=0.0),
+    embedding_dropout: float = hp.optional("embedding dropout rate", default=0.0)
+
+    def validate(self):
+        if self.num_classes is None:
+            raise ValueError("num_classes must be specified")
+        if self.image_size is None:
+            raise ValueError("image_size must be specified")
+        if self.channels is None:
+            raise ValueError("channels must be specified")
 
     def initialize_object(self):
         from composer.models import ViTSmallPatch16
--- composer/models/vit_small_patch16/model.py
+++ composer/models/vit_small_patch16/model.py
@@ -1,9 +1,8 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
-from typing import List, Optional
+from typing import Optional
 
 from composer.models.base import MosaicClassifier
-from composer.models.model_hparams import Initializer
 
 
 class ViTSmallPatch16(MosaicClassifier):
@@ -13,22 +12,29 @@ class ViTSmallPatch16(MosaicClassifier):
 
     Args:
         num_classes (int): The number of classes for the model.
+        image_size (int): input image size. If you have rectangular images, make sure your image size is the maximum of the width and height
+        channels (int): number of  image channels.
+        dropout (float): 0.0 - 1.0 dropout rate.
+        embedding_dropout (float): 0.0 - 1.0 embedding dropout rate.
+
     """
-    def __init__(self, num_classes: int,
-                 initializers: Optional[List[Initializer]] = None,
+    def __init__(self,
+                 num_classes: int,
+                 image_size: int = 224,
+                 channels: int = 3,
+                 dropout: Optional[float] = 0.0,
+                 embedding_dropout: Optional[float] = 0.0
                  ) -> None:
-        if initializers is None:
-            initializers = [] # not using initializers right now
         from vit_pytorch import ViT
-        model = ViT(image_size=224,
-                    channels=3,
+        model = ViT(image_size=image_size,
+                    channels=channels,
                     num_classes=num_classes,
                     dim=384,  # embed dim/width
                     patch_size=16,
                     depth=12,  # layers
                     heads=6,
                     mlp_dim=1536,
-                    dropout=0.1,
-                    emb_dropout=0.1
+                    dropout=dropout,
+                    emb_dropout=embedding_dropout
                     )
         super().__init__(module=model)
