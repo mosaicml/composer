1d77070f94c98cf665c7469c6178ce9f5c5600d1
ravi-mosaicml <87037432+ravi-mosaicml@users.noreply.github.com>
2022-01-14 15:07:55 -0800

    Make the run directory rank-local; fix checkpoints saving and restoring (#215)
    Fixed the run directory and deepspeed checkpointing:
    
    - Creating a run directory by default, if one was not set via an environment variable
    - Removed `run_directory.get_relative_to_run_directory(...)`; replaced that with `os.path.join(run_directory.get_run_directory(...))`
    - Having one run directory per node (i.e. sharing the run directory across local ranks) won't work in multi-node training. This change makes the run directory rank-local (so each rank sees its own run directory)
    - Added a helper method `run_directory.get_node_run_directory()`. This should not really be used directly, but rather 
    - Switched the run directory uploader and wandb to run on every rank instead of just rank zero. This ensures that all artifacts will be properly stored.
    - Fixed deepspeed checkpointing:
      
      Specifically, when using deepspeed with zero-1+, each rank wrote to the folder specified. Only rank 0's data was being stored, meaning that we were losing rank 1+'s state (i.e. the optimizer state). Deepspeed checkpointing tests were broken. Now, each rank's data is being stored by the rank-local run directory uploader (see above bullet). Meaning, there are N files per checkpoint now.
    
      To restore a checkpoint, you need the global rank zero checkpoint (which contains the model) and the rank n checkpoint (which contains the optimizer state). To support this, the checkpoint loader takes a checkpoint path that is parameterized by the rank, so each rank can fetch the files it needs. As an optimization, the LOCAL rank zero on each node is responsible for downloading the GLOBAL rank zero checkpoint (it would be redundant to download this file multiple times per node).

--- tests/trainer/test_ddp.py
+++ tests/trainer/test_ddp.py
@@ -28,16 +28,12 @@ from tests.fixtures.models import SimpleBatchPairModel
 
 def get_file_path(*, rank: int, is_train: bool) -> str:
     train_str = "train" if is_train else "val"
-    rundir = run_directory.get_run_directory()
-    assert rundir is not None
-    return os.path.join(rundir, f"{train_str}_rank_{rank}_num_accesses")
+    return os.path.join(run_directory.get_node_run_directory(), f"rank_{rank}", f"{train_str}_num_accesses")
 
 
 def get_batch_file_path(*, rank: int, epoch: int, is_train: bool) -> str:
     train_str = "train" if is_train else "val"
-    rundir = run_directory.get_run_directory()
-    assert rundir is not None
-    return os.path.join(rundir, f"{train_str}-rank-{rank}-epoch-{epoch}-batch0.pt")
+    return os.path.join(run_directory.get_node_run_directory(), f"rank_{rank}", f"{train_str}-epoch-{epoch}-batch0.pt")
 
 
 class TrackedDataset(types.Dataset):
