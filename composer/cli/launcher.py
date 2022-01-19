58a985c65c4d45171ee90f8b89b8bbd0b9cd7c09
Moin Nadeem <moin@mosaicml.com>
2022-01-19 23:25:29 +0000

    adding prints everywhere to debug

--- composer/cli/launcher.py
+++ composer/cli/launcher.py
@@ -126,18 +126,19 @@ def launch_processes(nproc: int, world_size: int, base_rank: int, master_addr: s
 
         log.info("Launching process for local_rank(%s), global_rank(%s)", local_rank, global_rank)
 
-        if local_rank == 0:
-            process = subprocess.Popen(cmd, env=current_env, text=True)
-        else:
-            logs_dir = os.path.join(run_directory, f"rank_{global_rank}", "logs")
-            os.makedirs(logs_dir, exist_ok=True)
-            process = subprocess.Popen(
-                cmd,
-                env=current_env,
-                stdout=open(os.path.join(logs_dir, f"rank_{global_rank}.stdout.txt"), "x"),
-                stderr=open(os.path.join(logs_dir, f"rank_{global_rank}.stderr.txt"), "x"),
-                text=True,
-            )
+        process = subprocess.Popen(cmd, env=current_env, text=True)
+        # if local_rank == 0:
+            # process = subprocess.Popen(cmd, env=current_env, text=True)
+        # else:
+            # logs_dir = os.path.join(run_directory, f"rank_{global_rank}", "logs")
+            # os.makedirs(logs_dir, exist_ok=True)
+            # process = subprocess.Popen(
+                # cmd,
+                # env=current_env,
+                # stdout=open(os.path.join(logs_dir, f"rank_{global_rank}.stdout.txt"), "x"),
+                # stderr=open(os.path.join(logs_dir, f"rank_{global_rank}.stderr.txt"), "x"),
+                # text=True,
+            # )
         processes.append(process)
 
     return set(processes)
