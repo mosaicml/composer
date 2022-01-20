e252a7e5b1e4ac5e36b8b3066c5ddb016ca9cdf9
austin <jacobsonaustinj@gmail.com>
2022-01-20 01:45:50 -0800

    fix conv0 out planes

--- composer/models/densenet161/model.py
+++ composer/models/densenet161/model.py
@@ -10,19 +10,27 @@ from torch import nn
 
 class DenseNet161(MosaicClassifier):
     """Torchvision Densenet161 https://github.com/pytorch/vision/blob/main/torchvision/models/densenet.py"""
-    def __init__(
-        self,
-        num_classes: int,
-        num_channels: int,
-        dropout: float = 0.,
-        pretrained: bool = False,
-        initializers: Optional[List[Initializer]] = None,
-    ) -> None:
-        if initializers is None:
-            initializers = []
+
+    def __init__(self,
+                num_classes: int,
+                num_channels: int,
+                dropout: float = 0.0,
+                pretrained: bool = False) -> None:
+
+
         model = torchvision.models.densenet161(pretrained=pretrained, drop_rate=dropout)
-        model.features.conv0 = nn.Conv2d(num_channels, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
-        
+        if num_channels != 3:
+            model.features[0] = nn.Conv2d(
+                num_channels,
+                out_channels=96,
+                kernel_size=7,
+                stride=2,
+                padding=3,
+                bias=False,
+            )
+
         if num_classes != 1000:
-            model.classifier = nn.Linear(in_features=2208, out_features=num_classes, bias=True)
+            model.classifier = nn.Linear(
+                in_features=2208, out_features=num_classes, bias=True
+            )
         super().__init__(module=model)
