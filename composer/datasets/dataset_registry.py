f863a5e928504be821f0112a907e104caef1189b
Anis Ehsani <anis@mosaicml.com>
2022-01-21 20:49:06 +0000

    merge bug fix

--- composer/datasets/dataset_registry.py
+++ composer/datasets/dataset_registry.py
@@ -1,5 +1,6 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
+from composer.datasets.ade20k import ADE20kDatasetHparams
 from composer.datasets.brats import BratsDatasetHparams
 from composer.datasets.cifar10 import CIFAR10DatasetHparams
 from composer.datasets.glue import GLUEHparams
@@ -8,6 +9,7 @@ from composer.datasets.lm_datasets import LMDatasetHparams
 from composer.datasets.mnist import MNISTDatasetHparams
 
 registry = {
+    "ade20k": ADE20kDatasetHparams,
     "brats": BratsDatasetHparams,
     "imagenet": ImagenetDatasetHparams,
     "cifar10": CIFAR10DatasetHparams,
