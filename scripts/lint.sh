ede16fcf921c4fc8947fda27b7a7588ba8adf977
Ravi Rahman <ravi@mosaicml.com>
2022-02-01 16:30:46 -0800

    Added Pylint and docformatter
    Will address pylint errors later

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -13,6 +13,9 @@ python -m isort . -c -v
 # Run yapf (installed via setup.py)
 python -m yapf -dr . # not using -p since that can lead to race conditions
 
+# Run docformatter
+python -m docformatter -rc --wrap-summaries 120 --wrap-descriptions 120
+
 # Install and run addlicense
 # TODO(ravi): Switch to https://pypi.org/project/licenseheaders/ since it can be installed via setup.py and pip
 
