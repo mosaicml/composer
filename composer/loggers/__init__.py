5112ca809b4632107f51d1ec56a9fc10d4cec7f3
ravi-mosaicml <ravi@mosaicml.com>
2022-02-03 12:15:17 -0800

    Added In Memory Logger, Timestamp Object (#352)
    * Added In Memory Logger, Timestamp Object
    
    1. Added a Timestamp object to `composer.core.time`. The Timestamp object is Tuple that represents a snapshot of state.timer
    2. Updated the logging API to pass the Timestamp object rather than (epoch, step).
    3. Added an In memory logger that records all logging calls, with easy helpers to the most recent values for each key. This new feature required `Timestamp` objects so it is possible, say, to create a plot of samples or tokens vs a logged value. It also cleans up the API compared to storing (epoch, batch, sample, token) as a non-named tuple.
    
    Closes #294
    
    * Doc foramtter
    
    * wandb conditional import
    
    * Fix isort
    
    * Apply suggestions from code review
    
    * Fix Tests
    
    * Fixed docs
    
    * Fix test

--- composer/loggers/__init__.py
+++ composer/loggers/__init__.py
@@ -5,8 +5,14 @@ from composer.core.logging.logger import Logger as Logger
 from composer.core.logging.logger import LogLevel as LogLevel
 from composer.core.logging.logger import TLogData as TLogData
 from composer.core.logging.logger import TLogDataValue as TLogDataValue
+from composer.loggers.file_logger import FileLoggerBackend as FileLoggerBackend
+from composer.loggers.in_memory_logger import InMemoryLogger as InMemoryLogger
 from composer.loggers.logger_hparams import BaseLoggerBackendHparams as BaseLoggerBackendHparams
 from composer.loggers.logger_hparams import FileLoggerBackendHparams as FileLoggerBackendHparams
+from composer.loggers.logger_hparams import InMemoryLoggerHaparms as InMemoryLoggerHaparms
 from composer.loggers.logger_hparams import MosaicMLLoggerBackendHparams as MosaicMLLoggerBackendHparams
 from composer.loggers.logger_hparams import TQDMLoggerBackendHparams as TQDMLoggerBackendHparams
 from composer.loggers.logger_hparams import WandBLoggerBackendHparams as WandBLoggerBackendHparams
+from composer.loggers.mosaicml_logger import MosaicMLLoggerBackend as MosaicMLLoggerBackend
+from composer.loggers.tqdm_logger import TQDMLoggerBackend as TQDMLoggerBackend
+from composer.loggers.wandb_logger import WandBLoggerBackend as WandBLoggerBackend
