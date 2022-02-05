06182ec14e6d2ee843b698a8f8704847924aeda4
Matthew <growlix@users.noreply.github.com>
2022-02-04 17:31:27 -0800

    Updated add_dataset_transform() to have flexible insertion point (#320)
    * Updated add_dataset_transform() to have flexible insertion point
    
    * removed superfluous import
    
    * Clean up some typing
    
    * Update composer/utils/data.py
    
    * change assert to error
    
    * data.add_dataset_transform() is now generalized to insert around any transform instead of before/after ToTensor
    
    * removed logging
    
    * type check
    
    * removed unused import
    
    * removed dataset type checking in signature
    
    * removed type check to confirm as torchvision.VisionDataset
    
    * updated run_mosaic* -> run_composer*
    
    * added testing and warnings
    
    * minor fixes
    
    * simplified add_dataset_transform(), created SyntheticPILDataset class, tests
    
    * minor formatting:
    
    * Updated as per Ravi's review
    
    * removed unnecesssary imports
    
    * docstring formatting:
    
    Co-authored-by: Ravi Rahman <ravi@mosaicml.com>
    Co-authored-by: hanlint <hanlin@mosaicml.com>

--- tests/datasets/test_synthetic_data.py
+++ tests/datasets/test_synthetic_data.py
@@ -4,7 +4,8 @@ from typing import Optional
 
 import pytest
 
-from composer.datasets.synthetic import SyntheticBatchPairDataset, SyntheticDataLabelType, SyntheticDataType
+from composer.datasets.synthetic import (SyntheticBatchPairDataset, SyntheticDataLabelType, SyntheticDataType,
+                                         SyntheticPILDataset)
 
 
 @pytest.mark.parametrize('data_type', [
@@ -74,3 +75,59 @@ def test_synthetic_classification_param_validation(label_type: SyntheticDataLabe
                                   data_shape=(2, 2),
                                   label_type=label_type,
                                   num_classes=num_classes)
+
+
+@pytest.mark.parametrize('data_type', [
+    SyntheticDataType.GAUSSIAN,
+    SyntheticDataType.SEPARABLE,
+])
+@pytest.mark.parametrize('label_type', [
+    SyntheticDataLabelType.CLASSIFICATION_ONE_HOT,
+    SyntheticDataLabelType.CLASSIFICATION_INT,
+])
+def test_synthetic_image_data_creation(data_type: SyntheticDataType, label_type: SyntheticDataLabelType):
+    if data_type == SyntheticDataType.SEPARABLE:
+        if label_type != SyntheticDataLabelType.CLASSIFICATION_INT:
+            pytest.skip("Seperable data requires classification int labels")
+        num_classes = 2
+        label_shape = None
+    else:
+        num_classes = 10
+        label_shape = (1, 10, 12)
+        # run run
+        return
+
+    dataset_size = 1000
+    data_shape = (32, 32)
+    num_samples_to_create = 100
+    dataset = SyntheticPILDataset(total_dataset_size=dataset_size,
+                                  data_shape=data_shape,
+                                  num_unique_samples_to_create=num_samples_to_create,
+                                  data_type=data_type,
+                                  label_type=label_type,
+                                  num_classes=num_classes,
+                                  label_shape=label_shape)
+    assert len(dataset) == dataset_size
+
+    # verify datapoints are correct
+    x, y = dataset[0]
+    assert x.size == data_shape
+    if label_type == SyntheticDataLabelType.CLASSIFICATION_INT:
+        assert isinstance(y.item(), int)
+    elif label_type == SyntheticDataLabelType.CLASSIFICATION_ONE_HOT:
+        assert y.size() == (num_classes,)
+        assert min(y) == 0
+        assert max(y) == 1
+
+    # check that points were allocated in memory after the first call to __getitem__
+    assert dataset.input_data is not None
+    assert dataset.input_target is not None
+    # check that the correct number of points were allocated in memory
+    assert dataset.input_data.shape[0] == num_samples_to_create
+    assert dataset.input_target.shape[0] == num_samples_to_create
+
+    # verify that you can getch points outside the num_samples_to_create range
+    # (still within the total dataset size range)
+    x, y = dataset[num_samples_to_create + 1]
+    assert x is not None
+    assert y is not None
