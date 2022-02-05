97aae1fbe73865c72f67b8e371b69cb6a7aa9197
Matthew Leavitt <matthew@mosaicml.com>
2022-02-05 07:51:28 +0000

    replicated torch.optim.swa_utils.update_bn in swa because torch update_bn not compatible with ComposerClassifier

--- composer/algorithms/swa/swa.py
+++ composer/algorithms/swa/swa.py
@@ -6,11 +6,71 @@ import logging
 from typing import Optional
 
 import torch
-from torch.optim.swa_utils import SWALR, AveragedModel, update_bn
+from torch.optim.swa_utils import SWALR, AveragedModel
 
-from composer.core.types import Algorithm, Event, Logger, State
+from composer.core.types import Algorithm, DataLoader, Event, Logger, State
 
 log = logging.getLogger(__name__)
+
+
+@torch.no_grad()
+def update_bn(loader: DataLoader, model: torch.nn.Module, device: torch.device):
+    """Updates BatchNorm running_mean, running_var buffers in the model.
+
+    It performs one pass over data in `loader` to estimate the activation
+    statistics for BatchNorm layers in the model.
+    Args:
+        loader (torch.utils.data.DataLoader): dataset loader to compute the
+            activation statistics on. Each data batch should be either a
+            tensor, or a list/tuple whose first element is a tensor
+            containing data.
+        model (torch.nn.Module): model for which we seek to update BatchNorm
+            statistics.
+        device (torch.device, optional): If set, data will be transferred to
+            :attr:`device` before being passed into :attr:`model`.
+
+    Example:
+        >>> loader, model = ...
+        >>> torch.optim.swa_utils.update_bn(loader, model)
+
+    .. note::
+        The `update_bn` utility assumes that each data batch in :attr:`loader`
+        is either a tensor or a list or tuple of tensors; in the latter case it
+        is assumed that :meth:`model.forward()` should be called on the first
+        element of the list or tuple corresponding to the data batch.
+    """
+    momenta = {}
+    for module in model.modules():
+        if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
+            assert module.running_mean is not None
+            module.running_mean = torch.zeros_like(module.running_mean)
+            assert module.running_var is not None
+            module.running_var = torch.ones_like(module.running_var)
+            momenta[module] = module.momentum
+
+    if not momenta:
+        return
+
+    was_training = model.training
+    model.train()
+    for module in momenta.keys():
+        module.momentum = None
+        module.num_batches_tracked *= 0
+
+    for data in loader:
+        import composer.trainer.devices as devices
+        if device.type == "cuda":
+            composer_device = devices.DeviceGPU()
+        elif device.type == "cpu":
+            composer_device = devices.DeviceCPU()
+        else:
+            raise ValueError("`device` must be one of 'cuda', 'cpu'")
+        data = composer_device.batch_to_device(data)
+        model(data)
+
+    for bn_module in momenta.keys():
+        bn_module.momentum = momenta[bn_module]
+    model.train(was_training)
 
 
 class SWA(Algorithm):
