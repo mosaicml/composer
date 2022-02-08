711d0edf0e77ddd2ee138f80cc1295207bb849e7
ravi-mosaicml <ravi@mosaicml.com>
2022-02-08 09:55:26 -0800

    Remove the `walkthrough` section of the docs; replace with module-level docstrings (#417)
    Removed the composer/walkthough docs since that will be replaced by "using our trainer" section. Instead, put all content that was previously in composer/walkthrough into the appropriate module-level docstrings.
    Also fixed the autodoc tables for public exports of private reimports.

--- composer/core/time.py
+++ composer/core/time.py
@@ -1,5 +1,17 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
+"""Track training progress in terms of epochs, batches, samples, and tokens.
+
+Callbacks, algorithms, and schedulers can use the current training time to fire at certain points in the training process.
+
+The :class:`~composer.core.Timer` class tracks the total number of epochs, batches, samples, and tokens.
+The trainer is responsible for updating the :class:`~composer.core.Timer` at the end of every epoch and batch.
+There is only one instance of the :class:`~composer.core.Timer`, which is attached to the :class:`~composer.core.State`.
+
+The :class:`~composer.core.Time` class represents static durations of training time or points in the
+training process in terms of a specific :class:`~composer.core.TimeUnit` enum. The :class:`~composer.core.Time` class
+supports comparisons, arithmetic, and conversions.
+"""
 from __future__ import annotations
 
 import re
