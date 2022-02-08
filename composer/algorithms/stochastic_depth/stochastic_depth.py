a0d61387a49883d94b7a8d57dcda8cd34508e073
Ajay Saini <ajay@mosaicml.com>
2022-02-07 16:00:06 -0800

    Typo fix

--- composer/algorithms/stochastic_depth/stochastic_depth.py
+++ composer/algorithms/stochastic_depth/stochastic_depth.py
@@ -118,7 +118,7 @@ def apply_stochastic_depth(model: torch.nn.Module,
         target_layer_name: Block to replace with a stochastic block
             equivalent. The name must be registered in ``STOCHASTIC_LAYER_MAPPING``
             dictionary with the target layer class and the stochastic layer class.
-            Currently, only ``'ResNetBottleneck'`` is supported.
+            Currently, only ``torchvision.models.resnet.Bottleneck`` is supported.
         optimizers (Optimizers, optional):  Existing optimizers bound to ``model.parameters()``.
             All optimizers that have already been constructed with,
             ``model.parameters()`` must be specified here so they will optimize
@@ -203,7 +203,7 @@ class StochasticDepth(Algorithm):
         target_layer_name: Block to replace with a stochastic block
             equivalent. The name must be registered in ``STOCHASTIC_LAYER_MAPPING``
             dictionary with the target layer class and the stochastic layer class.
-            Currently, only ``'ResNetBottleneck'`` is supported.
+            Currently, only ``torchvision.models.resnet.Bottleneck`` is supported.
         drop_rate: The base probability of dropping a layer or sample. Must be
             between 0.0 and 1.0.
         drop_distribution: How ``drop_rate`` is distributed across
