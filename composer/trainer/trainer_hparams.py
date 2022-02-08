4830675cc9e81ecb15e5ae9e493c9e2fffcd5355
Hanlin Tang <hanlin@mosaicml.com>
2022-02-08 18:10:55 +0000

    add in_memory to registry

--- composer/trainer/trainer_hparams.py
+++ composer/trainer/trainer_hparams.py
@@ -22,8 +22,8 @@ from composer.core.types import JSON, Precision
 from composer.datasets import DataloaderHparams
 from composer.datasets.dataset_registry import get_dataset_registry
 from composer.datasets.evaluator import EvaluatorHparams
-from composer.loggers import (FileLoggerHparams, LoggerCallbackHparams, MosaicMLLoggerHparams, TQDMLoggerHparams,
-                              WandBLoggerHparams)
+from composer.loggers import (FileLoggerHparams, InMemoryLoggerHaparms, LoggerCallbackHparams, MosaicMLLoggerHparams,
+                              TQDMLoggerHparams, WandBLoggerHparams)
 from composer.models import (BERTForClassificationHparams, BERTHparams, CIFARResNet9Hparams, CIFARResNetHparams,
                              DeepLabV3Hparams, EfficientNetB0Hparams, GPT2Hparams, MnistClassifierHparams, ModelHparams,
                              ResNetHparams, TimmHparams, UnetHparams)
@@ -96,6 +96,7 @@ logger_registry = {
     "wandb": WandBLoggerHparams,
     "tqdm": TQDMLoggerHparams,
     "mosaicml": MosaicMLLoggerHparams,
+    "in_memory": InMemoryLoggerHaparms,
 }
 
 device_registry = {
