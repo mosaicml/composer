f3c742545a0716b1875a72dfce09279e2d8d8458
Niklas Q Nielsen <niklas@mosaicml.com>
2022-02-10 10:52:33 -0800

    Added panels dependency

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ extra_deps['dev'] = [
     'myst-parser>=0.15.2',
     'pylint>=2.12.2',
     'docformatter>=1.4',
+    'sphinx_panels>=0.6.0',
 ]
 
 extra_deps['logging'] = ['wandb>=0.12.2', 'apache-libcloud>=3.4.1']
