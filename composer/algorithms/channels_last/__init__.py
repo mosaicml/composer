a7d4662f8d7a6670501f252b62c62d049aa87a27
Hanlin Tang <hanlin@mosaicml.com>
2022-02-11 22:21:56 +0000

    move metadata to json file

--- composer/algorithms/channels_last/__init__.py
+++ composer/algorithms/channels_last/__init__.py
@@ -2,11 +2,3 @@
 
 from composer.algorithms.channels_last.channels_last import ChannelsLast as ChannelsLast
 from composer.algorithms.channels_last.channels_last import apply_channels_last as apply_channels_last
-
-_name = 'Channels Last'
-_class_name = 'ChannelsLast'
-_functional = ''
-_tldr = 'Uses channels last memory format (NHWC)'
-_attribution = 'PyTorch'
-_link = 'https://pytorch.org/tutorials/intermediate/memory_format_tutorial.html'
-_method_card = ''
--- composer/algorithms/scale_schedule/__init__.py
+++ composer/algorithms/scale_schedule/__init__.py
@@ -3,9 +3,9 @@
 from composer.algorithms.scale_schedule.scale_schedule import ScaleSchedule as ScaleSchedule
 
 _name = 'Scale Schedule'
-_class_name = 'ScaleScheduler'
-_functional = 'scale_scheduler'
+_class_name = 'ScaleSchedule'
+_functional = ''
 _tldr = 'Scales the learning schedule by a factor'
-_attribution = ''
-_link = ''
+_attribution = 'Many'
+_link = 'www.mosaicml.com'
 _method_card = ''
