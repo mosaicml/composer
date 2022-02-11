cb9e8e43b2e9ce90c6379b7a8f7b6db9a315dffe
Cory Stephenson <cory@mosaicml.com>
2022-02-11 22:47:31 +0000

    Update ColOut docstrings

--- composer/algorithms/colout/__init__.py
+++ composer/algorithms/colout/__init__.py
@@ -1,5 +1,11 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
+"""Drops a fraction of the rows and columns of an input image. If the fraction of
+rows/columns dropped isn't too large, this does not significantly alter the content
+of the image, but reduces its size and provides extra variability. See the
+:doc:`Method Card </method_cards/col_out>` for more details.
+"""
+
 from composer.algorithms.colout.colout import ColOut as ColOut
 from composer.algorithms.colout.colout import colout_batch as colout_batch
 from composer.algorithms.colout.colout import colout_image as colout_image
@@ -10,4 +16,6 @@ _functional = 'colout_batch'
 _tldr = 'Removes columns and rows from the image for augmentation and efficiency.'
 _attribution = 'MosaicML'
 _link = ''
-_method_card = ''
+_method_card = 'docs/source/method_cards/col_out.md'
+
+__all__ = ["ColOut", "ColOutTransform", "colout_image", "colout_batch"]
