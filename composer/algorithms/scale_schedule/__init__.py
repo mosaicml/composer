e778d643894482180b5821d71f93a68e8860e6b7
Hanlin Tang <hanlin@mosaicml.com>
2022-02-15 00:01:50 +0000

    clean up ssr init

--- composer/algorithms/scale_schedule/__init__.py
+++ composer/algorithms/scale_schedule/__init__.py
@@ -1,11 +1,3 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
 from composer.algorithms.scale_schedule.scale_schedule import ScaleSchedule as ScaleSchedule
-
-_name = 'Scale Schedule'
-_class_name = 'ScaleSchedule'
-_functional = ''
-_tldr = 'Scales the learning schedule by a factor'
-_attribution = 'Many'
-_link = 'www.mosaicml.com'
-_method_card = ''
