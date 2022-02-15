ad3c1e227136cdad64bf8118c690d65967d12080
Cory Stephenson <cory@mosaicml.com>
2022-02-15 20:28:19 +0000

    Update label smoothing docstrings

--- composer/algorithms/label_smoothing/__init__.py
+++ composer/algorithms/label_smoothing/__init__.py
@@ -1,5 +1,11 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
+"""Shrinks targets towards a uniform distribution to counteract label noise as in `Szegedy et al
+<https://arxiv.org/abs/1512.00567>`_. Introduced in `Rethinking the Inception Architecture for
+Computer Vision <https://arxiv.org/abs/1512.00567>`_. See the :doc:`Method Card </method_cards/label_smoothing>`
+for more details.
+"""
+
 from composer.algorithms.label_smoothing.label_smoothing import LabelSmoothing as LabelSmoothing
 from composer.algorithms.label_smoothing.label_smoothing import smooth_labels as smooth_labels
 
@@ -9,4 +15,6 @@ _functional = 'smooth_labels'
 _tldr = 'Smooths the labels with a uniform prior'
 _attribution = '(Szegedy et al, 2015)'
 _link = 'https://arxiv.org/abs/1512.00567'
-_method_card = ''
+_method_card = 'docs/source/method_cards/label_smoothing.md'
+
+__all__ = ["LabelSmoothing", "smooth_labels"]
