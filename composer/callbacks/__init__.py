aa97b22b452e46a018d3909447ed5f5024db4476
Tyler Lee <tyler@mosaicml.com>
2022-02-15 14:09:46 -0800

    Add back benchmarker callback

--- composer/callbacks/__init__.py
+++ composer/callbacks/__init__.py
@@ -41,6 +41,8 @@ Callbacks can be implemented in two ways:
                 if event == Event.EPOCH_START:
                     print(f'Epoch {state.epoch}/{state.max_epochs}')
 """
+from composer.callbacks.benchmarker import Benchmarker
+from composer.callbacks.callback_hparams import BenchmarkerHparams as BenchmarkerHparams
 from composer.callbacks.callback_hparams import CallbackHparams as CallbackHparams
 from composer.callbacks.callback_hparams import GradMonitorHparams as GradMonitorHparams
 from composer.callbacks.callback_hparams import LRMonitorHparams as LRMonitorHparams
@@ -53,6 +55,7 @@ from composer.callbacks.run_directory_uploader import RunDirectoryUploader
 from composer.callbacks.speed_monitor import SpeedMonitor
 
 __all__ = [
+    "Benchmarker",
     "GradMonitor",
     "LRMonitor",
     "RunDirectoryUploader",
