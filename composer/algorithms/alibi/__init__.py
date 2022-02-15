041460a8b542bd932e1f22f2c2ce961aa4346ca6
hanlint <hanlin@mosaicml.com>
2022-02-15 08:18:11 +0800

    Store `metadata` in json files for `algorithms` (#471)

--- composer/algorithms/alibi/__init__.py
+++ composer/algorithms/alibi/__init__.py
@@ -3,11 +3,3 @@
 from composer.algorithms.alibi import gpt2_alibi as gpt2_alibi
 from composer.algorithms.alibi.alibi import Alibi as Alibi
 from composer.algorithms.alibi.alibi import apply_alibi as apply_alibi
-
-_name = 'Alibi'
-_class_name = 'Alibi'
-_functional = 'apply_alibi'
-_tldr = 'Replace attention with AliBi'
-_attribution = '(Press et al, 2021)'
-_link = 'https://arxiv.org/abs/2108.12409v1'
-_method_card = ''
--- composer/algorithms/seq_length_warmup/__init__.py
+++ composer/algorithms/seq_length_warmup/__init__.py
@@ -3,11 +3,3 @@
 from composer.algorithms.seq_length_warmup.seq_length_warmup import SeqLengthWarmup as SeqLengthWarmup
 from composer.algorithms.seq_length_warmup.seq_length_warmup import \
     set_batch_sequence_length as set_batch_sequence_length
-
-_name = 'Sequential Length Warmup'
-_class_name = 'SeqLengthWarmup'
-_functional = 'apply_seq_length_warmup'
-_tldr = 'Progressively increase sequence length.'
-_attribution = '(Li et al, 2021)'
-_link = 'https://arxiv.org/abs/2108.06084'
-_method_card = ''
