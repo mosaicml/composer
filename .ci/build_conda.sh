7cde3ebd4a1c41a3106aeb751ebe9ec2d2159aa8
Ravi Rahman <ravi@mosaicml.com>
2022-02-17 14:24:49 -0800

    Using a base docker image with conda preinstalled

--- .ci/build_conda.sh
+++ .ci/build_conda.sh
@@ -6,46 +6,6 @@ set -euo pipefail
 # are executed. See `meta.yaml` for the conda package
 # configuration
 
-# Only use 'sudo' to run apt commands if not already root
-SUDO="sudo"
-if [ "$UID" == "0" ]; then
-    SUDO=""
-fi
-
-CONDA_PATH=$HOME/miniconda3
-
-# Download and install miniconda if it's not installed
-echo "Checking to see if conda is installed"
-if ! command -v conda &> /dev/null ; then
-    echo "Downloading and installing curl"
-    $SUDO apt-get update
-    $SUDO apt-get install -y --no-install-recommends curl ca-certificates
-    echo "Downloading conda"
-    curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output /tmp/miniconda.sh
-    chmod u+x /tmp/miniconda.sh
-    echo "Installing conda"
-    /tmp/miniconda.sh -b -f -p $CONDA_PATH 
-else
-    echo "Conda is already installed"
-    CONDA_PATH="$(conda info --base)"
-fi
-
-echo "Sourcing conda"
-source $CONDA_PATH/etc/profile.d/conda.sh
-
-echo "Checking to see if the 'composer' conda environment exists"
-if [[ "$(conda info --envs)" != *"composer"* ]];then
-    # If we don't already have a composer conda environment, create it
-    echo "Creating the 'composer' conda environment"
-    conda create -y -n composer
-else
-    echo "The 'composer' conda environment already exists"
-fi
-
-# Activate this environment
-echo "Activating the composer conda environment"
-conda activate composer
-
 # Prepare the conda package
 echo "Adding 'mosaicml' to the conda channels"
 conda config --add channels mosaicml
