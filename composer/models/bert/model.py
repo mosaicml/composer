f58945359ebdbfcc0af0e0ed62a91be9945210af
austin <jacobsonaustinj@gmail.com>
2022-02-18 12:57:16 -0800

    fix hf link

--- composer/models/bert/model.py
+++ composer/models/bert/model.py
@@ -19,9 +19,8 @@ __all__ = ["BERTModel"]
 
 
 class BERTModel(ComposerTransformer):
-    """Implements a BERT wrapper around a ComposerTransformer. Works with `Hugging Face Transformers.
-
-    <https://huggingface.co/transformers/>`_.
+    """Implements a BERT wrapper around a ComposerTransformer. Works with Hugging Face Transformers
+    `<https://huggingface.co/transformers/>`_.
 
     To create a bert model for Language Model pretraining:
 
