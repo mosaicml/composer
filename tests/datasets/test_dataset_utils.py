8655031d16a8bd5826e03c7e431f99b76aeba006
Landan Seguin <landanjs@gmail.com>
2022-02-19 09:59:17 -0800

    Remove parentheses from fixture

--- tests/datasets/test_dataset_utils.py
+++ tests/datasets/test_dataset_utils.py
@@ -8,32 +8,32 @@ from PIL import Image
 from composer.datasets.utils import pil_image_collate
 
 
-@pytest.fixture()
+@pytest.fixture
 def num_samples():
     return 4
 
 
-@pytest.fixture()
+@pytest.fixture
 def image_size():
     return (16, 16)
 
 
-@pytest.fixture()
+@pytest.fixture
 def pil_image_list(num_samples: int, image_size: Tuple[int, int]):
     return [Image.new(mode='RGB', size=image_size, color=(i, i, i)) for i in range(num_samples)]
 
 
-@pytest.fixture()
+@pytest.fixture
 def pil_target_list(num_samples: int, image_size: Tuple[int, int]):
     return [Image.new(mode='L', size=image_size, color=i) for i in range(num_samples)]
 
 
-@pytest.fixture()
+@pytest.fixture
 def correct_image_tensor(num_samples: int, image_size: Tuple[int, int]):
     return torch.arange(num_samples).expand(3, *image_size, -1).permute(3, 0, 1, 2)
 
 
-@pytest.fixture()
+@pytest.fixture
 def scalar_target_list(num_samples: int):
     return np.arange(num_samples)
 
