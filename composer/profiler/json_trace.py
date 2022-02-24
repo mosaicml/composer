f9bf0dbd6b14381f9e3755ab63f9c24c3e6e5200
Ravi Rahman <ravi@mosaicml.com>
2022-02-23 22:56:44 -0800

    Fix

--- composer/profiler/json_trace.py
+++ composer/profiler/json_trace.py
@@ -22,7 +22,7 @@ __all__ = ["JSONTraceHandler"]
 
 
 class JSONTraceHandler(ProfilerEventHandler):
-    """Records trace events in `JSON trace format<https://\\
+    """Records trace events in `JSON trace format <https://\\
     docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview>`_.
 
     Traces are output to ``output_directory``.  Traces can be visualized using the Chrome Trace Viewer.
