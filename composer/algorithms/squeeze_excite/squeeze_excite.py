a545178d447e7fe939b054fb4613223f0d2525ae
Davis Blalock <davis@mosaicml.com>
2022-02-24 18:50:46 -0800

    improve squeeze excite docstrings

--- composer/algorithms/squeeze_excite/squeeze_excite.py
+++ composer/algorithms/squeeze_excite/squeeze_excite.py
@@ -20,7 +20,39 @@ def apply_squeeze_excite(
     min_channels: int = 128,
     optimizers: Optional[Optimizers] = None,
 ):
-    """See :class:`SqueezeExcite`"""
+    """Adds Squeeze-and-Excitation blocks (`Hu et al, 2019 <https://arxiv.org/abs/1709.01507>`_) after Conv2d layers.
+
+    A Squeeze-and-Excitation block applies global average pooling to the input,
+    feeds the resulting vector to a single-hidden-layer fully-connected
+    network (MLP), and uses the output of this MLP as attention coefficients
+    to rescale the input. This allows the network to take into account global
+    information about each input, as opposed to only local receptive fields
+    like in a convolutional layer.
+
+    Args:
+        num_features (int, optional): Number of features or channels in the input
+        latent_channels (float, optional): Dimensionality of the hidden layer within the added
+            MLP. If less than 1, interpreted as a fraction of ``num_features``.
+        optimizers (Optimizers, optional):  Existing optimizers bound to ``model.parameters()``.
+            All optimizers that have already been constructed with
+            ``model.parameters()`` must be specified here so they will optimize
+            the correct parameters.
+
+            If the optimizer(s) are constructed *after* calling this function,
+            then it is safe to omit this parameter. These optimizers will see the correct
+            model parameters.
+
+    Returns:
+        The modified model
+
+    Example:
+        .. testcode::
+
+            import composer.functional as cf
+            from torchvision import models
+            model = models.resnet50()
+            cf.apply_stochastic_depth(model, target_layer_name='ResNetBottleneck')
+    """
 
     def convert_module(module: torch.nn.Module, module_index: int):
         assert isinstance(module, torch.nn.Conv2d), "should only be called with conv2d"
@@ -43,8 +75,8 @@ class SqueezeExcite2d(torch.nn.Module):
     as opposed to only local receptive fields like in a convolutional layer.
 
     Args:
-        num_features: Number of features or channels in the input
-        latent_channels: Dimensionality of the hidden layer within the added
+        num_features (int): Number of features or channels in the input
+        latent_channels (float, optional): Dimensionality of the hidden layer within the added
             MLP. If less than 1, interpreted as a fraction of ``num_features``.
     """
 
