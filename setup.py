7d7675cd33dc0236edfb9a67333b80faeb7ea186
Laura Florescu <laura@mosaicml.com>
2022-02-28 23:06:57 +0000

    change pycoco version

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ extra_deps['dev'] = [
     'jupyter>=1.0.0',
     'yamllint>=1.26.2',
     'pytest-timeout>=1.4.2',
-    'pycocotools>=2.0.1',
+    'pycocotools==2.0.1',
     'pyright==1.1.224.post1',
     'recommonmark==0.7.1',
     'sphinx>=4.2.0',
