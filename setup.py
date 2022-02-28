f2a2817fbc4c97aab20b849f74ea9c964326f172
ravi-mosaicml <ravi@mosaicml.com>
2022-02-28 11:54:11 -0800

    Fix Lint (#611)
    * Fix the run directory uploader doctest
    
    The doctest example was never actually running, but because it was launching subprocesses, it somehow did not trigger a failure exit code.
    
    * Fix lint issues
    
    1. Pin pyright version
    2. Fixed pyright issues from the version pinning
    
    * Fixed doctest with group
    
    * Fixed all doctests that patch env variables
    
    Must use doctest groups to ensure that env variables don't conflict

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ extra_deps['dev'] = [
     'jupyter>=1.0.0',
     'yamllint>=1.26.2',
     'pytest-timeout>=1.4.2',
-    'pyright>=0.0.13',
+    'pyright==1.1.224.post1',
     'recommonmark>=0.7.1',
     'sphinx>=4.2.0',
     'sphinx_copybutton>=0.4.0',
