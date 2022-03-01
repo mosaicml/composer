3741f535469888655c855a84276fe2073c4b3be8
root <nikhil@mosaic.com>
2022-03-01 20:28:00 +0000

    reduce test epochs

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -12,7 +12,7 @@ from composer.core.types import Precision
 from composer.datasets.hparams import SyntheticHparamsMixin
 from composer.trainer import TrainerHparams
 from composer.trainer.devices import GPUDeviceHparams
-
+import warnings
 
 def run_and_measure_memory(precision: Precision) -> int:
     hparams_f = os.path.join(os.path.dirname(composer.__file__), "yamls", "models", "resnet56_cifar10_synthetic.yaml")
@@ -22,7 +22,7 @@ def run_and_measure_memory(precision: Precision) -> int:
     hparams.precision = precision
     hparams.dataloader.num_workers = 0
     hparams.dataloader.persistent_workers = False
-    hparams.max_duration = "2ep"
+    hparams.max_duration = "1ep"
     assert isinstance(hparams.train_dataset, SyntheticHparamsMixin)
     hparams.train_dataset.use_synthetic = True
     assert isinstance(hparams.val_dataset, SyntheticHparamsMixin)
@@ -35,7 +35,7 @@ def run_and_measure_memory(precision: Precision) -> int:
     return torch.cuda.max_memory_allocated()
 
 
-@pytest.mark.timeout(150)
+@pytest.mark.timeout(120)
 @pytest.mark.gpu
 @pytest.mark.parametrize("precision", [Precision.AMP, Precision.BF16])
 def test_precision_memory(precision):
@@ -43,4 +43,5 @@ def test_precision_memory(precision):
         pytest.skip("Test required torch >= 1.10")
     memory_full = run_and_measure_memory(Precision.FP32)
     memory_precision = run_and_measure_memory(precision)
+    warnings.warn("Precision: " + str(precision) + " " + str(memory_full) + " " + str(memory_precision))
     assert memory_precision < 0.7 * memory_full
