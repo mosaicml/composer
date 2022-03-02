7df583fffa202648d6a3dcd0f181504831906391
Cory Stephenson <cory@mosaicml.com>
2022-03-02 20:59:44 +0000

    Switch to faster averaging

--- composer/algorithms/exponential_moving_average/exponential_moving_average.py
+++ composer/algorithms/exponential_moving_average/exponential_moving_average.py
@@ -16,11 +16,10 @@ log = logging.getLogger(__name__)
 __all__ = ["ExponentialMovingAverage", "exponential_moving_average"]
 
 
-def exponential_moving_average(model: Model, ema_model: Model, alpha: float) -> Model:
-    """Updates an exponentially weighted moving average of the model parameters"""
-    for key, param in ema_model.state_dict().items():
-        ema_model.state_dict()[key].data = alpha * param.data + (1 - alpha) * model.state_dict()[key].data
-    return ema_model
+def moving_average(model: Model, ema_model: Model, alpha: float = 1):
+    for param1, param2 in zip(ema_model.parameters(), model.parameters()):
+        param1.data *= alpha
+        param1.data += param2.data * (1.0 - alpha)
 
 
 class ExponentialMovingAverage(Algorithm):
@@ -38,16 +37,14 @@ class ExponentialMovingAverage(Algorithm):
     def apply(self, event: Event, state: State, logger: Logger) -> None:
         if event == Event.FIT_START:
             # Initialize the ema model
-            self.ema_model = copy.deepcopy(state.model)
+            self.ema_model = copy.deepcopy(state.model.module)
         if event == Event.BATCH_END:
             # Update the ema model
-            #self.ema_model = exponential_moving_average(state.model, self.ema_model, self.alpha)
-            pass
+            moving_average(state.model.module, self.ema_model, alpha=self.alpha)
         if event == Event.EVAL_START:
             # Swap out the training model for the ema model in state
-            self.training_model = state.model
-            state.model = self.ema_model
+            self.training_model = state.model.module
+            state.model.module = self.ema_model
         if event == Event.EVAL_END:
-            print(state.model)
             # Swap out the ema model for the training model in state
-            state.model = self.training_model
+            state.model.module = self.training_model
