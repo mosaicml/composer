fe1c27b8465ac08b63e02a65aed7e81cc7b5a95d
Ravi Rahman <ravi@mosaicml.com>
2022-03-02 22:13:25 -0800

    Composer.core docstring touch-up
    Fixed a few minor nits with composer.core docstrings

--- composer/core/time.py
+++ composer/core/time.py
@@ -11,6 +11,8 @@ responsible for updating it at the end of every epoch and batch.  There is only 
 
 The :class:`~.time.Time` class represents static durations of training time or points in the training process in terms
 of a specific :class:`~.time.TimeUnit` enum. This class supports comparisons, arithmetic, and conversions.
+
+See the :doc:`Time Guide </trainer/time>` for more details on tracking time during training.
 """
 from __future__ import annotations
 
@@ -59,6 +61,8 @@ TValue = TypeVar("TValue", int, float)
 class Time(Generic[TValue]):
     """Time represents static durations of training time or points in the training process in terms of a
     :class:`TimeUnit` enum (epochs, batches, samples, tokens, or duration).
+
+    See the :doc:`Time Guide </trainer/time>` for more details on tracking time during training.
 
     To construct an instance of :class:`Time`, you can either:
 
@@ -354,7 +358,15 @@ class Time(Generic[TValue]):
 
 
 class Timer(Serializable):
-    """Timer tracks the current training progress, in terms of epochs, batches, samples, and tokens."""
+    """Timer tracks the current training progress, in terms of epochs, batches, samples, and tokens.
+
+    See the :doc:`Time Guide </trainer/time>` for more details on tracking time during training.
+
+    .. note::
+
+        An instance of this class is automatically constructed by the :class:`~composer.core.state.State` constructor.
+        A user need not instantiate this class.
+    """
 
     def __init__(self):
         self._epoch = Time(0, TimeUnit.EPOCH)
@@ -554,13 +566,13 @@ class Timestamp(NamedTuple):
         :class:`Timestamp` should not be instantiated directly; instead use :meth:`Timer.get_timestamp`.
 
     Attributes:
-        epoch (Time[int]): epoch at a snapshot.
-        batch (Time[int]): batch at a snapshot.
-        batch_in_epoch (Time[int]): The number of batches seen in the epoch at a snapshot.
-        sample (Time[int]): sample at a snapshot.
-        sample_in_epoch (Time[int]): The number of samples seen in the epoch at a snapshot.
-        token (Time[int]): token at a snapshot.
-        token_in_epoch (Time[int]): The number of tokens seen in the epoch at a snapshot.
+        epoch (Time[int]): The epoch.
+        batch (Time[int]): The batch.
+        batch_in_epoch (Time[int]): The number of batch seen in the last epoch.
+        sample (Time[int]): The sample count.
+        sample_in_epoch (Time[int]): The number of samples seen in the last epoch.
+        token (Time[int]): The token count.
+        token_in_epoch (Time[int]): The number of tokens seen in the last epoch.
     """
     epoch: Time[int]
     batch: Time[int]
