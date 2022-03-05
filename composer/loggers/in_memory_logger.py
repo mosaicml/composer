5e8a0fbdbb4668a100b100b279ab1b996a203c6e
Matthew <growlix@users.noreply.github.com>
2022-03-04 22:58:14 -0800

    InMemoryLogger has get_timeseries() method (#644)
    * InMemoryLogger has get_timeseries() method
    
    * docstring update
    
    * removed time_unit
    
    * fixed type error
    
    * removed unused import
    
    * make style
    
    * return as numpy arrays
    
    * added test
    
    * test and plotting example
    
    * removed unused imports
    
    * typo in trainer testcode
    
    * attempted doctest fix
    
    * doctest attempted fix
    
    * reformatted doctest
    
    * Fixed test_dataset_registry
    
    * more docstring example comments
    
    * cleaned up tests as per Hanlin's recommendations
    
    * merge dev

--- composer/loggers/in_memory_logger.py
+++ composer/loggers/in_memory_logger.py
@@ -9,6 +9,9 @@ from __future__ import annotations
 
 from typing import Dict, List, Tuple, Union
 
+import numpy as np
+from torch import Tensor
+
 from composer.core.logging import LoggerCallback, LogLevel, TLogData
 from composer.core.logging.logger import TLogDataValue
 from composer.core.time import Timestamp
@@ -74,3 +77,71 @@ class InMemoryLogger(LoggerCallback):
             self.data[k].append((timestamp, log_level, v))
         self.most_recent_values.update(data)
         self.most_recent_timestamps.update({k: timestamp for k in data})
+
+    def get_timeseries(self, metric: str) -> Dict[str, TLogData]:
+        """Returns logged data as dict containing values of a desired metric over time.
+
+        Args:
+            metric (str): Metric of interest. Must be present in self.data.keys().
+
+        Returns:
+            timeseries (Dict[str, TLogData]): Dictionary in which one key is ``metric``,
+                and the associated value is a list of values of that metric. The remaining
+                keys are each a unit of time, and the associated values are each a list of
+                values of that time unit for the corresponding index of the metric. For
+                example:
+                >>> InMemoryLogger.get_timeseries(metric="accuracy/val")
+                {"accuracy/val": [31.2, 45.6, 59.3, 64.7, "epoch": [1, 2, 3, 4, ...],
+                ...], "batch": [49, 98, 147, 196, ...], ...}
+
+        Example:
+            .. testcode::
+
+                import matplotlib.pyplot as plt
+
+                from composer.core.logging import LogLevel
+                from composer.core.time import Time, Timestamp
+                from composer.loggers import InMemoryLogger
+
+                in_mem_logger = InMemoryLogger(LogLevel.BATCH)
+
+                # Populate the logger with data
+                for b in range(0,3):
+                    datapoint = b * 3
+                    timestamp = Timestamp(epoch=Time(0, "ep"),
+                                        batch=Time(b, "ba"),
+                                        batch_in_epoch=Time(0, "ba"),
+                                        sample=Time(0, "sp"),
+                                        sample_in_epoch=Time(0, "sp"),
+                                        token=Time(0, "tok"),
+                                        token_in_epoch=Time(0, "tok"))
+                    in_mem_logger.log_metric(timestamp=timestamp, 
+                        log_level=LogLevel.BATCH, data={"accuracy/val": datapoint})
+                timeseries = in_mem_logger.get_timeseries("accuracy/val")
+                plt.plot(timeseries["batch"], timeseries["accuracy/val"])
+                plt.xlabel("Batch")
+                plt.ylabel("Validation Accuracy")
+        """
+
+        # Check that desired metric is in present data
+        if metric not in self.data.keys():
+            raise ValueError(f"Invalid value for argument `metric`: {metric}. Requested "
+                             "metric is not present in self.data.keys().")
+
+        timeseries = {}
+        # Iterate through datapoints
+        for datapoint in self.data[metric]:
+            timestamp, _, metric_value = datapoint
+            timeseries.setdefault(metric, []).append(metric_value)
+            # Iterate through time units and add them all!
+            for field in timestamp._fields:
+                time_value = getattr(timestamp, field).value
+                timeseries.setdefault(field, []).append(time_value)
+        # Convert to numpy arrays
+        for k, v in timeseries.items():
+            if isinstance(v[0], Tensor):
+                v = Tensor(v).numpy()
+            else:
+                v = np.array(v)
+            timeseries[k] = v
+        return timeseries
