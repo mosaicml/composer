c2fcfe8100ba929a11bdaac8b540b3cce8fb1546
austin <jacobsonaustinj@gmail.com>
2022-03-07 15:09:02 -0800

    update metadata

--- composer/models/vit_small_patch16/__init__.py
+++ composer/models/vit_small_patch16/__init__.py
@@ -7,7 +7,7 @@ from composer.models.vit_small_patch16.model import ViTSmallPatch16
 _task = 'Image Classification'
 _dataset = 'ImageNet'
 _name = 'ViT-Small-Patch16'
-_quality = '74.4'  # target
+_quality = '74.52'
 _metric = 'Top-1 Accuracy'
-_ttt = ''
-_hparams = ''
+_ttt = '1d 59m'
+_hparams = 'vit_small_patch16.yaml'
