777ced6b11fa4b424d2a5e285fd0aa7a6b3ca14a
Hanlin Tang <hanlin@mosaicml.com>
2022-03-10 18:02:46 -0800

    fix warnings

--- composer/models/resnet9_cifar10/model.py
+++ composer/models/resnet9_cifar10/model.py
@@ -13,14 +13,14 @@ __all__ = ["ResNet9", "CIFAR10_ResNet9"]
 # adapted from https://raw.githubusercontent.com/matthias-wright/cifar10-resnet/master/model.py
 # under the MIT license
 class ResNet9(nn.Module):
-    """A 9-layer residual network, excluding BatchNorms and activation functions, as described in the `myrtle.ai blog.
-
-    <https://myrtle.ai/learn/how-to-train-your-resnet-4-architecture/>`_.
+    """A 9-layer residual network, excluding BatchNorms and activation functions, as described in the |myrtle.ai|_ blog.
 
     See `Deep Residual Learning for Image Recognition <https://arxiv.org/abs/1512.03385>`_ (He et al, 2015) for details on the residual network architecture.
 
     Args:
         num_classes (int, optional): The number of classes. Needed for classification tasks. Default: ``10``.
+
+    .. _myrtle.ai: https://myrtle.ai/learn/how-to-train-your-resnet-4-architecture/
     """
 
     def __init__(self, num_classes: int = 10):
