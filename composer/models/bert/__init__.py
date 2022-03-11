67272af780c09eb67d91dcd4693c243f0c2af988
Ravi Rahman <ravi@mosaicml.com>
2022-03-10 18:30:33 -0800

    Formatting

--- composer/models/bert/__init__.py
+++ composer/models/bert/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
-"""The `BERT <https://huggingface.co/docs/transformers/master/en/model_doc/bert>`_ model family using
-`Hugging Face Transformers <https://huggingface.co/transformers/>`_."""
+"""The `BERT <https://huggingface.co/docs/transformers/master/en/model_doc/bert>`_ model family using `Hugging Face
+Transformers <https://huggingface.co/transformers/>`_."""
 
 from composer.models.bert.bert_hparams import BERTForClassificationHparams as BERTForClassificationHparams
 from composer.models.bert.bert_hparams import BERTHparams as BERTHparams
