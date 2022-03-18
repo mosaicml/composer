cb349b201ba26b03741904e41e3a72650b70e68a
austin <jacobsonaustinj@gmail.com>
2022-03-17 22:37:58 -0700

    better module docstring

--- composer/models/templates/__init__.py
+++ composer/models/templates/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
-"""Model templates are ComposerModels with common forward passes built in for many common deep learning tasks."""
+"""Model templates are ComposerModels with forward passes and logging built-in for many common deep learning tasks."""
 
 from composer.models.templates.classification import ComposerClassifier as ComposerClassifier
 
