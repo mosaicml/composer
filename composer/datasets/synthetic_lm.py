99fc8bdbbad1c307b291f4fffea2aa7bc82c7b84
Moin Nadeem <moin@mosaicml.com>
2022-03-23 21:28:09 +0000

    isort

--- composer/datasets/synthetic_lm.py
+++ composer/datasets/synthetic_lm.py
@@ -15,8 +15,7 @@ from composer.utils import MissingConditionalImportError
 if TYPE_CHECKING:
     import tokenizers.models as tokenizers_models
     from datasets import Dataset
-    from tokenizers import decoders, normalizers
-    from tokenizers import pre_tokenizers
+    from tokenizers import decoders, normalizers, pre_tokenizers
     from transformers import PreTrainedTokenizer
 
 __all__ = ["SyntheticTokenizerParams", "generate_synthetic_tokenizer", "synthetic_hf_dataset_builder"]
