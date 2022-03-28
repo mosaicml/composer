5588e71ea0cd826e2d13f2b69a65bfcd23e750a7
Ravi Rahman <ravi@mosaicml.com>
2022-03-28 11:35:05 -0700

    Fixed FFCV Test

--- tests/datasets/test_ffcv_utils.py
+++ tests/datasets/test_ffcv_utils.py
@@ -2,22 +2,21 @@
 
 import os
 import pathlib
-from typing import List
 
 import pytest
 
 from composer.datasets.ffcv_utils import write_ffcv_dataset
-from composer.datasets.synthetic import SyntheticBatchPairDataset, SyntheticDataLabelType
+from composer.datasets.synthetic import SyntheticDataLabelType, SyntheticPILDataset
 
 
 @pytest.mark.vision
-@pytest.mark.timeout(30)
-def test_write_ffcv_dataset(tmpdir: pathlib.Path, dummy_in_shape: List[int]):
-    dataset = SyntheticBatchPairDataset(total_dataset_size=1,
-                                        num_classes=1,
-                                        data_shape=list(dummy_in_shape),
-                                        label_type=SyntheticDataLabelType.CLASSIFICATION_INT,
-                                        num_unique_samples_to_create=1)
+@pytest.mark.timeout(10)
+def test_write_ffcv_dataset(tmpdir: pathlib.Path):
+    dataset = SyntheticPILDataset(total_dataset_size=1,
+                                  num_classes=1,
+                                  data_shape=[1, 1, 3],
+                                  label_type=SyntheticDataLabelType.CLASSIFICATION_INT,
+                                  num_unique_samples_to_create=1)
     output_file = str(tmpdir / "ffcv")
-    write_ffcv_dataset(dataset, write_path=output_file, num_workers=1)
+    write_ffcv_dataset(dataset, write_path=output_file, num_workers=0)
     assert os.path.exists(output_file)
