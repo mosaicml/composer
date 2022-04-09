9325fd9ddf97a0e6a4257b566d5cf4fc869c746a
Matthew <growlix@users.noreply.github.com>
2022-04-08 18:18:58 -0700

    Update composer/algorithms/no_op_model/__init__.py

--- composer/algorithms/no_op_model/__init__.py
+++ composer/algorithms/no_op_model/__init__.py
@@ -2,7 +2,7 @@
 
 """Replaces model with a dummy model of type :class:`NoOpModelClass`.
 
-The algorithm runs on Event.INIT. It replaces the model in the state with 
+The algorithm runs on :attr:`Event.INIT`. It replaces the model in the state with 
 a :class:`NoOpModelClass` and then updates the parameters in the optimizer
 through module surgery.
 
