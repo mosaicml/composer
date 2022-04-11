6890c66bd11c8d8469f285f70ce5a92d72603a34
Mihir Patel <mihir.v.patel7@gmail.com>
2022-04-11 13:53:07 -0700

    remove duplicate fn

--- composer/utils/checkpoint.py
+++ composer/utils/checkpoint.py
@@ -50,14 +50,6 @@ def _format_path_with_current_rank(path: str) -> str:
         node_rank=dist.get_node_rank(),
     )
 
-def _format_path_with_rank_zero(path: str) -> str:
-    """Formats ``path`` formatted with rank values of 0."""
-    return path.format(
-        rank=0,
-        local_rank=0,
-        node_rank=0,
-    )
-
 
 def _get_write_mode(name: str) -> str:
     """Get the write mode to use with :func:`tarfile.open`."""
