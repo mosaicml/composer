19b50a649c65e615a8eedec1ad2849e12776f0ce
ravi-mosaicml <ravi@mosaicml.com>
2022-04-15 09:41:58 -0700

    Add Computed Metrics to State (#842)
    Add the current metrics to the state.
    Reconfigured test case tolerances and deterministic mode, as the metrics are stored on the state. While deterministic mode works well for CPU tests, it does not fully work on GPU tests, especially for large models. So, the state test cases use tolerances for all tensor type comparisons.
    Closes #830

--- tests/fixtures/dummy_fixtures.py
+++ tests/fixtures/dummy_fixtures.py
@@ -203,7 +203,6 @@ def composer_trainer_hparams(
             timeout=0.0,
         ),
         device=CPUDeviceHparams(),
-        deterministic_mode=True,
         loggers=[],
         model=dummy_model_hparams,
         val_dataset=dummy_val_dataset_hparams,
