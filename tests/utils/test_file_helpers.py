1b8f4fc04640e8da5f9983ffb9538c3f78bf7341
Mihir Patel <mihir.v.patel7@gmail.com>
2022-04-15 14:38:28 -0700

    Object store symlinks for graceful resumption (#887)
    * add object store symlinks
    
    * Update composer/loggers/logger_destination.py
    
    Co-authored-by: ravi-mosaicml <ravi@mosaicml.com>
    
    * Update composer/utils/object_store.py
    
    Co-authored-by: ravi-mosaicml <ravi@mosaicml.com>
    
    * Update composer/utils/object_store.py
    
    Co-authored-by: ravi-mosaicml <ravi@mosaicml.com>
    
    * add symlink artifact params
    
    * add in tests
    
    * Update composer/callbacks/checkpoint_saver.py
    
    Co-authored-by: ravi-mosaicml <ravi@mosaicml.com>
    
    * update docstrings based on review comments
    
    * pyright, my mortal enemy, you were correct this time
    
    * remove print
    
    Co-authored-by: ravi-mosaicml <ravi@mosaicml.com>

--- tests/utils/test_file_helpers.py
+++ tests/utils/test_file_helpers.py
@@ -56,6 +56,39 @@ def test_get_file_object_store(tmpdir: pathlib.Path, monkeypatch: pytest.MonkeyP
         f.read() == b"checkpoint1"
 
 
+def test_get_file_object_store_with_symlink(tmpdir: pathlib.Path, monkeypatch: pytest.MonkeyPatch):
+    remote_dir = tmpdir / "remote_dir"
+    os.makedirs(remote_dir)
+    monkeypatch.setenv("OBJECT_STORE_KEY", str(remote_dir))  # for the local option, the key is the path
+    provider = ObjectStoreHparams(
+        provider='local',
+        key_environ="OBJECT_STORE_KEY",
+        container=".",
+    ).initialize_object()
+    # Add file to object store
+    with open(str(remote_dir / "checkpoint.txt"), 'wb') as f:
+        f.write(b"checkpoint1")
+    # Add symlink to object store
+    with open(str(remote_dir / "latest.symlink"), "w") as f:
+        f.write("checkpoint.txt")
+    # Fetch object, should automatically follow symlink
+    get_file(path="latest.symlink",
+             object_store=provider,
+             destination=str(tmpdir / "example"),
+             chunk_size=1024 * 1024,
+             progress_bar=False)
+    with open(str(tmpdir / "example"), "rb") as f:
+        f.read() == b"checkpoint1"
+    # Fetch object without specifying .symlink, should automatically follow
+    get_file(path="latest",
+             object_store=provider,
+             destination=str(tmpdir / "example"),
+             chunk_size=1024 * 1024,
+             progress_bar=False)
+    with open(str(tmpdir / "example"), "rb") as f:
+        f.read() == b"checkpoint1"
+
+
 def test_get_file_object_store_not_found(tmpdir: pathlib.Path, monkeypatch: pytest.MonkeyPatch):
     remote_dir = tmpdir / "remote_dir"
     os.makedirs(remote_dir)
