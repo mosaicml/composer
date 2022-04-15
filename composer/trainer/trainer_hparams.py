b4531d1e9c814eec0c561b66a31087d3eb4f2af4
ravi-mosaicml <ravi@mosaicml.com>
2022-04-15 15:04:37 -0700

    Console log level (#900)
    Added back console logging. Before, this functionality lived in the FileLogger, which could previously write to `stdout`. That functionality was removed in #710, which introduced stdout and stderr capture to the file logger. This PR brings it back and merges it with the progress bar logger. Doing so in the same class allows prints to go through `tqdm.write`, so log lines do not interfere with the progress bar.
    
    In addition, the progress bar logger parameters are flattened into `Trainer.__init__`. This change makes it is possible to always append the progress bar logger (unless if it is explicitly set to `False` via the `progress_bar: bool` argument).

--- composer/trainer/trainer_hparams.py
+++ composer/trainer/trainer_hparams.py
@@ -24,6 +24,7 @@ from composer.datasets import DataLoaderHparams, DatasetHparams
 from composer.datasets.dataset_registry import get_dataset_registry
 from composer.datasets.evaluator import EvaluatorHparams
 from composer.loggers import LoggerDestinationHparams, logger_registry
+from composer.loggers.logger import LogLevel
 from composer.models import (BERTForClassificationHparams, BERTHparams, DeepLabV3Hparams, EfficientNetB0Hparams,
                              GPT2Hparams, MnistClassifierHparams, ModelHparams, ResNetCIFARHparams, ResNetHparams,
                              SSDHparams, TimmHparams, UnetHparams, ViTSmallPatch16Hparams)
@@ -170,7 +171,11 @@ class TrainerHparams(hp.Hparams):
             to log to. (default: ``[]``)
 
             .. seealso:: :mod:`composer.loggers` for the different loggers built into Composer.
-        log_level (str): The Python log level to use for log statements in the :mod:`composer`
+        progress_bar (bool, optional): See :class:`.Trainer`.
+        log_to_console (bool, optional): See :class:`.Trainer`.
+        console_log_level (bool, optional): See :class:`.Trainer`.
+        stream (bool, optional): See :class:`.Trainer`.
+        python_log_level (str): The Python log level to use for log statements in the :mod:`composer`
             module. (default: ``INFO``)
 
             .. seealso:: The :mod:`logging` module in Python.
@@ -311,11 +316,18 @@ class TrainerHparams(hp.Hparams):
         deterministic implementations, which will result in a crash."""),
                                            default=False)
 
-    # logging and callbacks
-    run_name: Optional[str] = hp.optional("Experiment name", default=None)
-    loggers: List[LoggerDestinationHparams] = hp.optional(doc="loggers to use", default_factory=list)
-    log_level: str = hp.optional(doc="Python loglevel to use composer", default="INFO")
+    # callbacks
     callbacks: List[CallbackHparams] = hp.optional(doc="Callback hparams", default_factory=list)
+
+    # logging
+    loggers: List[LoggerDestinationHparams] = hp.optional(doc="loggers to use", default_factory=list)
+    python_log_level: str = hp.optional(doc="Python loglevel to use composer", default="INFO")
+    run_name: Optional[str] = hp.optional("Experiment name", default=None)
+    progress_bar: bool = hp.optional("Whether to show a progress bar.", default=True)
+    log_to_console: Optional[bool] = hp.optional("Whether to print log statements to the console.", default=None)
+    console_log_level: LogLevel = hp.optional("The maximum log level for console logging.", default=LogLevel.EPOCH)
+    console_stream: str = hp.optional("The stream at which to write the progress bar and log statements.",
+                                      default="stderr")
 
     # load checkpoint
     load_path: Optional[str] = hp.optional(doc=textwrap.dedent("""\
@@ -474,8 +486,7 @@ class TrainerHparams(hp.Hparams):
     def initialize_object(self) -> Trainer:
         self.validate()
         import composer
-        logging.getLogger(composer.__name__).setLevel(self.log_level)
-
+        logging.getLogger(composer.__name__).setLevel(self.python_log_level)
         # devices and systems
         device = self.device.initialize_object()
 
@@ -572,10 +583,16 @@ class TrainerHparams(hp.Hparams):
             seed=seed,
             deterministic_mode=self.deterministic_mode,
 
-            # Callbacks and logging
+            # Callbacks
+            callbacks=callbacks,
+
+            # Logging
             run_name=self.run_name,
             loggers=loggers,
-            callbacks=callbacks,
+            progress_bar=self.progress_bar,
+            log_to_console=self.log_to_console,
+            console_log_level=self.console_log_level,
+            console_stream=self.console_stream,
 
             # Profiler
             prof_trace_handlers=[x.initialize_object() for x in self.prof_trace_handlers],
