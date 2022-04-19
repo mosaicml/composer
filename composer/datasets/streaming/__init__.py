e4846d9059a9941d55e59bc2881ef00573beff9c
James Knighton <iamknighton@gmail.com>
2022-04-18 19:47:04 -0700

    Add __all__.

--- composer/datasets/streaming/__init__.py
+++ composer/datasets/streaming/__init__.py
@@ -1,3 +1,6 @@
 from composer.datasets.streaming.dataset import StreamingDataset as StreamingDataset
 from composer.datasets.streaming.dataset import StreamingImageClassDataset as StreamingImageClassDataset
 from composer.datasets.streaming.writer import StreamingDatasetWriter as StreamingDatasetWriter
+
+
+__all__ = 'StreamingDataset', 'StreamingImageClassDataset', 'StreamingDatasetWriter'
