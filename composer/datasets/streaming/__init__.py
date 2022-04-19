60ec26416fa6f4d0c7c36c013baaab349456e4d6
James Knighton <iamknighton@gmail.com>
2022-04-18 20:59:03 -0700

    Jenkins

--- composer/datasets/streaming/__init__.py
+++ composer/datasets/streaming/__init__.py
@@ -2,5 +2,4 @@ from composer.datasets.streaming.dataset import StreamingDataset as StreamingDat
 from composer.datasets.streaming.dataset import StreamingImageClassDataset as StreamingImageClassDataset
 from composer.datasets.streaming.writer import StreamingDatasetWriter as StreamingDatasetWriter
 
-
 __all__ = 'StreamingDataset', 'StreamingImageClassDataset', 'StreamingDatasetWriter'
