dd012bbcbd94dfa4ec8ce62493d42019676a4115
Ravi Rahman <ravi@mosaicml.com>
2022-04-19 17:36:04 -0700

    Run lint and doctests through pytest

--- .ci/lint_doctests.sh
+++ .ci/lint_doctests.sh
@@ -5,10 +5,25 @@ set -euxo pipefail
 # Doctests are executed on the linting worker, so they are
 # executed only once on an install with all dependencies
 
+# Install dependencies
 sudo npm install -g pyright@1.1.224
-
 pip install .[all]
-make lint
 
-# Must build the html first to ensure that doctests in .. autosummary:: generated pages are included
+# Clean and make the output directory
+BUILD_DIR="build/output"
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+
+function cleanup()
+{
+    # Combine the coverage reports
+    python -m coverage combine
+    python -m coverage xml -o build/output/build${BUILD_NUMBER}.coverage.xml
+}
+
+trap cleanup EXIT
+
+# Run lint and doctests through pytest
+coverage run -m pytest $(dirname $0)/test_lint_doctests.py --junitxml ${BUILD_DIR}/build${BUILD_NUMBER}_lint_doctests.junit.xml
+
 cd docs && make html && make doctest && cd ..
