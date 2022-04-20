e767112e2208a09502e2a17753cfeb68b30ed6d4
Hanlin Tang <hanlin@mosaicml.com>
2022-04-20 08:06:46 -0700

    support ghost batchnorm for inference

--- tests/algorithms/test_torchscript.py
+++ tests/algorithms/test_torchscript.py
@@ -5,12 +5,20 @@ from torchvision.models import resnet50
 from composer.functional import (apply_blurpool, apply_channels_last, apply_factorization, apply_ghost_batchnorm,
                                  apply_squeeze_excite, apply_stochastic_depth)
 
-algo_kwargs = {apply_stochastic_depth: {'stochastic_method': 'block', 'target_layer_name': 'ResNetBottleneck'}}
+algo_kwargs = {
+    apply_stochastic_depth: {
+        'stochastic_method': 'block',
+        'target_layer_name': 'ResNetBottleneck'
+    },
+    apply_ghost_batchnorm: {
+        'ghost_batch_size': 2
+    }
+}
 
 
 @pytest.fixture
 def input():
-    return torch.Tensor(1, 3, 224, 224)
+    return torch.Tensor(4, 3, 224, 224)
 
 
 @pytest.mark.parametrize("surgery_method", [
@@ -33,13 +41,11 @@ def test_surgery_torchscript_train(surgery_method, input):
     scripted_func = torch.jit.script(model)
     torch.testing.assert_allclose(scripted_func(input), model(input))  # type: ignore (third-party)
 
-    scripted_func(input)
-
 
 @pytest.mark.parametrize("surgery_method", [
     pytest.param(apply_blurpool),
     pytest.param(apply_factorization, marks=pytest.mark.xfail),
-    pytest.param(apply_ghost_batchnorm, marks=pytest.mark.xfail),
+    pytest.param(apply_ghost_batchnorm),
     pytest.param(apply_squeeze_excite),
     pytest.param(apply_stochastic_depth),
     pytest.param(apply_channels_last)
