2ad09dda7d43f45360c55ebb90e09cb7d82b400a
Matthew Leavitt <matthew@mosaicml.com>
2022-04-21 01:43:29 +0000

    updated bce to convert targets to one-hot

--- composer/loss/loss.py
+++ composer/loss/loss.py
@@ -9,12 +9,55 @@ import torch
 from torch import Tensor
 from torch.nn import functional as F
 
-from composer.loss.utils import infer_target_type
+from composer.loss.utils import ensure_targets_one_hot, infer_target_type
 
 __all__ = ["bce", "loss_registry", "soft_cross_entropy"]
 
 
-bce = F.binary_cross_entropy_with_logits
+def bce(
+    input: Tensor,
+    target: Tensor,
+    weight: Optional[Tensor] = None,
+    size_average: Optional[bool] = None,
+    reduce: Optional[bool] = None,
+    reduction: str = "mean",
+    pos_weight: Optional[Tensor] = None,
+) -> torch.Tensor:
+    r"""Drop-in replacement for 
+    :class:`~torch.nn.functional.binary_cross_entropy_with_logits` that can handle class 
+    indices or one-hot labels. 
+    Args:
+        input (torch.Tensor) : :math:`(N, C)` where `C = number of classes` or :math:`(N, C, H, W)`
+            in case of 2D Loss, or :math:`(N, C, d_1, d_2, ..., d_K)` where :math:`K \geq 1`
+            in the case of K-dimensional loss. `input` is expected to contain unnormalized scores
+            (often referred to as logits).
+        target (torch.Tensor) : If containing class indices, shape :math:`(N)` where each value is
+            :math:`0 \leq \text{targets}[i] \leq C-1`, or :math:`(N, d_1, d_2, ..., d_K)` with
+            :math:`K \geq 1` in the case of K-dimensional loss. If containing class probabilities,
+            same shape as the input.
+        weight (torch.Tensor, optional): a manual rescaling weight given to each
+            class. If given, has to be a Tensor of size `C`. Default: ``None``.
+        size_average (bool, optional): Deprecated (see `reduction`). By default,
+            the losses are averaged over each loss element in the batch. Note that for
+            some losses, there multiple elements per sample. If the field ``size_average``
+            is set to ``False``, the losses are instead summed for each minibatch. Ignored
+            when reduce is ``False``. Default: ``True``
+        reduce (bool, optional): Deprecated (see ``reduction``). By default, the
+            losses are averaged or summed over observations for each minibatch depending
+            on `size_average`. When ``reduce`` is ``False``, returns a loss per
+            batch element instead and ignores `size_average`. Default: ``True``
+        reduction (str, optional): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+            ``'mean'``: the sum of the output will be divided by the number of
+            elements in the output, ``'sum'``: the output will be summed. Note: ``size_average``
+            and ``reduce`` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override ``reduction``. Default:
+            ``'mean'``
+        pos_weight (Tensor, optional): a weight of positive examples.
+                Must be a vector with length equal to the number of classes.        
+    """
+    target = ensure_targets_one_hot(input, target)
+    return F.binary_cross_entropy_with_logits(input, target, weight, size_average, reduce, reduction, pos_weight)
 
 
 def soft_cross_entropy(input: Tensor,
@@ -97,7 +140,4 @@ def soft_cross_entropy(input: Tensor,
         raise ValueError(f"Unrecognized target type {target_type}")
 
 
-loss_registry = {
-    "bce": bce,
-    "soft_cross_entropy": soft_cross_entropy
-}
+loss_registry = {"bce": bce, "soft_cross_entropy": soft_cross_entropy}
