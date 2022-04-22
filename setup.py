aca6bdfbfe3212bca1780c42aa80441bd0359ac9
Abhinav Venigalla <abhi@mosaicml.com>
2022-04-22 16:32:59 -0700

    upgrade boto3 req

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ extra_deps["mlperf"] = [
 ]
 
 extra_deps["streaming"] = [
-    "boto3==1.21.40",
+    "boto3>=1.21.45",
 ]
 
 extra_deps["all"] = set(dep for deps in extra_deps.values() for dep in deps)
