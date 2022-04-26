2970aa50ca97d6c429fc4537818d9febecd26c6a
Abhinav Venigalla <abhi@mosaicml.com>
2022-04-25 18:07:49 -0700

    start multiprocessing tests

--- tests/datasets/test_streaming.py
+++ tests/datasets/test_streaming.py
@@ -7,6 +7,7 @@ from typing import Dict, Iterable, List, Optional
 import numpy as np
 import pytest
 import torch
+from torch.utils.data import DataLoader
 
 from composer.datasets.streaming import StreamingDataset, StreamingDatasetWriter
 
@@ -50,19 +51,21 @@ def test_writer(num_samples: int, shard_size_limit: int) -> None:
 
 @pytest.mark.parametrize("share_remote_local", [False, True])
 @pytest.mark.parametrize("shuffle", [False, True])
-def test_reader_disk_single_process(share_remote_local: bool, shuffle: bool):
+def test_reader(share_remote_local: bool, shuffle: bool):
     num_samples = 117
-    shard_size_limit = 1 << 16
+    shard_size_limit = 1 << 8
     samples, decoders = get_fake_samples_decoders(num_samples)
     remote = write_synthetic_streaming_dataset(samples=samples, shard_size_limit=shard_size_limit)
     if share_remote_local:
         local = remote
     else:
         local = tempfile.TemporaryDirectory().name
+
+    # Build StreamingDataset
     dataset = StreamingDataset(remote=remote, local=local, shuffle=shuffle, decoders=decoders)
 
     # Test length
-    assert len(dataset) == num_samples
+    assert len(dataset) == num_samples, f"Got dataset length={len(dataset)} samples, expected {num_samples}"
 
     # Test basic sample order
     shuffle_matches = 0
@@ -77,6 +80,35 @@ def test_reader_disk_single_process(share_remote_local: bool, shuffle: bool):
             assert uid == expected_uid == uid, f"sample ix={ix} has uid={uid}, expected {expected_uid}"
             assert value == expected_value, f"sample ix={ix} has value={value}, expected {expected_value}"
 
-    # If shuffling, there should not be many matches
+    # If shuffling, there should be few matches
+    # The probability of k matches in a random permutation is ~1/(e*(k!))
     if shuffle:
-        assert shuffle_matches < 10  # The probability of k matches in a random permutation is ~1/(e*(k!))
+        assert shuffle_matches < 10
+
+
+@pytest.mark.parametrize("batch_size", [1, 2, 5, 128])
+@pytest.mark.parametrize("drop_last", [False, True])
+@pytest.mark.parametrize("num_workers", [0, 1, 8])
+@pytest.mark.parametrize("share_remote_local", [False, True])
+@pytest.mark.parametrize("shuffle", [False, True])
+def test_dataloader_single_device(batch_size: int, drop_last: bool, num_workers: int, share_remote_local: bool,
+                                  shuffle: bool):
+    num_samples = 1234
+    shard_size_limit = 1 << 8
+    samples, decoders = get_fake_samples_decoders(num_samples)
+    remote = write_synthetic_streaming_dataset(samples=samples, shard_size_limit=shard_size_limit)
+    if share_remote_local:
+        local = remote
+    else:
+        local = tempfile.TemporaryDirectory().name
+
+    # Build StreamingDataset
+    dataset = StreamingDataset(remote=remote, local=local, shuffle=shuffle, decoders=decoders, batch_size=batch_size)
+
+    # Build DataLoader
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, num_workers=num_workers, drop_last=drop_last)
+
+    # Test dataloader length
+    expected_num_batches = (num_samples // batch_size) if drop_last else math.ceil(num_samples / batch_size)
+    num_batches = len(dataloader)
+    assert num_batches == expected_num_batches, f"Got dataloader length={num_batches} batches, expected {expected_num_batches}"
