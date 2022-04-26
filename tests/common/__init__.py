46cf48e8c89c7156ca5adfa2cbef06173ae7d0a2
Ravi Rahman <ravi@mosaicml.com>
2022-04-26 09:04:14 -0700

    More cleanup
    - Surpressing `NoSchedulerWarning` by default
    - Moved utility tests into `tests/utils`
    - Fixed `test_state_serialize` by ensuring that each state has a unique model object and that `optimizer.zero_grad` is called
    - Changed default number of in features to 1.

--- tests/common/__init__.py
+++ tests/common/__init__.py
@@ -5,8 +5,10 @@ from tests.common.events import EventCounterCallback, EventCounterCallbackHparam
 from tests.common.markers import device, world_size
 from tests.common.models import (SimpleConvModel, SimpleConvModelHparams, SimpleModel, SimpleModelHparams,
                                  configure_model_hparams_for_synthetic)
+from tests.common.state import assert_state_equivalent
 
 __all__ = [
+    "assert_state_equivalent",
     "RandomClassificationDataset",
     "RandomClassificationDatasetHparams",
     "RandomImageDataset",
--- tests/test_state.py
+++ tests/test_state.py
@@ -9,20 +9,19 @@ from torch.functional import Tensor
 
 from composer.algorithms import ChannelsLastHparams
 from composer.core import DataSpec, Precision, State
-from composer.core.types import Batch, DataLoader
+from composer.core.types import Batch
 from composer.datasets.dataloader import DataLoaderHparams
 from composer.datasets.hparams import DatasetHparams
-from composer.models.base import ComposerModel
-from tests.common import SimpleModel, deep_compare
+from tests.common import SimpleModel, assert_state_equivalent
 
 
 def random_tensor(size=(4, 10)):
     return torch.rand(*size)
 
 
-def get_dummy_state(model: ComposerModel, train_dataloader: DataLoader):
+def get_dummy_state():
+    model = SimpleModel()
     optimizers = torch.optim.Adadelta(model.parameters())
-
     state = State(model=model,
                   grad_accum=random.randint(0, 100),
                   rank_zero_seed=random.randint(0, 100),
@@ -34,21 +33,15 @@ def get_dummy_state(model: ComposerModel, train_dataloader: DataLoader):
     state.loss = random_tensor()
     state.batch = (random_tensor(), random_tensor())
     state.outputs = random_tensor()
-    state.set_dataloader(train_dataloader, "train")
     return state
-
-
-def assert_state_equivalent(state1: State, state2: State):
-    assert state1.serialized_attributes == state2.serialized_attributes
-    assert state1.is_model_deepspeed == state2.is_model_deepspeed
-
-    # Using a loose tolerance as GPU determinism does not work properly
-    deep_compare(state1.state_dict(), state2.state_dict(), atol=0.1, rtol=0.1)
 
 
 def train_one_step(state: State, batch: Batch) -> None:
     _, y = batch
     state.batch = batch
+
+    for optimizer in state.optimizers:
+        optimizer.zero_grad()
 
     state.outputs = state.model(state.batch_pair)
     assert isinstance(y, Tensor)
@@ -73,16 +66,11 @@ def get_batch(dataset_hparams: DatasetHparams, dataloader_hparams: DataLoaderHpa
 
 def test_state_serialize(
     tmpdir: pathlib.Path,
-    dummy_model: ComposerModel,
     dummy_dataloader_hparams: DataLoaderHparams,
     dummy_train_dataset_hparams: DatasetHparams,
-    dummy_train_dataloader: DataLoader,
 ):
-
-    assert isinstance(dummy_model, SimpleModel)
-
-    state1 = get_dummy_state(dummy_model, dummy_train_dataloader)
-    state2 = get_dummy_state(dummy_model, dummy_train_dataloader)
+    state1 = get_dummy_state()
+    state2 = get_dummy_state()
 
     # train one step to set the optimizer states
     batch = get_batch(dummy_train_dataset_hparams, dummy_dataloader_hparams)
