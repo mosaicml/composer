0508fffd495e92a550144066a7ed760ab684b832
Matthew Leavitt <matthew@mosaicml.com>
2022-04-26 17:53:57 -0700

    Start LAMB implementation

--- composer/optim/decoupled_weight_decay.py
+++ composer/optim/decoupled_weight_decay.py
@@ -13,7 +13,7 @@ import math
 from typing import List, Tuple
 
 import torch
-from torch.optim import SGD, AdamW
+from torch.optim import SGD, AdamW, Optimizer
 from torch.optim.optimizer import required  # type: ignore
 
 log = logging.getLogger(__name__)
@@ -300,5 +300,119 @@ class DecoupledAdamW(AdamW):
                        initial_lr=initial_lr,
                        weight_decay=weight_decay,
                        eps=eps)
+
+        return loss
+
+
+
+class DecoupledNVLAMB(Optimizer):
+    r"""NVIDIA-style LAMB optimizer (NVLAMB) with the weight decay term decoupled from 
+    the learning rate.
+
+    LAMB is a layerwise adaptive optimizer inspired by LARS and described in `You et
+    al., (2019) <https://arxiv.org/abs/1904.00962>`_. This implementation is based on 
+    https://github.com/NUS-HPC-AI-Lab/LARS-ImageNet-PyTorch/blob/main/lamb.py and 
+    `NVLAMB <https://developer.nvidia.com/blog/pretraining-bert-with-layer-wise-adaptive-learning-rates/>`_.
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        adam (bool, optional): always use trust ratio = 1, which turns this into
+            Adam. Useful for comparison purposes.
+    """
+
+    def __init__(self,
+                 params: List[torch.Tensor],
+                 lr: float = 1e-3,
+                 betas: Tuple[float, float] = (0.9, 0.999),
+                 eps: float = 1e-8,
+                 weight_decay: float = 1e-2):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(
+                "Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(
+                "Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        torch.nn.utils.clip_grad_norm_(
+            parameters=[
+                p for group in self.param_groups for p in group['params']],
+            max_norm=1.0,
+            norm_type=2
+        )
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError(
+                        'Lamb does not support sparse gradients, consider SparseAdam instad.')
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                # Decay the first and second moment running average coefficient
+                # m_t
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                # v_t
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                # Paper v3 does not use debiasing.
+                # bias_correction1 = 1 - beta1 ** state['step']
+                # bias_correction2 = 1 - beta2 ** state['step']
+                # Apply bias to lr to avoid broadcast.
+                # * math.sqrt(bias_correction2) / bias_correction1
+                scaled_lr = group['lr']
+                update = exp_avg / exp_avg_sq.sqrt().add(group['eps'])
+                if group['weight_decay'] != 0:
+                    update.add_(p.data, alpha=group['weight_decay'])
+                    w_norm = torch.norm(p)
+                    g_norm = torch.norm(update)
+                    trust_ratio = torch.where(
+                        w_norm > 0 and g_norm > 0,
+                        w_norm / g_norm,
+                        torch.ones_like(w_norm)
+                    )
+                    scaled_lr *= trust_ratio.item()
+
+                p.data.add_(update, alpha=-scaled_lr)
 
         return loss
