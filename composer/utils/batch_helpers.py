d323d7bbdf545a836c272a25fe8f1258e11b64d6
Evan Racah <evan@mosaicml.com>
2022-04-29 16:43:41 -0700

    Add batch_set function and tests

--- composer/utils/batch_helpers.py
+++ composer/utils/batch_helpers.py
@@ -47,3 +47,53 @@ def batch_get(batch: Any, key: BatchKeys) -> Any:
         return [getattr(batch, k) for k in key]  # Sequence of attributes for object or named tuple.
     except:
         raise RuntimeError(f"Batch object can't be indexed by nor has attribute {key}")
+
+
+def batch_set(batch: Any, key: BatchKeys, value: Any):
+    """Indexes into the batch given the key and sets the element at that index to value.
+
+    Tries all the common combination of batch and key pairs:
+        * int for sequence
+        * slice for sequence
+        * string for dict
+        * string atribute for object or named_tuple
+        * sequence of ints for sequence
+        * sequence of slices for sequence
+        * sequence of keys for dict
+        * sequence of attribute names for object or named_tuple
+
+    Args:
+        batch (Any): An object that contains the input and label of the items in the batch.
+            Can be any abritrary type that user creates, but we assume some sort of
+            sequence (list, tuple, tensor, array), mapping (dictionary),
+            or attribute store (object with data members, namedtuple).
+        key (BatchKeys): A key to index into the batch. BatchKeys is of type Union[BatchKey Sequence[BatchKey]]
+            and BatchKey is of type Union[int, str, slice].
+        value (Any): The value that batch[key] or batch.key gets set to.
+
+    Raises:
+        RuntimeError if the batch can't be indexed by the key or batch can't be set at that key.
+    """
+    try:
+        batch[key] = value  # int or slice for sequence or string for dict.
+        return
+    except:
+        pass
+    try:
+        for k, v in zip(key, value):  # Sequence of ints, slices, or strings.
+            batch[k] = v
+        return
+    except:
+
+        pass
+    try:
+        setattr(batch, key, value)  # Attribute for object or named tuple.
+        return
+    except:
+        pass
+    try:
+        for k, v in zip(key, value):  # Sequence of attributes for object or named tuple.
+            setattr(batch, k, v)
+        return
+    except:
+        raise RuntimeError(f"Batch object of type {str(type(batch))} can't be set at index or attribute {key}")
--- tests/utils/test_batch_helpers.py
+++ tests/utils/test_batch_helpers.py
@@ -1,11 +1,11 @@
-from collections import namedtuple, OrderedDict
+from collections import OrderedDict
+from typing import NamedTuple
 
 import numpy as np
 import pytest
 import torch
-from matplotlib import get_backend
 
-from composer.utils.batch_helpers import batch_get
+from composer.utils.batch_helpers import batch_get, batch_set
 
 my_list = [3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -19,7 +19,8 @@ class myClass(object):
             setattr(self, k, v)
 
 
-my_named_tuple = namedtuple('nt', keys)
+list_types = [type(element) for element in my_list]
+my_named_tuple = NamedTuple('nt', **dict(zip(keys, list_types)))
 
 
 @pytest.fixture(scope="module",
@@ -32,35 +33,54 @@ def example_sequence(request):
     return request.param
 
 
+# All key value pair data structures that have a __getitem__ function thats takes str.
 @pytest.fixture(scope="module",
-                params=[dict(zip(keys, my_list)),
-                        myClass(**dict(zip(keys, my_list))),
-                        my_named_tuple(*my_list),
-                        OrderedDict(**dict(zip(keys, my_list)))])
+                params=[
+                    dict(zip(keys, my_list)),
+                    myClass(**dict(zip(keys, my_list))),
+                    my_named_tuple(*my_list),
+                    OrderedDict(**dict(zip(keys, my_list)))
+                ])
 def example_map(request):
     return request.param
 
 
+# All key-value pair data structures that are mutable.
+@pytest.fixture(
+    scope="module",
+    params=[dict(zip(keys, my_list)),
+            myClass(**dict(zip(keys, my_list))),
+            OrderedDict(**dict(zip(keys, my_list)))])
+def example_mutable_map(request):
+    return request.param
+
+
+# Test whether sequences can be indexed by an int.
 def test_int_key(example_sequence, key=2, expected=5):
     assert batch_get(example_sequence, key) == expected
 
 
+# Test whether kv pair data structures can be indexed by a str.
 def test_str_key(example_map, key='d', expected=6):
     assert batch_get(example_map, key) == expected
 
 
+# Test whether sequences can be indexed by a sequence of ints.
 def test_sequence_of_ints_key(example_sequence, key=[2, 5, 7], expected=[5, 8, 10]):
     assert list(batch_get(example_sequence, key)) == expected
 
 
+# Test whether kv pair data structures can be indexed by a sequence of strings.
 def test_sequence_of_strs_key(example_map, key=['c', 'f'], expected=[5, 8]):
     assert list(batch_get(example_map, key)) == expected
 
 
+# Test whether sequences can be indexed by a slice object.
 def test_slice_key(example_sequence, key=slice(1, 6, 2), expected=[4, 6, 8]):
     assert list(batch_get(example_sequence, key)) == expected
 
 
+# Test whether sequences can be indexed by a sequence of slice objects.
 @pytest.mark.parametrize('batch,key,expected', [(my_list, [slice(1, 4), slice(5, 7)], [[4, 5, 6], [8, 9]]),
                                                 (tuple(my_list), [slice(1, 4), slice(5, 7)], [(4, 5, 6), (8, 9)]),
                                                 (my_named_tuple(*my_list), [slice(1, 4), slice(5, 7)], [(4, 5, 6),
@@ -69,9 +89,76 @@ def test_seq_of_slices_key(batch, key, expected):
     assert batch_get(batch, key) == expected
 
 
+# Test whether arrays and tensors can be indexed by a sequence of slice objects.
 @pytest.mark.parametrize('batch,key,expected', [(torch.tensor(my_list), [slice(1, 4), slice(5, 7)], [
     torch.tensor([4, 5, 6]), torch.tensor([8, 9])
 ]), (np.asarray(my_list), [slice(1, 4), slice(5, 7)], [np.asarray([4, 5, 6]), np.asarray([8, 9])])])
 def test_seq_of_slices_key_for_tensors_and_arrays(batch, key, expected):
     for actual, expectation in zip(batch_get(batch, key), expected):
         assert all(actual == expectation)
+
+
+@pytest.fixture
+def example_list():
+    return [3, 4, 5, 6, 7, 8, 9, 10]
+
+
+@pytest.fixture
+def example_tensor():
+    return torch.tensor([3, 4, 5, 6, 7, 8, 9, 10])
+
+
+@pytest.fixture
+def example_array():
+    return np.asarray([3, 4, 5, 6, 7, 8, 9, 10])
+
+
+# Test whether lists can be set using batch_set.
+@pytest.mark.parametrize('key,value', [(1, 2), (3, 7), ([2, 5], [11, 13]), (slice(1, 6, 2), [-1, -3, -5]),
+                                       ([slice(0, 3, 1), slice(4, 7, 1)], [[10, 11, 12], [13, 14, 15]])])
+def test_batch_set_list(example_list, key, value):
+    batch_set(example_list, key, value)
+    assert batch_get(example_list, key) == value
+
+
+# Test whether tensors can be set using batch_set.
+@pytest.mark.parametrize('key,value', [(1, torch.tensor(2)), (3, torch.tensor(7)), ([2, 5], torch.tensor([11, 13])),
+                                       (slice(1, 6, 2), torch.tensor([-1, -3, -5]))])
+def test_batch_set_tensor(example_tensor, key, value):
+    batch_set(example_tensor, key, value)
+    assert torch.equal(batch_get(example_tensor, key), value)
+
+
+# Test whether tensors can be set using batch_set with a list of slices.
+def test_batch_set_tensor_list_of_slices(example_tensor,
+                                         key=[slice(0, 3, 1), slice(4, 7, 1)],
+                                         value=[torch.tensor([10, 11, 12]),
+                                                torch.tensor([13, 14, 15])]):
+    batch_set(example_tensor, key, value)
+    for actual, expectation in zip(batch_get(example_tensor, key), value):
+        assert torch.equal(actual, expectation)
+
+
+# Test whether arrays can be set using batch_set.
+@pytest.mark.parametrize('key,value', [(1, np.asarray(2)), (3, np.asarray(7)), ([2, 5], np.asarray([11, 13])),
+                                       (slice(1, 6, 2), np.asarray([-1, -3, -5]))])
+def test_batch_set_array(example_array, key, value):
+    batch_set(example_array, key, value)
+    assert np.all(batch_get(example_array, key) == value)
+
+
+# Test whether arrays can be set using batch_set with a list of slices.
+def test_batch_set_array_list_of_slices(example_array,
+                                        key=[slice(0, 3, 1), slice(4, 7, 1)],
+                                        value=[np.asarray([10, 11, 12]),
+                                               np.asarray([13, 14, 15])]):
+    batch_set(example_array, key, value)
+    for actual, expectation in zip(batch_get(example_array, key), value):
+        assert np.all(actual == expectation)
+
+
+# Test whether mutable key value data structures can be set using batch_set.
+@pytest.mark.parametrize('key,value', [('b', -10), ('c', -20), (['d', 'e'], [100, 101])])
+def test_batch_set_map(example_mutable_map, key, value):
+    batch_set(example_mutable_map, key, value)
+    assert batch_get(example_mutable_map, key) == value
