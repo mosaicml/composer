8affed034b454c746e299d531cc51c85dbfc0e5c
Anis Ehsani <anis@mosaicml.com>
2022-05-02 22:14:03 +0000

    review changes

--- composer/callbacks/early_stopper.py
+++ composer/callbacks/early_stopper.py
@@ -5,12 +5,13 @@
 from __future__ import annotations
 
 import logging
-from typing import Callable
+from typing import Callable, Union
 
 import numpy as np
 
-from composer.core import State
+from composer.core import State, Time
 from composer.core.callback import Callback
+from composer.core.time import TimeUnit
 from composer.loggers import Logger
 
 log = logging.getLogger(__name__)
@@ -19,89 +20,111 @@ __all__ = ["EarlyStopper"]
 
 
 class EarlyStopper(Callback):
-    __doc__ = f"""Callback to halt training early.
+    """This callback tracks a training or evaluation metric and halts training if the metric does not
+    improve within a given interval.
+
+    Example
+
+    .. doctest::
+
+        >>> from composer.callbacks import EarlyStopper
+        >>> from torchmetrics.classification.accuracy import Accuracy
+        >>> # constructing trainer object with this callback
+        >>> early_stopper = EarlyStopper("Accuracy", "my_evaluator", patience=1)
+        >>> evaluator = Evaluator(
+        ...     dataloader = eval_dataloader,
+        ...     label = 'my_evaluator',
+        ...     metrics = Accuracy()
+        ... )
+        >>> trainer = Trainer(
+        ...     model=model,
+        ...     train_dataloader=train_dataloader,
+        ...     eval_dataloader=evaluator,
+        ...     optimizers=optimizer,
+        ...     max_duration="1ep",
+        ...     callbacks=[early_stopper],
+        ... )
+
+    Args:
+        monitor (str): The name of the metric to monitor.
+        dataloader_label (str): The label of the dataloader or evaluator associated with the tracked metric. If 
+            monitor is in an Evaluator, the dataloader_label field should be set to the Evaluator's label. If 
+            monitor is a training metric or an ordinary evaluation metric not in an Evaluator, dataloader_label
+            should be set to 'train' or 'eval' respectively.
+        comp (Callable, optional): A comparison operator to measure change of the monitored metric. The comparison
+            operator will be called `comp(current_value, prev_best)`. For metrics where the optimal value is low
+            (error, loss, perplexity), use a less than operator and for metrics like accuracy whee the optimal value
+            is higher, use a greater than operator. Defaults to numpy.less if loss, error, or perplexity are substrings
+            of the monitored metric, otherwise defaults to numpy.greater
+        min_delta (float, optional): An optional float that requires a new value to exceed the best value by at
+            least that amount. Defaults to 0.
+        patience (int | str | Time, optional): The interval of time the monitored metric can not improve without stopping
+            training. Defaults to 1 epoch.
+        
     """
 
     def __init__(
         self,
         monitor: str,
-        eval_label: str,
+        dataloader_label: str,
         comp: Callable = None,
         min_delta: float = 0.0,
-        patience: int = 1,
+        patience: Union[int, str, Time] = 1,
     ):
         self.monitor = monitor
-        self.eval_label = eval_label
+        self.dataloader_label = dataloader_label
         self.comp = comp
         self.min_delta = abs(min_delta)
         if self.comp is None:
-            self.comp = np.less if 'loss' in monitor.lower() or 'error' in monitor.lower() else np.greater
-            if self.comp == np.less:
-                self.min_delta *= -1
+            if any(substr in monitor.lower() for substr in ["loss", "error", "perplexity"]):
+                self.comp = np.less
+            else:
+                self.comp = np.greater
 
-        if self.comp(1, 2):
-            self.best = float('inf')
-            self.min_delta *= -1
+        self.best = None
+        self.best_occurred = None
+
+        if isinstance(patience, str):
+            self.patience = Time.from_timestring(patience)
+        elif isinstance(patience, int):
+            self.patience = Time(patience, TimeUnit.EPOCH)
         else:
-            self.best = -float('inf')
+            self.patience = patience
+            if self.patience.unit not in (TimeUnit.EPOCH, TimeUnit.BATCH):
+                raise ValueError("If `patience` is an instance of Time, it must have units of EPOCH or BATCH.")
 
-        self.patience = patience
-        self.wait = 0
+    def _get_monitored_metric(self, state: State):
+        if self.dataloader_label in state.current_metrics:
+            if self.monitor in state.current_metrics[self.dataloader_label]:
+                return state.current_metrics[self.dataloader_label][self.monitor]
+        raise ValueError(f"Couldn't find the metric {self.monitor} with the dataloader label {self.dataloader_label}."
+                         "Check that the dataloader_label is set to 'eval', 'train' or the evaluator name.")
+
+    def _update_stopper_state(self, state: State):
+        metric_val = self._get_monitored_metric(state)
+
+        if self.best is None:
+            self.best = metric_val
+            self.best_occurred = state.timer.get_timestamp()
+        elif self.comp(metric_val, self.best) and abs(metric_val - self.best) > self.min_delta:
+            self.best = metric_val
+            self.best_occurred = state.timer.get_timestamp()
+
+        if self.patience.unit == TimeUnit.EPOCH:
+            if int(state.timer.epoch) - int(self.best_occurred.epoch) > int(self.patience):
+                state.max_duration = state.timer.batch
+        elif self.patience.unit == TimeUnit.BATCH:
+            if int(state.timer.batch_in_epoch) - int(self.best_occurred.batch_in_epoch) > int(self.patience):
+                state.max_duration = state.timer.batch
+        else:
+            raise ValueError(f"The units of `patience` should be EPOCH or BATCH.")
 
     def eval_end(self, state: State, logger: Logger) -> None:
-        if self.eval_label == "train":
+        if self.dataloader_label != "train":
             # if the monitored metric is an eval metric or in an evaluator
-            return
-
-        monitored_metric = None
-        current_metrics = state.current_metrics
-        if self.eval_label in current_metrics:
-            if self.monitor in current_metrics[self.eval_label]:
-                monitored_metric = current_metrics[self.eval_label][self.monitor]
-            else:
-                logger.warning(f"Couldn't find the metric {self.monitor} in the current_metrics/{self.eval_label}")
-                return
-        else:
-            logger.warning(
-                f"The label {self.eval_label} isn't in the state's current_metrics. Use the values train, eval, or the name of the Evaluator if using Evaluators."
-            )
-            return
-
-        if self.comp(monitored_metric - self.min_delta, self.best):
-            self.best = monitored_metric
-            self.wait = 0
-        else:
-            self.wait += 1
-
-        if self.wait >= self.patience:
-            # stop the training the training
-            state.max_duration = state.timer.batch
+            self._update_stopper_state(state)
 
     def epoch_end(self, state: State, logger: Logger) -> None:
-        if self.eval_label != "train":
+        if self.dataloader_label == "train":
             # if the monitored metric is not an eval metric, the right logic is run on EPOCH_END
-            return
-
-        monitored_metric = None
-        current_metrics = state.current_metrics
-        if self.eval_label in current_metrics:
-            if self.monitor in current_metrics[self.eval_label]:
-                monitored_metric = current_metrics[self.eval_label][self.monitor]
-            else:
-                logger.warning(f"Couldn't find the metric {self.monitor} in the current_metrics/{self.eval_label}")
-                return
-        else:
-            logger.warning(
-                f"The label {self.eval_label} isn't in the state's current_metrics. Use the values train, eval, or the name of the Evaluator if using Evaluators."
-            )
-            return
-
-        if self.comp(monitored_metric - self.min_delta, self.best):
-            self.best = monitored_metric
-            self.wait = 0
-        else:
-            self.wait += 1
-
-        if self.wait >= self.patience:
-            # stop the training the training
-            state.max_duration = state.timer
+            self._update_stopper_state(state)
--- tests/callbacks/test_early_stopper.py
+++ tests/callbacks/test_early_stopper.py
@@ -1,28 +1,44 @@
-from types import MethodType
-from typing import List
+from typing import List, Sequence
 
-import numpy as np
 import pytest
 from torch import tensor
 from torch.utils.data import DataLoader
-from torchmetrics import Accuracy, MetricCollection
 
 from composer import Trainer
 from composer.callbacks.early_stopper import EarlyStopper
-from composer.core.evaluator import Evaluator
-from composer.loggers import LogLevel
+from composer.core import State
+from composer.core.callback import Callback
+from composer.loggers import Logger
 from tests.common import RandomClassificationDataset, SimpleModel
 
 
-@pytest.mark.parametrize('metric_sequence', [[0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], [0.1, 0.2]])
-def test_threshold_stopper(metric_sequence: List[int]):
+class TestMetricSetter(Callback):
+
+    def __init__(self, monitor: str, dataloader_label: str, metric_sequence: Sequence):
+        self.monitor = monitor
+        self.dataloader_label = dataloader_label
+        self.metric_sequence = metric_sequence
+
+    def _update_metrics(self, state: State):
+        idx = min(len(self.metric_sequence) - 1, state.timer.epoch.value)
+        metric_val = self.metric_sequence[idx]
+        state.current_metrics[self.dataloader_label][self.monitor] = tensor(metric_val)
+
+    def eval_end(self, state: State, logger: Logger) -> None:
+        if self.dataloader_label != "train":
+            self._update_metrics(state)
+
+    def epoch_end(self, state: State, logger: Logger) -> None:
+        if self.dataloader_label == "train":
+            self._update_metrics(state)
+
+
+@pytest.mark.parametrize('metric_sequence', [[0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2, 1.3], [0.1, 0.2]])
+def test_threshold_stopper(metric_sequence: List[float]):
 
     early_stopper = EarlyStopper("Accuracy", "eval", patience=3)
 
-    def test_compute_and_log_metrics(self, dataloader_label: str, log_level: LogLevel, metrics: MetricCollection):
-        idx = min(len(metric_sequence) - 1, self.state.timer.epoch.value)
-        metric_val = metric_sequence[idx]
-        self.state.current_metrics[dataloader_label] = {"Accuracy": tensor(metric_val)}
+    test_metric_setter = TestMetricSetter("Accuracy", "eval", metric_sequence)
 
     trainer = Trainer(
         model=SimpleModel(num_features=5),
@@ -35,46 +51,9 @@ def test_threshold_stopper(metric_sequence: List[int]):
             batch_size=4,
         ),
         max_duration="30ep",
-        callbacks=[early_stopper],
+        callbacks=[test_metric_setter, early_stopper],
     )
-
-    trainer._compute_and_log_metrics = MethodType(test_compute_and_log_metrics, trainer)
 
     trainer.fit()
 
-    assert trainer.state.timer.epoch.value == len(metric_sequence) + early_stopper.patience - 1
-
-
-def test_threshold_stopper_evaluator():
-    metric_sequence = [0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-
-    early_stopper = EarlyStopper("Accuracy", "evaluator_1", patience=3)
-
-    def test_compute_and_log_metrics(self, dataloader_label: str, log_level: LogLevel, metrics: MetricCollection):
-        idx = min(len(metric_sequence) - 1, self.state.timer.epoch.value)
-        metric_val = metric_sequence[idx]
-        self.state.current_metrics[dataloader_label] = {"Accuracy": tensor(metric_val)}
-
-    evaluator = Evaluator(label="evaluator_1",
-                          dataloader=DataLoader(
-                              RandomClassificationDataset(shape=(5, 1, 1)),
-                              batch_size=4,
-                          ),
-                          metrics=Accuracy())
-
-    trainer = Trainer(
-        model=SimpleModel(num_features=5),
-        train_dataloader=DataLoader(
-            RandomClassificationDataset(shape=(5, 1, 1)),
-            batch_size=4,
-        ),
-        eval_dataloader=evaluator,
-        max_duration="30ep",
-        callbacks=[early_stopper],
-    )
-
-    trainer._compute_and_log_metrics = MethodType(test_compute_and_log_metrics, trainer)
-
-    trainer.fit()
-
-    assert trainer.state.timer.epoch.value == len(metric_sequence) + early_stopper.patience - 1
+    assert trainer.state.timer.epoch.value == len(metric_sequence) + early_stopper.patience
