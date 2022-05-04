9d7956f7a723424c24d20c4c4a4c14b8d93c0fe5
Hanlin Tang <hanlin@mosaicml.com>
2022-05-04 07:28:58 -0700

    log accuracy as float

--- composer/callbacks/mlperf.py
+++ composer/callbacks/mlperf.py
@@ -221,10 +221,12 @@ class MLPerfCallback(Callback):
         for key, value in data.items():
             self.mllogger.event(key=key, value=value)
 
-    def _get_accuracy(self, state: State):
+    def _get_accuracy(self, state: State) -> float:
         if self.metric_name not in state.current_metrics[self.metric_label]:
-            raise ValueError('Accuracy must be a validation metric.')
-        return state.current_metrics[self.metric_label][self.metric_name]
+            raise ValueError(f'{self.metric_name} must be a validation metric.')
+
+        metric = state.current_metrics[self.metric_label][self.metric_name]
+        return float(metric)
 
     def fit_start(self, state: State, logger: Logger) -> None:
         if rank_zero():
