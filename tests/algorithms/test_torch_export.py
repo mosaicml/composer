b21d79940aa0c06ac6cfd88a873cac9b0c36688e
Hanlin Tang <hanlin@mosaicml.com>
2022-05-11 09:13:23 -0700

    add test for torch fx for inference

--- tests/algorithms/test_torch_export.py
+++ tests/algorithms/test_torch_export.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+import torch.fx
 from torchvision.models import resnet50
 
 from composer.functional import (apply_blurpool, apply_channels_last, apply_factorization, apply_ghost_batchnorm,
@@ -63,3 +64,25 @@ def test_surgery_torchscript_eval(surgery_method, input):
     scripted_func.eval()  # type: ignore (third-party)
     model.eval()
     torch.testing.assert_allclose(scripted_func(input), model(input))  # type: ignore (third-party)
+
+
+@pytest.mark.parametrize("surgery_method", [
+    pytest.param(apply_blurpool, marks=pytest.mark.xfail(reason="control flow")),
+    pytest.param(apply_factorization),
+    pytest.param(apply_ghost_batchnorm, marks=pytest.mark.xfail(reason="control flow")),
+    pytest.param(apply_squeeze_excite),
+    pytest.param(apply_stochastic_depth),
+    pytest.param(apply_channels_last)
+])
+@pytest.mark.timeout(5)
+def test_surgery_torchfx_eval(surgery_method, input):
+    """Tests torch.fx model in eval mode."""
+    model = resnet50()
+    kwargs = algo_kwargs.get(surgery_method, {})
+
+    surgery_method(model, **kwargs)
+
+    model.eval()
+
+    traced_func = torch.fx.symbolic_trace(model)
+    torch.testing.assert_allclose(traced_func(input), model(input))  # type: ignore (third-party)
