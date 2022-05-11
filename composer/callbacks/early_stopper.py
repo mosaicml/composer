8e13f8ac4f7575562586d2c1d15169af32400008
mcneela <daniel.mcneela@gmail.com>
2022-05-11 10:35:25 -0700

    proofread  docstrings

--- composer/callbacks/early_stopper.py
+++ composer/callbacks/early_stopper.py
@@ -21,13 +21,14 @@ __all__ = ["EarlyStopper"]
 
 
 class EarlyStopper(Callback):
-    """This callback tracks a training or evaluation metric and halts training if the metric does not
-    improve within a given interval.
+    """This callback tracks a training or evaluation metric and halts 
+    training if the metric does not improve within a given interval.
 
     Example
 
     .. doctest::
 
+        >>> from composer import Evaluator, Trainer
         >>> from composer.callbacks.early_stopper import EarlyStopper
         >>> from torchmetrics.classification.accuracy import Accuracy
         >>> # constructing trainer object with this callback
@@ -49,18 +50,18 @@ class EarlyStopper(Callback):
     Args:
         monitor (str): The name of the metric to monitor.
         dataloader_label (str): The label of the dataloader or evaluator associated with the tracked metric. If 
-            monitor is in an Evaluator, the dataloader_label field should be set to the Evaluator's label. If 
-            monitor is a training metric or an ordinary evaluation metric not in an Evaluator, dataloader_label
-            should be set to 'train' or 'eval' respectively.
-        comp (Callable[[Any, Any], Any], optional): A comparison operator to measure change of the monitored metric. The comparison
-            operator will be called ``comp(current_value, prev_best)``. For metrics where the optimal value is low
-            (error, loss, perplexity), use a less than operator and for metrics like accuracy where the optimal value
+            monitor is in an :class:`.Evaluator`, the ``dataloader_label`` field should be set to the :class:`.Evaluator`'s label.
+            If monitor is a training metric or an ordinary evaluation metric not in an :class:`.Evaluator`, ``dataloader_label``
+            should be set to ``'train'`` or ``'eval'``, respectively.
+        comp (Callable[[Any, Any], Any], optional): A comparison operator to measure change of the monitored metric. 
+            The comparison operator will be called ``comp(current_value, prev_best)``. For metrics where the optimal value is low
+            (error, loss, perplexity), use a less than operator, and for metrics like accuracy where the optimal value
             is higher, use a greater than operator. Defaults to :func:`torch.less` if loss, error, or perplexity are substrings
-            of the monitored metric, otherwise defaults to :func:`torch.greater`
-        min_delta (float, optional): An optional float that requires a new value to exceed the best value by at
-            least that amount. Defaults to 0.
-        patience (int | str | Time, optional): The interval of time the monitored metric can not improve without stopping
-            training. Defaults to 1 epoch. If patience is an integer, it is interpreted as the number of epochs.
+            of the monitored metric, otherwise defaults to :func:`torch.greater`.
+        min_delta (float, optional): An optional float that requires a new value to exceed the best value by at least that amount.
+            Default: ``0.0``.
+        patience (int | str | :class:`.Time`, optional): The interval of time the monitored metric can not improve without stopping
+            training. Default: 1 epoch. If patience is an integer, it is interpreted as the number of epochs.
     """
 
     def __init__(
