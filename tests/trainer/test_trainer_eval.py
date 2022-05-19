ee4f15f0435f72f02edb6efa700a635574c19ad3
Moin Nadeem <moin@mosaicml.com>
2022-05-18 17:19:29 -0700

    Run evaluation at the end of training (#1045)
    Runs evaluation at the end of training if eval_interval is not a multiple of the training duration

--- tests/trainer/test_trainer_eval.py
+++ tests/trainer/test_trainer_eval.py
@@ -5,7 +5,7 @@ import torchmetrics
 from torch.utils.data import DataLoader
 
 from composer.core import Event
-from composer.core.evaluator import Evaluator
+from composer.core.evaluator import Evaluator, evaluate_periodically
 from composer.core.state import State
 from composer.core.time import Time, TimeUnit
 from composer.datasets.evaluator import EvaluatorHparams
@@ -89,6 +89,50 @@ def test_trainer_eval_timestamp():
     assert trainer.state.eval_timestamp.batch == trainer.state.eval_timestamp.batch_in_epoch
 
 
+@pytest.mark.parametrize("eval_at_fit_end", [
+    True,
+    False,
+])
+def test_eval_at_fit_end(eval_at_fit_end: bool):
+    """Test the `eval_subset_num_batches` and `eval_interval` works when specified on init."""
+
+    # Construct the trainer
+    train_dataloader = DataLoader(dataset=RandomClassificationDataset())
+    event_counter_callback = EventCounterCallback()
+    eval_interval = "2ep"
+    evaluator = Evaluator(
+        label="eval",
+        dataloader=DataLoader(dataset=RandomClassificationDataset()),
+        metrics=torchmetrics.Accuracy(),
+    )
+
+    evaluator.eval_interval = evaluate_periodically(eval_interval=eval_interval, eval_at_fit_end=eval_at_fit_end)
+
+    trainer = Trainer(
+        model=SimpleModel(),
+        train_dataloader=train_dataloader,
+        eval_dataloader=evaluator,
+        eval_subset_num_batches=1,
+        max_duration='3ep',
+        callbacks=[event_counter_callback],
+    )
+
+    # Train (should evaluate once)
+    trainer.fit()
+
+    expected_eval_start_calls = 1
+    expected_eval_batch_start_calls = 1
+
+    # depending on eval_at_fit_end, ensure the appropriate amount of calls are invoked
+    if eval_at_fit_end:
+        # we should have one extra call from eval_at_fit_end
+        assert event_counter_callback.event_to_num_calls[Event.EVAL_START] == expected_eval_start_calls + 1
+        assert event_counter_callback.event_to_num_calls[Event.EVAL_BATCH_START] == expected_eval_batch_start_calls + 1
+    else:
+        assert event_counter_callback.event_to_num_calls[Event.EVAL_START] == expected_eval_start_calls
+        assert event_counter_callback.event_to_num_calls[Event.EVAL_BATCH_START] == expected_eval_batch_start_calls
+
+
 @pytest.mark.parametrize("eval_dataloader", [
     DataLoader(dataset=RandomClassificationDataset()),
     Evaluator(
@@ -140,7 +184,7 @@ def test_eval_hparams(composer_trainer_hparams: TrainerHparams):
     composer_trainer_hparams.evaluators = [
         EvaluatorHparams(
             label="eval1",
-            eval_interval='3ep',  # should NEVER run, since we train just 2 epochs
+            eval_interval='3ep',  # will run, since eval_at_fit_end = True
             subset_num_batches=1,
             eval_dataset=RandomClassificationDatasetHparams(),
         ),
@@ -164,7 +208,7 @@ def test_eval_hparams(composer_trainer_hparams: TrainerHparams):
 
     # Validate that `eval_interval` and `subset_num_batches` was set correctly for the evaluator that actually
     # ran
-    assert "eval1" not in trainer.state.current_metrics
+    assert "eval1" in trainer.state.current_metrics
     assert "eval2" in trainer.state.current_metrics
     event_counter_callback = None
     for callback in trainer.state.callbacks:
@@ -172,9 +216,10 @@ def test_eval_hparams(composer_trainer_hparams: TrainerHparams):
             event_counter_callback = callback
             break
     assert event_counter_callback is not None
-    assert event_counter_callback.event_to_num_calls[Event.EVAL_START] == 1
+    assert event_counter_callback.event_to_num_calls[Event.EVAL_START] == 2
+    # increment by one for the extra call to `Event.EVAL_BATCH_START` during the evaluation at FIT end.
     assert event_counter_callback.event_to_num_calls[
-        Event.EVAL_BATCH_START] == composer_trainer_hparams.eval_subset_num_batches
+        Event.EVAL_BATCH_START] == composer_trainer_hparams.eval_subset_num_batches + 1
 
 
 def test_eval_params_evaluator():
