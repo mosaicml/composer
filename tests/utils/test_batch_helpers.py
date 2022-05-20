b7cca40eae6a7bf0bed53457b2b906d1a1699023
Evan Racah <evan@mosaicml.com>
2022-05-20 15:50:55 -0700

    Fix pyright error

--- tests/utils/test_batch_helpers.py
+++ tests/utils/test_batch_helpers.py
@@ -355,26 +355,6 @@ def test_batch_set_1D_array_list_of_slices_key(example_array):
         assert np.all(actual == expectation)
 
 
-@pytest.mark.parametrize('key,expected', [((1, 2), 5)])
-def test_batch_get_2D_array_tensor_2D_tuple_key(example_2D_array_tensor, key, expected):
-    actual = batch_get(example_2D_array_tensor, key)
-    assert int(actual) == expected
-
-
-@pytest.mark.parametrize('key,expected', [([1, 2], [[3, 4, 5], [6, 7, 8]]),
-                                          (np.asarray([1, 2]), [[3, 4, 5], [6, 7, 8]]),
-                                          (torch.tensor([1, 2]), [[3, 4, 5], [6, 7, 8]])])
-def test_batch_get_2D_array_tensor_2D_key(example_2D_array_tensor, key, expected):
-    actual = batch_get(example_2D_array_tensor, key)
-    assert actual.tolist() == expected
-
-
-@pytest.mark.parametrize('key,expected', [((slice(2, 4), slice(1, 3)), [[7, 8], [10, 11]])])
-def test_batch_get_2D_array_tensor_2D_slice_key(example_2D_array_tensor, key, expected):
-    actual = batch_get(example_2D_array_tensor, key)
-    assert actual.tolist() == expected
-
-
 @pytest.mark.parametrize('key,value', [((1, 2), 6)])
 def test_batch_set_2D_array_and_tensor_2D_tuple_key(example_2D_array_tensor, key, value):
     batch = batch_set(example_2D_array_tensor, key=key, value=value)
