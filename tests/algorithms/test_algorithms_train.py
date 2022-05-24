3d0d7f6cae30e05474d8ac4388822c2d8681bd02
Ravi Rahman <ravi@mosaicml.com>
2022-05-24 13:19:43 -0700

    Fixed layer freezing in `test_algorithm_trains`

--- tests/algorithms/test_algorithms_train.py
+++ tests/algorithms/test_algorithms_train.py
@@ -7,31 +7,30 @@ import pytest
 from torch.utils.data import DataLoader, Dataset
 
 from composer import Algorithm, ComposerModel, Trainer
+from composer.algorithms.layer_freezing.layer_freezing import LayerFreezing
 from tests.algorithms.algorithm_settings import get_algorithm_parametrization
-from tests.common import device
 
 
-class TestTrainerAlgorithms:
+@pytest.mark.timeout(5)
+@pytest.mark.gpu
+@pytest.mark.parametrize("alg_cls,alg_kwargs,model,dataset", get_algorithm_parametrization())
+def test_algorithm_trains(
+    alg_cls: Callable[..., Algorithm],
+    alg_kwargs: Dict[str, Any],
+    model: ComposerModel,
+    dataset: Dataset,
+):
+    trainer = Trainer(
+        model=model,
+        train_dataloader=DataLoader(dataset=dataset, batch_size=4),
+        max_duration='2ep',
+        algorithms=alg_cls(**alg_kwargs),
+    )
+    trainer.fit()
 
-    @pytest.mark.timeout(5)
-    @device('gpu')
-    @pytest.mark.parametrize("alg_cls,alg_kwargs,model,dataset", get_algorithm_parametrization())
-    def test_algorithm_trains(
-        self,
-        device: str,
-        alg_cls: Callable[..., Algorithm],
-        alg_kwargs: Dict[str, Any],
-        model: ComposerModel,
-        dataset: Dataset,
-    ):
-        trainer = Trainer(
-            model=model,
-            train_dataloader=DataLoader(dataset=dataset, batch_size=4),
-            max_duration='2ep',
-            device=device,
-            algorithms=alg_cls(**alg_kwargs),
-        )
-        trainer.fit()
+    if alg_cls is LayerFreezing:
+        pytest.xfail(("Layer freezing is incompatible with a second call to .fit() "
+                      "since all layers are frozen, and it does not unfreeze layers."))
 
-        # fit again for another epoch
-        trainer.fit(duration='1ep')
+    # fit again for another epoch
+    trainer.fit(duration='1ep')
