43f8d1464394bb5b779155f365c3aa77efc11587
Ravi Rahman <ravi@mosaicml.com>
2022-05-24 14:37:41 -0700

    Refactored imports

--- examples/checkpoint_with_wandb.py
+++ examples/checkpoint_with_wandb.py
@@ -16,7 +16,7 @@ from composer.models.classify_mnist import MNIST_Classifier
 # The project name must be deterministic, so we can restore from it
 wandb_logger = WandBLogger(
     log_artifacts=True,
-    init_params={
+    init_kwargs={
         'project': 'my-wandb-project-name',
     },
 )
