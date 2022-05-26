04b4a83b6237594deb2cf8b525c22936c2bdaa6a
ravi-mosaicml <ravi@mosaicml.com>
2022-05-26 05:46:43 -0700

    AutoYAHP Part 2: Cleanup the Callbacks and Loggers for AutoYAHP (#1071)
    Similar to #1056, this PR cleans up the callbacks and loggers for AutoYAHP. It does not depend on AutoYAHP itself (a future PR will remove the underlying hparam classes).
    
    - Refactored callback and logger tests to not depend on hparams
    - Reformatted the docstrings so they would be correctly parsed by auto-yahp
    - Added `callback_settings.py`, similar to `algorithm_settings.py`, to return a list of pytest param objects for parameterization across callback tests. These param objects include appropriate markers (e.g. conditional skipping for wandb and mlperf; requiring that the memory monitor runs on GPU, ...)
    - Moved the `TestTrainerAssets` into `tests/callbacks/test_callbacks.py`, since it tests the individual callbacks and loggers, not the trainer, and thus should live in `tests/callbacks`.
    - Cleaned up the `MemoryMonitor` warnings when cuda is not available. Now, it warns when the model is not on cuda, to catch the edge case where one does CPU training when GPUs are available.

--- tests/callbacks/test_memory_monitor.py
+++ tests/callbacks/test_memory_monitor.py
@@ -1,76 +1,44 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import cast
-from unittest.mock import MagicMock
-
 import pytest
-import torch
-from torch.cuda import device_count
+from torch.utils.data import DataLoader
 
-from composer.callbacks import MemoryMonitorHparams
-from composer.loggers import LoggerDestination
-from composer.trainer import TrainerHparams
-from composer.trainer.devices import DeviceGPU
-from composer.trainer.devices.device_hparams import GPUDeviceHparams
-from composer.utils import ensure_tuple
+from composer.callbacks import MemoryMonitor
+from composer.loggers import InMemoryLogger
+from composer.trainer import Trainer
+from tests.common import RandomClassificationDataset, SimpleModel, device
 
 
-def _do_trainer_fit(composer_trainer_hparams: TrainerHparams, testing_with_gpu: bool = False):
-    memory_monitor_hparams = MemoryMonitorHparams()
-    composer_trainer_hparams.callbacks.append(memory_monitor_hparams)
-
-    max_epochs = 1
-    composer_trainer_hparams.max_duration = f"{max_epochs}ep"
-
-    trainer = composer_trainer_hparams.initialize_object()
-
-    # Default model uses CPU
-    if testing_with_gpu:
-        trainer._device = DeviceGPU()
-
-    log_destination = MagicMock()
-    log_destination = cast(LoggerDestination, log_destination)
-    trainer.logger.destinations = ensure_tuple(log_destination)
-    trainer.fit()
-
-    num_train_steps = composer_trainer_hparams.train_subset_num_batches
-    assert isinstance(num_train_steps, int)
-
-    expected_calls = num_train_steps * max_epochs
-
-    return log_destination, expected_calls
-
-
-@pytest.mark.timeout(60)
-def test_memory_monitor_cpu(composer_trainer_hparams: TrainerHparams):
-    log_destination, _ = _do_trainer_fit(composer_trainer_hparams, testing_with_gpu=False)
-
-    if torch.cuda.device_count() > 0:
-        pytest.skip('Skip CPU memory monitor tests if CUDA is available.')
-
-    memory_monitor_called = False
-    for log_call in log_destination.log_data.mock_calls:
-        metrics = log_call[1][2]
-        if "memory/alloc_requests" in metrics:
-            if metrics["memory/alloc_requests"] > 0:
-                memory_monitor_called = True
-                break
-    assert not memory_monitor_called
+@device('cpu', 'gpu')
+def test_memory_monitor_warnings_on_cpu_models(device: str):
+    # Error if the user sets device=cpu even when cuda is available
+    del device  # unused. always using cpu
+    trainer = Trainer(
+        model=SimpleModel(),
+        callbacks=MemoryMonitor(),
+        device='cpu',
+        train_dataloader=DataLoader(RandomClassificationDataset()),
+        max_duration="1ba",
+    )
+    with pytest.warns(UserWarning, match="The memory monitor only works on CUDA devices"):
+        trainer.fit()
 
 
 @pytest.mark.gpu
-def test_memory_monitor_gpu(composer_trainer_hparams: TrainerHparams):
-    n_cuda_devices = device_count()
-    composer_trainer_hparams.device = GPUDeviceHparams()
-    if n_cuda_devices > 0:
-        log_destination, expected_calls = _do_trainer_fit(composer_trainer_hparams, testing_with_gpu=True)
+def test_memory_monitor_gpu():
+    # Construct the trainer
+    memory_monitor = MemoryMonitor()
+    in_memory_logger = InMemoryLogger()
+    trainer = Trainer(
+        model=SimpleModel(),
+        callbacks=memory_monitor,
+        loggers=in_memory_logger,
+        train_dataloader=DataLoader(RandomClassificationDataset()),
+        max_duration="1ba",
+    )
+    trainer.fit()
 
-        num_memory_monitor_calls = 0
+    num_memory_monitor_calls = len(in_memory_logger.data["memory/alloc_requests"])
 
-        for log_call in log_destination.log_data.mock_calls:
-            metrics = log_call[1][2]
-            if "memory/alloc_requests" in metrics:
-                if metrics["memory/alloc_requests"] > 0:
-                    num_memory_monitor_calls += 1
-        assert num_memory_monitor_calls == expected_calls
+    assert num_memory_monitor_calls == int(trainer.state.timestamp.batch)
