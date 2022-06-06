1a6440ffcbf47526a58c5fc78be9d23272ac6c4f
Ravi Rahman <ravi@mosaicml.com>
2022-06-06 08:15:46 -0700

    Removed memory format re-export

--- composer/datasets/__init__.py
+++ composer/datasets/__init__.py
@@ -8,11 +8,11 @@ from composer.datasets.brats import PytTrain, PytVal
 from composer.datasets.c4 import C4Dataset
 from composer.datasets.coco import COCODetection, StreamingCOCO
 from composer.datasets.imagenet import StreamingImageNet1k
-from composer.datasets.synthetic import (MemoryFormat, SyntheticBatchPairDataset, SyntheticDataLabelType,
-                                         SyntheticDataType, SyntheticPILDataset)
+from composer.datasets.synthetic import (SyntheticBatchPairDataset, SyntheticDataLabelType, SyntheticDataType,
+                                         SyntheticPILDataset)
 
 __all__ = [
     "ADE20k", "StreamingADE20k", "PytTrain", "PytVal", "C4Dataset", "COCODetection", "StreamingCOCO",
-    "StreamingImageNet1k", "MemoryFormat", "SyntheticBatchPairDataset", "SyntheticDataLabelType", "SyntheticDataType",
+    "StreamingImageNet1k", "SyntheticBatchPairDataset", "SyntheticDataLabelType", "SyntheticDataType",
     "SyntheticPILDataset"
 ]
