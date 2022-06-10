1c6016ec97e0ea339ead070d76627540cdb19f36
Evan Racah <evan@mosaicml.com>
2022-06-10 15:19:08 -0700

    fix doc build error

--- composer/algorithms/gradient_clipping/__init__.py
+++ composer/algorithms/gradient_clipping/__init__.py
@@ -4,7 +4,7 @@
 """`Adaptive gradient Clipping <https://arxiv.org/abs/2102.06171>`_ Clips all gradients in model based on ratio of
 gradient norms to parameter norms.
 
-See the :doc:`Method Card </method_cards/agc>` for more details.
+See the :doc:`Method Card </method_cards/gradient_clipping>` for more details.
 """
 
 from composer.algorithms.gradient_clipping.gradient_clipping import GradientClipping, apply_gradient_clipping
