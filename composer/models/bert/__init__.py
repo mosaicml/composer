c5233a792c352667ae13f6a34c962f882936093f
Jessica Forde <jzf2101@users.noreply.github.com>
2022-06-24 01:24:01 +0900

    updating bert init for testing

--- composer/models/bert/__init__.py
+++ composer/models/bert/__init__.py
@@ -4,12 +4,21 @@
 """The `BERT <https://huggingface.co/docs/transformers/master/en/model_doc/bert>`_ model family using `Hugging Face
 Transformers <https://huggingface.co/transformers/>`_."""
 
+from composer.models.bert.bert_hparams import BERTForClassificationHparams
 from composer.models.bert.bert_hparams import BERTForClassificationHparams as BERTForClassificationHparams
 from composer.models.bert.bert_hparams import BertForPretrainingHparams as BertForPretrainingHparams
+from composer.models.bert.bert_hparams import BERTForRegressionHparams
 from composer.models.bert.bert_hparams import BERTHparams as BERTHparams
-from composer.models.bert.model import BertForClassification, BertForPretraining, BERTModel
+from composer.models.bert.model import BertForClassification, BertForPretraining, BertForRegression, BERTModel
 
 __all__ = [
-    'BERTModel', 'BERTHparams', 'BERTForClassificationHparams', 'BertForClassification', 'BertForPretraining',
-    'BertForPretrainingHparams'
+    'BERTModel',
+    'BERTHparams',
+    'BERTForClassificationHparams',
+    'BertForClassification',
+    'BertForPretraining',
+    'BertForPretrainingHparams',
+    'BertForRegression',
+    'BERTForClassificationHparams',
+    'BERTForRegressionHparams',
 ]
