fc6a42038bfecc23a24a1d02ff75a2ba367074da
Evan Racah <evan@mosaicml.com>
2022-06-27 16:42:33 -0700

    Change arguments to constructor (add run_name, remove artifact_name) and add corresponding documentation

--- composer/loggers/tensorboard_logger.py
+++ composer/loggers/tensorboard_logger.py
@@ -20,44 +20,38 @@ class TensorboardLogger(LoggerDestination):
     """Log to `Tensorboard <https://www.tensorflow.org/tensorboard/>`_.
 
     Args:
-        log_dir (str, optional): The directory to store the logs. If
-            ``None``, the logs will be placed in ``./runs/{month}{day}{HH-MM-SS}_{device_name}.local``.
-        artifact_name (str, optional): Format string for the logfile's artifact name.
-
-            The logfile will be periodically logged (according to the ``flush_interval``) as a file artifact.
-            The artifact name will be determined by this format string.
-
-            .. seealso:: :meth:`~.log_file_artifact` for file artifact logging.
-
-            The same format variables for ``filename`` are available. Setting this parameter to ``None``
-            (the default) will use the same format string as ``filename``. It is sometimes helpful to deviate
-            from this default. For example, when ``filename`` contains an absolute path, it is recommended to
-            set this parameter explicitly, so the absolute path does not appear in any artifact stores.
-
-            Leading slashes (``'/'``) will be stripped.
-
-            Default: ``None`` (which uses the same format string as ``filename``)
-        flush_interval (int, optional): How frequently to flush the log to the file,
-            relative to the ``log_level``. For example, if the ``log_level`` is
-            :attr:`~.LogLevel.EPOCH`, then the logfile will be flushed every n epochs.  If
-            the ``log_level`` is :attr:`~.LogLevel.BATCH`, then the logfile will be
-            flushed every n batches. Default: ``100``.
+        run_name (str, optional): Tensorboard run name.
+            This is the name of the run as will appear in the Tensorboard GUI. Also, this
+            will be the name of the directory where the log files for this run will be
+            stored. The full path to this directory will be {log_dir}/{run_name}.
+            If not specified, the :attr:`.State.run_name` will be used.
+        log_dir (str, optional): The path to the directory where all the tensorboard logs
+            will be saved. This is also the value that should be specified when starting
+            a tensorboard server. e.g. `tensorboard --logdir={log_dir}`. If not specified
+            `./tensorboard_logs` will be used.
+        flush_interval (int, optional): How frequently by batch to flush the log to a file.
+            For example, a flush interval of 10 means the log will be flushed to a file
+            every 10 batches. The logs will also be automatically flushed at the start and
+            end of every evaluation phase (`EVENT.EVAL_START` and `EVENT.EVAL_END` ),
+            the end of every epoch (`EVENT.EPOCH_END`), and the end of training
+            (`EVENT.FIT_END`). Default: ``100``.
         rank_zero_only (bool, optional): Whether to log only on the rank-zero process.
-        log_level (LogLevel, optional):
-            :class:`~.logger.LogLevel` (i.e. unit of resolution) at
-            which to record. Default: :attr:`~.LogLevel.EPOCH`.
+            Recommended to be true since the rank 0 will have access to most global metrics.
+            A setting of `False` may lead to logging of duplicate values.
+            Default: :attr:`True`.
     """
 
     def __init__(self,
+                 run_name: Optional[str] = None,
                  log_dir: Optional[str] = None,
-                 artifact_name: Optional[str] = None,
                  flush_interval: int = 100,
                  rank_zero_only: bool = True):
 
+        self.run_name = run_name
         self.log_dir = log_dir
-        self.artifact_name = artifact_name
         self.flush_interval = flush_interval
         self.rank_zero_only = rank_zero_only
+        self.artifact_name: str = None
         self.writer: Optional[SummaryWriter] = None
 
     def log_data(self, state: State, log_level: LogLevel, data: Dict[str, Any]):
@@ -73,12 +67,12 @@ class TensorboardLogger(LoggerDestination):
                     pass
 
     def init(self, state: State, logger: Logger) -> None:
+        if self.run_name is None:
+            self.run_name = f'{state.run_name}'
         if self.log_dir is None:
-            self.log_dir = f'tensorboard/{state.run_name}'
-        if self.artifact_name is None:
-            self.artifact_name = self.log_dir
-        self.writer = SummaryWriter(log_dir=self.log_dir)
-        #self.should_eval = evaluate_periodically(state.evaluators[0].eval_interval)
+            self.log_dir = 'tensorboard_logs'
+        summary_writer_log_dir = Path(self.log_dir) / self.run_name
+        self.writer = SummaryWriter(log_dir=summary_writer_log_dir)
 
     def batch_end(self, state: State, logger: Logger) -> None:
         if int(state.timestamp.batch) % self.flush_interval == 0:
@@ -88,11 +82,9 @@ class TensorboardLogger(LoggerDestination):
         self._flush(logger)
 
     def eval_start(self, state: State, logger: Logger) -> None:
-        # Flush any log calls that occurred during INIT when using the trainer in eval-only mode
         self._flush(logger)
 
     def eval_end(self, state: State, logger: Logger) -> None:
-        # Flush any log calls that occurred during INIT when using the trainer in eval-only mode
         self._flush(logger)
 
     def fit_end(self, state: State, logger: Logger) -> None:
@@ -103,6 +95,6 @@ class TensorboardLogger(LoggerDestination):
     def _flush(self, logger: Logger):
         self.writer.flush()
         logger.file_artifact(LogLevel.FIT,
-                             self.artifact_name,
+                             artifact_name=self.run_name,
                              file_path=self.writer.file_writer.event_writer._file_name,
                              overwrite=True)
