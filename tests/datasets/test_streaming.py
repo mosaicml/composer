7f7a3fed9a64b9e645e6f4b85d3ebd68c9eaa4eb
James Knighton <iamknighton@gmail.com>
2022-06-27 13:50:33 -0700

    remote = none, same, different.

--- tests/datasets/test_streaming.py
+++ tests/datasets/test_streaming.py
@@ -64,16 +64,24 @@ def test_writer(remote_local: Tuple[str, str], num_samples: int, shard_size_limi
 
 @pytest.mark.timeout(10)
 @pytest.mark.parametrize('batch_size', [None, 1, 2])
-@pytest.mark.parametrize('share_remote_local', [False, True])
+@pytest.mark.parametrize('remote_arg', ['none', 'same', 'different'])
 @pytest.mark.parametrize('shuffle', [False, True])
-def test_reader(remote_local: Tuple[str, str], batch_size: int, share_remote_local: bool, shuffle: bool):
+def test_reader(remote_local: Tuple[str, str], batch_size: int, remote_arg: str, shuffle: bool):
     num_samples = 117
     shard_size_limit = 1 << 8
     samples, decoders = get_fake_samples_decoders(num_samples)
-    remote, local = remote_local
-    if share_remote_local:
-        local = remote
-    write_synthetic_streaming_dataset(dirname=remote, samples=samples, shard_size_limit=shard_size_limit)
+    if remote_arg == 'none':
+        remote, local = remote_local
+        dirname = local
+        remote = None
+    elif remote_arg == 'same':
+        remote, local = remote_local
+        dirname = local
+        remote = local
+    elif remote_arg == 'different':
+        remote, local = remote_local
+        dirname = remote
+    write_synthetic_streaming_dataset(dirname=dirname, samples=samples, shard_size_limit=shard_size_limit)
 
     # Build StreamingDataset
     dataset = StreamingDataset(remote=remote, local=local, shuffle=shuffle, decoders=decoders, batch_size=batch_size)
