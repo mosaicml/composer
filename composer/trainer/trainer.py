55c7e4aff62b06e95d99406ff31ddd2a93d81f26
Evan Racah <evan@mosaicml.com>
2022-06-29 15:17:57 -0700

    fix indent error

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -370,7 +370,7 @@ class Trainer:
         eval_interval (int | str | Time | (State, Event) -> bool, optional): Specifies how frequently to run evaluation.
             An integer, which will be interpreted to be epochs, a str (e.g. ``1ep``, or ``10ba``), a :class:`.Time`
             object, or a callable.
-             Defaults to ``1`` (evaluate every epoch).
+            Defaults to ``1`` (evaluate every epoch).
 
             If an integer (in epochs), :class:`.Time` string, or :class:`.Time` instance, the evaluator will be run
             with this frequency. :class:`.Time` strings or :class:`.Time` instances must have units of
