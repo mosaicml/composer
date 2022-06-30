2e6b57f3a97a4366912835f506d4714a2179c491
Landan Seguin <landanjs@gmail.com>
2022-06-30 00:08:43 +0000

    Support multiple losses in backwards pass

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -1814,8 +1814,10 @@ class Trainer:
                     loss.mul_(microbatch_num_samples / current_batch_size)
                     total_loss += loss.detach().clone()
             else:
+                final_loss = 0
                 for loss in ensure_tuple(self.state.loss):
-                    loss.backward(create_graph=self._backwards_create_graph)
+                    final_loss += loss
+                final_loss.backward(create_graph=self._backwards_create_graph)
 
             self.engine.run_event(Event.AFTER_BACKWARD)
 
