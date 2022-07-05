57cec2f4e0aacbdcbb16c361f5978d0db6836f9a
Laura Florescu <laura@mosaicml.com>
2022-07-05 19:06:50 +0000

    cleanup

--- composer/trainer/trainer_hparams.py
+++ composer/trainer/trainer_hparams.py
@@ -21,7 +21,6 @@ import composer
 from composer.algorithms.algorithm_hparams_registry import algorithm_registry
 from composer.callbacks.callback_hparams_registry import callback_registry
 from composer.core import Algorithm, Callback, DataSpec, Evaluator, Event, Precision, State, Time
-
 from composer.core.types import JSON, PyTorchScheduler
 from composer.datasets.dataset_hparams import DataLoaderHparams, DatasetHparams
 from composer.datasets.dataset_hparams_registry import dataset_registry
--- setup.py
+++ setup.py
@@ -65,19 +65,18 @@ while True:
         long_description = long_description[:start] + long_description[end + len(end_tag):]
 
 install_requires = [
-    "pyyaml>=5.4.1,<6",
-    "tqdm>=4.62.3,<5",
-    "torchmetrics>=0.7.0,<0.8",
-    "torch_optimizer>=0.1.0,<0.2",
-    #"torchvision>=0.10.0",  # torchvision has strict pytorch requirements
-    #"torch>=1.9,<2",
-    "yahp==0.1.1",
-    "requests>=2.26.0,<3",
-    "numpy>=1.21.5,<2",
-    "apache-libcloud>=3.3.1,<4",
-    "psutil>=5.8.0,<6",
-    "coolname>=1.1.0,<2",
-    "py-cpuinfo>=8.0.0",
+    'pyyaml>=6.0,<7',
+    'tqdm>=4.62.3,<5',
+    'torchmetrics>=0.7.0,<0.8',
+    'torch_optimizer>=0.1.0,<0.2',
+    'torchvision>=0.10.0',  # torchvision has strict pytorch requirements
+    'torch>=1.9,<2',
+    'yahp>=0.1.1,<0.2',
+    'requests>=2.26.0,<3',
+    'numpy>=1.21.5,<2',
+    'psutil>=5.8.0,<6',
+    'coolname>=1.1.0,<2',
+    'py-cpuinfo>=8.0.0',
 ]
 extra_deps = {}
 
