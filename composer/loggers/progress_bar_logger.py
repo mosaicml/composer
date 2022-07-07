fb045079de00e95d3032a8ed427556fe67e0af1e
Ravi Rahman <ravi@mosaicml.com>
2022-07-07 14:17:44 -0700

    debugging

--- composer/loggers/progress_bar_logger.py
+++ composer/loggers/progress_bar_logger.py
@@ -45,7 +45,7 @@ class _ProgressBar:
             position=position,
             bar_format=bar_format,
             file=file,
-            ncols=None if os.isatty(self.file.fileno()) else 80,
+            ncols=None if os.isatty(self.file.fileno()) else 120,
             dynamic_ncols=os.isatty(self.file.fileno()),
             # We set `leave=False` so TQDM does not jump around, but we emulate `leave=True` behavior when closing
             # by printing a dummy newline and refreshing to force tqdm to print to a stale line
@@ -261,16 +261,12 @@ class ProgressBarLogger(LoggerDestination):
 
             timestamp_key = unit.name.lower()
 
-        padding = '' #  ' ' * (0 if os.isatty(self.stream.fileno()) else 80)
-
-        bar_format = desc + ' ' + '{l_bar}{bar:25}{r_bar}' + padding + '{bar:-1b}'
-
         return _ProgressBar(
             file=self.stream,
             total=total,
             position=position,
             keys_to_log=_IS_TRAIN_TO_KEYS_TO_LOG[is_train],
-            bar_format=bar_format,
+            bar_format=desc + ' {l_bar}{bar:25}{r_bar}{bar:-1b}',
             unit=unit.value.lower(),
             metrics={},
             timestamp_key=timestamp_key,
