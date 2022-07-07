99794b632548123c8e69f33bd3a60f2a8c931a8a
Ravi Rahman <ravi@mosaicml.com>
2022-07-07 14:12:01 -0700

    debugging

--- composer/loggers/progress_bar_logger.py
+++ composer/loggers/progress_bar_logger.py
@@ -260,7 +260,7 @@ class ProgressBarLogger(LoggerDestination):
 
             timestamp_key = unit.name.lower()
 
-        padding = ' ' * (0 if os.isatty(self.stream.fileno()) else 120)
+        padding = ' ' * (0 if os.isatty(self.stream.fileno()) else 20)
 
         bar_format = desc + ' ' + '{l_bar}{bar:25}{r_bar}' + padding + '{bar:-1b}'
 
