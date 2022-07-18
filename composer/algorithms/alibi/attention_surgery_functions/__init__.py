5b828bd5ba50cb7983bf77644087ef33ff13bf45
Matthew Leavitt <matthew@mosaicml.com>
2022-07-18 15:15:23 -0700

    precommit hooks

--- composer/algorithms/alibi/attention_surgery_functions/__init__.py
+++ composer/algorithms/alibi/attention_surgery_functions/__init__.py
@@ -1,13 +1,9 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-from composer.algorithms.alibi.attention_surgery_functions.utils import replacement_policy_mapping_builder
-
 # Import files that add functions to the `replacement_policy_mapping_builder` registry in order to actually
 # register those functions.
-from composer.algorithms.alibi.attention_surgery_functions import ( # pyright: reportUnusedImport=none
-    _bert,
-    _gpt2,
-)
+from composer.algorithms.alibi.attention_surgery_functions import _bert, _gpt2  # pyright: reportUnusedImport=none
+from composer.algorithms.alibi.attention_surgery_functions.utils import replacement_policy_mapping_builder
 
 __all__ = ['replacement_policy_mapping_builder']
