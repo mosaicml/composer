c3d8036ab04beb095f034e642f3d279bbe6959ff
laura <lara.florescu@gmail.com>
2022-07-21 20:25:23 +0000

    cleanup

--- composer/trainer/devices/device_hparams_registry.py
+++ composer/trainer/devices/device_hparams_registry.py
@@ -1,3 +1,4 @@
+
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
@@ -12,11 +13,12 @@ import yahp as hp
 from composer.trainer.devices.device import Device
 from composer.trainer.devices.device_cpu import DeviceCPU
 from composer.trainer.devices.device_gpu import DeviceGPU
+from composer.trainer.devices.device_tpu import DeviceTPU
 
 __all__ = ['device_registry']
 
 device_registry: Dict[str, Union[Type[Device], Type[hp.Hparams]]] = {
     'gpu': DeviceGPU,
     'cpu': DeviceCPU,
-    'amd_gpu': DeviceGPU,
+    'tpu': DeviceTPU,
 }
