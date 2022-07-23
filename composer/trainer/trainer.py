408719002426753f17ed70557feb026d51dbcaad
Mihir Patel <mihir.v.patel7@gmail.com>
2022-07-23 10:06:45 -0700

    turn off barrier for non adaptive

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -1671,14 +1671,15 @@ class Trainer:
 
             # Use monitored barrier to error on deadlock. If one rank OOMs and another doesn't and gets stuck
             # on a dist reduction in gradient syncronization, the monitored barrier will fail after the timeout.
-            try:
-                dist.monitored_barrier(timeout=datetime.timedelta(seconds=max(10, 0.5 * self.batch_compute_time)))
-            except RuntimeError as e:
-                raise RuntimeError(
-                    'A deadlock was encountered in the train loop, likely because a strict subset of '
-                    'ranks encountered CUDA OOM. If `grad_accum=auto`, try manually setting `grad_accum` '
-                    'instead. If `grad_accum` is not set to `auto`, try increasing `grad_accum` as at '
-                    'least one rank encountered a CUDA OOM error.') from e
+            # If `adaptive_gradient_accumulation=False`, the OOMing rank will instead crash, avoiding deadlock risk.
+            if self.adaptive_gradient_accumulation:
+                try:
+                    dist.monitored_barrier(timeout=datetime.timedelta(seconds=max(10, 0.5 * self.batch_compute_time)))
+                except RuntimeError as e:
+                    raise RuntimeError(
+                        'A deadlock was encountered in the train loop, likely because a strict subset of '
+                        'ranks encountered CUDA OOM when `grad_accum=auto`. Try manually setting `grad_accum` '
+                        'instead.') from e
             # Synchronize new batch compute time
             batch_compute_time = end_time - start_time
             batch_compute_time = self._device.tensor_to_device(torch.tensor([batch_compute_time], dtype=torch.float))
