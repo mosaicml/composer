a452faafe9b7ff7f518ed9488352e842d702f2f9
Mihir Patel <mihir.v.patel7@gmail.com>
2022-07-25 18:42:06 -0700

    generate schemas

--- composer/json_schemas/generate_json_schemas.py
+++ composer/json_schemas/generate_json_schemas.py
@@ -1,3 +1,8 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Generate JSON Schemas for all Hparam classes."""
+
 import os
 
 # Dataset Hparams
@@ -5,7 +10,8 @@ from composer.datasets.ade20k_hparams import ADE20kDatasetHparams, StreamingADE2
 from composer.datasets.brats_hparams import BratsDatasetHparams
 from composer.datasets.c4_hparams import C4DatasetHparams, StreamingC4Hparams
 from composer.datasets.cifar_hparams import CIFAR10DatasetHparams, StreamingCIFAR10Hparams
-from composer.datasets.dataset_hparams import DatasetHparams, DataLoaderHparams
+from composer.datasets.coco_hparams import COCODatasetHparams, StreamingCOCOHparams
+from composer.datasets.dataset_hparams import DataLoaderHparams, DatasetHparams
 from composer.datasets.evaluator_hparams import EvaluatorHparams
 from composer.datasets.glue_hparams import GLUEHparams
 from composer.datasets.imagenet_hparams import ImagenetDatasetHparams, StreamingImageNet1kHparams
@@ -15,32 +21,41 @@ from composer.datasets.synthetic_hparams import SyntheticHparamsMixin
 # Logger Hparams
 from composer.loggers.logger_hparams_registry import ObjectStoreLoggerHparams
 # Model Hparams
-from composer.models.bert import BERTHparams, BERTForClassificationHparams
+from composer.models.bert import BERTForClassificationHparams, BERTHparams
 from composer.models.classify_mnist import MnistClassifierHparams
 from composer.models.deeplabv3 import DeepLabV3Hparams
 from composer.models.efficientnetb0 import EfficientNetB0Hparams
 from composer.models.gpt2 import GPT2Hparams
+from composer.models.model_hparams import ModelHparams
 from composer.models.resnet import ResNetHparams
 from composer.models.resnet_cifar import ResNetCIFARHparams
 from composer.models.ssd import SSDHparams
 from composer.models.timm import TimmHparams
 from composer.models.unet import UnetHparams
 from composer.models.vit_small_patch16 import ViTSmallPatch16Hparams
-from composer.models.model_hparams import ModelHparams
 # Optimizer Hparams
-from composer.optim.optimizer_hparams_registry import AdamHparams, RAdamHparams, AdamWHparams, DecoupledAdamWHparams, SGDHparams, DecoupledSGDWHparams, RMSpropHparams
+from composer.optim.optimizer_hparams_registry import (AdamHparams, AdamWHparams, DecoupledAdamWHparams,
+                                                       DecoupledSGDWHparams, OptimizerHparams, RAdamHparams,
+                                                       RMSpropHparams, SGDHparams)
 # Trainer Hparams
-from composer.trainer.trainer_hparams import TrainerHparams, FitHparams, EvalHparams, ExperimentHparams
+from composer.trainer.trainer_hparams import EvalHparams, ExperimentHparams, FitHparams, TrainerHparams
 # Object Store Hparams
-from composer.utils.object_store.object_store_hparams import ObjectStoreHparams, LibcloudObjectStoreHparams, S3ObjectStoreHparams, SFTPObjectStoreHparams
+from composer.utils.object_store.object_store_hparams import (LibcloudObjectStoreHparams, ObjectStoreHparams,
+                                                              S3ObjectStoreHparams, SFTPObjectStoreHparams)
 
-hparam_classes = [ADE20kDatasetHparams, StreamingADE20kHparams, BratsDatasetHparams, C4DatasetHparams, StreamingC4Hparams, CIFAR10DatasetHparams, StreamingCIFAR10Hparams, DatasetHparams, DataLoaderHparams, EvaluatorHparams,
-    GLUEHparams, ImagenetDatasetHparams, StreamingImageNet1kHparams, LMDatasetHparams, MNISTDatasetHparams, SyntheticHparamsMixin, ObjectStoreLoggerHparams, BERTHparams, BERTForClassificationHparams,
-    MnistClassifierHparams, DeepLabV3Hparams, EfficientNetB0Hparams, GPT2Hparams, ResNetHparams, ResNetCIFARHparams, SSDHparams, TimmHparams, UnetHparams, ViTSmallPatch16Hparams,
-    ModelHparams, AdamHparams, RAdamHparams, AdamWHparams, DecoupledAdamWHparams, SGDHparams, DecoupledSGDWHparams, RMSpropHparams, TrainerHparams, FitHparams, EvalHparams, ExperimentHparams,
-    ObjectStoreHparams, LibcloudObjectStoreHparams, S3ObjectStoreHparams, SFTPObjectStoreHparams]
+hparam_classes = [
+    ADE20kDatasetHparams, StreamingADE20kHparams, BratsDatasetHparams, C4DatasetHparams, StreamingC4Hparams,
+    CIFAR10DatasetHparams, StreamingCIFAR10Hparams, COCODatasetHparams, StreamingCOCOHparams, DatasetHparams,
+    DataLoaderHparams, EvaluatorHparams, GLUEHparams, ImagenetDatasetHparams, StreamingImageNet1kHparams,
+    LMDatasetHparams, MNISTDatasetHparams, SyntheticHparamsMixin, ObjectStoreLoggerHparams, BERTHparams,
+    BERTForClassificationHparams, MnistClassifierHparams, DeepLabV3Hparams, EfficientNetB0Hparams, GPT2Hparams,
+    ResNetHparams, ResNetCIFARHparams, SSDHparams, TimmHparams, UnetHparams, ViTSmallPatch16Hparams, ModelHparams,
+    AdamHparams, RAdamHparams, AdamWHparams, DecoupledAdamWHparams, SGDHparams, DecoupledSGDWHparams, RMSpropHparams,
+    OptimizerHparams, TrainerHparams, FitHparams, EvalHparams, ExperimentHparams, ObjectStoreHparams,
+    LibcloudObjectStoreHparams, S3ObjectStoreHparams, SFTPObjectStoreHparams
+]
 
 for hparam_class in hparam_classes:
-    file_name = hparam_class.__name__.lower().replace('hparams', '_hparams')
-    with open(os.path.join(os.path.dirname(__name__), f'{file_name}.json'), 'w') as f:
+    file_name = hparam_class.__name__.lower().replace('hparams', '')
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{file_name}_hparams.json'), 'w') as f:
         hparam_class.dump_jsonschema(f=f)
