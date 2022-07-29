d87b61af4e67684bdbb9f07159227cfb437a4755
Alex Trott <alex@mosaicml.com>
2022-07-28 23:02:14 -0700

    Docstring tweaks

--- composer/algorithms/alibi/attention_surgery_functions/utils.py
+++ composer/algorithms/alibi/attention_surgery_functions/utils.py
@@ -17,50 +17,55 @@ AlibiReplacementFunction = Callable[[torch.nn.Module, int, int], Optional[torch.
 
 
 class PolicyRegistry(Dict[Type[torch.nn.Module], AlibiReplacementFunction]):
-    """A registry mapping """
+    """A registry mapping for ALiBi surgery."""
 
     def register(self,
                  *modules: Type[torch.nn.Module]) -> Callable[[AlibiReplacementFunction], AlibiReplacementFunction]:
-        """This decorator builds a registry that maps torch module types to their applicable `AlibiReplacementFunction`.
+        """This decorator registers mappings from torch module types to their ALiBi surgery functions.
 
-        To accommodate the specifics of composer's module surgery, our ALiBi implementation uses a registry to create
-        a ``Mapping[torch.nn.Module, AlibiReplacementFunction]``, where `AlibiReplacementFunction` is any function that
-        has a :data:`~.module_surgery.ReplacementFunction` signature but with an additional ``max_sequence_length`` argument.
+        To accommodate the specifics of composer's module surgery, our ALiBi implementation uses a
+        registry to create a ``Mapping[torch.nn.Module, AlibiReplacementFunction]``, where
+        `AlibiReplacementFunction` is any function that has a :data:`~.module_surgery.ReplacementFunction`
+        signature but with an additional ``max_sequence_length`` argument.
 
-        Implementation files (e.g., :file:`_gpt2.py`) populate :data:`policy_registry` (an instance of this class)
-        by defining instances of `AlibiReplacementFunction` functions and decorating them with
-        :meth:`policy_registry.register` (this method). One or more ``Type[torch.nn.Module]`` source classes must be supplied
-        as inputs to the decorator, which tells :data:`policy_registry` to map those source classes to the decorated function.
+        Implementation files (e.g., :file:`_gpt2.py`) populate :data:`policy_registry` (an instance of
+        this class) by defining instances of `AlibiReplacementFunction` functions and decorating them
+        with :meth:`policy_registry.register` (this method). One or more ``Type[torch.nn.Module]`` source
+        classes must be supplied as inputs to the decorator, which tells :data:`policy_registry`
+        to map those classes to the decorated function.
 
         Example:
 
-            .. code-block::
+        .. code-block::
 
-                from composer.algorithms.alibi.attention_surgery_functions.utils import policy_registry
-                from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
+           from composer.algorithms.alibi.attention_surgery_functions.utils import policy_registry
+           from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
 
-                @policy_registry.register(GPT2Attention)
-                def convert_gpt2_attention(module: torch.nn.Module, index: int, max_sequence_length: int):
-                    # Do surgery (change ``module`` or generate a new ``module`` instance to return)
-                    # Note that this function can (and often should for ALiBi) depend on ``max_sequence_length``
+           @policy_registry.register(GPT2Attention)
+           def convert_gpt2_attention(module: torch.nn.Module, index: int, max_sequence_length: int):
+               # Do surgery (change ``module`` or generate a new ``module`` instance to return)
+               # Note that this function should depend on ``max_sequence_length``
 
-                    # YOUR CODE HERE
+               # YOUR CODE HERE
 
-                    return module
+               return module
 
-        In the above example, ``convert_gpt2_attention`` (which is an instance of a `AlibiReplacementFunction` function)
-        is decorated with ``@policy_registry.register(GPT2Attention)``. Using the decorator this way instructs the ALiBi algorithms to
-        apply surgery to any instance of `GPT2Attention` within the model using ``convert_gpt2_attention`` (the decorated function).
+        In the above example, ``convert_gpt2_attention`` (an instance of a `AlibiReplacementFunction`
+        function) is decorated with ``@policy_registry.register(GPT2Attention)``. Using the decorator
+        this way instructs the ALiBi algorithms to apply surgery to any instance of `GPT2Attention`
+        within the model using ``convert_gpt2_attention`` (the decorated function).
 
-        Note that ``convert_gpt2_attention`` follows the specific signature of an `AlibiReplacementFunction`. :meth:`policy_registry.register`
-        will raise an exception if it is used to decorate a function that does not follow this signature. The requirements are:
-            * The function takes 3 input arguments
-            * Argument 1 has type ``torch.nn.Module``
-            * Argument 2 has type ``int``
-            * Argument 3 is named ``max_sequence_length`` and has type ``int``
+        Note that ``convert_gpt2_attention`` follows the specific signature of an `AlibiReplacementFunction`.
+        :meth:`policy_registry.register` will raise an exception if it is used to decorate a function that
+        does not follow this signature. The requirements are:
+        * The function takes 3 input arguments
+        * Argument 1 has type ``torch.nn.Module``
+        * Argument 2 has type ``int``
+        * Argument 3 is named ``max_sequence_length`` and has type ``int``
 
-        To better understand these requirements, it may be helpful to review composer's module surgery (:file:`composer/utils/module_surgery.py`)
-        and the way ALiBi's implementation uses `policy_registry` in :func:`composer.algorithms.alibi.apply_alibi`.
+        To better understand these requirements, it may be helpful to review composer's module
+        surgery (:file:`composer/utils/module_surgery.py`) and the way ALiBi's implementation uses
+        `policy_registry` in :func:`composer.algorithms.alibi.apply_alibi`.
         """
         if len(modules) == 0:
             raise ValueError('Registry decoration without any module class inputs has no effect.')
