1f955a93286910bb95c835991d7d088b7ba7b160
Ishana Shastri <ishana@mosaicml.com>
2022-08-02 17:12:11 -0700

    added __init__ to examples

---
+++ examples/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Examples of Composer entrypoints."""
+
+from examples.glue.nlp_trainer_hparams import GLUETrainerHparams, NLPTrainerHparams
+
+__all__ = ['GLUETrainerHparams', 'NLPTrainerHparams']
