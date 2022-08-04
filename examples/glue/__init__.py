6b2a8f27ff5a7b5169b4413b2b6fee758259ff37
Ishana Shastri <ishana@mosaicml.com>
2022-08-03 20:53:41 -0700

    Glue Entrypoint (#1263)
    This PR introduces an entrypoint through which one can pretrain NLP models and finetune on GLUE tasks all through the same script. The call structure is as follows:
    
    `python examples/glue/run_glue_trainer.py -f examples/glue/glue_example.yaml --training_scheme {pretrain, finetune, all}`
    
    The `examples/glue/glue_example.yaml` file demonstrates a simple entrypoint that pretrains Bert and finetunes on all the checkpoints saved using Object Store. Note the `pretrain_hparams` and `finetune_hparams` split structure to allow for more configurability!
    
    Closes: https://mosaicml.atlassian.net/browse/CO-596

---
+++ examples/glue/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""GLUE entrypoint dependencies."""
+
+from nlp_trainer_hparams import GLUETrainerHparams, NLPTrainerHparams
+
+__all__ = ['GLUETrainerHparams', 'NLPTrainerHparams']
