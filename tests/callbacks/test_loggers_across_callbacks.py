125e1bac45b262cc36576d475b101820b0f1e5ea
Hanlin Tang <hanlin@mosaicml.com>
2022-08-11 15:23:23 -0700

    test

--- tests/callbacks/test_loggers_across_callbacks.py
+++ tests/callbacks/test_loggers_across_callbacks.py
@@ -18,6 +18,7 @@ from tests.common import RandomClassificationDataset, SimpleModel
 @pytest.mark.parametrize('logger_cls', get_cbs_and_marks(loggers=True))
 @pytest.mark.parametrize('callback_cls', get_cbs_and_marks(callbacks=True))
 def test_loggers_on_callbacks(logger_cls: Type[LoggerDestination], callback_cls: Type[Callback]):
+
     logger_kwargs = get_cb_kwargs(logger_cls)
     if issubclass(logger_cls, ObjectStoreLogger):
         # Ensure that the remote directory does not conflict with any directory used by callbacks
