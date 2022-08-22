4d0e01ad02efca298ca1ade76c4a6ee236b78a72
Moin Nadeem <moin@mosaicml.com>
2022-08-22 17:09:06 +0000

    removing debugging files

--- log_free_memory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo "      date     time $(free -g | grep total | sed -E 's/^    (.*)/\1/g')"
-while true; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -g | grep Mem: | sed 's/Mem://g')"
-    sleep 1
-done
