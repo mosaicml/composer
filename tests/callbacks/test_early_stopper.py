b93a009292c1ee0c2c2495ab4638c515004f2a39
Hanlin Tang <hanlin@mosaicml.com>
2022-09-01 15:25:35 -0700

    fix unrelated test?

--- tests/callbacks/test_early_stopper.py
+++ tests/callbacks/test_early_stopper.py
@@ -45,6 +45,7 @@ def test_early_stopper(metric_sequence: List[float], unit: TimeUnit, device: str
         ),
         max_duration='30ep',
         callbacks=[test_metric_setter, early_stopper],
+        device=test_device,
     )
 
     trainer.fit()
