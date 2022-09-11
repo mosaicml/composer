d99ac887bdbc5b91e035cbaaf14a8abd95ca1aab
Mihir Patel <mihir.v.patel7@gmail.com>
2022-09-11 00:27:24 -0700

    disable check

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -1797,8 +1797,8 @@ class Trainer:
                         if use_grad_scaling:
 
                             def _train_microbatches_wrapper(**kwargs):
-                                if 'total_loss_dict' not in kwargs:
-                                    kwargs['total_loss_dict'] = total_loss_dict
+                                # if 'total_loss_dict' not in kwargs:
+                                kwargs['total_loss_dict'] = total_loss_dict
                                 self._train_microbatches(microbatches, **kwargs)
 
                             self.state.scaler.step(optimizer, closure=_train_microbatches_wrapper)
