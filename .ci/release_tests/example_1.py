a98939a3f7cd2a5f18e802aac1a55e9bf7d6fc32
Nikhil <nikhil@mosaicml.com>
2022-09-20 02:05:15 -0700

    Update example ci release test

--- .ci/release_tests/example_1.py
+++ .ci/release_tests/example_1.py
@@ -4,7 +4,7 @@ from torchvision import models
 my_model = models.resnet18()
 
 # add blurpool and squeeze excite layers
-my_model = cf.apply_blurpool(my_model)
-my_model = cf.apply_squeeze_excite(my_model)
+cf.apply_blurpool(my_model)
+cf.apply_squeeze_excite(my_model)
 
 # your own training code starts here
