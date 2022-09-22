34dcae2dc477ef6f6e0c7b5ba190d8edbd35bfdd
Mihir Patel <mihir.v.patel7@gmail.com>
2022-09-21 19:22:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ extra_deps['dev'] = [
     'sphinx_panels==0.6.0',
     'sphinxcontrib-images==0.9.4',
     'pytest_codeblocks==0.16.1',
-    'traitlets==5.3.0',  # required by testbook. Version 5.2.2 has an import bug, so pinning to 5.1.1, which worked previously.
+    'traitlets==5.3.0',
     'nbsphinx==0.8.9',
     'pandoc==2.2',
     'pypandoc==1.8.1',
@@ -138,7 +138,7 @@ extra_deps['dev'] = [
 ]
 
 extra_deps['deepspeed'] = [
-    'deepspeed==0.7.3',  # TODO should this be >=0.5.10,<0.6
+    'deepspeed>=0.7.0,<0.8',
 ]
 
 extra_deps['wandb'] = [
