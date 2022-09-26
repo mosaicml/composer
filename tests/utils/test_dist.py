c8976e385ae903d128f4e5831d15905432a007b4
Mihir Patel <mihir.v.patel7@gmail.com>
2022-09-26 11:37:35 -0700

    fix test

--- tests/utils/test_dist.py
+++ tests/utils/test_dist.py
@@ -1,12 +1,8 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-import contextlib
-import datetime
-
 import pytest
 
-from composer.trainer.devices import DeviceGPU
 from composer.utils import dist
 from tests.common import world_size
 
@@ -16,4 +12,4 @@ def test_run_local_rank_first_context(world_size):
     with pytest.raises(RuntimeError) as e:
         with dist.run_local_rank_zero_first():
             pass
-    assert 'If calling this function outside Trainer' in str(e) # Verify error raised is intended
+    assert 'If calling this function outside Trainer' in str(e)  # Verify error raised is intended
