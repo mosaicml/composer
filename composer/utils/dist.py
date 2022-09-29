8325cb6fd5fa3e7d1f3a576ee18be1de7ebdd1b8
Daniel King <daniel@mosaicml.com>
2022-09-29 12:08:09 -0700

    Mihir's fix to dist.py

--- composer/utils/dist.py
+++ composer/utils/dist.py
@@ -465,7 +465,7 @@ def run_local_rank_zero_first():
     ranks attempt to download the dataset to the
     same location.
     """
-    if dist.is_available() and dist.is_initialized():
+    if is_available() and is_initialized():
         # hold non-zero ranks until rank zero done
         if get_local_rank() != 0:
             dist.barrier()
