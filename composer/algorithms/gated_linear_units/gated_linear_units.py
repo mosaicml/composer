267be80196a59a8e53f82242f3ee33fcfb629814
jacobfulano <62222220+jacobfulano@users.noreply.github.com>
2022-09-29 13:59:06 -0400

    Update gated_linear_units.py
    Allow BERTModel to not be from HuggingFace. Also check for BertForMaskedLM and BertForSequenceClassification

--- composer/algorithms/gated_linear_units/gated_linear_units.py
+++ composer/algorithms/gated_linear_units/gated_linear_units.py
@@ -82,10 +82,13 @@ def apply_gated_linear_units(model: torch.nn.Module,
         raise MissingConditionalImportError(extra_deps_group='nlp', conda_package='transformers')
 
     # ensure that the model is an instance of a BERT model, since our replacement policy is only defined for BERTs
-    if not isinstance(model, HuggingFaceModel) and not (hasattr(model, 'model') and
-                                                        (isinstance(model.model, BertForMaskedLM) or
-                                                         isinstance(model.model, BertForSequenceClassification))):
-        raise TypeError('Gated Linear Units only has a surgery policy defined for instances of BERT models.')
+    unwrapped_model = model.model if isinstance(model, HuggingFaceModel) else model
+
+    if not isinstance(unwrapped_model, BERTModel):
+       raise TypeError('Gated Linear Units only has a surgery policy defined for instances of BERT models.')
+    
+    if not (isinstance(unwrapped_model, BertForMaskedLM) or isinstance(unwrapped_model, BertForSequenceClassification))):
+        raise TypeError('Gated Linear Units only has a surgery policy defined for instances of BERT models with BertForMaskedLM or BertForSequenceClassification modules.')
 
     if act_fn is None:
         intermediate_modules = {module for module in model.modules() if isinstance(module, BertIntermediate)}
