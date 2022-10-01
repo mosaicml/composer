3d62a94223514d909f708af74959650d2788aaa1
Evan Racah <evan@mosaicml.com>
2022-09-30 19:49:38 -0700

    Remove tensorflow-io from setup.py

--- setup.py
+++ setup.py
@@ -149,7 +149,6 @@ extra_deps['comet_ml'] = ['comet_ml>=3.31.12,<4.0.0']
 
 extra_deps['tensorboard'] = [
     'tensorboard>=2.9.1,<3.0.0',
-    'tensorflow-io>=0.26.0,<0.28',
 ]
 
 extra_deps['unet'] = [
