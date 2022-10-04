9b64b6072d90c25a74140a5d34afa987accf8af8
Nikhil <nikhil@mosaicml.com>
2022-10-04 14:26:11 -0700

    Add FusedLamb optimizer

--- composer/optim/optimizer_hparams_registry.py
+++ composer/optim/optimizer_hparams_registry.py
@@ -5,7 +5,7 @@
 
 from abc import ABC
 from dataclasses import asdict, dataclass
-from typing import Dict, Iterable, List, Optional, Type, Union
+from typing import Dict, Iterable, List, Optional, Tuple, Type, Union
 
 import torch
 import torch_optimizer
@@ -214,6 +214,37 @@ class RMSpropHparams(OptimizerHparams):
     centered: float = hp.auto(torch.optim.RMSprop, 'centered', ignore_docstring_errors=True)
 
 
+@dataclass
+class FusedLAMBHparams(OptimizerHparams):
+    """Hyperparameters for the :class:`~apex.optimizers.FusedLAMB` optimizer.
+
+    See :class:`~apex.optimizers.FusedLAMB` for documentation.
+
+    Args:
+        lr (float, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        betas (Tuple[float, float], optional): See :class:`~apex.optimizers.FusedLAMB`.
+        eps (float, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        weight_decay (float, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        amsgrad (bool, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        adam_w_mode (bool, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        grad_averaging (bool, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        set_grad_none (bool, optional): See :class:`~apex.optimizers.FusedLAMB`.
+        max_grad_norm (float, optional): See :class:`~apex.optimizers.FusedLAMB`.
+    """
+    from apex.optimizers import FusedLAMB
+    optimizer_cls = FusedLAMB
+
+    lr: float = hp.auto(FusedLAMB, 'lr', ignore_docstring_errors=True)
+    betas: Tuple[float, float] = hp.auto(FusedLAMB, 'betas', ignore_docstring_errors=True)
+    eps: float = hp.auto(FusedLAMB, 'eps', ignore_docstring_errors=True)
+    weight_decay: float = hp.auto(FusedLAMB, 'weight_decay', ignore_docstring_errors=True)
+    amsgrad: bool = hp.auto(FusedLAMB, 'amsgrad', ignore_docstring_errors=True)
+    adam_w_mode: bool = hp.auto(FusedLAMB, 'adam_w_mode', ignore_docstring_errors=True)
+    grad_averaging: bool = hp.auto(FusedLAMB, 'grad_averaging', ignore_docstring_errors=True)
+    set_grad_none: bool = hp.auto(FusedLAMB, 'set_grad_none', ignore_docstring_errors=True)
+    max_grad_norm: float = hp.auto(FusedLAMB, 'max_grad_norm', ignore_docstring_errors=True)
+
+
 optimizer_registry = {
     'adam': AdamHparams,
     'adamw': AdamWHparams,
@@ -222,4 +253,5 @@ optimizer_registry = {
     'sgd': SGDHparams,
     'decoupled_sgdw': DecoupledSGDWHparams,
     'rmsprop': RMSpropHparams,
+    'fusedlamb': FusedLAMBHparams,
 }
