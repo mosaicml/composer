aec34d588b93beffb1f603782c8e959a7187e50e
Brandon Cui <bcui@Brandons-MacBook-Pro.local>
2022-10-14 15:37:21 -0700

    Reverting edit to run_composer_trainer.py

--- examples/run_composer_trainer.py
+++ examples/run_composer_trainer.py
@@ -17,9 +17,6 @@ import sys
 import tempfile
 import warnings
 
-import performance
-
-from composer.loggers import LogLevel
 from composer.trainer.trainer_hparams import TrainerHparams
 from composer.utils import dist, warn_yahp_deprecation
 from composer.utils.misc import warning_on_one_line
@@ -41,8 +38,6 @@ def _main():
 
     if len(sys.argv) == 1:
         sys.argv.append('--help')
-
-    performance.register_all_algorithms()
 
     hparams = TrainerHparams.create(cli_args=True)  # reads cli args from sys.argv
 
