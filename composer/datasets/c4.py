bcaa23be38c6a98a2d50b036d1fe8b89d4fa480d
Mihir Patel <mihir.v.patel7@gmail.com>
2022-10-19 14:31:24 -0700

    update dataset

--- composer/datasets/c4.py
+++ composer/datasets/c4.py
@@ -8,7 +8,7 @@ This dataset is a colossal, cleaned version of Common Crawl's web crawl corpus a
 """
 import logging
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Iterator, Optional
 
 from torch.utils.data import DataLoader
 
@@ -23,8 +23,7 @@ __all__ = ['StreamingC4']
 
 class StreamingC4(StreamingDataset):
     """
-    Implementation of the C4 (Colossal Cleaned Common Crawl) dataset using StreamingDataset.
-
+    Implementation of the C4 (Colossal Cleaned Common Crawl) dataset using StreamingDataset V1.
     Args:
         remote (str): Remote directory (S3 or local filesystem) where dataset is stored.
         local (str): Local filesystem directory where dataset is cached during operation.
@@ -32,25 +31,11 @@ class StreamingC4(StreamingDataset):
         shuffle (bool): Whether to shuffle the samples in this dataset.
         tokenizer_name (str): The name of the HuggingFace tokenizer to use to tokenize samples.
         max_seq_len (int): The max sequence length of each token sample.
-        group_method (str): How to group text samples into token samples. Currently only supporting ``'truncate'``.
+        group_method (str): How to group text samples into token samples. Supports 'truncate' or 'concat'.
         max_retries (int): Number of download re-attempts before giving up. Default: 2.
         timeout (float): How long to wait for shard to download before raising an exception. Default: 120 sec.
         batch_size (Optional[int]): Hint batch_size that will be used on each device's DataLoader. Default: ``None``.
     """
-
-    def _decode(self, data: bytes) -> str:
-        return data.decode('utf-8')
-
-    def _tokenize(self, text_sample):
-        if self.group_method == 'truncate':
-            truncation = True
-            padding = 'max_length'
-            max_length = self.max_seq_len
-        else:
-            truncation = False
-            padding = False
-            max_length = None
-        return self.tokenizer(text_sample['text'], truncation=truncation, padding=padding, max_length=max_length)
 
     def __init__(self,
                  remote: str,
@@ -64,7 +49,6 @@ class StreamingC4(StreamingDataset):
                  timeout: float = 120,
                  batch_size: Optional[int] = None):
 
-        # HF Transformers is needed to build the tokenizer
         try:
             import transformers
         except ImportError as e:
@@ -73,8 +57,8 @@ class StreamingC4(StreamingDataset):
         # Validation
         if split not in ['train', 'val']:
             raise ValueError(f"split='{split}' must be one of ['train', 'val'].")
-        if group_method not in ['truncate']:
-            raise ValueError(f"Only group_method='truncate' is supported at this time.")
+        if group_method not in ['truncate', 'concat']:
+            raise ValueError(f"group_method='{group_method}' must be one of ['truncate', 'concat'].")
 
         # Build StreamingDataset
         decoders = {
@@ -98,12 +82,70 @@ class StreamingC4(StreamingDataset):
         if self.tokenizer.pad_token is None:
             # Some tokenizers (e.g. GPT2 tokenizer) have no padding token which causes bugs
             self.tokenizer.pad_token = self.tokenizer.eos_token
+        # suppress warnings when using group_method='concat' and no truncation
+        self.tokenizer.model_max_length = int(1e30)
 
+    # How to decode binary data from .mds files to python strings
+    def _decode(self, data: bytes) -> str:
+        return data.decode('utf-8')
+
+    # How to tokenize a text sample to a token sample
+    def _tokenize(self, text_sample):
+        if self.group_method == 'truncate':
+            truncation = True
+            padding = 'max_length'
+            max_length = self.max_seq_len
+        elif self.group_method == 'concat':
+            truncation = False
+            padding = False
+            max_length = None
+        else:
+            raise ValueError(f"Got unknown group_method='{self.group_method}'.")
+        return self.tokenizer(text_sample['text'], truncation=truncation, padding=padding, max_length=max_length)
+
+    # How to process a sample
     def __getitem__(self, idx: int) -> Dict[str, Any]:
         text_sample = super().__getitem__(idx)
         token_sample = self._tokenize(text_sample)
-        # Skip any token grouping, currently only supporting group_method='truncate'
         return token_sample
+
+    # Define iterable over samples
+    # Usually this can be left alone and inherited directly from super() class StreamingDataset, but concatenating samples is custom behavior.
+    # If group_method=='truncate', we simply return the token sample.
+    # If group_method=='concat', then we keep fetching token samples until we fill up max_seq_len.
+    def __iter__(self) -> Iterator[Any]:
+        if self.group_method == 'truncate':
+            iterator = super().__iter__()
+            yield from iterator
+
+        elif self.group_method == 'concat':
+            buffer = {}
+            while True:
+                iterator = super().__iter__()
+                for sample in iterator:
+
+                    for k, v in sample.items():
+                        buffer[k] = buffer.get(k, []) + v
+                    while len(buffer['input_ids']) >= self.max_seq_len:
+                        concat_sample = {}
+                        for k, v in buffer.items():
+                            concat_sample[k] = v[:self.max_seq_len]
+                            buffer[k] = v[self.max_seq_len:]
+                        yield concat_sample
+        else:
+            raise ValueError(f"Got unknown group_method='{self.group_method}'.")
+
+    # Define length
+    # Usually this can be left alone and inherited directly from super() class StreamingDataset, but concatenating samples is custom behavior.
+    # If group_method=='truncate', we simply return the # samples.
+    # If group_method=='concat', we repeat forever, and we don't have a defined length.
+    def __len__(self) -> Optional[int]:
+        if self.group_method == 'truncate':
+            return super().__len__()
+        elif self.group_method == 'concat':
+            return None
+        else:
+            raise ValueError(f"Got unknown group_method='{self.group_method}'.")
 
 
 def build_streamingc4_dataloader(
