5b765411d8acb5354428d94bc893508e3d5318ea
Evan Racah <ejracah@gmail.com>
2022-11-01 15:56:59 -0700

    pre-commit

--- composer/loggers/wandb_logger.py
+++ composer/loggers/wandb_logger.py
@@ -310,7 +310,7 @@ def _convert_to_wandb_image(image: Union[np.ndarray, torch.Tensor], channels_las
     # Add in length-one dimensions to get back up to 3
     # putting channels last.
     if image.ndim == 1:
-        image =  np.expand_dims(image, (1, 2))
+        image = np.expand_dims(image, (1, 2))
         channels_last = True
     if image.ndim == 2:
         image = np.expand_dims(image, 2)
