7a6f7cfa248270dc82341080ada5ebccdbfb3507
Evan Racah <ejracah@gmail.com>
2022-11-03 11:41:58 -0700

    add assert

--- tests/loggers/test_cometml_logger.py
+++ tests/loggers/test_cometml_logger.py
@@ -43,6 +43,7 @@ def comet_logger(monkeypatch, comet_offline_directory):
                                                   ([torch.rand(32, 32, 3), torch.rand(32, 32, 3)], True)])
 def test_comet_ml_log_image_saves_images(images: torch.Tensor, channels_last: bool, comet_logger: CometMLLogger,
                                          comet_offline_directory: str):
+    assert isinstance(comet_offline_directory, str)
     # Count expected images and generate numpy arrays from torch tensors.
     if isinstance(images, Sequence):
         expected_num_images = len(images)
