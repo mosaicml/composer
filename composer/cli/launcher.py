67d72cb21c7cba7ef5fc013bda9cf5e895a03bd8
Mihir Patel <mihir.v.patel7@gmail.com>
2022-11-02 21:10:53 -0700

    add env var

--- composer/cli/launcher.py
+++ composer/cli/launcher.py
@@ -289,8 +289,8 @@ def _launch_processes(
                 MASTER_ADDR=master_addr,
                 MASTER_PORT=str(master_port),
                 PYTHONUNBUFFERED='1',
+                NCCL_ASYNC_ERROR_HANDLING='1',
         ):
-
             # Populate the distributed variables in all launcher args
             for arg in training_script_args:
                 cmd.append(os.path.expandvars(os.path.expanduser(arg)))
