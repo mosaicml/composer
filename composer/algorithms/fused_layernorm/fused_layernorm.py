fce853333254e42b5b18c26cbaea71b3895e7e21
Nikhil <101217697+nik-mosaic@users.noreply.github.com>
2022-11-16 07:45:14 -0800

    Change from weight copy to zero/one init

--- composer/algorithms/fused_layernorm/fused_layernorm.py
+++ composer/algorithms/fused_layernorm/fused_layernorm.py
@@ -37,9 +37,8 @@ def from_LayerNorm(layer: torch.nn.Module, module_index: int) -> APEXFusedLayerN
     assert isinstance(layer,
                       torch.nn.LayerNorm), 'The replacement policy will look for all instances of torch.nn.LayerNorm'
     fused_layernorm = APEXFusedLayerNorm(normalized_shape=layer.normalized_shape, eps=layer.eps)
-    with torch.no_grad():
-        fused_layernorm.weight.copy_(layer.weight)
-        fused_layernorm.bias.copy_(layer.bias)
+    torch.nn.init.ones_(fused_layernorm.weight)
+    torch.nn.init.zeros_(fused_layernorm.bias)
     return fused_layernorm
 
 
