9d63975fb9c63f12ede86a6700afeb404c7b14ee
Evan Racah <ejracah@gmail.com>
2022-11-18 16:35:02 -0800

    remove commented out test

--- tests/callbacks/test_image_visualizer.py
+++ tests/callbacks/test_image_visualizer.py
@@ -51,39 +51,6 @@ def comet_logger(monkeypatch, comet_offline_directory):
     return comet_logger
 
 
-# @pytest.mark.parametrize('interval', ['9ba', '90ba', '2ep', '3ep', '7ep'])
-# def test_image_visualizer(interval: str):
-#     pytest.importorskip('wandb', reason='wandb is optional')
-#     # Construct the callback
-#     image_visualizer = ImageVisualizer(interval=interval)
-#     in_memory_logger = InMemoryLogger()  # track the logged images in the in_memory_logger
-
-#     num_train_epochs = 9
-
-#     # Construct the trainer and train
-#     trainer = Trainer(
-#         model=SimpleConvModel(),
-#         callbacks=image_visualizer,
-#         loggers=in_memory_logger,
-#         train_dataloader=DataLoader(RandomImageDataset()),
-#         eval_dataloader=DataLoader(RandomImageDataset()),
-#         max_duration=f'{num_train_epochs}ep',
-#     )
-
-#     trainer.fit()
-#     num_train_steps = int(trainer.state.timestamp.batch)
-#     num_train_tables = len(in_memory_logger.data['Images/Train'])
-#     num_eval_tables = len(in_memory_logger.data['Images/Eval'])
-
-#     expected_train_tables = (((num_train_steps - 1) // image_visualizer.interval.value) +
-#                              1) if image_visualizer.interval.unit == TimeUnit.BATCH else ((
-#                                  (num_train_epochs - 1) // image_visualizer.interval.value) + 1)
-
-#     assert isinstance(image_visualizer.interval, Time)
-#     assert num_train_tables == expected_train_tables
-#     assert num_eval_tables == num_train_epochs
-
-
 @pytest.mark.parametrize('interval', ['9ba', '90ba', '2ep', '3ep', '7ep'])
 def test_image_visualizer_with_wandb(test_wandb_logger, interval):
     wandb = pytest.importorskip('wandb', reason='wandb is optional')
