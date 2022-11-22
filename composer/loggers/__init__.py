fcc7c5aced544b52f6f111692afde1f5755f5984
Evan Racah <ejracah@gmail.com>
2022-11-21 17:31:05 -0800

    leave trailing comma in

--- composer/loggers/__init__.py
+++ composer/loggers/__init__.py
@@ -26,5 +26,5 @@ from composer.loggers.wandb_logger import WandBLogger
 # All needs to be defined properly for sphinx autosummary
 __all__ = [
     'Logger', 'LoggerDestination', 'FileLogger', 'InMemoryLogger', 'ProgressBarLogger', 'WandBLogger',
-    'RemoteUploaderDownloader', 'TensorboardLogger', 'CometMLLogger', 'ConsoleLogger'
+    'RemoteUploaderDownloader', 'TensorboardLogger', 'CometMLLogger', 'ConsoleLogger',
 ]
