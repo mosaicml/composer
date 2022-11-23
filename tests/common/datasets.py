bea101dc7ef5220830fe636a3ef8aadc2aa921e9
Daniel King <daniel@mosaicml.com>
2022-11-22 17:42:26 -0800

    Add random text dataset for LM

--- tests/common/datasets.py
+++ tests/common/datasets.py
@@ -168,3 +168,40 @@ class RandomTextClassificationDataset(torch.utils.data.Dataset):
             return {'input_ids': x, 'labels': y}
         else:
             return x, y
+
+
+class RandomTextLMDataset(torch.utils.data.Dataset):
+    """ Text classification dataset with values (just input token ids) drawn uniformly
+    Args:
+        vocab_size (int): vocab size to use (default: 10)
+        size (int): number of samples (default: 100)
+        sequence_length (int): sequence length to use, all sequences will be of this length with no padding (default: 8)
+        use_keys: (bool): whether to return the item in a dictionary with keys for input and output
+    """
+
+    def __init__(self, size: int = 100, vocab_size: int = 10, sequence_length: int = 8, use_keys: bool = False):
+        self.vocab_size = vocab_size
+        self.sequence_length = sequence_length
+        self.use_keys = use_keys
+
+        self.input_key = 'input_ids'
+
+        self.size = size
+        self.x = None
+        self.y = None
+
+        super().__init__()
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, index: int):
+        if self.x is None:
+            self.x = torch.randint(low=0, high=self.vocab_size, size=(self.size, self.sequence_length))
+
+        x = self.x[index]
+
+        if self.use_keys:
+            return {'input_ids': x}
+        else:
+            return x
