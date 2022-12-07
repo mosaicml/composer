c36d36112e18d55aec797b79ce0e92b7e8553087
Evan Racah <ejracah@gmail.com>
2022-12-06 17:49:56 -0800

    Add MLFlow Logger

--- setup.py
+++ setup.py
@@ -194,6 +194,10 @@ extra_deps['onnx'] = [
     'onnxruntime>=1.12.1,<2',
 ]
 
+extra_deps['mlflow'] = [
+    'mlflow>=2.0.1'
+]
+
 extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)
 
 composer_data_files = ['py.typed']
