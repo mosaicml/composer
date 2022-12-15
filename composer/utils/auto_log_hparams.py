7be598ed2be370d24cb2994f4124abeb60320f59
Evan Racah <ejracah@gmail.com>
2022-12-14 18:14:54 -0800

    Add dcostrings

--- composer/utils/auto_log_hparams.py
+++ composer/utils/auto_log_hparams.py
@@ -8,6 +8,84 @@ import numpy as np
 import torch
 
 __all__ = ['extract_hparams', 'convert_nested_dict_to_flat_dict', 'convert_flat_dict_to_nested_dict']
+def extract_hparams(locals_dict: Dict[str, Any]) -> Dict[str, Any]:
+    """Takes in local symbol table and recursively grabs any hyperparameter
+
+    Args:
+        locals_dict (Dict[str, Any]): The local symbol table returned when calling locals(),
+            which maps any free local variables' names to their values.
+
+    Raises:
+        RecursionError: If recursion depth reached.
+
+    Returns:
+        Dict[str, Any]: A nested dictionary with every element of locals_dict mapped to its
+            value or to another sub_dict.
+    """
+    hparams = {}
+    if 'self' in locals_dict:
+        locals_dict.pop('self')
+    try:
+        for k, v in locals_dict.items():
+            hparams_to_add = _grab_hparams(v)
+            hparams[k] = hparams_to_add
+    except RecursionError as e:
+        raise RecursionError(
+            f'Crawled too deeply into Trainer members and members of members, etc. during auto-logging of hparams: {str(e)}'
+        )
+    return hparams
+
+
+def convert_nested_dict_to_flat_dict(nested_dict: Dict, prefix='') -> Dict:
+    """Takes in a nested dict converts it to a flat dict with keys separated by slashes.
+
+    Args:
+        nested_dict (Dict): A dictionary containing at least one other dictionary.
+        prefix (str, optional): A prefix to left append to the keys in the dictionary.
+            'Defaults to ''.
+
+    Returns:
+        Dict: A flat dictionary representation of the nested one (contains no other
+            dictionaries inside of it)
+    """
+    flat_dict = {}
+    for k, v in nested_dict.items():
+        key = prefix + '/' + k if prefix != '' else k
+        # Recursively crawl sub-dictionary.
+        if isinstance(v, dict):
+            sub_flat_dict = convert_nested_dict_to_flat_dict(prefix=key, nested_dict=v)
+            flat_dict.update(sub_flat_dict)
+        else:
+            flat_dict[key] = v
+    return flat_dict
+
+
+def convert_flat_dict_to_nested_dict(flat_dict: Dict) -> Dict:
+    """Converts flat dictionary separated by slashes to nested dictionary.
+
+    Args:
+        flat_dict (Dict): flat dictionary containing no sub-dictionary with keys
+            separated by slashes. e.g. {'a':1, 'b/c':2}
+
+    Returns:
+        Dict: a nested dict.
+    """
+    nested_dict = {}
+    for k, v in flat_dict.items():
+        # Initially sub_dict is the main nested_dict, but we will continually update it to be the
+        # sub-dictionary of sub_dict.
+        sub_dict = nested_dict
+        sub_keys = k.split('/')
+        for sub_key in sub_keys[:-1]:
+            if sub_key not in sub_dict:
+                # Create a new sub-dictionary inside of sub_dict.
+                sub_dict[sub_key] = {}
+            # Change the sub_dict reference to be the sub-dictionary of sub_dict (i.e. go one level deeper).
+            sub_dict = sub_dict[sub_key]
+        # The last key in sub_keys does not map to a dict. It just maps to v.
+        sub_dict[sub_keys[-1]] = v
+    # Changes to sub_dict will be reflected in nested_dict, so we can just return nested_dict.
+    return nested_dict
 
 
 def _parse_dict_for_hparams(dic) -> Dict:
@@ -23,7 +101,7 @@ def _parse_dict_for_hparams(dic) -> Dict:
     return hparams_to_add
 
 
-def _grab_hparams(obj):
+def _grab_hparams(obj) -> Any:
     """Helper function that recursively parses objects for their hyperparameters."""
 
     if any([isinstance(obj, torch.Tensor), isinstance(obj, np.ndarray), isinstance(obj, array.array)]):
@@ -89,72 +167,3 @@ def _grab_hparams(obj):
             return {obj.__class__.__name__: obj}
 
 
-def extract_hparams(locals_dict: Dict[str, Any]) -> Dict[str, Any]:
-    """Takes in local symbol table and recursively grabs any hyperparameter
-
-    Args:
-        locals_dict (Dict[str, Any]): The local symbol table returned when calling locals(),
-            which maps any free local variables' names to their values.
-
-    Raises:
-        RecursionError: If recursion depth reached.
-
-    Returns:
-        Dict[str, Any]: A nested dictionary with every element of locals_dict mapped to its
-            value or to another sub_dict.
-    """
-    hparams = {}
-    if 'self' in locals_dict:
-        locals_dict.pop('self')
-    try:
-        for k, v in locals_dict.items():
-            hparams_to_add = _grab_hparams(v)
-            hparams[k] = hparams_to_add
-    except RecursionError as e:
-        raise RecursionError(
-            f'Crawled too deeply into Trainer members and members of members, etc. during auto-logging of hparams: {str(e)}'
-        )
-    return hparams
-
-
-def convert_nested_dict_to_flat_dict(nested_dict: Dict, prefix='') -> Dict:
-    """Takes in a nested dict converts it to a flat dict with keys separated by slashes.
-
-    Args:
-        nested_dict (Dict): A dictionary containing at least one other dictionary.
-        prefix (str, optional): A prefix to left append to the keys in the dictionary.
-            'Defaults to ''.
-
-    Returns:
-        Dict: A flat dictionary representation of the nested one (contains no other
-            dictionaries inside of it)
-    """
-    flat_dict = {}
-    for k, v in nested_dict.items():
-        key = prefix + '/' + k if prefix != '' else k
-        # Recursively crawl sub-dictionary.
-        if isinstance(v, dict):
-            sub_flat_dict = convert_nested_dict_to_flat_dict(prefix=key, nested_dict=v)
-            flat_dict.update(sub_flat_dict)
-        else:
-            flat_dict[key] = v
-    return flat_dict
-
-
-def convert_flat_dict_to_nested_dict(flat_dict: Dict):
-    nested_dict = {}
-    for k, v in flat_dict.items():
-        # Initially sub_dict is the main nested_dict, but we will continually update it to be the
-        # sub-dictionary of sub_dict.
-        sub_dict = nested_dict
-        sub_keys = k.split('/')
-        for sub_key in sub_keys[:-1]:
-            if sub_key not in sub_dict:
-                # Create a new sub-dictionary inside of sub_dict.
-                sub_dict[sub_key] = {}
-            # Change the sub_dict reference to be the sub-dictionary of sub_dict (i.e. go one level deeper).
-            sub_dict = sub_dict[sub_key]
-        # The last key in sub_keys does not map to a dict. It just maps to v.
-        sub_dict[sub_keys[-1]] = v
-    # Changes to sub_dict will be reflected in nested_dict, so we can just return nested_dict.
-    return nested_dict
