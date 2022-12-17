a8c779292a51c8052679ef0c9a19a0c7c25b6dd2
Daniel King <daniel@mosaicml.com>
2022-12-16 16:57:04 -0800

    add gpt2 factory test

---
+++ tests/models/test_gpt2.py
@@ -0,0 +1,28 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from torch.utils.data import DataLoader
+
+from composer.models.gpt2 import create_gpt2
+from composer.trainer import Trainer
+from tests.common.datasets import RandomTextLMDataset
+
+
+def test_gpt2_hf_factory(gpt2_config, gpt2_tokenizer, monkeypatch):
+    transformers = pytest.importorskip('transformers')
+    monkeypatch.setattr('transformers.AutoConfig.from_pretrained', lambda x: gpt2_config)
+    gpt2_composer_model = create_gpt2(use_pretrained=False,
+                                      pretrained_model_name='dummy',
+                                      model_config=None,
+                                      tokenizer_name=None,
+                                      gradient_checkpointing=False)
+
+    train_dataset = RandomTextLMDataset(size=8, vocab_size=gpt2_tokenizer.vocab_size, sequence_length=8, use_keys=True)
+    collator = transformers.DataCollatorForLanguageModeling(tokenizer=gpt2_tokenizer, mlm=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=4, collate_fn=collator)
+
+    trainer = Trainer(model=gpt2_composer_model, train_dataloader=train_dataloader, max_duration='1ep')
+    trainer.fit()
+
+    assert trainer.state.train_metrics['Perplexity'].compute() > 0.0
