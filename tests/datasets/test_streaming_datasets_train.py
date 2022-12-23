f4efe6dac44c52b036b807535673c1584aac64e6
Daniel King <daniel@mosaicml.com>
2022-12-23 01:12:09 +0000

    WIP

--- tests/datasets/test_streaming_datasets_train.py
+++ tests/datasets/test_streaming_datasets_train.py
@@ -17,7 +17,7 @@ from tests.common import device, world_size
 
 @device('cpu', 'gpu')
 @world_size(1, 2)
-@pytest.mark.parametrize('num_workers', [0, 1, 2])
+@pytest.mark.parametrize('num_workers', [1, 2])
 @pytest.mark.parametrize('dataset,dataset_args,seed',
                          [('c4', {
                              'remote': 's3://mosaicml-internal-dataset-c4/mds/2/',
@@ -25,23 +25,25 @@ from tests.common import device, world_size
                              'max_seq_len': 256,
                              'group_method': 'truncate'
                          }, 1),
-                          ('pile', {
-                              'remote': 's3://mosaicml-internal-dataset-the-pile/mds/2/',
-                              'tokenizer_name': 'bert-base-uncased',
-                              'max_seq_len': 256,
-                              'group_method': 'truncate'
-                          }, 2), ('enwiki', {
-                              'remote': 's3://mosaicml-internal-dataset-enwiki-20200101/mds/2b/'
-                          }, 3)
+                        #   ('pile', {
+                        #       'remote': 's3://mosaicml-internal-dataset-the-pile/mds/2/',
+                        #       'tokenizer_name': 'bert-base-uncased',
+                        #       'max_seq_len': 256,
+                        #       'group_method': 'truncate'
+                        #   }, 2), ('enwiki', {
+                        #       'remote': 's3://mosaicml-internal-dataset-enwiki-20200101/mds/2b/'
+                        #   }, 3)
                           ])
 def test_streaming_datasets(num_workers, dataset, dataset_args, seed, tiny_bert_tokenizer, tiny_bert_model, world_size,
                             device, tmp_path):
+    os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+
     # Need to initialize dist before we get to streaming, because streaming always uses NCCL
     if not dist.is_initialized():
         dist.initialize_dist(device=device)
 
-    if num_workers == 2 and device == 'gpu':
-        pytest.xfail("don't know. fatal python error")
+    # if num_workers == 2 and device == 'gpu':
+    #     pytest.xfail("don't know. fatal python error")
 
     streaming = pytest.importorskip('streaming')
     transformers = pytest.importorskip('transformers')
@@ -50,17 +52,20 @@ def test_streaming_datasets(num_workers, dataset, dataset_args, seed, tiny_bert_
         'pile': streaming.text.pile.StreamingPile,
         'enwiki': streaming.text.enwiki.StreamingEnWiki
     }
+    
+    full_seed = seed + (num_workers + 1) * 10 + (world_size + 1) * 100 + (1 if device == 'cpu' else 2) * 1000
+    # This seed setting is necessary to prevent a shared memory collision due to a streaming bug
+    np.random.seed(full_seed)
 
     # distribute the local dataset path from rank 0
     local_path = [os.path.abspath(tmp_path)]
     dist.broadcast_object_list(local_path, src=0)
-    local_path = Path(local_path[0]) / dataset
+    local_path = Path(local_path[0]) / dataset / str(full_seed)
 
-    # This seed setting is necessary to prevent a shared memory collision due to a streaming bug
-    np.random.seed(seed + (num_workers + 1) * 10 + (world_size + 1) * 100 + (1 if device == 'cpu' else 2) * 1000)
+
     streaming_dataset = name_to_cls[dataset](local=local_path,
                                              split='val',
-                                             predownload=None,
+                                             predownload=1,
                                              batch_size=8,
                                              **dataset_args)
 
@@ -71,8 +76,8 @@ def test_streaming_datasets(num_workers, dataset, dataset_args, seed, tiny_bert_
     model = HuggingFaceModel(model=tiny_bert_model, use_logits=True, metrics=pretraining_metrics)
     collator = transformers.DataCollatorForLanguageModeling(tokenizer=tiny_bert_tokenizer,
                                                             mlm_probability=0.15) if dataset != 'enwiki' else None
-    dataloader = DataLoader(streaming_dataset, batch_size=8, num_workers=num_workers, collate_fn=collator)
+    dataloader = DataLoader(streaming_dataset, batch_size=16, num_workers=num_workers, collate_fn=collator)
 
-    trainer = Trainer(model=model, train_dataloader=dataloader, max_duration='2ba', device=device)
+    trainer = Trainer(model=model, train_dataloader=dataloader, max_duration='4ba', device=device)
 
     trainer.fit()
