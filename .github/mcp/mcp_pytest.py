1cb04df83f04e1d5da6578624965aaef423a2af9
Mihir Patel <mihir.v.patel7@gmail.com>
2023-01-11 07:54:16 -0800

    run on pr

--- .github/mcp/mcp_pytest.py
+++ .github/mcp/mcp_pytest.py
@@ -1,66 +1,78 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""A hello world script using MCP."""
+"""Run pytest using MCP."""
 
 import argparse
 
 from mcli.sdk import RunConfig, RunStatus, create_run, follow_run_logs, wait_for_run_status
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--name', type=str, default='mcp_pytest', help='Name of run')
-parser.add_argument('--platform', type=str, default='r1z1', help='Platform to use')
-parser.add_argument('--gpu_type', type=str, default='a100_80gb', help='Type of GPU to use')
-parser.add_argument('--gpu_num', type=int, default=8, help='Number of the GPU to use')
-parser.add_argument('--image', type=str, default='mosaicml/pytorch:latest', help='Docker image to use')
-parser.add_argument('--git_branch', type=str, default='master', help='Name of the git branch to use')
-parser.add_argument('--pytest_markers', type=str, help='Markers to pass to pytest')
-parser.add_argument('--pytest_command', type=str, help='Command to run pytest')
-args = parser.parse_args()
+if __name__ == '__main__':
 
-command = f'''
-export COMMON_ARGS="-v --durations=20 -m '{args.pytest_markers}'"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, default='mcp_pytest', help='Name of run')
+    parser.add_argument('--platform', type=str, default='r1z1', help='Platform to use')
+    parser.add_argument('--gpu_type', type=str, default='a100_80gb', help='Type of GPU to use')
+    parser.add_argument('--gpu_num', type=int, default=8, help='Number of the GPU to use')
+    parser.add_argument('--image', type=str, default='mosaicml/pytorch:latest', help='Docker image to use')
+    parser.add_argument('--git_branch', type=str, default='dev', help='Git branch to check out')
+    parser.add_argument('--pr_number', type=int, help='PR number to check out. Overrides git_branch if specified')
+    parser.add_argument('--pytest_markers', type=str, help='Markers to pass to pytest')
+    parser.add_argument('--pytest_command', type=str, help='Command to run pytest')
+    args = parser.parse_args()
 
-cd composer
+    command = 'cd composer'
 
-# git config --global --add safe.directory .
+    # Checkout a specific PR if specified
+    if args.pr_number:
+        command += f'''
 
-make test PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS --codeblocks"
+        git fetch origin pull/{args.pr_number}/head:pr_branch
 
-make test-dist PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS" WORLD_SIZE=2
+        git checkout pr_branch
+        '''
 
-python -m coverage combine
+    command += f'''
 
-python -m coverage report
-'''
+    export COMMON_ARGS="-v --durations=20 -m '{args.pytest_markers}'"
 
-config = RunConfig(
-    name=args.name,
-    platform=args.platform,
-    gpu_type=args.gpu_type,
-    gpu_num=args.gpu_num,
-    image=args.image,
-    integrations=[{
-        'integration_type': 'git_repo',
-        'git_repo': 'mosaicml/composer',
-        'git_branch': args.git_branch,
-        'ssh_clone': 'False',
-        'pip_install': '--user -e .[all]',
-    }],
-    command=command,
-)
+    make test PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS --codeblocks"
 
-# Create run
-run = create_run(config)
+    make test-dist PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS" WORLD_SIZE=2
 
-# Wait till run starts before fetching logs
-run = wait_for_run_status(run, status='running')
+    python -m coverage combine
 
-# Print logs
-for line in follow_run_logs(run):
-    print(line.rstrip('\n'))
+    python -m coverage report
+    '''
 
-run = wait_for_run_status(run, 'completed')
-assert run.status == RunStatus.COMPLETED
+    config = RunConfig(
+        name=args.name,
+        platform=args.platform,
+        gpu_type=args.gpu_type,
+        gpu_num=args.gpu_num,
+        image=args.image,
+        integrations=[{
+            'integration_type': 'git_repo',
+            'git_repo': 'mosaicml/composer',
+            'git_branch': args.git_branch,
+            'ssh_clone': 'False',
+            'pip_install': '--user -e .[all]',
+        }],
+        command=command,
+    )
 
-# delete_runs([run])
+    # Create run
+    run = create_run(config)
+
+    # Wait till run starts before fetching logs
+    run = wait_for_run_status(run, status='running')
+
+    # Print logs
+    for line in follow_run_logs(run):
+        print(line.rstrip('\n'))
+
+    # Fail if command exited with non-zero exit code
+    run = wait_for_run_status(run, 'completed')
+    assert run.status == RunStatus.COMPLETED
+
+    # delete_runs([run])
