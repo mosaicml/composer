c99230435cd23728e9e5d8cbdc62bc92cbbaa64d
nik-mosaic <101217697+nik-mosaic@users.noreply.github.com>
2023-01-17 15:06:30 -0800

    Add NLP Algorithms Tests (#1839)
    This PR updates many algorithms tests to run on NLP models

--- tests/algorithms/test_low_precision_layernorm.py
+++ tests/algorithms/test_low_precision_layernorm.py
@@ -11,24 +11,31 @@ from composer.algorithms.low_precision_layernorm.low_precision_layernorm import 
 from composer.core.event import Event
 from composer.core.precision import Precision
 from composer.loggers import Logger
+from composer.models.huggingface import HuggingFaceModel
 from tests.common import device
-from tests.fixtures.synthetic_hf_state import make_dataset_configs, synthetic_hf_state_maker
+from tests.fixtures.synthetic_hf_state import (make_dataset_configs, synthetic_hf_state_maker,
+                                               synthetic_simple_transformer_state_maker)
 
 
-@pytest.fixture()
-def synthetic_bert_state(request: pytest.FixtureRequest):
-    synthetic_config = make_dataset_configs(model_family=['bert'])[0]
-    return synthetic_hf_state_maker(synthetic_config, request.session)
+def make_synthetic_state(family, session):
+    """Supported model families are 'simple_transformer', 'bert', 'gpt2', and 'bert_classification'.
+    """
+    if family == 'simple_transformer':
+        return synthetic_simple_transformer_state_maker(session)
+    synthetic_config = make_dataset_configs(model_family=[family])[0]
+    return synthetic_hf_state_maker(synthetic_config, session)
 
 
 def assert_is_lpln_instance(model):
     pytest.importorskip('transformers')
-    from transformers import BertForMaskedLM, BertForSequenceClassification
 
-    assert isinstance(model, BertForMaskedLM) or isinstance(model, BertForSequenceClassification)
+    # When checking modules of a HuggingFace model, we need to parse the model object it wraps
+    # This is not necessary for SimpleTransformerClassifier models.
+    if isinstance(model, HuggingFaceModel):
+        model = model.model
+
     # ensure that within the entire model, no PyTorch LayerNorm exists, and at least one APEX FLN does.
     assert model.modules is not None, 'model has .modules method'
-
     for module_class in model.modules():
         if isinstance(module_class, LayerNorm):
             assert isinstance(
@@ -41,27 +48,33 @@ def assert_is_lpln_instance(model):
 
 
 @device('gpu')
-def test_low_precision_layernorm_functional(synthetic_bert_state: Tuple, device: str):
-    state, _, _ = synthetic_bert_state
+@pytest.mark.parametrize('synthetic_state_family', [
+    'bert',
+    'simple_transformer',
+])
+def test_low_precision_layernorm_functional(synthetic_state_family: Tuple, device: str, request: pytest.FixtureRequest):
+    state, _, _ = make_synthetic_state(synthetic_state_family, request.session)
+    if device == 'gpu':
+        state.model = state.model.cuda()  # move the model to gpu
     state._precision = Precision('amp_fp16')
+
     apply_low_precision_layernorm(state.model, state.optimizers, state._precision)
-    assert_is_lpln_instance(state.model.model)
+    assert_is_lpln_instance(state.model)
 
 
 @device('gpu')
-def test_low_precision_layernorm_algorithm(synthetic_bert_state: Tuple, empty_logger: Logger, device: str):
-    pytest.importorskip('transformers')
-    from transformers import BertForMaskedLM, BertForSequenceClassification
-
-    state, _, _ = synthetic_bert_state
+@pytest.mark.parametrize('synthetic_state_family', [
+    'bert',
+    'simple_transformer',
+])
+def test_low_precision_layernorm_algorithm(synthetic_state_family: Tuple, empty_logger: Logger, device: str,
+                                           request: pytest.FixtureRequest):
+    state, _, _ = make_synthetic_state(synthetic_state_family, request.session)
     state._precision = Precision('amp_fp16')
     low_precision_layernorm = LowPrecisionLayerNorm()
     if device == 'gpu':
         state.model = state.model.cuda()  # move the model to gpu
 
-    # state.model wrapped in HuggingFaceModel wrapped
-    assert isinstance(state.model.model, BertForMaskedLM) or isinstance(state.model.model,
-                                                                        BertForSequenceClassification)
     low_precision_layernorm.apply(Event.INIT, state, empty_logger)
 
-    assert_is_lpln_instance(state.model.model)
+    assert_is_lpln_instance(state.model)
