1ae5b61c0d552e9fad8b949d8db6c4dc2410ff3d
Mihir Patel <mihir.v.patel7@gmail.com>
2023-01-26 13:16:48 -0800

    change to commit

--- .github/mcp/mcp_pytest.py
+++ .github/mcp/mcp_pytest.py
@@ -15,16 +15,30 @@ if __name__ == '__main__':
     parser.add_argument('--gpu_type', type=str, default='a100_40gb', help='Type of GPU to use')
     parser.add_argument('--gpu_num', type=int, default=2, help='Number of the GPU to use')
     parser.add_argument('--image', type=str, default='mosaicml/pytorch:latest', help='Docker image to use')
-    parser.add_argument('--git_branch', type=str, default='dev', help='Git branch to check out')
-    parser.add_argument('--pr_number', type=int, help='PR number to check out. Overrides git_branch if specified')
+    parser.add_argument('--git_branch', type=str, help='Git branch to check out')
+    parser.add_argument('--git_commit', type=str, help='Git commit to check out. Overrides git_branch if specified')
+    parser.add_argument('--pr_number',
+                        type=int,
+                        help='PR number to check out. Overrides git_branch/git_commit if specified')
     parser.add_argument('--pytest_markers', type=str, help='Markers to pass to pytest')
     parser.add_argument('--pytest_command', type=str, help='Command to run pytest')
     args = parser.parse_args()
 
+    git_integration = {
+        'integration_type': 'git_repo',
+        'git_repo': 'mosaicml/composer',
+        'ssh_clone': 'False',
+        'pip_install': '--user -e .[all]',
+    }
+    if args.git_branch is not None and args.git_commit is None:
+        git_integration['git_branch'] = args.git_branch
+    if args.git_commit is not None:
+        git_integration['git_commit'] = args.git_commit
+
     command = 'cd composer'
 
     # Checkout a specific PR if specified
-    if args.pr_number:
+    if args.pr_number is not None:
         command += f'''
 
         git fetch origin pull/{args.pr_number}/head:pr_branch
@@ -52,13 +66,7 @@ if __name__ == '__main__':
         gpu_type=args.gpu_type,
         gpu_num=args.gpu_num,
         image=args.image,
-        integrations=[{
-            'integration_type': 'git_repo',
-            'git_repo': 'mosaicml/composer',
-            'git_branch': args.git_branch,
-            'ssh_clone': 'False',
-            'pip_install': '--user -e .[all]',
-        }],
+        integrations=[git_integration],
         command=command,
     )
 
