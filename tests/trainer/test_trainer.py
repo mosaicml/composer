6f83e58e2789f7b8c94c189a2771f2edf6ac1170
Bandish Shah <bandish@mosaicml.com>
2023-02-08 22:40:26 -0800

    fix lint

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1143,7 +1143,8 @@ class TestTrainerEvents():
         with pytest.raises(AssertionError):
             trainer.fit()
 
-@pytest.mark.skip(reason="<Jira issue>: Temporarily disabling to debug failures")
+
+@pytest.mark.skip(reason='<Jira issue>: Temporarily disabling to debug failures')
 @pytest.mark.vision
 class TestFFCVDataloaders:
 
