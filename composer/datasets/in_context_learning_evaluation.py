ef8ada58cd6a7e9afb1f631431cb08cbb61cc01a
Daniel King <daniel@mosaicml.com>
2023-02-16 18:16:55 -0800

    fix

--- composer/datasets/in_context_learning_evaluation.py
+++ composer/datasets/in_context_learning_evaluation.py
@@ -105,8 +105,8 @@ class InContextLearningLMTaskDataset(Dataset):
             raise MissingConditionalImportError(extra_deps_group='nlp',
                                                 conda_package='datasets',
                                                 conda_channel='conda-forge') from e
-
-        get_file(dataset_uri, f'rank{dist.get_local_rank()}' + destination_path, overwrite=True)
+        destination_path = f'rank{dist.get_local_rank()}' + destination_path
+        get_file(dataset_uri, destination_path, overwrite=True)
         dataset = load_dataset('json', data_files=destination_path, split='train', streaming=False)
         self.samples = list(
             dataset.map(lambda examples: {
@@ -247,7 +247,8 @@ class InContextLearningMultipleChoiceTaskDataset(Dataset):
                                                 conda_package='datasets',
                                                 conda_channel='conda-forge') from e
 
-        get_file(dataset_uri, f'rank{dist.get_local_rank()}' + destination_path, overwrite=True)
+        destination_path = f'rank{dist.get_local_rank()}' + destination_path
+        get_file(dataset_uri, destination_path, overwrite=True)
         dataset = load_dataset('json', data_files=destination_path, split='train', streaming=False)
         self.samples = list(
             dataset.map(lambda examples: {
