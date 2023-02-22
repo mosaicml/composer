cb35e15eefbd27734ff8553c525498a99137743c
Mihir Patel <mihir.v.patel7@gmail.com>
2023-02-21 17:23:56 -0800

    add runtime estimator

--- composer/callbacks/runtime_estimator.py
+++ composer/callbacks/runtime_estimator.py
@@ -4,9 +4,11 @@
 """Estimate total time of training."""
 from __future__ import annotations
 
-from typing import Any, Deque, Dict
+import time
+import warnings
+from typing import Optional
 
-from composer.core import Callback, State
+from composer.core import Callback, State, TimeUnit
 from composer.loggers import Logger
 
 __all__ = ['RuntimeEstimator']
@@ -44,3 +46,47 @@ class RuntimeEstimator(Callback):
     | ``wall_clock/remaining_estimate`` | Estimated time to completion                            |
     +-----------------------------------+---------------------------------------------------------+
     """
+
+    def __init__(self) -> None:
+        self._enabled = True
+        self.start_time = time.time()
+        self.checkpoint_dur = None
+
+    def get_elapsed_duration(self, state: State) -> Optional[float]:
+        """Get the elapsed duration.
+
+        Unlike `state.get_elapsed_duration`, this method computes fractional progress in an epoch
+        provided at least 1 epoch has passed by recording how many batches were in each epoch.
+        """
+        if state.max_duration is None:
+            return None
+        if state.max_duration.unit == TimeUnit('ep') and state.timestamp.epoch.value >= 1:
+            batches_per_epoch = (state.timestamp.batch -
+                                 state.timestamp.batch_in_epoch).value / state.timestamp.epoch.value
+            return state.timestamp.get('ba').value / (state.max_duration.value * batches_per_epoch)
+        elapsed_dur = state.get_elapsed_duration()
+        if elapsed_dur is not None:
+            return elapsed_dur.value
+        return None
+
+    def fit_start(self, state: State, logger: Logger) -> None:
+        self.checkpoint_dur = self.get_elapsed_duration(state)
+        if self.checkpoint_dur is None:
+            warnings.warn('`max_duration` is not set. Cannot estimate remaining time.')
+            self._enabled = False
+
+    def batch_end(self, state: State, logger: Logger) -> None:
+        if not self._enabled:
+            return
+
+        elapsed_dur = self.get_elapsed_duration(state)
+        if elapsed_dur is None:
+            self._enabled = False
+            warnings.warn('`max_duration` is not set. Cannot estimate remaining time.')
+            return
+
+        assert self.checkpoint_dur is not None
+        elapsed_time = time.time() - self.start_time
+        rate = elapsed_time / (elapsed_dur - self.checkpoint_dur)
+        remaining_time = rate * (1 - elapsed_dur)
+        logger.log_metrics({'wall_clock/remaining_estimate': remaining_time})
