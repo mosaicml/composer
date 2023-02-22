a84c832c90833e3e38ff04ef62526d594e580408
bmosaicml <jeremy@mosaicml.com>
2023-02-21 21:03:37 -0500

    fix lp prevention

--- composer/callbacks/__init__.py
+++ composer/callbacks/__init__.py
@@ -10,7 +10,6 @@ from composer.callbacks.checkpoint_saver import CheckpointSaver
 from composer.callbacks.early_stopper import EarlyStopper
 from composer.callbacks.export_for_inference import ExportForInferenceCallback
 from composer.callbacks.image_visualizer import ImageVisualizer
-from composer.callbacks.loss_spike_intervention import LossSpikeIntervention
 from composer.callbacks.lr_monitor import LRMonitor
 from composer.callbacks.memory_monitor import MemoryMonitor
 from composer.callbacks.mlperf import MLPerfCallback
@@ -29,7 +28,6 @@ __all__ = [
     'EarlyStopper',
     'ExportForInferenceCallback',
     'ThresholdStopper',
-    'LossSpikeIntervention',
     'ImageVisualizer',
     'RuntimeEstimator',
 ]
