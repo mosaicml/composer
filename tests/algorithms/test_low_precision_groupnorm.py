d57fa6a9145064cce50a99ebfb4594d358f7e7b4
Mihir Patel <mihir.v.patel7@gmail.com>
2023-02-23 16:26:06 -0800

    tweaks

--- tests/algorithms/test_low_precision_groupnorm.py
+++ tests/algorithms/test_low_precision_groupnorm.py
@@ -23,36 +23,38 @@ def assert_is_lpgn_instance(model):
     assert any(isinstance(module_class, LPGroupNorm) for module_class in model.modules())
 
 
-def test_low_precision_groupnorm_functional(device: str):
+def test_low_precision_groupnorm_functional():
     model = ConvModel(norm='groupnorm')
     dataloader = DataLoader(RandomImageDataset(), batch_size=2)
     state = State(
         model=model,
         rank_zero_seed=0,
         run_name='run_name',
-        device=get_device(device),
         dataloader=dataloader,
         dataloader_label='train',
         max_duration='1ep',
         precision='amp_fp16',
+        device=get_device('cpu'),
     )
 
     apply_low_precision_groupnorm(state.model, state.optimizers, state._precision)
     assert_is_lpgn_instance(state.model)
 
 
-def test_low_precision_groupnorm_algorithm(empty_logger: Logger, device: str):
+def test_low_precision_groupnorm_algorithm(empty_logger: Logger):
     model = ConvModel(norm='groupnorm')
     dataloader = DataLoader(RandomImageDataset(), batch_size=2)
 
-    state = State(model=model,
-                  rank_zero_seed=0,
-                  run_name='run_name',
-                  device=get_device(device),
-                  dataloader=dataloader,
-                  dataloader_label='train',
-                  max_duration='1ep',
-                  precision='amp_fp16')
+    state = State(
+        model=model,
+        rank_zero_seed=0,
+        run_name='run_name',
+        dataloader=dataloader,
+        dataloader_label='train',
+        max_duration='1ep',
+        precision='amp_fp16',
+        device=get_device('cpu'),
+    )
     low_precision_groupnorm = LowPrecisionGroupNorm()
 
     low_precision_groupnorm.apply(Event.INIT, state, empty_logger)
