c6261f3facf0a1844042b6afb2e3845d34b653db
Mihir Patel <mihir.v.patel7@gmail.com>
2023-03-01 09:01:06 -0800

    Update tests/metrics/metric_setter_callback.py

--- tests/metrics/metric_setter_callback.py
+++ tests/metrics/metric_setter_callback.py
@@ -53,7 +53,6 @@ class MetricSetterCallback(Callback):
         if self.device is not None:
             self.device.tensor_to_device(metric_tensor)
 
-        print('Metric arguments:', self.metric_args)
         raw_metric = self.metric_cls(**self.metric_args)  # type: ignore
         preds, targets = self._generate_dummy_metric_inputs(metric_val)
         raw_metric.update(preds=preds, target=targets)
