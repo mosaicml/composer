48f9bcd6863f88ab4b2a9c283974abab627078cf
Daniel King <daniel@mosaicml.com>
2023-03-02 15:00:07 -0800

    change text

--- composer/models/huggingface.py
+++ composer/models/huggingface.py
@@ -102,7 +102,9 @@ class HuggingFaceModel(ComposerModel):
             # and should be done by the user if desired
             log.warning(
                 f'The number of tokens in the tokenizer is less than the number of tokens in the model.'
-                f'You may want to resize the model embeddings to {len(tokenizer)} from {self.config.vocab_size}.')
+                f'You may want to resize the model embeddings to {len(tokenizer)} from {self.config.vocab_size}'
+                f'by calling `model.resize_token_embeddings(len(tokenizer))` before calling the `HuggingFaceModel`'
+                f'constructor.')
 
         self.use_logits = use_logits
 
