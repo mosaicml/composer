5038442b8ceac9d635770365b8511034ea95c3c8
Daniel King <daniel@mosaicml.com>
2023-03-02 15:02:54 -0800

    period

--- composer/models/huggingface.py
+++ composer/models/huggingface.py
@@ -92,7 +92,7 @@ class HuggingFaceModel(ComposerModel):
         if tokenizer is not None and self.config.vocab_size < len(tokenizer):
             # when the embedding size is smaller than the tokenizer vocab size,
             # the embeddings should get resized to match the tokenizer vocab size
-            log.warning(f'The number of tokens in the tokenizer is greater than the number of tokens in the model'
+            log.warning(f'The number of tokens in the tokenizer is greater than the number of tokens in the model.'
                         f' This would cause an error during training.'
                         f' Resizing the model embeddings to {len(tokenizer)} from {self.config.vocab_size}.')
             self.model.resize_token_embeddings(len(tokenizer))
@@ -104,7 +104,7 @@ class HuggingFaceModel(ComposerModel):
                 f'The number of tokens in the tokenizer is less than the number of tokens in the model.'
                 f' You may want to resize the model embeddings to {len(tokenizer)} from {self.config.vocab_size}'
                 f' by calling `model.resize_token_embeddings(len(tokenizer))` before calling the `HuggingFaceModel`'
-                f' constructor.')
+                f' gconstructor.')
 
         self.use_logits = use_logits
 
