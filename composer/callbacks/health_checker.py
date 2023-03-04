fab675a71397b3ac50b156b325c97e48d3175ab2
Daniel King <daniel@mosaicml.com>
2023-03-03 16:05:24 -0800

    precommit

--- composer/callbacks/health_checker.py
+++ composer/callbacks/health_checker.py
@@ -14,7 +14,7 @@ import torch
 from composer.core import Callback, State
 from composer.core.time import Timestamp
 from composer.loggers import Logger
-from composer.utils import dist, MissingConditionalImportError
+from composer.utils import MissingConditionalImportError, dist
 
 log = logging.getLogger(__name__)
 
