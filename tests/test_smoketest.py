74f3117625f936ec9a56d6506859828d9e6c5f3e
Daniel King <43149077+dakinggg@users.noreply.github.com>
2023-03-03 16:45:19 -0800

    Update tests/test_smoketest.py
    Co-authored-by: Mihir Patel <mihir.v.patel7@gmail.com>

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -5,7 +5,7 @@ from composer import (algorithms, callbacks, core, datasets, devices, functional
                       profiler, trainer, utils)
 
 
-# this very simple test is just to use the above imports, which check and make sure we can import all the top-level
+# This very simple test is just to use the above imports, which check and make sure we can import all the top-level
 # modules from composer. This is mainly useful for checking that we have correctly conditionally imported all optional
 # dependencies.
 def test_smoketest():
