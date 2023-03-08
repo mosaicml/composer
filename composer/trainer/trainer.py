5fba45f2e02d8aee96dafea4b51d633accff972f
Evan Racah <ejracah@gmail.com>
2023-03-08 14:42:14 -0800

    Move log message down

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -1175,7 +1175,7 @@ class Trainer:
             'num_nodes': int(dist.get_world_size() / dist.get_local_world_size()),
             f'num_{device_name}s_per_node': dist.get_local_world_size(),
             'node_name': os.environ.get('NODENAME', 'unknown because NODENAME env variable not set')
-            })
+        })
 
         if not isinstance(self.state.model, ComposerModel):
             raise ValueError('Provided model should be a subclass of ComposerModel.')
