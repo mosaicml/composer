bfe5c9aa838e2890c74f0a91b59f18ad9e0a3acc
Mihir Patel <mihir.v.patel7@gmail.com>
2023-03-09 13:35:12 -0800

    disable checkpointing

--- composer/utils/checkpoint.py
+++ composer/utils/checkpoint.py
@@ -272,8 +272,8 @@ def download_checkpoint(
     local_rank_zero_path = _get_local_rank_zero_path(local_path)
     logging.log(logging.WARN, f'local_path={local_path}, local_rank_zero_path={local_rank_zero_path}')
 
+    raise Exception(f'local_path={local_path}, local_rank_zero_path={local_rank_zero_path}')
     try:
-        print(f'\n\n\n\n\nlocal_path={local_path}, local_rank_zero_path={local_rank_zero_path}\n\n\n\n\n')
         # Download on local rank 0 or if path is different from local rank 0
         if dist.get_local_rank() == 0 or local_path != local_rank_zero_path:
             get_file_succeeded = True
