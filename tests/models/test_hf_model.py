9c9d93ac80882236c68f4135afac498cf8ce9025
Daniel King <daniel@mosaicml.com>
2023-03-15 16:12:21 -0700

    fix name_or_path usage

--- tests/models/test_hf_model.py
+++ tests/models/test_hf_model.py
@@ -104,6 +104,11 @@ def check_hf_tokenizer_equivalence(tokenizer1, tokenizer2):
     tokenizer1.__dict__.pop('deprecation_warnings')
     tokenizer2.__dict__.pop('deprecation_warnings')
 
+    tokenizer1.__dict__.pop('name_or_path')
+    tokenizer2.__dict__.pop('name_or_path')
+    model_max_length_1 = tokenizer1.init_kwargs.pop('name_or_path', None)
+    model_max_length_2 = tokenizer2.init_kwargs.pop('name_or_path', None)
+
     # tokenizer.init_kwargs['model_max_length'] is unset when the tokenizer does not specify it, but is set
     # to a very large number when you save and reload, so here we just check that its the same if it is present in
     # both tokenizers. There is a separate tokenizer.model_max_length that will still get checked for equivalence
@@ -212,9 +217,10 @@ def test_hf_state_dict_info(tmp_path: Path, pass_in_tokenizer: bool, modify_toke
                             _tmp_file.write('\n')
             loaded_tokenizer = transformers.AutoTokenizer.from_pretrained(_tmp_dir)
             # we need to set the name_or_path back because otherwise it is the tmp dir we are loading from here
-            loaded_tokenizer.name_or_path = hf_tokenizer_state['tokenizer_config']['content']['name_or_path']
-            loaded_tokenizer.init_kwargs['name_or_path'] = hf_tokenizer_state['tokenizer_config']['content'][
-                'name_or_path']
+            loaded_tokenizer.name_or_path = hf_tokenizer_state['tokenizer_config']['content'].get('name_or_path', '')
+            if 'name_or_path' in loaded_tokenizer.init_kwargs:
+                loaded_tokenizer.init_kwargs['name_or_path'] = hf_tokenizer_state['tokenizer_config']['content'].get(
+                    'name_or_path', '')
 
         # for an unknown reason this key is missing when loading the saved tokenizer, but present with a value of None
         # for the original tokenizer
