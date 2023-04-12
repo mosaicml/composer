d279fa34a42ae78335137cba100f1c8bb8f8af9d
Mihir Patel <mihir.v.patel7@gmail.com>
2023-04-12 10:28:54 -0700

    update

--- composer/loggers/mosaic_logger.py
+++ composer/loggers/mosaic_logger.py
@@ -1,29 +1,37 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""Log to MPlatform."""
+"""Log to the MosaicML platform."""
 
 from __future__ import annotations
 
+import collections.abc
 import logging
+import operator
 import os
 import time
+from functools import reduce
 from typing import Any, Dict, Optional
 
-from composer.loggers.logger import format_log_data_value
+import torch
+
+from composer.core import State
+from composer.loggers import Logger
 from composer.loggers.logger_destination import LoggerDestination
 from composer.utils import MissingConditionalImportError, dist
 
 log = logging.getLogger(__name__)
 
-__all__ = ['MosaicLogger']
+__all__ = ['MosaicMLLogger']
+
+RUN_NAME_ENV_VAR = 'RUN_NAME'
 
 
-class MosaicLogger(LoggerDestination):
-    """Log to MPlatform.
+class MosaicMLLogger(LoggerDestination):
+    """Log to the MosaicML platform.
 
-    Logs metrics to MPlatform. Logging only happens on rank 0 every ``log_interval`` seconds to
-    avoid performance issues.
+    Logs metrics to the MosaicML platform. Logging only happens on rank 0 every ``log_interval``
+    seconds to avoid performance issues.
 
     Args:
         log_interval (int, optional): Buffer log calls more frequent than ``log_interval`` seconds.
@@ -47,12 +55,12 @@ class MosaicLogger(LoggerDestination):
                 raise MissingConditionalImportError(extra_deps_group='mcli',
                                                     conda_package='mcli',
                                                     conda_channel='conda-forge') from e
-            self.run_name = os.environ.get('RUN_NAME')
+            self.run_name = os.environ.get(RUN_NAME_ENV_VAR)
             if self.run_name is not None:
                 log.info(f'Logging to mosaic run {self.run_name}')
             else:
-                log.warning('Environment variable `RUN_NAME` not set, so MosaicLogger is disabled '
-                            'as it is unable to identify which run to log to.')
+                log.warning(f'Environment variable `{RUN_NAME_ENV_VAR}` not set, so MosaicMLLogger '
+                            'is disabled as it is unable to identify which run to log to.')
                 self._enabled = False
 
     def log_hyperparameters(self, hyperparameters: Dict[str, Any]):
@@ -61,17 +69,65 @@ class MosaicLogger(LoggerDestination):
     def log_metrics(self, metrics: Dict[str, Any], step: Optional[int] = None) -> None:
         self._log_metadata(metrics)
 
+    def batch_end(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
+    def epoch_end(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
+    def fit_end(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
+    def eval_end(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
+    def predict_end(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
+    def close(self, state: State, logger: Logger) -> None:
+        self._flush_metadata()
+
     def _log_metadata(self, metadata: Dict[str, Any]) -> None:
+        """Buffer metadata and prefix keys with mosaicml."""
         if self._enabled:
             for key, val in metadata.items():
-                self.buffered_metadata[key] = format_log_data_value(val)
-            if time.time() - self.time_last_logged > self.log_interval:
-                from mcli.api.exceptions import MAPIException
-                from mcli.sdk import update_run_metadata
-                try:
-                    update_run_metadata(self.run_name, self.buffered_metadata)
-                except MAPIException as e:
-                    log.error(f'Failed to log metadata to Mosaic with error: {e}')
+                self.buffered_metadata[key] = f'mosaicml/{format_data_to_json_serializable(val)}'
+            self._flush_metadata()
 
+    def _flush_metadata(self) -> None:
+        """Flush buffered metadata to MosaicML if enough time has passed since last flush."""
+        if time.time() - self.time_last_logged > self.log_interval:
+            from mcli.api.exceptions import MAPIException
+            from mcli.sdk import update_run_metadata
+            try:
+                update_run_metadata(self.run_name, self.buffered_metadata)
                 self.buffered_metadata = {}
                 self.time_last_logged = time.time()
+            except MAPIException as e:
+                log.error(f'Failed to log metadata to Mosaic with error: {e}')
+
+
+def format_data_to_json_serializable(data: Any):
+    """Recursively formats data to be JSON serializable.
+
+    Args:
+        data: Data to format.
+
+    Returns:
+        str: ``data`` as a string.
+    """
+    if data is None:
+        return 'None'
+    if type(data) in (str, int, float, bool):
+        return data
+    if isinstance(data, torch.Tensor):
+        if data.shape == () or reduce(operator.mul, data.shape, 1) == 1:
+            return format_data_to_json_serializable(data.cpu().item())
+        return 'Tensor of shape ' + str(data.shape)
+    if isinstance(data, collections.abc.Mapping):
+        return {format_data_to_json_serializable(k): format_data_to_json_serializable(v) for k, v in data.items()}
+    if isinstance(data, collections.abc.Iterable):
+        return [format_data_to_json_serializable(v) for v in data]
+
+    # Unknown format catch-all
+    return str(data)
