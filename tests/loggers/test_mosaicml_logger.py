e03ac4829937139466bb703f026c82d88ebebce4
Mihir Patel <mihir.v.patel7@gmail.com>
2023-04-12 20:35:40 -0700

    add test file

---
+++ tests/loggers/test_mosaicml_logger.py
@@ -0,0 +1,133 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+from typing import Type
+
+import pytest
+import torch
+from torch.utils.data import DataLoader
+
+from composer.core import Callback
+from composer.loggers.mosaicml_logger import MosaicMLLogger, format_data_to_json_serializable
+from composer.trainer import Trainer
+from composer.utils import dist
+from tests.callbacks.callback_settings import get_cb_kwargs, get_cbs_and_marks
+from tests.common import RandomClassificationDataset, SimpleModel
+from tests.common.markers import world_size
+
+
+class MockMAPI:
+
+    def __init__(self):
+        self.run_metadata = {}
+
+    def update_run_metadata(self, run_name, new_metadata):
+        if run_name not in self.run_metadata:
+            self.run_metadata[run_name] = {}
+        for k, v in new_metadata.items():
+            self.run_metadata[run_name][k] = v
+        # Serialize the data to ensure it is json serializable
+        json.dumps(self.run_metadata[run_name])
+
+
+@pytest.mark.parametrize('callback_cls', get_cbs_and_marks(callbacks=True))
+@world_size(1, 2)
+def test_logged_data_is_json_serializable(monkeypatch, callback_cls: Type[Callback], world_size):
+    """Test that all logged data is json serializable, which is a requirement to use MAPI."""
+    pytest.importorskip('mcli')
+
+    mock_mapi = MockMAPI()
+    import mcli.sdk
+    monkeypatch.setattr(mcli.sdk, 'update_run_metadata', mock_mapi.update_run_metadata)
+    run_name = 'small_chungus'
+    monkeypatch.setenv('RUN_NAME', run_name)
+
+    callback_kwargs = get_cb_kwargs(callback_cls)
+    callback = callback_cls(**callback_kwargs)
+    trainer = Trainer(
+        model=SimpleModel(),
+        train_dataloader=DataLoader(RandomClassificationDataset()),
+        train_subset_num_batches=2,
+        max_duration='1ep',
+        callbacks=callback,
+        loggers=MosaicMLLogger(),
+    )
+    trainer.fit()
+
+    if dist.get_global_rank() == 0:
+        assert len(mock_mapi.run_metadata[run_name].keys()) > 0
+    else:
+        assert len(mock_mapi.run_metadata.keys()) == 0
+
+
+def test_metric_partial_filtering(monkeypatch):
+    pytest.importorskip('mcli')
+
+    mock_mapi = MockMAPI()
+    import mcli.sdk
+    monkeypatch.setattr(mcli.sdk, 'update_run_metadata', mock_mapi.update_run_metadata)
+    run_name = 'small_chungus'
+    monkeypatch.setenv('RUN_NAME', run_name)
+
+    trainer = Trainer(
+        model=SimpleModel(),
+        train_dataloader=DataLoader(RandomClassificationDataset()),
+        train_subset_num_batches=2,
+        max_duration='1ep',
+        loggers=MosaicMLLogger(ignore_keys=['loss', 'accuracy']),
+    )
+    trainer.fit()
+
+    assert 'mosaicml/num_nodes' in mock_mapi.run_metadata[run_name]
+    assert 'mosaicml/loss' not in mock_mapi.run_metadata[run_name]
+
+
+def test_metric_full_filtering(monkeypatch):
+    pytest.importorskip('mcli')
+
+    mock_mapi = MockMAPI()
+    import mcli.sdk
+    monkeypatch.setattr(mcli.sdk, 'update_run_metadata', mock_mapi.update_run_metadata)
+    run_name = 'small_chungus'
+    monkeypatch.setenv('RUN_NAME', run_name)
+
+    trainer = Trainer(
+        model=SimpleModel(),
+        train_dataloader=DataLoader(RandomClassificationDataset()),
+        train_subset_num_batches=2,
+        max_duration='1ep',
+        loggers=MosaicMLLogger(ignore_keys=['*']),
+    )
+    trainer.fit()
+
+    assert len(mock_mapi.run_metadata[run_name].keys()) == 0
+
+
+def test_format_data_to_json_serializable():
+    data = {
+        'key1': 'value1',
+        'key2': 42,
+        'key3': 3.14,
+        'key4': True,
+        'key5': torch.tensor([1, 2, 3]),
+        'key6': {
+            'inner_key': 'inner_value'
+        },
+        'key7': [1, 2, 3],
+    }
+    formatted_data = format_data_to_json_serializable(data)
+
+    expected_formatted_data = {
+        'key1': 'value1',
+        'key2': 42,
+        'key3': 3.14,
+        'key4': True,
+        'key5': 'Tensor of shape torch.Size([3])',
+        'key6': {
+            'inner_key': 'inner_value'
+        },
+        'key7': [1, 2, 3],
+    }
+
+    assert formatted_data == expected_formatted_data
