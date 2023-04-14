e6f4ba5a05cf4028b04ea71bcc3786c68d601bac
brandon <bcui8377@gmail.com>
2023-04-14 21:45:55 +0000

    Updating EMA test

--- composer/algorithms/ema/ema.py
+++ composer/algorithms/ema/ema.py
@@ -5,12 +5,14 @@
 
 from __future__ import annotations
 
+import contextlib
 import itertools
 import logging
 from typing import Any, Dict, Optional, Union
 
 import torch
 
+import composer.utils.misc as misc
 from composer.callbacks.checkpoint_saver import CheckpointSaver
 from composer.core import Algorithm, Event, State, Time, TimeUnit
 from composer.loggers import Logger
@@ -25,78 +27,73 @@ def compute_ema(model: torch.nn.Module,
                 smoothing: float = 0.99) -> None:
     r"""Updates the weights of ``ema_model`` to be closer to the weights of ``model``
     according to an exponential weighted average. Weights are updated according to
-
     .. math::
         W_{ema_model}^{(t+1)} = smoothing\times W_{ema_model}^{(t)}+(1-smoothing)\times W_{model}^{(t)}
-
     The update to ``ema_model`` happens in place.
-
     The half life of the weights for terms in the average is given by
-
     .. math::
         t_{1/2} = -\frac{\log(2)}{\log(smoothing)}
-
     Therefore, to set smoothing to obtain a target half life, set smoothing according to
-
     .. math::
         smoothing = \exp\left[-\frac{\log(2)}{t_{1/2}}\right]
-
     Args:
         model (torch.nn.Module): the model containing the latest weights to use to update the moving average weights.
         ema_model (torch.nn.Module, EMAParameters): the model containing the moving average weights to be updated.
         smoothing (float, optional): the coefficient representing the degree to which older observations are kept.
             Must be in the interval :math:`(0, 1)`. Default: ``0.99``.
-
     Example:
         .. testcode::
-
                 import composer.functional as cf
                 from torchvision import models
                 model = models.resnet50()
                 ema_model = models.resnet50()
                 cf.compute_ema(model, ema_model, smoothing=0.9)
     """
-    with torch.no_grad():
-        # If the ema model is a pytorch module, can just use the state_dict
-        if isinstance(ema_model, torch.nn.Module):
-            ema_params = ema_model.state_dict()
-            for name, param in itertools.chain(model.named_parameters(), model.named_buffers()):
-                if name in ema_params:
-                    ema_params[name].copy_(ema_params[name] * smoothing + param.data * (1. - smoothing))
-        # Otherwise, the ema model needs to define the named_parameters and named_buffers dictionaries
-        # These should contain the parameters and buffers to average.
-        elif isinstance(ema_model, EMAParameters):
-            ema_parameters = ema_model.named_parameters_dict
-            ema_buffers = ema_model.named_buffers_dict
-            for name, param in itertools.chain(model.named_parameters(), model.named_buffers()):
-                if name in ema_parameters:
-                    ema_parameters[name].copy_(ema_parameters[name] * smoothing + param.data * (1. - smoothing))
-                if name in ema_buffers:
-                    ema_buffers[name].copy_(ema_buffers[name] * smoothing + param.data * (1. - smoothing))
-        else:
-            raise ValueError('ema_model must be a torch.nn.Module or EMAParameters')
+    model_context_manager = get_model_context_manager(model)
+
+    with model_context_manager:
+        with torch.no_grad():
+            # If the ema model is a pytorch module, can just use the state_dict
+            if isinstance(ema_model, torch.nn.Module):
+                ema_params = ema_model.state_dict()
+                for name, param in itertools.chain(model.named_parameters(), model.named_buffers()):
+                    if name in ema_params:
+                        ema_params[name].copy_(ema_params[name] * smoothing + param.data * (1. - smoothing))
+            # Otherwise, the ema model needs to define the named_parameters and named_buffers dictionaries
+            # These should contain the parameters and buffers to average.
+            elif isinstance(ema_model, EMAParameters):
+                ema_parameters = ema_model.named_parameters_dict
+                ema_buffers = ema_model.named_buffers_dict
+                for name, param in itertools.chain(model.named_parameters(), model.named_buffers()):
+                    if name in ema_parameters:
+                        ema_parameters[name].copy_(ema_parameters[name] * smoothing + param.data * (1. - smoothing))
+                    if name in ema_buffers:
+                        ema_buffers[name].copy_(ema_buffers[name] * smoothing + param.data * (1. - smoothing))
+            else:
+                raise ValueError('ema_model must be a torch.nn.Module or EMAParameters')
+
+
+def get_model_context_manager(model: torch.nn.Module):
+    fsdp_enabled = misc.is_model_fsdp(model)
+    model_context_manager = contextlib.nullcontext()
+    if fsdp_enabled:
+        model_context_manager = model.module.summon_full_params(model.module)  # type: ignore
+    return model_context_manager
 
 
 class EMA(Algorithm):
     r"""Maintains a set of weights that follow the exponential moving average of the training model weights.
-
     Weights are updated according to
-
     .. math::
         W_{ema_model}^{(t+1)} = smoothing\times W_{ema_model}^{(t)}+(1-smoothing)\times W_{model}^{(t)}
-
     Where the smoothing is determined from ``half_life`` according to
-
     .. math::
         smoothing = \exp\left[-\frac{\log(2)}{t_{1/2}}\right]
-
     Model evaluation is done with the moving average weights, which can result in better generalization. Because of the
     ema weights, EMA can double the model's memory consumption. Note that this does not mean that the total memory
     required doubles, since stored activations and the optimizer state are not duplicated. EMA also uses a small
     amount of extra compute to update the moving average weights.
-
     See the :doc:`Method Card </method_cards/ema>` for more details.
-
     Args:
         half_life (str, optional): The time string specifying the half life for terms in the average. A longer half
             life means old information is remembered longer, a shorter half life means old information is discared
@@ -115,10 +112,8 @@ class EMA(Algorithm):
             using ``smoothing``. If not specified, ``update_interval`` will default to ``1`` in the units of
             ``half_life``, or ``"1ba"`` if ``smoothing`` is specified. Time must be an integer value in the units
             specified. Default: ``None``.
-
     Example:
         .. testcode::
-
             from composer.algorithms import EMA
             algorithm = EMA(half_life='1000ba', update_interval='1ba')
             trainer = Trainer(
@@ -342,10 +337,8 @@ class EMA(Algorithm):
 
     def get_ema_model(self, model: torch.nn.Module) -> torch.nn.Module:
         """Replaces the parameters of the supplied model with the ema parameters if they are not already active.
-
         Args:
             model (torch.nn.Module): The model to replace the parameters of.
-
         Returns:
             torch.nn.Module: The model with the ema parameters.
         """
@@ -358,10 +351,8 @@ class EMA(Algorithm):
 
     def get_training_model(self, model: torch.nn.Module) -> torch.nn.Module:
         """Replaces the parameters of the supplied model with the training parameters if they are not already active.
-
         Args:
             model (torch.nn.Module): The model to replace the parameters of.
-
         Returns:
             torch.nn.Module: The model with the training parameters.
         """
@@ -378,11 +369,13 @@ class EMAParameters:
 
     def __init__(self, model: Union[None, torch.nn.Module]):
         if model is not None:
-            # Copy the trainable parameters and buffers.
-            self.named_parameters_dict = {
-                name: param.data.clone() for name, param in model.named_parameters() if param.requires_grad
-            }
-            self.named_buffers_dict = {name: buffer.data.clone() for name, buffer in model.named_buffers()}
+            model_context_manager = get_model_context_manager(model)
+            with model_context_manager:
+                # Copy the trainable parameters and buffers.
+                self.named_parameters_dict = {
+                    name: param.data.clone() for name, param in model.named_parameters() if param.requires_grad
+                }
+                self.named_buffers_dict = {name: buffer.data.clone() for name, buffer in model.named_buffers()}
         else:
             # Empty storage
             self.named_parameters_dict = {}
@@ -396,41 +389,50 @@ class EMAParameters:
 
     def swap_params(self, model: torch.nn.Module):
         """Swaps the parameters and buffers of a model with the ema parameters."""
+        model_context_manager = get_model_context_manager(model)
+
         with torch.no_grad():
             ema_params = self.named_parameters_dict
             ema_buffers = self.named_buffers_dict
 
-            for name, param in model.named_parameters():
-                if name in ema_params:
-                    # Use copy instead of raw data access (eg .data) doesn't work with FSDP
-                    dummy_param = param.clone()
-                    param.copy_(ema_params[name])
-                    ema_params[name].copy_(dummy_param)
+            with model_context_manager:
+                for name, param in model.named_parameters():
+                    if name in ema_params:
+                        # Use copy instead of raw data access (eg .data) doesn't work with FSDP
+                        dummy_param = param.clone()
+                        param.copy_(ema_params[name])
+                        ema_params[name].copy_(dummy_param)
 
-            for name, buffer in model.named_buffers():
-                if name in ema_buffers:
-                    # Use copy instead of raw data access (eg .data) doesn't work with FSDP
-                    dummy_buffer = buffer.clone()
-                    buffer.copy_(ema_buffers[name])
-                    ema_buffers[name].copy_(dummy_buffer)
+                for name, buffer in model.named_buffers():
+                    if name in ema_buffers:
+                        # Use copy instead of raw data access (eg .data) doesn't work with FSDP
+                        dummy_buffer = buffer.clone()
+                        buffer.copy_(ema_buffers[name])
+                        ema_buffers[name].copy_(dummy_buffer)
 
     def transfer_ema_params(self, model: torch.nn.Module):
         """Transfers the parameters and buffers from the ema model to the supplied model."""
-        with torch.no_grad():
-            for name, param in model.named_parameters():
-                if name in self.named_parameters_dict:
-                    param.copy_(self.named_parameters_dict[name])
+        model_context_manager = get_model_context_manager(model)
 
-            for name, buffer in model.named_buffers():
-                if name in self.named_buffers_dict:
-                    buffer.copy_(self.named_buffers_dict[name])
+        with model_context_manager:
+            with torch.no_grad():
+                for name, param in model.named_parameters():
+                    if name in self.named_parameters_dict:
+                        param.copy_(self.named_parameters_dict[name])
+
+                for name, buffer in model.named_buffers():
+                    if name in self.named_buffers_dict:
+                        buffer.copy_(self.named_buffers_dict[name])
 
     def move_params_to_device(self, destination_model: torch.nn.Module):
         """Moves the ema parameters and buffers to the device of a destination model."""
-        for name, param in destination_model.named_parameters():
-            if name in self.named_parameters_dict:
-                self.named_parameters_dict[name] = self.named_parameters_dict[name].to(param.device)
+        model_context_manager = get_model_context_manager(destination_model)
 
-        for name, buffer in destination_model.named_buffers():
-            if name in self.named_buffers_dict:
-                self.named_buffers_dict[name] = self.named_buffers_dict[name].to(buffer.device)
+        with model_context_manager:
+            for name, param in destination_model.named_parameters():
+                if name in self.named_parameters_dict:
+                    self.named_parameters_dict[name] = self.named_parameters_dict[name].to(param.device)
+
+            for name, buffer in destination_model.named_buffers():
+                if name in self.named_buffers_dict:
+                    self.named_buffers_dict[name] = self.named_buffers_dict[name].to(buffer.device)
