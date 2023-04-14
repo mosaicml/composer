beb89908c3d630716fcf87c410f1a0b9cf36f511
Wai Wu <92327646+waiwuc@users.noreply.github.com>
2023-04-14 16:59:21 -0700

    add tests, remove config

--- composer/loggers/slack_logger.py
+++ composer/loggers/slack_logger.py
@@ -1,26 +1,35 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""Log metrics to slack, using Slack postMessage api - https://api.slack.com/methods/chat.postMessage.
+"""Log metrics to slack, using Slack postMessage api."""
+import os
+import time
+from typing import Any, Callable, Dict, List, Optional, Set
 
-First export 2 environment variable to use this logger.
-1. SLACK_LOGGING_API_KEY: To get app credentials, follow tutorial here - https://api.slack.com/tutorials/tracks/posting-messages-with-curl?app_id_from_manifest=A053W1QCEF2
-2. SLACK_LOGGING_CHANNEL_ID: Channel id to send the message (Open slack channel in web browser to look this up)
+from composer.loggers.logger_destination import LoggerDestination
+from composer.utils import MissingConditionalImportError
 
-Next write script to output metrics / hparams / traces to slack channel. See example below:
 
-trainer = Trainer(
-    model=mnist_model(num_classes=10),
-    train_dataloader=train_dataloader,
-    max_duration='2ep',
-    algorithms=[
-        LabelSmoothing(smoothing=0.1),
-        CutMix(alpha=1.0),
-        ChannelsLast(),
-    ],
-    loggers=[
-        SlackLogger(
-            log_metrics_config=SlackLogConfig(
+class SlackLogger(LoggerDestination):
+    """Log metrics to slack, using Slack postMessage api - https://api.slack.com/methods/chat.postMessage.
+
+    First export 2 environment variable to use this logger.
+    1. SLACK_LOGGING_API_KEY: To get app credentials, follow tutorial here - https://api.slack.com/tutorials/tracks/posting-messages-with-curl?app_id_from_manifest=A053W1QCEF2
+    2. SLACK_LOGGING_CHANNEL_ID: Channel id to send the message (Open slack channel in web browser to look this up)
+
+    Next write script to output metrics / hparams / traces to slack channel. See example below:
+
+    trainer = Trainer(
+        model=mnist_model(num_classes=10),
+        train_dataloader=train_dataloader,
+        max_duration='2ep',
+        algorithms=[
+            LabelSmoothing(smoothing=0.1),
+            CutMix(alpha=1.0),
+            ChannelsLast(),
+        ],
+        loggers=[
+            SlackLogger(
                 formatter_func=(lambda data, **kwargs: [{
                     'type': 'section',
                     'text': {
@@ -31,29 +40,17 @@ trainer = Trainer(
                 include_keys={'loss/train/total'},
                 interval_in_seconds=1
             ),
-            log_hparams_config=SlackLogConfig(),
-        )
-    ],
-)
+        ],
+    )
 
-trainer.fit()
-"""
+    trainer.fit()
 
-import os
-import time
-from typing import Any, Callable, Dict, List, Optional, Set, Tuple
-
-from composer.loggers.logger_destination import LoggerDestination
-from composer.utils import MissingConditionalImportError
-
-
-class SlackLogConfig:
-    """Log config for slack logger, including formatter function, keys to include, and interval in seconds.
-
-    formatter_func ((...) -> Any | None): A formatter function that returns list of blocks to be sent to slack
-    include_keys (Set[str]): A set of metric/logs/traces keys to include in the message. If None, no keys are included.
-    interval_in_seconds (int): Interval in seconds to send logs to slack. If None, message is sent after every log call.
-    Note that no more than 50 items are allowed to send in a single message.
+    Args:
+        formatter_func ((...) -> Any | None): A formatter function that returns list of blocks to be sent to slack
+        include_keys (Set[str]): A set of metric/logs/traces keys to include in the message. If None, all keys are included.
+        interval_in_seconds (int): Interval in seconds to send logs to slack. If None, message is sent after every log call.
+        max_logs_per_message (int): Maximum number of logs to send in a single message. Note that no more than 50 items are allowed to send in a single message.
+        If more than 50 items are stored in buffer, the message flushed without waiting the full time interval.
     """
 
     def __init__(
@@ -61,82 +58,56 @@ class SlackLogConfig:
         formatter_func: Optional[Callable[..., List[Dict[str, Any]]]] = None,
         include_keys: Optional[Set[str]] = None,
         interval_in_seconds: Optional[int] = None,
+        max_logs_per_message: int = 50,
     ) -> None:
+        try:
+            import slack_sdk
+            self.client = slack_sdk.WebClient()
+            del slack_sdk
+        except ImportError as e:
+            raise MissingConditionalImportError('slack_logger', 'slack_sdk', None) from e
+
+        self.slack_logging_api_key = os.environ.get('SLACK_LOGGING_API_KEY', None)
+        self.channel_id = os.environ.get('SLACK_LOGGING_CHANNEL_ID', None)
+
+        if self.slack_logging_api_key is None:
+            raise RuntimeError('SLACK_LOGGING_API_KEY must be set as environment variable')
+        if self.channel_id is None:
+            raise RuntimeError('SLACK_LOGGING_CHANNEL_ID must be set as environment variable')
+
         self.formatter_func = formatter_func
         self.include_keys = include_keys
         self.interval_in_seconds = interval_in_seconds
 
-
-class SlackLogger(LoggerDestination):
-    """Logger to log metrics to slack.
-
-    This logger uses Slack's postMessage api - https://api.slack.com/methods/chat.postMessage.
-
-    Args:
-        log_metrics_config (SlackLogConfig): Configuration for logging metrics to slack.
-        log_traces_config (SlackLogConfig): Configuration for logging traces to slack.
-        log_hparams_config (SlackLogConfig): Configuration for logging hyperparameters to slack.
-    """
-
-    def __init__(
-        self,
-        log_metrics_config: Optional[SlackLogConfig] = None,
-        log_traces_config: Optional[SlackLogConfig] = None,
-        log_hparams_config: Optional[SlackLogConfig] = None,
-    ) -> None:
-        try:
-            from slack_sdk import WebClient
-            del slack_sdk
-        except ImportError as e:
-            raise MissingConditionalImportError('health_checker', 'slack_sdk', None) from e
-
-        self.client = WebClient()
-        self.slack_logging_api_key = os.environ.get('SLACK_LOGGING_API_KEY', None)
-        self.channel_id = os.environ.get('SLACK_LOGGING_CHANNEL_ID', None)
-
-        if self.slack_logging_api_key is None or self.channel_id is None:
-            raise RuntimeError(
-                'SLACK_LOGGING_API_KEY and SLACK_LOGGING_CHANNEL_ID must be set as environment variables')
-
-        self.log_metrics_config = log_metrics_config
-        self.log_traces_config = log_traces_config
-        self.log_hparams_config = log_hparams_config
-
-        # Maintain logger state
-        now = time.time()
-        self.logs_since_last_log, self.last_log_time = [], now
-        self.traces_since_last_log, self.last_trace_time = [], now
-        self.hparams_since_last_log, self.last_hparams_time = [], now
+        self.logs, self.last_log_time = [], time.time()
+        self.max_logs_per_message = min(max_logs_per_message, 50)
 
     # Rich message layouts can be created using Slack Blocks Kit.
     # See documentation here: https://api.slack.com/messaging/composing/layouts
     def _log_to_slack(
-        self,
-        data: Dict[str, Any],
-        last_time_flushed: float,
-        blocks_to_log: List[Dict[str, Any]],
-        config: SlackLogConfig,
-        **kwargs,  # can be used to pass additional arguments to the formatter function (eg for headers)
-    ) -> Tuple[float, List[Dict[str, Any]]]:
-        filtered_data = {k: v for k, v in data.items() if k in config.include_keys} if config.include_keys else data
-        blocks = config.formatter_func(
+            self,
+            data: Dict[str, Any],
+            **kwargs,  # can be used to pass additional arguments to the formatter function (eg for headers)
+    ):
+        filtered_data = {k: v for k, v in data.items() if k in self.include_keys
+                        } if self.include_keys is not None else data
+        blocks = self.formatter_func(
             filtered_data, **
-            kwargs) if config.formatter_func else self._default_log_bold_key_normal_value_pair_with_header(
+            kwargs) if self.formatter_func is not None else self._default_log_bold_key_normal_value_pair_with_header(
                 filtered_data, **kwargs)
 
-        updated_blocks_to_log = blocks + blocks_to_log
+        self.logs += blocks
+        now = time.time()
 
-        if len(updated_blocks_to_log) > 50:
-            raise RuntimeError('Cannot send more than 50 items in a single slack message')
-
-        if len(updated_blocks_to_log) > 0 and self.channel_id and (
-                config.interval_in_seconds is None or time.time() - last_time_flushed >= config.interval_in_seconds):
+        if len(self.logs) > 0 and self.channel_id is not None and (
+                len(self.logs) >= self.max_logs_per_message or self.interval_in_seconds is None or
+                now - self.last_log_time >= self.interval_in_seconds):
             self.client.chat_postMessage(token=f'{self.slack_logging_api_key}',
                                          channel=self.channel_id,
-                                         blocks=blocks_to_log)
-            return time.time(), []
-        else:
-            return last_time_flushed, updated_blocks_to_log
+                                         blocks=self.logs[:self.max_logs_per_message],
+                                         text=str(self.logs[:self.max_logs_per_message]))
+            self.last_log_time = now
+            self.logs = self.logs[self.max_logs_per_message:]
 
     def _default_log_bold_key_normal_value_pair_with_header(self, data: Dict[str, Any],
                                                             **kwargs) -> List[Dict[str, Any]]:
@@ -161,30 +132,10 @@ class SlackLogger(LoggerDestination):
         return blocks
 
     def log_metrics(self, metrics: Dict[str, Any], step: Optional[int] = None) -> None:
-        if self.log_metrics_config is None:
-            return
-        self.last_log_time, self.logs_since_last_log = self._log_to_slack(data=metrics,
-                                                                          last_time_flushed=self.last_log_time,
-                                                                          blocks_to_log=self.logs_since_last_log,
-                                                                          config=self.log_metrics_config,
-                                                                          header=step)
+        self._log_to_slack(data=metrics, header=step)
 
     def log_hyperparameters(self, hyperparameters: Dict[str, Any]):
-        if self.log_hparams_config is None:
-            return
-        self.last_hparams_time, self.hparams_since_last_log = self._log_to_slack(
-            data=hyperparameters,
-            last_time_flushed=self.last_hparams_time,
-            blocks_to_log=self.hparams_since_last_log,
-            config=self.log_hparams_config,
-        )
+        self._log_to_slack(data=hyperparameters)
 
     def log_traces(self, traces: Dict[str, Any]):
-        if self.log_traces_config is None:
-            return
-        self.last_trace_time, self.traces_since_last_log = self._log_to_slack(
-            data=traces,
-            last_time_flushed=self.last_trace_time,
-            blocks_to_log=self.traces_since_last_log,
-            config=self.log_traces_config,
-        )
+        self._log_to_slack(data=traces)
---
+++ tests/loggers/test_slack_logger.py
@@ -0,0 +1,169 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import time
+from typing import Optional, Set
+from unittest.mock import patch
+
+import pytest
+from torch.utils.data import DataLoader
+
+from composer.loggers.slack_logger import SlackLogger
+from composer.trainer import Trainer
+from tests.common import RandomClassificationDataset, SimpleModel
+
+
+@pytest.mark.parametrize('include_keys', [None, {}, {'loss/train/total'}])
+def test_slack_logger_metrics(include_keys: Optional[Set[str]]):
+    pytest.importorskip('slack_sdk')
+    os.environ['SLACK_LOGGING_API_KEY'] = str('1234')
+    os.environ['SLACK_LOGGING_CHANNEL_ID'] = 'C1234'
+
+    slack_logger = SlackLogger(
+        formatter_func=(lambda data, **kwargs: [{
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': f'*{k}:* {v}'
+            }
+        } for k, v in data.items()]),
+        include_keys=include_keys,
+        interval_in_seconds=None,
+    )
+
+    # Patch for hparams
+    with patch('slack_sdk.WebClient.chat_postMessage'):
+        trainer = Trainer(
+            model=SimpleModel(),
+            train_dataloader=DataLoader(RandomClassificationDataset()),
+            train_subset_num_batches=2,
+            max_duration='1ep',
+            loggers=[slack_logger],
+        )
+
+    with patch('slack_sdk.WebClient.chat_postMessage') as mock_post_logs:
+        trainer.fit()
+
+    if include_keys == {}:
+        # include no keys if empty set
+        assert mock_post_logs.call_count == 0
+        return
+
+    if include_keys is None:
+        # include all keys by default
+        assert mock_post_logs.call_count == 10
+    elif include_keys == {'loss/train/total'}:
+        assert mock_post_logs.call_count == 2
+        metrics_key = 'loss/train/total'
+        assert (metrics_key in mock_post_logs.call_args_list[0][1]['blocks'][0]['text']['text'])
+
+    mock_post_logs.call_args_list[0][1]['token'] == '1234'
+    mock_post_logs.call_args_list[0][1]['channel'] == 'C1234'
+
+    del trainer
+
+
+def test_slack_logger_hparams():
+    pytest.importorskip('slack_sdk')
+    os.environ['SLACK_LOGGING_API_KEY'] = str('1234')
+    os.environ['SLACK_LOGGING_CHANNEL_ID'] = 'C1234'
+
+    slack_logger = SlackLogger(
+        formatter_func=(lambda data, **kwargs: [{
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': f'*{k}:* {v}'
+            }
+        } for k, v in data.items()]),
+        interval_in_seconds=None,
+    )
+
+    # Patch for hparams
+    with patch('slack_sdk.WebClient.chat_postMessage') as mock_post_hparams:
+        trainer = Trainer(
+            model=SimpleModel(),
+            train_dataloader=DataLoader(RandomClassificationDataset()),
+            train_subset_num_batches=2,
+            max_duration='1ep',
+            loggers=[slack_logger],
+        )
+    assert (mock_post_hparams.call_count > 0)
+
+
+@pytest.mark.parametrize('time_interval', [None, 1])
+def test_slack_logger_time_interval(time_interval: Optional[int]):
+    pytest.importorskip('slack_sdk')
+    os.environ['SLACK_LOGGING_API_KEY'] = str('1234')
+    os.environ['SLACK_LOGGING_CHANNEL_ID'] = 'C1234'
+
+    slack_logger = SlackLogger(
+        formatter_func=(lambda data, **kwargs: [{
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': f'*{k}:* {v}'
+            }
+        } for k, v in data.items()]),
+        interval_in_seconds=time_interval,
+    )
+
+    # Patch for hparams
+    with patch('slack_sdk.WebClient.chat_postMessage') as mock_slack:
+        trainer = Trainer(
+            model=SimpleModel(),
+            train_dataloader=DataLoader(RandomClassificationDataset()),
+            train_subset_num_batches=2,
+            max_duration='5ep',
+            loggers=[slack_logger],
+        )
+        if time_interval is None:
+            assert (mock_slack.call_count > 0)
+        else:
+            assert (mock_slack.call_count == 0)
+        if time_interval is not None:
+            time.sleep(time_interval)
+
+        # Log metrics immediately if no time_interval.
+        # Otherwise log hparams from previous step.
+        trainer.fit()
+        if time_interval is None:
+            assert (mock_slack.call_count > 2)
+        else:
+            assert (mock_slack.call_count == 2)
+
+
+@pytest.mark.parametrize('max_logs', [50, 1])
+def test_slack_logger_max_num_logs(max_logs: Optional[int]):
+    pytest.importorskip('slack_sdk')
+    os.environ['SLACK_LOGGING_API_KEY'] = str('1234')
+    os.environ['SLACK_LOGGING_CHANNEL_ID'] = 'C1234'
+
+    slack_logger = SlackLogger(
+        formatter_func=(lambda data, **kwargs: [{
+            'type': 'section',
+            'text': {
+                'type': 'mrkdwn',
+                'text': f'*{k}:* {v}'
+            }
+        } for k, v in data.items()]),
+        interval_in_seconds=1,
+        max_logs_per_message=max_logs,
+    )
+
+    # Patch for hparams
+    with patch('slack_sdk.WebClient.chat_postMessage') as mock_slack:
+        trainer = Trainer(
+            model=SimpleModel(),
+            train_dataloader=DataLoader(RandomClassificationDataset()),
+            train_subset_num_batches=2,
+            max_duration='5ep',
+            loggers=[slack_logger],
+        )
+        if max_logs == 1:
+            # Since time interval is 1s, will not log
+            assert (mock_slack.call_count > 0)
+        else:
+            # But if max num of logs reached, disregard time interval and log
+            assert (mock_slack.call_count == 0)
