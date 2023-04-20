02b982bafb2d577a439a160d662d6ef3bd40228b
Daniel King <daniel@mosaicml.com>
2023-04-20 11:16:50 -0700

    fix typo

--- composer/callbacks/checkpoint_saver.py
+++ composer/callbacks/checkpoint_saver.py
@@ -72,7 +72,7 @@ def checkpoint_periodically(interval: Union[str, int, Time]) -> Callable[[State,
             )
 
         threshold_passed = math.floor(previous_count / interval.value) != math.floor(count / interval.value)
-        return event == save_event and threshold_passed:
+        return event == save_event and threshold_passed
 
     return save_interval
 
