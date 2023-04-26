fix gradient clipping tests

tests/algorithms/test_gradient_clipping.py
@@ -194,6 +194,7 @@ def test_gradient_clipping_algorithm_with_deepspeed_enabled(
 
 
 if not using_torch_2_0():
+
     def _auto_wrap_policy(module: torch.nn.Module, recurse: bool, unwrapped_params: int) -> bool:
         if recurse:
             return True
@@ -201,9 +202,15 @@ if not using_torch_2_0():
             return bool(module._fsdp_wrap)
         return False
 else:
+
     def _auto_wrap_policy(module: torch.nn.Module, recurse: bool, nonwrapped_numel: int) -> bool:
         if recurse:
             return True
+
+        # With Torch 2.0, there is a bug that emits a nasty warning if you wrap a module with no parameters
+        if len(list(module.parameters())) == 0:
+            return False
+
         if hasattr(module, '_fsdp_wrap'):
             return bool(module._fsdp_wrap)
         return False
@@ -229,9 +236,11 @@ def test_gradient_clipping_algorithm_with_fsdp_enabled_does_not_error(
 ):
     from torch.distributed.fsdp import FullyShardedDataParallel
 
+    model = model_with_grads()
+
     clipping_threshold = 0.1191
     state = dummy_state
-    state.model = FullyShardedDataParallel(model_with_grads(),
+    state.model = FullyShardedDataParallel(model,
                                            auto_wrap_policy=_auto_wrap_policy,
                                            device_id=torch.cuda.current_device(),
                                            use_orig_params=True)