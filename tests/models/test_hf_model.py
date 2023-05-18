72399e5a7d9ce09e6b8b7083862c8ddcf1d37dd1
Daniel King <daniel@mosaicml.com>
2023-05-18 16:31:42 -0700

    add equivalence test

--- tests/models/test_hf_model.py
+++ tests/models/test_hf_model.py
@@ -17,6 +17,7 @@ from torch.utils.data import DataLoader
 from torchmetrics import Metric
 from torchmetrics.classification import MulticlassAccuracy
 
+from composer.loggers import InMemoryLogger
 from composer.metrics import InContextLearningLMAccuracy, LanguageCrossEntropy, MaskedAccuracy
 from composer.models import HuggingFaceModel
 from composer.trainer import Trainer
@@ -275,7 +276,8 @@ def get_lm_trainer(hf_model,
                    load_path: Optional[str] = None,
                    is_conditional_generation: bool = False,
                    do_eval: bool = False,
-                   fsdp_config: Optional[Dict[str, Any]] = None):
+                   fsdp_config: Optional[Dict[str, Any]] = None,
+                   mlm: bool = True):
     transformers = pytest.importorskip('transformers')
 
     metrics: List[Metric] = [LanguageCrossEntropy(ignore_index=-100)]
@@ -297,7 +299,7 @@ def get_lm_trainer(hf_model,
                                         conditional_generation=is_conditional_generation)
 
     if not is_conditional_generation:
-        collator = transformers.DataCollatorForLanguageModeling(tokenizer=hf_tokenizer, mlm_probability=0.15)
+        collator = transformers.DataCollatorForLanguageModeling(tokenizer=hf_tokenizer, mlm=mlm, mlm_probability=0.15)
     else:
         # Note: this could be transformers.DataCollatorForSeq2Seq(tokenizer=hf_tokenizer, model=hf_model),
         # but we want to test the scenario where the input batch does not have decoder_input_ids,
@@ -316,6 +318,7 @@ def get_lm_trainer(hf_model,
                                      collate_fn=collator,
                                      sampler=dist.get_sampler(train_dataset))
 
+    in_memory_logger = InMemoryLogger() 
     trainer = Trainer(model=model,
                       train_dataloader=train_dataloader,
                       eval_dataloader=eval_dataloader,
@@ -324,9 +327,14 @@ def get_lm_trainer(hf_model,
                       save_interval='1ep',
                       save_filename='hf-checkpoint.pt',
                       load_path=load_path,
-                      fsdp_config=fsdp_config)
+                      fsdp_config=fsdp_config,
+                      loggers=in_memory_logger)
     return trainer
 
+def test_loss_vs_ce_metric(tiny_gpt2_tokenizer, tiny_gpt2_model):
+    trainer = get_lm_trainer(tiny_gpt2_model, tiny_gpt2_tokenizer, is_conditional_generation=False, save_folder=None, mlm=False)
+    trainer.fit()
+    assert True
 
 @pytest.mark.parametrize('pass_in_tokenizer', [True, False])
 def test_hf_no_tokenizer_warning(caplog, pass_in_tokenizer: bool, tiny_bert_model, tiny_bert_tokenizer):
