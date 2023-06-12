360a3474838b98396e2976b7f92bf7f132933851
Mihir Patel <mihir.v.patel7@gmail.com>
2023-06-12 11:49:17 -0700

    add more logs

--- composer/core/state.py
+++ composer/core/state.py
@@ -1056,12 +1056,14 @@ class State(Serializable):
                 #     with fsdp_state_dict_type_context(module=self.model, state_dict_type=self.fsdp_state_dict_type):
                 #         optim_state_dict = FSDP.optim_state_dict_to_load(  #  type: ignore
                 #             optim_state_dict=optim_state_dict, model=self.model, optim=optimizer)
+                # optimizer.load_state_dict(optim_state_dict)
                 log.info(f'Loading optimizer state dict with FSDP state_dict_type={self.fsdp_state_dict_type}')
                 if optim_state_dict is not None:
                     log.info(f'Optim state dict keys: {optim_state_dict.keys()}')
                 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
                 optim_state_dict = FSDP.scatter_full_optim_state_dict(full_optim_state_dict=optim_state_dict,
                                                                       model=self.model)
+                log.info('Scattered optimizer state dict')
                 optimizer.load_state_dict(optim_state_dict)
                 log.info('Loaded optimizer state dict')
             else:
