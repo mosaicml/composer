3013be237a6f2ff983f6417aef173feca0c90190
Mihir Patel <mihir.v.patel7@gmail.com>
2023-06-16 18:48:35 -0700

    one-line

--- composer/trainer/dist_strategy.py
+++ composer/trainer/dist_strategy.py
@@ -289,9 +289,7 @@ def prepare_fsdp_module(
                 optimizer_specific_group_info = {k: v for k, v in group.items() if k != 'params'}
                 group_num_to_param_group_info[group_num] = optimizer_specific_group_info
         else:
-            group = optim.param_groups[0]
-            optimizer_specific_info = {k: v for k, v in group.items() if k != 'params'}
-            log.info(f'optimizer_specific_info: {optimizer_specific_info}')
+            optimizer_specific_info = {k: v for k, v in optim.param_groups[0].items() if k != 'params'}
 
         optim.param_groups.clear()
         optim.state.clear()
@@ -553,7 +551,7 @@ def prepare_fsdp_module(
             for param_group in param_groups:
                 optim.add_param_group(param_group)
         else:
-            # assert optimizer_specific_info is not None
-            # optimizer_specific_info.update({'params': list(model.parameters())})
-            # optim.add_param_group(optimizer_specific_info)
-            optim.add_param_group({'params': list(model.parameters())})
+            assert optimizer_specific_info is not None
+            optimizer_specific_info.update({'params': list(model.parameters())})
+            optim.add_param_group(optimizer_specific_info)
+            # optim.add_param_group({'params': list(model.parameters())})
