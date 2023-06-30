d33a10f1a241f7b4e90d7d4fd1202915ffb12ca0
julian-q <julianhquevedo@gmail.com>
2023-06-29 17:17:19 -0700

    fix precision_config type in docs

--- composer/core/precision.py
+++ composer/core/precision.py
@@ -44,7 +44,7 @@ def get_precision_context(precision: Union[str, Precision],
 
     Args:
         precision (str | Precision): Precision for the context
-        precision_config (Dict[str, Any]): Config for FP8 scaling strategy. See parameters for
+        precision_config (Optional[Dict[str, Any]]): Config for FP8 scaling strategy. See parameters for
             `DelayedScaling <https://docs.nvidia.com/deeplearning/transformer-engine/user-guide/api/common.html?highlight=delayedscaling#transformer_engine.common.recipe.DelayedScaling>`_.
     """
     precision = Precision(precision)
--- composer/core/state.py
+++ composer/core/state.py
@@ -274,7 +274,7 @@ class State(Serializable):
         max_duration (str | Time, optional): The maximum duration to train for. (default: ``None``)
         precision (str | Precision): The numerical precision to use for training. See :class:`~.Precision` for
             the supported precisions.
-        precision_config (Dict[str, Any]): The config for FP8 scaling strategy. See parameters for
+        precision_config (Optional[Dict[str, Any]]): The config for FP8 scaling strategy. See parameters for
             `DelayedScaling <https://docs.nvidia.com/deeplearning/transformer-engine/user-guide/api/common.html?highlight=delayedscaling#transformer_engine.common.recipe.DelayedScaling>`_.
         optimizers (torch.optim.Optimizer | Sequence[torch.optim.Optimizer], optional): The optimizer being used to
             train the model. Multiple optimizers are not currently supported.
