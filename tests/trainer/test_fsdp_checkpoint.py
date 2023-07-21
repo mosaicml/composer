d7fcc4efc2c8a52a4a3c2f010db79a84cc62f4f7
eracah <ejracah@gmail.com>
2023-07-21 00:50:48 +0000

    add shard_grad_op supoort

--- tests/trainer/test_fsdp_checkpoint.py
+++ tests/trainer/test_fsdp_checkpoint.py
@@ -502,7 +502,7 @@ def test_fsdp_partitioned_state_dict_load(world_size, tmp_path: pathlib.Path, st
 @pytest.mark.parametrize('precision', ['amp_bf16', 'amp_fp16'])
 @pytest.mark.parametrize('autoresume', [False, True])  # True commented out for now
 @pytest.mark.parametrize('num_shards', [2, 4, 7])
-@pytest.mark.parametrize('sharding_strategy', ['FULL_SHARD'])
+@pytest.mark.parametrize('sharding_strategy', ['FULL_SHARD', 'SHARD_GRAD_OP'])
 @pytest.mark.skipif(version.parse(torch.__version__) < version.parse('2.0.1'), reason='requires PyTorch 2.01 or higher')
 @pytest.mark.filterwarnings(r'ignore:TypedStorage is deprecated.:UserWarning')
 @pytest.mark.filterwarnings(r'ignore:MosaicMLLogger is not in the state_dict.:UserWarning')
