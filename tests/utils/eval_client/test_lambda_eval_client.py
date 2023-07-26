ac0f079c4e390a7fcd855620b44b31f28c220c8d
Rishab Parthasarathy <rishab@mosaicml.com>
2023-07-26 13:48:20 -0700

    separate tests

--- tests/utils/eval_client/test_lambda_eval_client.py
+++ tests/utils/eval_client/test_lambda_eval_client.py
@@ -6,31 +6,17 @@ from composer.utils import LambdaEvalClient
 
 
 @pytest.mark.remote
-def test_lambda_invoke():
+@pytest.mark.parametrize('code_results', [('def add_1(x):\n    return x + 1', True),
+                                          ('def add_1(x):\n    return y + 1', False),
+                                          ('def add_1(x):\n    while True:\n        x += 1', False),
+                                          ('def add_1(x): return x + 2', False)])
+def test_lambda_invoke(code_results):
     """Test invocation function for LambdaEvalClient with code that succeeds, fails compilation, times out, and is incorrect.
     """
     eval_client = LambdaEvalClient()
     assert eval_client.invoke({
-        'code': 'def add_1(x):\n    return x + 1',
+        'code': code_results[0],
         'input': '(1,)',
         'output': '2',
         'entry_point': 'add_1'
-    })
-    assert not eval_client.invoke({
-        'code': 'def add_1(x):\n    return y + 1',
-        'input': '(1,)',
-        'output': '2',
-        'entry_point': 'add_1'
-    })
-    assert not eval_client.invoke({
-        'code': 'def add_1(x):\n    while True:\n        x += 1',
-        'input': '(1,)',
-        'output': '2',
-        'entry_point': 'add_1',
-    })
-    assert not eval_client.invoke({
-        'code': 'def add_1(x): return x + 2',
-        'input': '(1,)',
-        'output': '2',
-        'entry_point': 'add_1'
-    })
+    }) == code_results[1]
