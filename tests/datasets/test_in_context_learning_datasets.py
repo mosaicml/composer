1e88af5a761c8930889b5fead9667f8e3fec3b08
Rishab Parthasarathy <56666587+rishab-partha@users.noreply.github.com>
2023-07-26 15:46:28 -0700

    Update tests/datasets/test_in_context_learning_datasets.py
    Co-authored-by: Mihir Patel <mihir.v.patel7@gmail.com>

--- tests/datasets/test_in_context_learning_datasets.py
+++ tests/datasets/test_in_context_learning_datasets.py
@@ -1166,9 +1166,9 @@ def test_qa_task_evaluation(device, world_size, num_fewshot, dataset_uri, tiny_g
 @world_size(1, 2)
 @pytest.mark.parametrize('num_fewshot', [0])
 @pytest.mark.parametrize('generations_per_sample', range(1, 3))
-def test_code_eval_microbatching(device, world_size, num_fewshot, dataset_uri, tmp_path, generations_per_sample):
+def test_code_eval_microbatching(monkeypatch, device, world_size, num_fewshot, dataset_uri, tmp_path, generations_per_sample):
     pytest.importorskip('datasets')
-    os.environ['CODE_EVAL_DEVICE'] = 'LOCAL'
+    monkeypatch.setenv('CODE_EVAL_DEVICE', 'LOCAL')
     in_memory_logger = InMemoryLogger()  # track the logged metrics in the in_memory_logger
     local_data = os.path.join(os.path.dirname(__file__), 'local_data')
     dataset_uri = f'{local_data}/{dataset_uri}'
