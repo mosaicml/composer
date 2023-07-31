d8a18d54f0e67a80bf26c8b72bb9b7699e0f539c
Rishab Parthasarathy <rishab@mosaicml.com>
2023-07-31 15:57:21 -0700

    fix local world size'

--- .github/mcli/mcli_pytest.py
+++ .github/mcli/mcli_pytest.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 
     export COMMON_ARGS="-v --durations=20 -m '{args.pytest_markers}' {s3_bucket_flag}"
 
-    make test PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS --codeblocks"
+    make test PYTEST='{args.pytest_command}' LOCAL_WORLD_SIZE=1 EXTRA_ARGS="$COMMON_ARGS --codeblocks"
 
     make test-dist PYTEST='{args.pytest_command}' EXTRA_ARGS="$COMMON_ARGS" WORLD_SIZE=2
 
