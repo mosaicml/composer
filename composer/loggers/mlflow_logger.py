60db178c0754aa34724f2f89a1505817c42f866b
Irene Dea <irene.dea@databricks.com>
2023-08-14 11:55:01 -0700

    Add log_table to LoggerDestination, CometMLLogger, MLFlowLogger, and WandBLogger

--- composer/loggers/mlflow_logger.py
+++ composer/loggers/mlflow_logger.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 import os
 import pathlib
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from composer.core.state import State
 from composer.loggers.logger import Logger
@@ -86,6 +86,13 @@ class MLFlowLogger(LoggerDestination):
                 mlflow.start_run(run_id=env_run_id)
             else:
                 mlflow.start_run(run_name=self.run_name)
+
+    def log_table(self, columns: List[str], rows: List[List[Any]], name: str = 'Table') -> None:
+        if self._enabled:
+            import mlflow
+            import pandas as pd
+            table = pd.DataFrame.from_records(data=rows, columns=columns)
+            mlflow.log_table(table, f'{name}.json')
 
     def log_metrics(self, metrics: Dict[str, Any], step: Optional[int] = None) -> None:
         import mlflow
