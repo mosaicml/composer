c6ea510f8a345bcc5c2a3cb7e2c9b6477bcdeb4b
Mihir Patel <mihir.v.patel7@gmail.com>
2023-08-14 17:32:41 -0700

    update verison

--- docker/generate_build_matrix.py
+++ docker/generate_build_matrix.py
@@ -19,7 +19,7 @@ import tabulate
 import yaml
 
 LATEST_PYTHON_VERSION = '3.10'
-PRODUCTION_PYTORCH_VERSION = '1.13.1'
+PRODUCTION_PYTORCH_VERSION = '2.0.1'
 
 
 def _get_torchvision_version(pytorch_version: str):
@@ -197,7 +197,7 @@ def _main():
     composer_entries = []
 
     # The `GIT_COMMIT` is a placeholder and Jenkins will substitute it with the actual git commit for the `composer_staging` images
-    composer_versions = ['0.15.1']  # Only build images for the latest composer version
+    composer_versions = ['0.16.0']  # Only build images for the latest composer version
     composer_python_versions = [LATEST_PYTHON_VERSION]  # just build composer against the latest
 
     for product in itertools.product(composer_python_versions, composer_versions, cuda_options):
