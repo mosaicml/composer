d3be7f7fe5666d956fd981ac19d00089a31bd391
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-18 11:16:40 +0200

    draft neptune logger

---
+++ composer/loggers/neptune_logger.py
@@ -0,0 +1,152 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright 2023 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Log to `Neptune.ai <https://neptune.ai/>`_."""
+
+__all__ = ['NeptuneLogger']
+
+from typing import TYPE_CHECKING, Any, Dict, Optional, Sequence, Union
+
+import numpy as np
+import torch
+from neptune.internal.utils import verify_type
+
+from composer._version import __version__
+from composer.loggers import LoggerDestination
+from composer.utils import MissingConditionalImportError, dist
+
+if TYPE_CHECKING:
+    from composer import Logger
+    from composer.core import State
+
+
+class NeptuneLogger(LoggerDestination):
+    """LoggerDestination implementation that sends the logs to Neptune.ai.
+
+    Args:
+        project (str, optional): The name of your Neptune project. If nothing is
+            specified, environment variable NEPTUNE_PROJECT will be used.
+        api_token (str, optional): Your Neptune.ai api key. If nothing is specified,
+            environment variable NEPTUNE_API_TOKEN will be used.
+        rank_zero_only (bool, optional): Whether to log only on the rank-zero process.
+            (default: ``True``).
+        base_namespace (str): The name of the base namespace to log the metadata to.
+        neptune_kwargs (Dict[str, Any], optional): Any additional kwargs to neptune.init_run
+            function. (For help see `Run documentation <https://docs.neptune.ai/api/neptune/#init_run>`_)
+    """
+    METRIC_NAMESPACE = 'metrics'
+    HYPERPARAM_NAMESPACE = 'hyperparameters'
+    TRACE_NAMESPACE = 'traces'
+    INTEGRATION_VERSION_KEY = 'source_code/integrations/neptune-MosaicML'
+
+    def __init__(self,
+                 *,
+                 project: Optional[str] = None,
+                 api_token: Optional[str] = None,
+                 rank_zero_only: bool = True,
+                 base_namespace: str = 'training',
+                 **neptune_kwargs: Dict[str, Any]) -> None:
+        try:
+            from neptune import Run
+        except ImportError as e:
+            raise MissingConditionalImportError(extra_deps_group='neptune',
+                                                conda_package='neptune',
+                                                conda_channel='conda-forge') from e
+
+        verify_type('project', project, (str, type(None)))
+        verify_type('api_token', api_token, (str, type(None)))
+        verify_type('rank_zero_only', rank_zero_only, bool)
+        verify_type('base_namespace', base_namespace, str)
+
+        if not base_namespace:
+            raise ValueError("Argument 'base_namespace' cannot be an empty string.")
+
+        self._project = project
+        self._api_token = api_token
+        self._rank_zero_only = rank_zero_only
+        self._base_namespace = base_namespace
+        self._neptune_kwargs = neptune_kwargs
+
+        self._enabled = (not rank_zero_only) or dist.get_global_rank() == 0
+        self._neptune_run = None
+        self._base_handler = None
+
+        if self._enabled:
+            self._neptune_run = Run(
+                project=self._project,
+                api_token=self._api_token,
+                **self._neptune_kwargs,
+            )
+            self._base_handler = self._neptune_run[self._base_namespace]
+
+        super(NeptuneLogger, self).__init__()
+
+    def init(self, state: 'State', logger: 'Logger') -> None:
+        del logger  # unused
+
+        tags = [state.run_name]
+
+        if not self._rank_zero_only:
+            tags.append(f'rank{dist.get_global_rank()}')
+
+        if self._enabled:
+            assert self._neptune_run is not None
+            self._neptune_run['sys/tags'].add(tags)
+            self._neptune_run[self.INTEGRATION_VERSION_KEY] = __version__
+
+    def log_metrics(self, metrics: Dict[str, float], step: Optional[int] = None) -> None:
+        if not self._enabled:
+            return
+
+        assert self._base_handler is not None
+
+        self._base_handler[NeptuneLogger.METRIC_NAMESPACE].append(metrics, step=step)
+
+    def log_hyperparameters(self, hyperparameters: Dict[str, Any]) -> None:
+        if not self._enabled:
+            return
+
+        from neptune.utils import stringify_unsupported
+
+        assert self._base_handler is not None
+
+        self._base_handler[NeptuneLogger.HYPERPARAM_NAMESPACE] = stringify_unsupported(hyperparameters)
+
+    def log_traces(self, traces: Dict[str, Any]):
+        if not self._enabled:
+            return
+
+        from neptune.utils import stringify_unsupported
+
+        assert self._base_handler is not None
+
+        self._base_handler[NeptuneLogger.TRACE_NAMESPACE] = stringify_unsupported(traces)
+
+    def log_images(
+        self,
+        images: Union[np.ndarray, torch.Tensor, Sequence[Union[np.ndarray, torch.Tensor]]],
+        name: str = 'Images',
+        channels_last: bool = False,
+        step: Optional[int] = None,
+        masks: Optional[Dict[str, Union[np.ndarray, torch.Tensor, Sequence[Union[np.ndarray, torch.Tensor]]]]] = None,
+        mask_class_labels: Optional[Dict[int, str]] = None,
+        use_table: bool = True,
+    ):
+        if not self._enabled:
+            return
+
+        assert self._base_handler is not None
+
+        for img in images:
+            self._base_handler[name].append(img, step=step)
+
+    def post_close(self) -> None:
+        if not self._enabled:
+            return
+
+        assert self._neptune_run is not None
+        self._neptune_run.sync()
+        self._neptune_run.stop()
--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ install_requires = [
     'py-cpuinfo>=8.0.0,<10',
     'packaging>=21.3.0,<23',
     'importlib-metadata>=5.0.0,<7',
-    'mosaicml-cli>=0.4.12,<0.6',
+    'mosaicml-cli>=0.4.12,<0.5',
 ]
 extra_deps = {}
 
@@ -99,12 +99,12 @@ extra_deps['dev'] = [
     # Should manually update dependency versions occassionally.
     'custom_inherit==2.4.1',
     'junitparser==3.1.0',
-    'coverage[toml]==7.3.0',
+    'coverage[toml]==7.2.7',
     'fasteners==0.18',  # object store tests require fasteners
     'pytest==7.4.0',
     'toml==0.10.2',
     'ipython==8.11.0',
-    'ipykernel==6.25.1',
+    'ipykernel==6.24.0',
     'jupyter==1.0.0',
     'yamllint==1.32.0',
     'recommonmark==0.7.1',
@@ -132,7 +132,7 @@ extra_deps['dev'] = [
     'GitPython==3.1.31',
     'moto[s3]>=4.0.1,<5',
     'mock-ssh-server==0.9.1',
-    'cryptography==41.0.3',
+    'cryptography==38.0.4',
     'pytest-httpserver>=1.0.4,<1.1',
     'setuptools<=59.5.0',
 ]
@@ -158,6 +158,10 @@ extra_deps['comet_ml'] = [
     'comet_ml>=3.31.12,<4.0.0',
 ]
 
+extra_deps['neptune'] = [
+    'neptune >= 1.0.0',
+]
+
 extra_deps['tensorboard'] = [
     'tensorboard>=2.9.1,<3.0.0',
 ]
@@ -180,7 +184,7 @@ extra_deps['coco'] = [
 ]
 
 extra_deps['nlp'] = [
-    'transformers>=4.11,<4.32',
+    'transformers>=4.11,<4.31',
     'datasets>=2.4,<3',
 ]
 
@@ -207,10 +211,6 @@ extra_deps['libcloud'] = [
 
 extra_deps['oci'] = [
     'oci>=2.88.2,<3.0.0',
-]
-
-extra_deps['gcs'] = [
-    'google-cloud-storage>=2.0.0,<3.0',
 ]
 
 extra_deps['onnx'] = [
