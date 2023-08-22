a7aa55eed094f0d5ca3cb50fbcbabeb0bf14ffa9
Mihir Patel <mihir.v.patel7@gmail.com>
2023-08-21 22:14:35 -0700

    monkeypatch init

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -498,7 +498,7 @@ elif version.parse(torch.__version__) < version.parse('2.1.0'):
         auto_wrap_kwargs['process_group_cache'] = {}
         _custom_recursive_wrap_t2p0p1(**auto_wrap_kwargs, **fsdp_kwargs)
 
-    def init_fn(
+    def init_fn_t2p0p1(
         self,
         module: nn.Module,
         process_group: ProcessGroupType = None,
@@ -599,7 +599,7 @@ elif version.parse(torch.__version__) < version.parse('2.1.0'):
         _register_all_state_dict_hooks(self)
 
     # monkey patch __init__ where __init__ calls the custom _auto_wrap fn
-    FullyShardedDataParallel.__init__ = init_fn
+    FullyShardedDataParallel.__init__ = init_fn_t2p0p1
 
 elif version.parse(torch.__version__) < version.parse('2.1.1'):
     # FullyShardedDataParallel monkey patch for torch < 2.1.1 ie torch == 2.1.0
@@ -610,17 +610,18 @@ elif version.parse(torch.__version__) < version.parse('2.1.1'):
     from torch.distributed.fsdp._dynamo_utils import _annotate_modules_for_dynamo
     from torch.distributed.fsdp._init_utils import (HYBRID_SHARDING_STRATEGIES, ProcessGroupType,
                                                     _check_orig_params_flattened, _init_buffer_state, _init_core_state,
-                                                    _init_ignored_module_states, _init_param_handle_from_module,
-                                                    _init_prefetching_state, _init_process_group_state,
-                                                    _init_runtime_state, _init_state_dict_state)
+                                                    _init_device_handle, _init_ignored_module_states,
+                                                    _init_param_handle_from_module, _init_prefetching_state,
+                                                    _init_process_group_state, _init_runtime_state,
+                                                    _init_state_dict_state)
     from torch.distributed.fsdp._state_dict_utils import _register_all_state_dict_hooks
     from torch.distributed.fsdp._unshard_param_utils import _register_flat_param
     from torch.distributed.fsdp._utils import _contains_batchnorm, _override_batchnorm_mixed_precision
     from torch.distributed.fsdp._wrap_utils import (_check_nested_wrapping, _validate_frozen_params,
                                                     _warn_on_overridden_mixed_precision)
-    from torch.distributed.fsdp.wrap import (_construct_wrap_fn, _FSDPPolicy, _or_policy, _Policy, _post_order_apply,
-                                             _run_mixed_precision_override_policy, _wrap, _wrap_batchnorm_individually,
-                                             _wrap_module_cls_individually)
+    from torch.distributed.fsdp.wrap import (ModuleWrapPolicy, _construct_wrap_fn, _FSDPPolicy, _or_policy, _Policy,
+                                             _post_order_apply, _run_mixed_precision_override_policy, _wrap,
+                                             _wrap_batchnorm_individually, _wrap_module_cls_individually)
 
     def _custom_recursive_wrap_t2p1p0(
         module: nn.Module,
@@ -645,6 +646,7 @@ elif version.parse(torch.__version__) < version.parse('2.1.1'):
             module (nn.Module): Module to recursively wrap.
             auto_wrap_policy (Callable): A callable representing a policy that
                 determines which modules to recursively wrap with ``wrapper_cls``.
+            wrapper_cls: wrapper_cls
             ignored_modules (Set[torch.nn.Module]): Modules to ignore when
                 wrapping.
             ignored_params (Set[torch.nn.Parameter]): Parameters to ignore when
@@ -696,7 +698,7 @@ elif version.parse(torch.__version__) < version.parse('2.1.1'):
             # decide if we need to wrap the current module,
             # since the left over parameters exceed the number of params to wrap
             remainder = nonwrapped_numel - total_wrapped_numel
-            
+
             ### BEGIN CHANGE
             module_kwargs = auto_wrap_policy(module=module, recurse=False, nonwrapped_numel=remainder)
             if not only_wrap_children and module_kwargs:
@@ -801,7 +803,107 @@ elif version.parse(torch.__version__) < version.parse('2.1.1'):
 
         _custom_recursive_wrap_t2p1p0(**recursive_wrap_kwargs, **root_kwargs)  # type: ignore[arg-type]
 
-    FullyShardedDataParallel._auto_wrap = _custom_auto_wrap_t2p1p0  # type: ignore
+    def init_fn_t2p1p0(
+        self,
+        module: nn.Module,
+        process_group: ProcessGroupType = None,
+        sharding_strategy: Optional[ShardingStrategy] = None,
+        cpu_offload: Optional[CPUOffload] = None,
+        auto_wrap_policy: Optional[Union[Callable, ModuleWrapPolicy]] = None,
+        backward_prefetch: Optional[BackwardPrefetch] = BackwardPrefetch.BACKWARD_PRE,
+        mixed_precision: Optional[MixedPrecision] = None,
+        ignored_modules: Optional[Iterable[torch.nn.Module]] = None,
+        param_init_fn: Optional[Callable[[nn.Module], None]] = None,
+        device_id: Optional[Union[int, torch.device]] = None,
+        sync_module_states: bool = False,
+        forward_prefetch: bool = False,
+        limit_all_gathers: bool = True,
+        use_orig_params: bool = False,
+        ignored_states: Union[Optional[Iterable[torch.nn.Parameter]], Optional[Iterable[torch.nn.Module]]] = None,
+    ):
+        """Modified version of hhttps://github.com/pytorch/pytorch/blob/8ed169b1628285924e10fc98de53dbb75c92c43e/torch/distributed/fsdp/fully_sharded_data_parallel.py#L399C1."""
+        torch._C._log_api_usage_once('torch.distributed.fsdp')
+        ## BEGIN CHANGE
+        super(FullyShardedDataParallel).__init__()
+        ## END CHANGE
+        _init_ignored_module_states(self, module, ignored_modules, ignored_states)
+        _init_device_handle(self, module, self._ignored_params, device_id)
+
+        # Add module annotations for Dynamo support (see function for details)
+        _annotate_modules_for_dynamo(module, self._ignored_modules, use_orig_params)
+
+        # Initializes self.process_group, along with rank and world size. This will
+        # also set another attribute, _inter_node_pg, to control the process group
+        # over which sharding occurs, if sharding_strategy is {HYBRID_SHARD, _HYBRID_SHARD_ZERO2}.
+        # Note that this is done before auto_wrapping, so that child FSDP modules simply pick up
+        # the same process group state as the root FSDP module.
+        _init_process_group_state(self, process_group, sharding_strategy, auto_wrap_policy)
+        if auto_wrap_policy is not None:
+            root_kwargs = {
+                'process_group': process_group,
+                'sharding_strategy': sharding_strategy,
+                'cpu_offload': cpu_offload,
+                'backward_prefetch': backward_prefetch,
+                'mixed_precision': mixed_precision,
+                'param_init_fn': param_init_fn,
+                'device_id': device_id,
+                'sync_module_states': sync_module_states,
+                'forward_prefetch': forward_prefetch,
+                'limit_all_gathers': limit_all_gathers,
+                'use_orig_params': use_orig_params,
+                'ignored_states': self._ignored_params,
+            }
+            if sharding_strategy in HYBRID_SHARDING_STRATEGIES:
+                # Share root process groups with children to maintain
+                # the invariant that all FSDP modules will have the same
+                # process groups.
+                root_kwargs['process_group'] = (self.process_group, self._inter_node_pg)
+
+            ### BEGIN CHANGE
+            _custom_auto_wrap_t2p1p0(
+                module,
+                auto_wrap_policy,
+                self._ignored_modules,
+                self._ignored_params,
+                root_kwargs,
+                FullyShardedDataParallel,
+            )
+            ### END CHANGE
+
+        backward_prefetch_limit = 1
+        forward_prefetch_limit = 1
+        _init_core_state(
+            self,
+            sharding_strategy,
+            mixed_precision,
+            cpu_offload,
+            limit_all_gathers,
+            use_orig_params,
+            backward_prefetch_limit,
+            forward_prefetch_limit,
+        )
+        _init_runtime_state(self)
+        _init_prefetching_state(self, backward_prefetch, forward_prefetch)
+        _init_buffer_state(self, module)
+        _init_param_handle_from_module(
+            self,
+            module,
+            device_id,
+            param_init_fn,
+            sync_module_states,
+        )
+        self._fsdp_wrapped_module = module
+        if not use_orig_params:
+            _check_orig_params_flattened(self, self._ignored_params)
+            _register_flat_param(self, self)
+
+        # `_state_dict_type` controls the `state_dict()` behavior, which is
+        # implemented using post-save and pre-load hooks
+        _init_state_dict_state(self)
+        _register_all_state_dict_hooks(self)
+
+    # monkey patch __init__ where __init__ calls the custom _auto_wrap fn
+    FullyShardedDataParallel.__init__ = init_fn_t2p1p0
 
 elif version.parse(torch.__version__) >= version.parse('2.1.0'):
     raise NotImplementedError(
