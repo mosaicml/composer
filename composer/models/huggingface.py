e70f8c1757749235ffdef3ce629e304accfbf4d3
Daniel King <daniel@mosaicml.com>
2023-08-25 18:06:33 -0700

    adjust to have full path

--- composer/models/huggingface.py
+++ composer/models/huggingface.py
@@ -173,7 +173,14 @@ class HuggingFaceModel(ComposerModel):
             for filename, saved_content in hf_tokenizer_state.items():
                 # This cannot be a temporary directory because huggingface relies on the slow tokenizer file
                 # being persistent on disk
-                tokenizer_file_path = Path(tokenizer_save_dir) / f'{filename}{saved_content["file_extension"]}'
+
+                # For backwards compatibility, check if the filename already has the file extension
+                if filename.endswith(saved_content['file_extension']):
+                    tokenizer_file_name = filename
+                else:
+                    tokenizer_file_name = filename + saved_content['file_extension']
+
+                tokenizer_file_path = Path(tokenizer_save_dir) / tokenizer_file_name'
                 if saved_content['file_extension'] == '.json':
                     with open(tokenizer_file_path, 'w') as _f:
                         json.dump(saved_content['content'], _f)
@@ -508,7 +515,7 @@ class HuggingFaceModel(ComposerModel):
                     else:
                         raise ValueError(
                             f'Unexpected file ending {tokenizer_file_name} in output of tokenizer.save_pretrained.')
-                    tokenizer_output[tokenizer_file_path.stem] = {
+                    tokenizer_output[tokenizer_file_path] = {
                         'file_extension': tokenizer_file_extension,
                         'content': tokenizer_file_content
                     }
