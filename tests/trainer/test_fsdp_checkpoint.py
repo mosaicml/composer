e9023e9101d3df1b0338760e0e8492f2fa5df9a8
Brandon <bcui8377@gmail.com>
2023-08-31 18:48:52 +0000

    Add filter warnings

--- tests/trainer/test_fsdp_checkpoint.py
+++ tests/trainer/test_fsdp_checkpoint.py
@@ -326,6 +326,7 @@ def test_fsdp_mixed_with_sync(world_size, tmp_path: pathlib.Path, sync_module_st
                  marks=pytest.mark.filterwarnings(
                      r'ignore:MosaicMLLogger is not in the state_dict. Its state will not be restored.:UserWarning'))
 ])
+@pytest.mark.filterwarnings(r'ignore:.*metrics are not saved with sharded state dict.*:UserWarning')
 @pytest.mark.skipif(version.parse(torch.__version__) < version.parse('1.13.0'),
                     reason='requires PyTorch 1.13 or higher')
 def test_fsdp_load_old_checkpoint(world_size, tmp_path: pathlib.Path, precision: str, sharding_strategy: str,
