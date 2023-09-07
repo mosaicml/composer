7158eea919312b3bffff6fed1ae270b4f4f8cb20
Mihir Patel <mihir.v.patel7@gmail.com>
2023-09-07 16:46:00 -0700

    fix wrap

--- composer/trainer/mosaic_fsdp_utils.py
+++ composer/trainer/mosaic_fsdp_utils.py
@@ -695,95 +695,94 @@ def _custom_recursive_wrap_t2p1p0(
     return module, 0
 
 
-def _custom_auto_wrap_t2p1p0(
-        root_module: nn.Module,
-        policy: Union[Callable, _Policy],
-        ignored_modules: Set[nn.Module],
-        ignored_params: Set[nn.Parameter],
-        root_kwargs: Dict[str, Any],
-        fsdp_fn: Callable,  # e.g. `FullyShardedDataParallel` or `fully_shard`
-):
-    """Modified version of https://github.com/pytorch/pytorch/blob/f13101640f548f8fa139c03dfa6711677278c391/torch/distributed/fsdp/wrap.py#L487.
+if version.parse(torch.__version__) < version.parse('2.1.1'):
+    from torch.distributed.fsdp.wrap import ModuleWrapPolicy, _Policy
 
-    Calls custom _recursive_wrap fn and adds progress group cache.
+    def _custom_auto_wrap_t2p1p0(
+            root_module: nn.Module,
+            policy: Union[Callable, _Policy],
+            ignored_modules: Set[nn.Module],
+            ignored_params: Set[nn.Parameter],
+            root_kwargs: Dict[str, Any],
+            fsdp_fn: Callable,  # e.g. `FullyShardedDataParallel` or `fully_shard`
+    ):
+        """Modified version of https://github.com/pytorch/pytorch/blob/f13101640f548f8fa139c03dfa6711677278c391/torch/distributed/fsdp/wrap.py#L487.
 
-    Auto wraps modules in ``root_module`` 's tree according to ``policy``
-    following a post-order traversal.
+        Calls custom _recursive_wrap fn and adds progress group cache.
 
-    Precondition: ``root_kwargs`` should contain all arguments except
-    ``module``. This function accepts the kwargs dict directly since it gets
-    forwarded into the post-order traversal function.
-    """
-    from torch.distributed.fsdp._common_utils import _override_module_mixed_precision
-    from torch.distributed.fsdp._wrap_utils import (_check_nested_wrapping, _validate_frozen_params,
-                                                    _warn_on_overridden_mixed_precision)
-    from torch.distributed.fsdp.wrap import (_construct_wrap_fn, _or_policy, _Policy, _post_order_apply,
-                                             _run_mixed_precision_override_policy, _wrap_module_cls_individually)
+        Auto wraps modules in ``root_module`` 's tree according to ``policy``
+        following a post-order traversal.
 
-    mixed_precision = root_kwargs['mixed_precision']
-    is_wrapper = inspect.isclass(fsdp_fn)
-    # TODO: We may relax this no-nested-wrapping constraint to support manual
-    # wrapping followed by auto wrapping.
-    _check_nested_wrapping(root_module)
+        Precondition: ``root_kwargs`` should contain all arguments except
+        ``module``. This function accepts the kwargs dict directly since it gets
+        forwarded into the post-order traversal function.
+        """
+        from torch.distributed.fsdp._common_utils import _override_module_mixed_precision
+        from torch.distributed.fsdp._wrap_utils import (_check_nested_wrapping, _validate_frozen_params,
+                                                        _warn_on_overridden_mixed_precision)
+        from torch.distributed.fsdp.wrap import (_construct_wrap_fn, _or_policy, _post_order_apply,
+                                                 _run_mixed_precision_override_policy, _wrap_module_cls_individually)
 
-    if isinstance(policy, _Policy):
-        root_kwargs['auto_wrap_policy' if is_wrapper else 'policy'] = None
-        target_module_to_kwargs = policy._run_policy(root_module, ignored_modules, root_kwargs)
-        if mixed_precision is not None:
-            target_module_to_kwargs = _run_mixed_precision_override_policy(
+        mixed_precision = root_kwargs['mixed_precision']
+        is_wrapper = inspect.isclass(fsdp_fn)
+        # TODO: We may relax this no-nested-wrapping constraint to support manual
+        # wrapping followed by auto wrapping.
+        _check_nested_wrapping(root_module)
+
+        if isinstance(policy, _Policy):
+            root_kwargs['auto_wrap_policy' if is_wrapper else 'policy'] = None
+            target_module_to_kwargs = policy._run_policy(root_module, ignored_modules, root_kwargs)
+            if mixed_precision is not None:
+                target_module_to_kwargs = _run_mixed_precision_override_policy(
+                    root_module,
+                    mixed_precision._module_classes_to_ignore,
+                    ignored_modules,
+                    root_kwargs,
+                    target_module_to_kwargs,
+                )
+                overridden_module_classes = _override_module_mixed_precision(root_module,
+                                                                             mixed_precision._module_classes_to_ignore)
+                _warn_on_overridden_mixed_precision(overridden_module_classes)
+            use_orig_params = root_kwargs.get('use_orig_params', False)
+            _validate_frozen_params(
                 root_module,
-                mixed_precision._module_classes_to_ignore,
-                ignored_modules,
-                root_kwargs,
-                target_module_to_kwargs,
+                set(target_module_to_kwargs.keys()),
+                ignored_params,
+                use_orig_params,
             )
+            wrap_fn = _construct_wrap_fn(root_module, target_module_to_kwargs, fsdp_fn)
+            _post_order_apply(root_module, wrap_fn)
+            return
+
+        recursive_wrap_kwargs = {
+            'module': root_module,
+            'auto_wrap_policy': policy,
+            'wrapper_cls': fsdp_fn,
+            'ignored_modules': ignored_modules,
+            'ignored_params': ignored_params,
+            'only_wrap_children': True,
+        }
+        if mixed_precision is not None:
+            # Wrap modules of the ignored types separately and register forward
+            # hooks to cast to fp32 and back to the original dtype, respectively
             overridden_module_classes = _override_module_mixed_precision(root_module,
                                                                          mixed_precision._module_classes_to_ignore)
+            policy = functools.partial(
+                _or_policy,
+                policies=[
+                    policy,
+                    partial(
+                        _wrap_module_cls_individually,
+                        module_classes=mixed_precision._module_classes_to_ignore,
+                    ),
+                ],
+            )
+            recursive_wrap_kwargs['auto_wrap_policy'] = policy
             _warn_on_overridden_mixed_precision(overridden_module_classes)
-        use_orig_params = root_kwargs.get('use_orig_params', False)
-        _validate_frozen_params(
-            root_module,
-            set(target_module_to_kwargs.keys()),
-            ignored_params,
-            use_orig_params,
-        )
-        wrap_fn = _construct_wrap_fn(root_module, target_module_to_kwargs, fsdp_fn)
-        _post_order_apply(root_module, wrap_fn)
-        return
 
-    recursive_wrap_kwargs = {
-        'module': root_module,
-        'auto_wrap_policy': policy,
-        'wrapper_cls': fsdp_fn,
-        'ignored_modules': ignored_modules,
-        'ignored_params': ignored_params,
-        'only_wrap_children': True,
-    }
-    if mixed_precision is not None:
-        # Wrap modules of the ignored types separately and register forward
-        # hooks to cast to fp32 and back to the original dtype, respectively
-        overridden_module_classes = _override_module_mixed_precision(root_module,
-                                                                     mixed_precision._module_classes_to_ignore)
-        policy = functools.partial(
-            _or_policy,
-            policies=[
-                policy,
-                partial(
-                    _wrap_module_cls_individually,
-                    module_classes=mixed_precision._module_classes_to_ignore,
-                ),
-            ],
-        )
-        recursive_wrap_kwargs['auto_wrap_policy'] = policy
-        _warn_on_overridden_mixed_precision(overridden_module_classes)
+        recursive_wrap_kwargs['process_group_cache'] = {}
 
-    recursive_wrap_kwargs['process_group_cache'] = {}
-
-    _custom_recursive_wrap_t2p1p0(**recursive_wrap_kwargs, **root_kwargs)  # type: ignore[arg-type]
-
-
-if version.parse(torch.__version__) < version.parse('2.1.1'):
-    from torch.distributed.fsdp.wrap import ModuleWrapPolicy
+        _custom_recursive_wrap_t2p1p0(**recursive_wrap_kwargs, **root_kwargs)  # type: ignore[arg-type]
 
     def init_fn_t2p1p0(
         self,
