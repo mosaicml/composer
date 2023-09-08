d6aa7543a055c2e6750fbfefe29413f45b469cc1
Brian <23239305+b-chu@users.noreply.github.com>
2023-09-08 14:30:37 -0400

    Update pre-commit in setup.py (#2522)

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ extra_deps['dev'] = [
     'yamllint==1.32.0',
     'recommonmark==0.7.1',
     'sphinx==4.4.0',
-    'pre-commit>=2.18.1,<3',
+    'pre-commit>=3.4.0,<4',
     # embedding md in rst require docutils>=0.17. See
     # https://myst-parser.readthedocs.io/en/latest/sphinx/use.html?highlight=parser#include-markdown-files-into-an-rst-file
     'docutils==0.17.1',
