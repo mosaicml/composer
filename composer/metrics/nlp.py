f8c3a3db919d52693445fd55260abf53c02677d3
Michael Carbin <michael.carbin@databricks.com>
2023-09-21 21:19:06 -0400

    update docstring for estimator

--- composer/metrics/nlp.py
+++ composer/metrics/nlp.py
@@ -544,8 +544,12 @@ class InContextLearningCodeEvalAccuracy(InContextLearningMetric):
         return client
 
     def estimator(self, n: int, c: int, k: int) -> float:
-        """
-        Calculates 1 - comb(n - c, k) / comb(n, k).
+        """Computes the pass@k metric.
+
+        Given the number of generated samples, n, the number of correct samples, c, and the k of interest,
+        this function calculates pass@k as 1 - comb(n - c, k) / comb(n, k) as per the definition of
+        pass@k in the HumanEval paper (https://arxiv.org/abs/2107.03374) and it's associated implementation:
+        https://github.com/openai/human-eval.
         """
         if n - c < k:
             return 1.0
