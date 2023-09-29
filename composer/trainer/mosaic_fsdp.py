4c3a63f4232433d697b2f3ba511c33df4b4a9b51
Cheng Li <pistasable@gmail.com>
2023-09-29 21:37:16 +0000

    remove patching torch 2.1.1

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -39,16 +39,5 @@ def patch_pytorch():
         ChunkShardingSpec.build_metadata = build_metadata
         ChunkShardingSpec.shard = shard
 
-    elif version.parse(torch.__version__) < version.parse('2.1.1'):
-        # Monkey path for torch < 2.1.1 ie torch == 2.1.0
-
-        # Monkey patch __init__ where __init__ calls the custom _auto_wrap fn
-        from composer.trainer.mosaic_fsdp_utils import init_fn_t2p1p0
-        FullyShardedDataParallel.__init__ = init_fn_t2p1p0  # type: ignore
-
-        # Monkey patch sharding method
-        ChunkShardingSpec.build_metadata = build_metadata
-        ChunkShardingSpec.shard = shard
-
     elif version.parse(torch.__version__) >= version.parse('2.1.1'):
         raise NotImplementedError(f'FullyShardedDataParallel is not supported for torch >= 2.2.0')
