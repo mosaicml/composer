28e11a7bb7cc6126c5734d26d40176908938c9d9
Mihir Patel <mihir.v.patel7@gmail.com>
2023-10-02 17:49:29 -0400

    lint

--- composer/callbacks/free_train_metrics.py
+++ composer/callbacks/free_train_metrics.py
@@ -3,11 +3,13 @@
 
 """Free train metrics ."""
 
+import torch
 from composer.core import Callback, State
 from composer.loggers import Logger
+
 
 class FreeTrainMetrics(Callback):
     """Free train metrics on AFTER_LOSS to reduce peak memory usage if not using train metrics."""
 
     def after_loss(self, state: State, logger: Logger) -> None:
-        state.outputs = None
+        state.outputs = torch.Tensor()
