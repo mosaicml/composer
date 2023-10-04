bb375e8181b0ae9b2e17ebf885183aa3ae99a346
Mihir Patel <mihir.v.patel7@gmail.com>
2023-10-04 16:23:31 -0400

    lint

--- composer/optim/decoupled_weight_decay.py
+++ composer/optim/decoupled_weight_decay.py
@@ -50,7 +50,7 @@ class DecoupledSGDW(SGD):
 
     def __init__(self,
                  params: Union[Iterable[torch.Tensor], Iterable[dict]],
-                 lr: float = required,
+                 lr: float = required,  # type: ignore
                  momentum: float = 0,
                  dampening: float = 0,
                  weight_decay: float = 0,
