d850c10fb231999e1a116d8f9cb119974dcb16eb
Chuck Tang <chuck@mosaicml.com>
2023-10-06 11:44:25 -0700

    add

---
+++ tests/test_docker.py
@@ -0,0 +1,20 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import platform
+
+import PIL
+import pytest
+from torch.utils.data import DataLoader
+
+from composer.trainer import Trainer
+from tests.common import RandomClassificationDataset, SimpleModel
+
+
+@pytest.mark.skipif('composer-python' not in os.environ['PATH'] or 'Linux' not in platform.system(),
+                    reason='Pillow-simd test only checks if using the composer docker')
+class TestDocker:
+
+    def test_pillow_simd(self):
+        assert 'post' in PIL.__version__, 'pillow-simd is not installed'
