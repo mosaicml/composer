dce3cc967be5ae230f447183e93f846da249748d
Daniel King <daniel@mosaicml.com>
2023-10-12 20:38:40 -0700

    precommit

--- composer/__init__.py
+++ composer/__init__.py
@@ -10,15 +10,15 @@ from composer.models import ComposerModel
 from composer.trainer import Trainer
 
 try:
-    import transformers
     import flash_attn
+    import transformers
     import version
-    
+
     # Before importing any transformers models, we need to disable transformers flash attention if
     # we are in an environment with flash attention version <2. Transformers hard errors on a not properly
     # gated import otherwise.
     if version.parse(flash_attn.__version__) < version.parse('2.0.0'):
-        transformers.utils.is_flash_attn_available = lambda : False
+        transformers.utils.is_flash_attn_available = lambda: False
 except:
     pass
 
