58f2d548456e8ba9976c793c43544d70a0b80c3a
Daniel King <daniel@mosaicml.com>
2023-10-12 20:37:56 -0700

    disable flash v1 for transformers

--- composer/__init__.py
+++ composer/__init__.py
@@ -9,6 +9,19 @@ from composer.loggers import Logger
 from composer.models import ComposerModel
 from composer.trainer import Trainer
 
+try:
+    import transformers
+    import flash_attn
+    import version
+    
+    # Before importing any transformers models, we need to disable transformers flash attention if
+    # we are in an environment with flash attention version <2. Transformers hard errors on a not properly
+    # gated import otherwise.
+    if version.parse(flash_attn.__version__) < version.parse('2.0.0'):
+        transformers.utils.is_flash_attn_available = lambda : False
+except:
+    pass
+
 __all__ = [
     'Algorithm',
     'Callback',
