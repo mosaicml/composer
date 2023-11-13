1162a78ae82469653be162da35a9f7596669647e
Mihir Patel <mihir.v.patel7@gmail.com>
2023-11-13 11:38:09 -0500

    test

--- tests/trainer/test_fsdp.py
+++ tests/trainer/test_fsdp.py
@@ -85,3 +85,34 @@ def test_fsdp_meta_initialization_none(model: ComposerClassifier, mixed_precisio
         },
         max_duration='3ba',
     )
+
+
+@pytest.mark.parametrize('forward_prefetch_limit', [1, 2])
+@pytest.mark.parametrize('backward_prefetch_limit', [1, 2])
+@pytest.mark.gpu
+@pytest.mark.skipif(version.parse(torch.__version__) < version.parse('1.13.0'),
+                    reason='FSDP requires PyTorch 1.13 or higher')
+def test_fsdp_device_initialization(forward_prefetch_limit: int, backward_prefetch_limit: int):
+    """test FSDP device initialization for a simple model with weight tying and a model where two modules
+    from separate submodules have weight tying applied. This test also covers both 'cpu' and
+    'meta' devices. This is because 'meta' will result in deferred initialization until FSDP is initialized
+
+    """
+    num_classes = 10
+    model = SimpleModel(num_features=num_classes, device='meta')
+    dataset = RandomClassificationDataset(shape=(num_classes,), size=2, num_classes=num_classes)
+    dataloader = DataLoader(dataset, sampler=dist.get_sampler(dataset))
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    trainer = Trainer(
+        model=model,
+        optimizers=optimizer,
+        train_dataloader=dataloader,
+        fsdp_config={
+            'forward_prefetch_limit': forward_prefetch_limit,
+            'backward_prefetch_limit': backward_prefetch_limit,
+        },
+        max_duration='3ba',
+    )
+
+    trainer.fit()
