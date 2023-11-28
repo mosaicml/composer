514a34a98faf3e3f77b25251b21113c0461484f0
Bandish Shah <bandish.shah@databricks.com>
2023-11-27 19:20:06 -0800

    Adding full override string

--- docker/generate_build_matrix.py
+++ docker/generate_build_matrix.py
@@ -58,17 +58,27 @@ def _get_cuda_version_tag(cuda_version: str):
 
 def _get_cuda_override(cuda_version: str):
     if cuda_version == '12.1.0':
-        cuda_121_override_string = ('cuda>=12.1 brand=tesla,driver>=470,driver<471 '
-                        'brand=unknown,driver>=470,driver<471 brand=nvidia,driver>=470,driver<471 '
-                        'brand=nvidiartx,driver>=470,driver<471 brand=geforce,driver>=470,driver<471 '
-                        'brand=geforcertx,driver>=470,driver<471 brand=quadro,driver>=470,driver<471 '
-                        'brand=quadrortx,driver>=470,driver<471 brand=titan,driver>=470,driver<471 '
-                        'brand=titanrtx,driver>=470,driver<471 brand=tesla,driver>=515,driver<516 '
-                        'brand=unknown,driver>=515,driver<516 brand=nvidia,driver>=515,driver<516 '
-                        'brand=nvidiartx,driver>=515,driver<516 brand=geforce,driver>=515,driver<516 '
-                        'brand=geforcertx,driver>=515,driver<516 brand=quadro,driver>=515,driver<516 '
-                        'brand=quadrortx,driver>=515,driver<516 brand=titan,driver>=515,driver<516 '
-                        'brand=titanrtx,driver>=515,driver<516')
+        cuda_121_override_string = ('cuda>=12.1 brand=tesla,driver>=450,driver<451 '
+                            'brand=tesla,driver>=470,driver<471 brand=unknown,driver>=470,driver<471 '
+                            'brand=nvidia,driver>=470,driver<471 brand=nvidiartx,driver>=470,driver<471 '
+                            'brand=geforce,driver>=470,driver<471 brand=geforcertx,driver>=470,driver<471 '
+                            'brand=quadro,driver>=470,driver<471 brand=quadrortx,driver>=470,driver<471 '
+                            'brand=titan,driver>=470,driver<471 brand=titanrtx,driver>=470,driver<471 '
+                            'brand=tesla,driver>=510,driver<511 brand=unknown,driver>=510,driver<511 '
+                            'brand=nvidia,driver>=510,driver<511 brand=nvidiartx,driver>=510,driver<511 '
+                            'brand=geforce,driver>=510,driver<511 brand=geforcertx,driver>=510,driver<511 '
+                            'brand=quadro,driver>=510,driver<511 brand=quadrortx,driver>=510,driver<511 '
+                            'brand=titan,driver>=510,driver<511 brand=titanrtx,driver>=510,driver<511 '
+                            'brand=tesla,driver>=515,driver<516 brand=unknown,driver>=515,driver<516 '
+                            'brand=nvidia,driver>=515,driver<516 brand=nvidiartx,driver>=515,driver<516 '
+                            'brand=geforce,driver>=515,driver<516 brand=geforcertx,driver>=515,driver<516 '
+                            'brand=quadro,driver>=515,driver<516 brand=quadrortx,driver>=515,driver<516 '
+                            'brand=titan,driver>=515,driver<516 brand=titanrtx,driver>=515,driver<516 '
+                            'brand=tesla,driver>=525,driver<526 brand=unknown,driver>=525,driver<526 '
+                            'brand=nvidia,driver>=525,driver<526 brand=nvidiartx,driver>=525,driver<526 '
+                            'brand=geforce,driver>=525,driver<526 brand=geforcertx,driver>=525,driver<526 '
+                            'brand=quadro,driver>=525,driver<526 brand=quadrortx,driver>=525,driver<526 '
+                            'brand=titan,driver>=525,driver<526 brand=titanrtx,driver>=525,driver<526')
         
         return cuda_121_override_string
 
