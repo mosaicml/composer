760ecb0a3da144ae7ff331a00a257ae15051ae93
Charles Tang <j316chuck@users.noreply.github.com>
2023-11-29 17:11:51 -0800

    Update composer/trainer/mosaic_fsdp.py
    Co-authored-by: Mihir Patel <mihir.v.patel7@gmail.com>

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -50,6 +50,3 @@ def patch_pytorch():
         # Monkey patch partial state dict handling
         _state_dict_utils._sharded_pre_load_state_dict_hook = (_sharded_pre_load_state_dict_hook)
 
-    else:
-        # Nothing to patch yet for torch >= 2.1.1
-        pass
