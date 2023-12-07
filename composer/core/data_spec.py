fd0e204a77a632ab6dd74458c744b03e3889ed7c
Max Marion <max.marion@databricks.com>
2023-12-07 23:39:17 +0000

    wip

--- composer/core/data_spec.py
+++ composer/core/data_spec.py
@@ -60,7 +60,11 @@ def _split_mapping(m, microbatch_size: int):
     for k, v in m.items():
         if isinstance(v, (int, float, str, bool)):
             chunked[k] = [v] * num_chunks
-    return [{k: v[idx] for k, v in chunked.items()} for idx in range(num_chunks)]
+    try:
+        return [{k: v[idx] for k, v in chunked.items()} for idx in range(num_chunks)]
+    except:
+        lens = {k: len(v) for k, v in chunked.items()}
+        print(f"Failed returning batches. Here's the dictionary: {lens}")
 
 
 def _check_list_is_primitives(l):
