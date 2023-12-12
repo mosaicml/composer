1b8feb61a72e00ed7a3ade1161e848d2aa5fe7ab
will.gleich <22464726+willgleich@users.noreply.github.com>
2023-12-11 17:19:25 -0700

    Add platform AWS and bump aws ofi nccl version

--- docker/generate_build_matrix.py
+++ docker/generate_build_matrix.py
@@ -224,7 +224,7 @@ def _main():
         if interconnect != 'EFA':
             entry['AWS_OFI_NCCL_VERSION'] = ''
         else:
-            entry['AWS_OFI_NCCL_VERSION'] = 'v1.7.3-aws'
+            entry['AWS_OFI_NCCL_VERSION'] = 'v1.7.4-aws'
 
         pytorch_entries.append(entry)
     nightly_entry = {
