52ac18c773662c4adc4d9b0adbb3a156956abef8
snarayan21 <saaketh@mosaicml.com>
2024-01-02 14:26:42 -0800

    Better communication computation overlap (#2811)
    * patched torch
    
    * fixed torch imports
    
    * fixed torch imports
    
    * fixed torch imports
    
    * patching through composer
    
    * patching through composer
    
    * patching typingr
    
    * comment added
    
    * don't patch torch 2.1.0
    
    * patch torch 2.1.1 and 2.2.0
    
    * linting fix

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -41,7 +41,7 @@ def patch_pytorch():
         ChunkShardingSpec.build_metadata = build_metadata
 
     elif version.parse(torch.__version__) < version.parse('2.1.1'):
-        # Monkey path for torch < 2.1.1 ie torch == 2.1.0
+        # Monkey patch for torch < 2.1.1 ie torch == 2.1.0
 
         # Monkey patch sharding method
         ChunkShardingSpec.build_metadata = build_metadata
@@ -55,8 +55,21 @@ def patch_pytorch():
         _runtime_utils._validate_and_get_hybrid_shard_state = lambda *args, **kwargs: None
 
     elif version.parse(torch.__version__) < version.parse('2.2.0'):
-        # Monkey path for torch < 2.2.0 ie torch == 2.1.1, 2.1.2
+        # Monkey patch for torch < 2.2.0 ie torch == 2.1.1, 2.1.2
 
         # Allow 2D HSDP
         from torch.distributed.fsdp import _runtime_utils
         _runtime_utils._validate_and_get_hybrid_shard_state = lambda *args, **kwargs: None
+
+        # Better overlap communication and computation
+        from composer.trainer.mosaic_fsdp_utils import _share_state_and_init_handle_attrs_t2p1
+        _runtime_utils._share_state_and_init_handle_attrs = _share_state_and_init_handle_attrs_t2p1
+
+    elif version.parse(torch.__version__) < version.parse('2.2.1'):
+        # Monkey patch for torch < 2.2.1 ie torch == 2.2.0
+
+        # Better overlap communication and computation
+        from torch.distributed.fsdp import _runtime_utils
+
+        from composer.trainer.mosaic_fsdp_utils import _share_state_and_init_handle_attrs_t2p2
+        _runtime_utils._share_state_and_init_handle_attrs = _share_state_and_init_handle_attrs_t2p2
