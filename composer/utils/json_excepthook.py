ab40e5dc04f9cd5f90537faee3a1e8a7bfb27e01
jjanezhang <jane.zhang@databricks.com>
2024-01-03 11:15:24 -0800

    added rank as param to excepthook

--- composer/utils/json_excepthook.py
+++ composer/utils/json_excepthook.py
@@ -4,15 +4,15 @@ import sys
 from composer.utils.json_log_formatter import JsonLogFormatter
 
 
-def override_excepthook():
+def override_excepthook(rank=None):
     log = logging.getLogger(__name__)
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setFormatter(JsonLogFormatter())
-    log.addHandler(stderr_handler)
+    if not log.handlers:
+        stderr_handler = logging.StreamHandler(sys.stderr)
+        stderr_handler.setFormatter(JsonLogFormatter(rank=rank))
+        log.addHandler(stderr_handler)
 
     def log_exception(type, value, tb):
-        log.exception()
-        sys.__excepthook__(type, value, tb)
+        log.exception('An exception occurred', exc_info=(type, value, tb))
 
     sys.excepthook = log_exception
 
