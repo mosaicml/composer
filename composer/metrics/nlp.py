7997cc791ac9c2a4668707c3778b865245b6f7c0
Max Marion <max.marion@databricks.com>
2024-01-11 00:21:52 +0000

    fix tensor logging

--- composer/metrics/nlp.py
+++ composer/metrics/nlp.py
@@ -1038,13 +1038,13 @@ class MTBenchJudge(InContextLearningMetric):
             self.humanities_score += score
 
     def compute(self):
-        log.info('Math score:       ', self.math_score / self.math_total)
-        log.info('Writing score:    ', self.writing_score / self.writing_total)
-        log.info('Roleplay score:   ', self.roleplay_score / self.roleplay_total)
-        log.info('Reasoning score:  ', self.reasoning_score / self.reasoning_total)
-        log.info('Coding score:     ', self.coding_score / self.coding_total)
-        log.info('Extraction score: ', self.extraction_score / self.extraction_total)
-        log.info('STEM score:       ', self.stem_score / self.stem_total)
-        log.info('Humanities score: ', self.humanities_score / self.humanities_total)
-        log.info('Combined score:   ', self.all_scores / self.total)
+        log.info('Math score:       ', (self.math_score / self.math_total).item())
+        log.info('Writing score:    ', (self.writing_score / self.writing_total).item())
+        log.info('Roleplay score:   ', (self.roleplay_score / self.roleplay_total).item())
+        log.info('Reasoning score:  ', (self.reasoning_score / self.reasoning_total).item())
+        log.info('Coding score:     ', (self.coding_score / self.coding_total).item())
+        log.info('Extraction score: ', (self.extraction_score / self.extraction_total).item())
+        log.info('STEM score:       ', (self.stem_score / self.stem_total).item())
+        log.info('Humanities score: ', (self.humanities_score / self.humanities_total).item())
+        log.info('Combined score:   ', (self.all_scores / self.total).item())
         return self.all_scores / self.total
