101de6c47866777862340b5f8c96d387e0483e3d
Mihir Patel <mihir.v.patel7@gmail.com>
2024-01-10 23:25:08 -0500

    remove

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -72,15 +72,15 @@ def patch_pytorch():
     elif version.parse(torch.__version__) < version.parse('2.2.1'):
         # Monkey patch for torch < 2.2.1 ie torch == 2.2.0
 
-        # Better overlap communication and computation
-        from torch.distributed.fsdp import _runtime_utils
+        # # Better overlap communication and computation
+        # from torch.distributed.fsdp import _runtime_utils
 
-        from composer.trainer.mosaic_fsdp_utils import (_root_pre_forward, _share_state_and_init_handle_attrs_t2p2,
-                                                        _wait_for_computation_stream, forward)
-        _runtime_utils._share_state_and_init_handle_attrs = _share_state_and_init_handle_attrs_t2p2
-        _runtime_utils._wait_for_computation_stream = _wait_for_computation_stream
-        _runtime_utils._root_pre_forward = _root_pre_forward
-        FullyShardedDataParallel.forward = forward
+        # from composer.trainer.mosaic_fsdp_utils import (_root_pre_forward, _share_state_and_init_handle_attrs_t2p2,
+        #                                                 _wait_for_computation_stream, forward)
+        # _runtime_utils._share_state_and_init_handle_attrs = _share_state_and_init_handle_attrs_t2p2
+        # _runtime_utils._wait_for_computation_stream = _wait_for_computation_stream
+        # _runtime_utils._root_pre_forward = _root_pre_forward
+        # FullyShardedDataParallel.forward = forward
 
         # Monkeypatch dtensor support
         from composer.trainer.mosaic_fsdp_utils import init_fn_t2p2p0
