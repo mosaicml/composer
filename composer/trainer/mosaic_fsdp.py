ef5b04c36a1a8025b04ed94128e7e6e09230a5ff
Mihir Patel <mihir.v.patel7@gmail.com>
2024-01-14 18:20:57 -0500

    fix gate

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -127,7 +127,7 @@ def patch_pytorch():
         state_dict._verify_options = _verify_options_t2p2p0
 
         # Monkeypatch sharding optim state
-        from torch.distributed import optim
+        from torch.distributed.fsdp import _optim_utils
 
         from composer.trainer.mosaic_fsdp_utils import _shard_orig_param_state
-        optim._shard_orig_param_state = _shard_orig_param_state
+        _optim_utils._shard_orig_param_state = _shard_orig_param_state
