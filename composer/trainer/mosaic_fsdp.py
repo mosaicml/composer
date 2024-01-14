0df31d0c03f9a0c2d275ff9c579fc0f529f9ed68
Mihir Patel <mihir.v.patel7@gmail.com>
2024-01-14 18:12:50 -0500

    offload

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -125,3 +125,9 @@ def patch_pytorch():
 
         from composer.trainer.mosaic_fsdp_utils import _verify_options_t2p2p0
         state_dict._verify_options = _verify_options_t2p2p0
+
+        # Monkeypatch sharding optim state
+        from torch.distributed import optim
+
+        from composer.trainer.mosaic_fsdp_utils import _shard_orig_param_state
+        optim._shard_orig_param_state = _shard_orig_param_state
