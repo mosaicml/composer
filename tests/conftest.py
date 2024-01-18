c923125ed0e027f7ccd21dad8d57fcb059b972c3
Daniel King <daniel@mosaicml.com>
2024-01-18 13:51:40 -0800

    add more back

--- tests/conftest.py
+++ tests/conftest.py
@@ -116,11 +116,6 @@ def pytest_configure():
                                              tiny_mistral_tokenizer_helper, tiny_opt_config_helper,
                                              tiny_opt_model_helper, tiny_opt_tokenizer_helper, tiny_t5_config_helper,
                                              tiny_t5_model_helper, tiny_t5_tokenizer_helper)
-        # from tests.fixtures.fixtures import (tiny_bert_config_helper, tiny_bert_model_helper,
-        #                                      tiny_bert_tokenizer_helper, tiny_gpt2_config_helper,
-        #                                      tiny_gpt2_model_helper, tiny_gpt2_tokenizer_helper, tiny_opt_config_helper,
-        #                                      tiny_opt_model_helper, tiny_opt_tokenizer_helper, tiny_t5_config_helper,
-        #                                      tiny_t5_model_helper, tiny_t5_tokenizer_helper)
         pytest.tiny_bert_config = tiny_bert_config_helper()  # type: ignore
         pytest.tiny_bert_model = tiny_bert_model_helper(pytest.tiny_bert_config)  # type: ignore
         pytest.tiny_bert_tokenizer = tiny_bert_tokenizer_helper()  # type: ignore
--- tests/models/test_hf_model.py
+++ tests/models/test_hf_model.py
@@ -25,9 +25,9 @@ from composer.trainer import Trainer
 from composer.utils import dist, is_model_fsdp
 from tests.common.datasets import RandomTextClassificationDataset, RandomTextLMDataset, RandomTextRegressionDataset
 from tests.common.markers import device, world_size
-# from tests.common.models import (configure_tiny_bert_model, configure_tiny_bert_tokenizer, configure_tiny_gpt2_model,
-#                                  configure_tiny_gpt2_tokenizer, configure_tiny_mistral_model,
-#                                  configure_tiny_mistral_tokenizer, configure_tiny_t5_model, configure_tiny_t5_tokenizer)
+from tests.common.models import (configure_tiny_bert_model, configure_tiny_bert_tokenizer, configure_tiny_gpt2_model,
+                                 configure_tiny_gpt2_tokenizer, configure_tiny_mistral_model,
+                                 configure_tiny_mistral_tokenizer, configure_tiny_t5_model, configure_tiny_t5_tokenizer)
 from tests.common.models import (configure_tiny_bert_model, configure_tiny_bert_tokenizer, configure_tiny_gpt2_model,
                                  configure_tiny_gpt2_tokenizer, configure_tiny_t5_model, configure_tiny_t5_tokenizer)
 from tests.loggers.test_remote_uploader_downloader import DummyObjectStore
