f78d58048f6f28de6454b70d40bc790903db0cca
Daniel King <daniel@mosaicml.com>
2024-01-24 17:41:12 -0800

    export peft installed

--- composer/models/huggingface.py
+++ composer/models/huggingface.py
@@ -38,7 +38,7 @@ if TYPE_CHECKING:
 
 log = logging.getLogger(__name__)
 
-__all__ = ['HuggingFaceModel']
+__all__ = ['HuggingFaceModel', 'peft_installed']
 
 
 class HuggingFaceModel(ComposerModel):
