451420a004e1709da48a9bf9367c0eb28bc82679
Mihir Patel <mihir.v.patel7@gmail.com>
2024-01-25 11:02:17 -0800

    fix

--- composer/utils/checkpoint.py
+++ composer/utils/checkpoint.py
@@ -552,6 +552,7 @@ def load_sharded_checkpoint(
                 #     storage_reader.process_group = process_group
                 #     log.debug(f'Loading on global_rank={dist.get_global_rank()}, {expect_file=}')
                 process_group = device_mesh.get_group(0)  # Shard process_group
+                storage_reader.process_group = process_group
                 log.info(f'Ranks: {dist_torch._world.pg_group_ranks[process_group]}')
             else:
                 expect_file = True
