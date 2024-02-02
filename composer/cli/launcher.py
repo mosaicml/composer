5fab5135665b05cfed3749de75a86ced8a3d86f1
jjanezhang <jane.zhang@databricks.com>
2024-02-02 15:58:42 -0800

    set platform to false for provided stdout and err

--- composer/cli/launcher.py
+++ composer/cli/launcher.py
@@ -487,7 +487,7 @@ def main():
         log_file_format = f'{os.environ.get(MOSAICML_LOG_DIR_ENV_VAR)}/gpu_{{rank}}.txt'
         if args.stderr is not None or args.stdout is not None:
             warnings.warn(
-                'Logging to Mosaic Platform. Ignoring provided stdout and stderr args. Set MOSAICML_PLATFORM=false to use provided stdout and stderr args.'
+                'Logging to Mosaic Platform. Ignoring provided stdout and stderr args. Set env var MOSAICML_PLATFORM=false to use provided stdout and stderr args.'
             )
         args.stdout = log_file_format
         args.stderr = None
