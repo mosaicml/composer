07a8becf237044dd8b54420c76e8d4faac419fa6
Cheng Li <pistasable@gmail.com>
2024-02-02 19:37:19 +0000

    update test

--- tests/callbacks/test_oom_observer.py
+++ tests/callbacks/test_oom_observer.py
@@ -12,10 +12,9 @@ from composer import State, Trainer
 from composer.callbacks import MemorySnapshot, OOMObserver
 from composer.loggers import LoggerDestination
 from composer.trainer import Trainer
-from tests.common import RandomClassificationDataset, SimpleModel, device
+from tests.common import RandomClassificationDataset, SimpleModel
 
 
-@device('cpu', 'gpu')
 @pytest.mark.skipif(version.parse(torch.__version__) < version.parse('2.1.0'),
                     reason='OOM Observer requires PyTorch 2.1 or higher')
 def test_oom_observer_warnings_on_cpu_models(device: str):
