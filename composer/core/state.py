487d222443d0f62015cb9ca75eb9cc3f5400779d
Mihir Patel <mihir.v.patel7@gmail.com>
2024-02-13 16:33:22 -0500

    add back w deprecation

--- composer/core/state.py
+++ composer/core/state.py
@@ -732,6 +732,12 @@ class State(Serializable):
         return self.fsdp_config is not None and self.fsdp_enabled and self.fsdp_state_dict_type == 'sharded'
 
     @property
+    def fsdp_elastic_sharded_enabled(self):
+        # Deprecation warnign
+        warnings.warn('state.fsdp_elastic_sharded_enabled is deprecated and will be removed v0.21.0')
+        return self.fsdp_sharded_state_dict_enabled
+
+    @property
     def fsdp_device_mesh(self):
         if self.fsdp_enabled:
             if not hasattr(self.model, 'model'):
