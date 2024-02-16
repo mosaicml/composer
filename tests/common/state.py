72353ed37093f145e0a815a1bb83dbd8f4534d15
root <23239305+b-chu@users.noreply.github.com>
2024-02-16 19:10:07 +0000

    Change random line

--- tests/common/state.py
+++ tests/common/state.py
@@ -22,7 +22,7 @@ def assert_state_equivalent(state1: State, state2: State):
     # Using a loose tolerance for GPU states as GPU determinism does not work properly
     is_gpu = next(state1.model.parameters()).device.type == 'cuda'
     atol = 0.1 if is_gpu else 0.0
-    rtol = 0.1 if is_gpu else 0.0
+    rtol = 0.1 if is_gpu else 1.0
 
     state_dict_1 = state1.state_dict()
     state_dict_2 = state2.state_dict()
