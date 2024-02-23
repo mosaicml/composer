3b6c1150b1b13b7e38c6954be9e3650396ccd367
Bruce Fontaine <bruce.fontaine@databricks.com>
2024-02-23 15:09:52 -0800

    formatting

--- composer/devices/device_neuron.py
+++ composer/devices/device_neuron.py
@@ -31,6 +31,7 @@ class DeviceNeuron(Device):
 
     def __init__(self):
         import torch_xla.core.xla_model as xm
+
         # Turn off compiler based mixed precision (we use torch's amp support)
         # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/general/appnotes/neuronx-cc/neuronx-cc-training-mixed-precision.html
         os.environ['NEURON_CC_FLAGS'] = '--auto-cast=none'
