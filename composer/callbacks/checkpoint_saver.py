0da0c964b48341e968e4ea7041f88d5ec7964076
relogu <lollonasi97@gmail.com>
2024-02-24 13:55:58 +0000

    correct order of return None

--- composer/callbacks/checkpoint_saver.py
+++ composer/callbacks/checkpoint_saver.py
@@ -388,14 +388,16 @@ class CheckpointSaver(Callback):  # noqa: D101
             weights_only=self.weights_only,
             ignore_keys=self.ignore_keys,
         )
+
+        if not saved_path:  # not all ranks save
+            return
+
         log.debug(f'Checkpoint locally saved to {saved_path}')
+
         # Add old checkpoints when the run is resumed with the same path
         for file in pathlib.Path(saved_path).parent.iterdir():
             if saved_path not in str(file) and file not in self.saved_checkpoints and not file.is_symlink():  
                 self.saved_checkpoints.append(file)
-
-        if not saved_path:  # not all ranks save
-            return
 
         metadata_local_file_path = None
         if dist.get_global_rank() == 0 and state.fsdp_elastic_sharded_enabled:
