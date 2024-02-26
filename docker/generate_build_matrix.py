f103011dbb775b5ab269f4445520a7101b71af47
Mihir Patel <mihir.v.patel7@gmail.com>
2024-02-26 17:10:43 -0500

    fix torchvision

--- docker/generate_build_matrix.py
+++ docker/generate_build_matrix.py
@@ -24,7 +24,7 @@ PRODUCTION_PYTORCH_VERSION = '2.1.2'
 
 def _get_torchvision_version(pytorch_version: str):
     if pytorch_version == '2.2.1':
-        return '0.17.0'
+        return '0.17.1'
     if pytorch_version == '2.1.2':
         return '0.16.2'
     if pytorch_version == '2.0.1':
