85795fd121f01eb40acbd92b4f6e1f1d138473fb
Sebastien Ehrhardt <sebastien.ehrhardt@helsing.ai>
2024-03-11 18:38:00 +0000

    metrics on CPU for MPS device

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -2204,12 +2204,19 @@ class Trainer:
         self.engine.close()
         dist.barrier()
 
-    def _ensure_metrics_device_and_dtype(self, metrics: Dict[str, Metric]):
+    def _ensure_metrics_device_and_dtype(
+        self, metrics: Dict[str, Metric], ensure_cpu: bool = False
+    ):
         for name, metric in metrics.items():
             # Safety check to ensure the metric and data are on the same device. Normally not
             # needed because the metric is automatically on the same device as the model.
             # See https://torchmetrics.readthedocs.io/en/latest/pages/overview.html for details.
-            metrics[name] = self.state.device.module_to_device(metric)
+
+            # Force all metrics to go on the CPU
+            if ensure_cpu:
+                metrics[name] = DeviceCPU().module_to_device(metric)
+            else:
+                metrics[name] = self.state.device.module_to_device(metric)
             if is_model_deepspeed(self.state.model):
                 # HACK: DeepSpeed somehow manages to convert metric internal states to its own dtype. When
                 # running with FP16, this tends to result in overflows. Let's assume FP32 is good enough.
@@ -3202,7 +3209,10 @@ class Trainer:
 
             self.engine.run_event(Event.EVAL_START)
 
-            metrics = self._ensure_metrics_device_and_dtype(metrics)
+            # On MPS device we ensure the eval metrics are computed on CPU to avoid numerical errors
+            metrics = self._ensure_metrics_device_and_dtype(
+                metrics, ensure_cpu=isinstance(self.state.device, DeviceMPS)
+            )
 
             for metric in metrics.values():
                 metric.reset()
