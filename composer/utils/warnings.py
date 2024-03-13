1600f8b65fd1b1a1edd485d264b88123de2d65a0
Irene Dea <deaairene@gmail.com>
2024-03-13 15:39:51 -0700

    maybe fix docstring

--- composer/utils/warnings.py
+++ composer/utils/warnings.py
@@ -12,19 +12,6 @@ class VersionedDeprecationWarning(DeprecationWarning):
     Attributes:
         message (str): The deprecation message describing why the feature is deprecated.
         remove_version (str): The version in which the feature will be removed.
-
-    Example:
-        >>> import warnings
-        >>> def deprecated_function():
-        ...     warnings.warn(
-        ...         VersionedDeprecationWarning(
-        ...             "Function XYZ is deprecated.",
-        ...             remove_version="2.0.0",
-        ...         )
-        ...     )
-        ...
-        >>> deprecated_function()
-        DeprecationWarning: Function XYZ is deprecated. It will be removed in version 2.0.0.
     """
 
     def __init__(self, message: str, remove_version: str) -> None:
