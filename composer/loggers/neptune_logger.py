e847c8afd0f713db3c82cbb5376b7f4943adfb8e
Matt Broadway <mattdbway@gmail.com>
2024-03-19 23:20:14 +0000

    remove type ignore
    Co-authored-by: Mihir Patel <mihir.v.patel7@gmail.com>

--- composer/loggers/neptune_logger.py
+++ composer/loggers/neptune_logger.py
@@ -122,7 +122,7 @@ class NeptuneLogger(LoggerDestination):
             self._neptune_run = Run(
                 project=self._project,
                 api_token=self._api_token,
-                mode=self._mode,  # type: ignore
+                mode=self._mode,
                 **self._neptune_kwargs,
             )
         return self._neptune_run
