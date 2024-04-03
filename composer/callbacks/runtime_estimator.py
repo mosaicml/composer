049c3d658433c0ff97a713eaa1a64ae5d89e26d1
Anna Pfohl <anna.pfohl@databricks.com>
2024-04-03 09:10:49 -0700

    Add benchmark option to runtime estimator

--- composer/callbacks/runtime_estimator.py
+++ composer/callbacks/runtime_estimator.py
@@ -57,9 +57,22 @@ class RuntimeEstimator(Callback):
             warming, and other reasons. Defaults to 1.
         time_unit (str, optional): Time unit to use for `time` logging. Can be one of
             'seconds', 'minutes', 'hours', or 'days'. Defaults to 'hours'.
+        benchmark (bool, optional): If True, the callback will log 4 additional static keys:
+    +-----------------------------------+----------------------------------------------------------------+
+    | Key                               | Logged data                                                    |
+    +===================================+================================================================+
+    | `time/estimate_0`                 | First estimated time to completion logged                      |
+    +-----------------------------------+----------------------------------------------------------------+
+    | `time/estimate_25`                | Estimated time to completion at 25% of total duration          |
+    +-----------------------------------+----------------------------------------------------------------+
+    | `time/estimate_50`                | Estimated time to completion at 50% of total duration          |
+    +-----------------------------------+----------------------------------------------------------------+
+    | `time/estimate_75`                | Estimated time to completion at 75% of total duration          |
+    +-----------------------------------+----------------------------------------------------------------+
+        These can be used to track how the estimate changes over time. Defaults to True.
     """
 
-    def __init__(self, skip_batches: int = 1, time_unit: str = 'hours') -> None:
+    def __init__(self, skip_batches: int = 1, time_unit: str = 'hours', benchmark: bool = True) -> None:
         self._enabled = True
         self.batches_left_to_skip = skip_batches
         self.start_time = None
@@ -87,6 +100,11 @@ class RuntimeEstimator(Callback):
         # How often eval is called as fraction of total training time
         self.eval_frequency_per_label: Dict[str, float] = {}
         self.last_elapsed_fraction: float = 0.0
+
+        if benchmark:
+            self.benchmarks = {}
+        else:
+            self.benchmarks = None
 
     def _get_elapsed_duration(self, state: State) -> Optional[float]:
         """Get the elapsed duration.
@@ -124,6 +142,25 @@ class RuntimeEstimator(Callback):
             if state.dataloader_len is not None:
                 self.train_dataloader_len = state.dataloader_len.value
 
+    def log_benchmarks(self, estimate: float, state: State, logger: Logger):
+        if not self.benchmarks:
+            self.benchmarks['estimate_0'] = estimate
+            logger.log_metrics({'time/estimate_0': estimate})
+
+        elapsed = state.get_elapsed_duration()
+        if elapsed is None:
+            return
+
+        for percent in [25, 50, 75]:
+            key = f'time/estimate_{percent}'
+
+            if elapsed.value < percent / 100:
+                return
+
+            if key not in self.benchmarks:
+                self.benchmarks[f'estimate_{percent}'] = estimate
+                logger.log_metrics({f'time/estimate_{percent}': estimate})
+
     def batch_end(self, state: State, logger: Logger) -> None:
         if not self._enabled:
             return
@@ -157,7 +194,10 @@ class RuntimeEstimator(Callback):
                 remaining_calls = num_total_evals - num_evals_finished
                 remaining_time += eval_wct_avg * remaining_calls
 
-            logger.log_metrics({'time/remaining_estimate': remaining_time / self.divider})
+            estimate = remaining_time / self.divider
+            logger.log_metrics({'time/remaining_estimate': estimate})
+            if self.benchmarks is not None:
+                self.log_benchmarks(estimate, state, logger)
 
     def eval_end(self, state: State, logger: Logger) -> None:
         # If eval is called before training starts, ignore it
