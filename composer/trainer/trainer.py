64728167cc4cbecb27e7719c2a392930037a2aa6
Saaketh <narayan.saaketh@gmail.com>
2024-04-04 14:44:49 -0400

    disable closures for bf16

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -3497,7 +3497,7 @@ class Trainer:
         if self.state.device.dist_backend == 'xla':
             return False
 
-        if self.state.precision != Precision.AMP_FP16:
+        if self.state.precision != Precision.AMP_FP16 and self.state.precision != Precision.AMP_BF16:
             return True
 
         if not hasattr(self.state, 'optimizers'):
