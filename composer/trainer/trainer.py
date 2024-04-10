52cf3ad9943302d2d79dbe59fec8f2f13f07f89e
Shashank Rajput <shashank.rajput@databricks.com>
2024-04-10 19:18:59 +0000

    refactoring the code slightly

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -2257,18 +2257,22 @@ class Trainer:
 
         # log computed metrics
         computed_metrics = {}
+        table_metrics = {}
         for metric_name, metric in metrics.items():
             metric_value = metric.compute()
             if isinstance(metric_value, dict) and metric_value.get('log_as_table', False):
-                for k, v in metric_value.items():
-                    if k != 'log_as_table':
-                        self.logger.log_table(
-                            columns=['context_length', k],
-                            rows=[[i, b] for (i, b) in enumerate(v.tolist())],
-                            name=
-                            f'metrics/{dataloader_label}/{metric_name}/{k}/{self.logger._state.timestamp.batch.value}')
+                table_metrics[metric_name] = metric_value
             else:
                 computed_metrics[metric_name] = metric_value
+
+        for metric_name, metric_value in table_metrics.items():
+            for k, v in metric_value.items():
+                if k != 'log_as_table':
+                    self.logger.log_table(
+                        columns=['context_length', k],
+                        rows=[[i, b] for (i, b) in enumerate(v.tolist())],
+                        name=f'metrics/{dataloader_label}/{metric_name}/{k}/{self.logger._state.timestamp.batch.value}'
+                    )
 
         self.logger.log_metrics({f'metrics/{dataloader_label}/{name}': val for (name, val) in computed_metrics.items()
                                 },)
