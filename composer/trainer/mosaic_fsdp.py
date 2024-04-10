52776a78622101dd95b3692fe9b29cde7811cff1
bigning <ning.wang@databricks.com>
2024-04-10 13:17:42 -0700

    Fix the FSDP.optim_state_dict_to_load OOM (#3184)
    * up
    
    * up
    
    * up
    
    * a
    
    * a
    
    * up
    
    * up
    
    * comments
    
    * up
    
    * lint
    
    * line

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -63,7 +63,13 @@ def patch_pytorch():
 
     elif version.parse(torch.__version__) < version.parse('2.2.9'):
         # Monkey patch for torch < 2.3.0 ie torch == 2.2.1/2.2.2 currently
-        pass
+
+        # Fix memory leak for FSDP.optim_state_dict_to_load
+        # https://github.com/pytorch/pytorch/issues/116553
+        from torch.distributed.fsdp import _optim_utils
+
+        from composer.trainer.mosaic_fsdp_utils import _shard_orig_param_state
+        _optim_utils._shard_orig_param_state = _shard_orig_param_state
 
     elif version.parse(torch.__version__) < version.parse('2.3.1'):
         # Monkey patch for torch < 2.3.1 ie torch == 2.3.0
