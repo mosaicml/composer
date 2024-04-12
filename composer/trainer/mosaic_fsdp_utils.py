a19bb0c09d6e563a0af93a271e51213fc42fd0e1
Ning Wang <ning.wang@databricks.com>
2024-04-12 16:55:00 -0700

    a

--- composer/trainer/mosaic_fsdp_utils.py
+++ composer/trainer/mosaic_fsdp_utils.py
@@ -1382,133 +1382,104 @@ if version.parse(torch.__version__) > version.parse('2.2.9') and version.parse(
         local_tensor: torch.Tensor,
         current_spec: DTensorSpec,
         target_spec: DTensorSpec,
-        *,
-        async_op: bool = False,
         is_backward: bool = False,
     ) -> torch.Tensor:
-        """
-        This redistribute the local tensor (torch.Tensor) from the current DTensorSpec to
-        the target DTensorSpec, which involves the necessary collective calls to transform
-        the local shard of the DTensor from its current spec to the target spec.
-        """
-
         if current_spec.mesh != target_spec.mesh:
             # TODO: alltoall/permute reshuffling to change device_mesh if they are not the same
             raise NotImplementedError("Cross device mesh comm not supported yet!")
 
         new_local_tensor = None
+
+        current_placements = current_spec.placements
+        target_placements = target_spec.placements
+        sorted_placements = list(enumerate(zip(current_placements, target_placements)))
+        sorted_placements = _decompose_reshard(sorted_placements)
+        sorted_placements.sort(key=_replicate_then_shard)
+
         device_mesh = current_spec.mesh
 
-        my_coordinate = device_mesh.get_coordinate()
-
-        if my_coordinate is None:
-            # if rank is not part of mesh, we skip redistribute and simply return local_tensor,
-            # which should be an empty tensor
-            return local_tensor
-
-        transform_infos = _gen_transform_infos(current_spec, target_spec)
-
-        for transform_info in transform_infos:
-            i = transform_info.mesh_dim
-            current, target = transform_info.src_dst_placements
+        for i, (current, target) in sorted_placements:
+            my_coordinate = device_mesh.get_coordinate()
             num_chunks = device_mesh.size(mesh_dim=i)
-            print(f"bigning debug redistribute {target.is_replicate()=}, {current.is_shard()=}, {target.is_shard()=}, {current.is_replicate()=}")
+
+            if my_coordinate is None:
+                # if rank is not part of mesh, we simply return local_tensor,
+                # which should be an empty tensor
+                return local_tensor
 
             if current == target:
                 # short cut, just use the original local tensor
                 new_local_tensor = local_tensor
                 continue
+            print(f"bigning debug redistribute tensor {target.is_replicate()=}, {target.is_shard()=}, {current.is_shard()=}, {current.is_partial()=}")
 
             if target.is_replicate():
                 # Case 1: target is Replicate
                 if current.is_partial():
                     partial_spec = cast(_Partial, current)
-                    new_local_tensor = partial_spec._reduce_value(
+                    new_local_tensor = partial_spec._to_replicate(
                         local_tensor, device_mesh, i
                     )
                 elif current.is_shard():
                     current_placement = cast(Shard, current)
                     current_placement._to_replicate_tensor = _to_replicate_tensor
-                    print(f"bigning debug replace to_replicate_tensor")
+                    print(f"bigning debug using my to_replicate_tensor") 
                     new_local_tensor = current_placement._to_replicate_tensor(
-                        local_tensor, device_mesh, i, transform_info.logical_shape
+                        local_tensor, current_spec.shape, device_mesh, i
                     )
                 else:
                     raise RuntimeError(
-                        f"redistribute from {current} to {target} not supported yet"
+                        f"redistribute from {current_placements} to {target_placements} not supported yet"
                     )
             elif target.is_shard():
                 # Case 2: target is Shard
                 target_placement = cast(Shard, target)
-                target_dim = target_placement.dim
                 if current.is_partial():
                     partial_spec = cast(_Partial, current)
-                    new_local_tensor = partial_spec._reduce_shard_value(
+                    new_local_tensor = partial_spec._to_shard(
                         local_tensor, device_mesh, i, target_placement
                     )
                 elif current.is_replicate():
                     # split the tensor and return the corresponding cloned local shard
-                    new_local_tensor = target_placement._replicate_to_shard(
-                        local_tensor, device_mesh, i, my_coordinate[i]
+                    shards, _ = target_placement._split_tensor(
+                        local_tensor,
+                        num_chunks,
+                        with_padding=False,
+                        contiguous=False,
                     )
+                    new_local_tensor = shards[my_coordinate[i]].clone()
                 else:
-                    # NOTE: we don't support this case efficiently yet, the fallback path we are going here is
-                    # to decompose Shard(0) -> Shard(1) into Shard(0) -> Replicate -> Shard(1)
-                    # TODO: enable this with all_to_all
+                    # NOTE: this case shouldn't hit _decompose_sharding, decompose sharding should
+                    # decompose Shard(0) -> Shard(1) into Shard(0) -> Replicate -> Shard(1)
                     assert (
                         current.is_shard()
                     ), f"Current placement should be shard but found {current}"
                     shard_spec = cast(Shard, current)
                     if shard_spec.dim != target_placement.dim:
-                        new_local_tensor = shard_spec._to_replicate_tensor(
-                            local_tensor, device_mesh, i, transform_info.logical_shape
+                        # TODO: enable this with all_to_all
+                        raise NotImplementedError(
+                            "Changing sharding dim is not supported yet!"
                         )
-                        shards, _ = target_placement._split_tensor(
-                            new_local_tensor,
-                            num_chunks,
-                            with_padding=False,
-                            contiguous=False,
-                        )
-                        new_local_tensor = shards[my_coordinate[i]]
+
             elif target.is_partial():
                 if current.is_replicate():
-                    partial_spec = cast(_Partial, target)
-                    # skip the replicate to partial transformation when we are in backward pass
-                    # In this case we keep the grad as replicate, this is because we don't
-                    # want to convert the replicated gradients back to partial, although
-                    # that's logically conform with the same layout, converting the gradients
-                    # back to partial is actually useless as you would have to do reduce later
-                    # which would be more expensive than keeping it replicate! For this reason,
-                    # we keep the replicate grad here.
+                    # For replicate -> partial forward pass we perform division to num of chunks
+                    # and generate parial, and recover it back when pending sum get cleared.
+                    # Skip/pass through when replicate -> partial is in backward pass.
                     new_local_tensor = (
-                        partial_spec._partition_value(local_tensor, device_mesh, i)
-                        if not is_backward
-                        else local_tensor
-                    )
-                elif current.is_shard():
-                    if not is_backward:
-                        raise RuntimeError(
-                            f"redistribute from {current} to {target} not supported yet"
-                        )
-                    # for backward shard -> partial, we just need to convert the shard to replicate
-                    current_placement = cast(Shard, current)
-                    new_local_tensor = current_placement._to_replicate_tensor(
-                        local_tensor, device_mesh, i, transform_info.logical_shape
+                        local_tensor / num_chunks if not is_backward else local_tensor
                     )
                 else:
-                    # partial -> partial no op, should never hit
-                    new_local_tensor = local_tensor
+                    raise RuntimeError(
+                        f"redistribute from {current_placements} to {target_placements} not supported yet"
+                    )
 
             assert new_local_tensor is not None
             local_tensor = new_local_tensor
 
         assert new_local_tensor is not None, "redistribute failed!"
 
-        if not async_op and isinstance(new_local_tensor, funcol.AsyncCollectiveTensor):
-            new_local_tensor = new_local_tensor.wait()
-
-        return new_local_tensor
-        
+        return new_local_tensor 
     from torch.distributed._tensor import redistribute
     print(f"bigning debug replace redistribute tensor")
     redistribute.redistribute_local_tensor = redistribute_local_tensor
