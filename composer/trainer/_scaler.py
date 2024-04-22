97dcc428cd252ea5d387adca09170b1040cf3b9f
Saaketh <narayan.saaketh@gmail.com>
2024-04-21 21:20:07 -0700

    torch 240 compatible

--- composer/trainer/_scaler.py
+++ composer/trainer/_scaler.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 from typing import Optional, Union
 
 import torch
-from torch.cuda.amp.grad_scaler import GradScaler, OptState, _refresh_per_optimizer_state
+from torch.cuda.amp.grad_scaler import GradScaler, OptState
+from torch.amp.grad_scaler import _refresh_per_optimizer_state
 from torch.optim import Optimizer
 
 from composer.utils import dist
