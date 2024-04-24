6a50ed1c10a4939a09e7f2f798db6d1ccf912e31
Saaketh <narayan.saaketh@gmail.com>
2024-04-24 15:26:49 -0700

    patch fqns

--- composer/trainer/mosaic_fsdp_utils.py
+++ composer/trainer/mosaic_fsdp_utils.py
@@ -955,8 +955,9 @@ if version.parse(torch.__version__) > version.parse('2.2.9') and version.parse(
         skip_ddp_prefix: bool = True,
         skip_compiler_prefix: bool = True,
     ) -> FQNS_T:
-        """
-        This API is used to convert the name of a parameter to the FQNs. For FSDP
+        """Used to convert the name of a parameter to the FQNs.
+
+        For FSDP
         without `use_orig_params`, the name of FlatParameter can be mapped to
         multiple original parameters. As a result, the return type of this function
         is `Set[str]`.
@@ -976,32 +977,32 @@ if version.parse(torch.__version__) > version.parse('2.2.9') and version.parse(
         from torch.distributed.fsdp._common_utils import FSDP_WRAPPED_MODULE
 
         # Remove the checkpoint prefix, if it exists.
-        name = name.replace(_CHECKPOINT_PREFIX, "")
-        if "." not in name:
+        name = name.replace(_CHECKPOINT_PREFIX, '')
+        if '.' not in name:
             return {name}
 
-        obj_names = name.split(".")
+        obj_names = name.split('.')
         fqn_obj_names = []
         curr_obj = model
         for i, curr_obj_name in enumerate(obj_names):
             if isinstance(curr_obj, DDP):
-                assert curr_obj_name == "module"
+                assert curr_obj_name == 'module'
                 curr_obj = curr_obj.module
                 if not skip_ddp_prefix:
                     fqn_obj_names.append(curr_obj_name)
             elif isinstance(curr_obj, FSDP):
                 if i < len(obj_names) - 1 and obj_names[i + 1] == FLAT_PARAM:
-                    prefix = ".".join(fqn_obj_names)
+                    prefix = '.'.join(fqn_obj_names)
                     flat_param = getattr(curr_obj, FLAT_PARAM)
                     if prefix:
-                        prefix = f"{prefix}."
-                    return {f"{prefix}{fqn}" for fqn in flat_param._fqns}
+                        prefix = f'{prefix}.'
+                    return {f'{prefix}{fqn}' for fqn in flat_param._fqns}
                 curr_obj = getattr(curr_obj, FSDP_WRAPPED_MODULE)
                 if curr_obj_name != FSDP_WRAPPED_MODULE:
                     fqn_obj_names.append(curr_obj_name)
                     curr_obj = getattr(curr_obj, curr_obj_name)
             elif isinstance(curr_obj, torch._dynamo.eval_frame.OptimizedModule):
-                assert curr_obj_name == "_orig_mod"
+                assert curr_obj_name == '_orig_mod'
                 curr_obj = curr_obj._orig_mod
                 if not skip_compiler_prefix:
                     fqn_obj_names.append(curr_obj_name)
@@ -1009,7 +1010,7 @@ if version.parse(torch.__version__) > version.parse('2.2.9') and version.parse(
                 fqn_obj_names.append(curr_obj_name)
                 curr_obj = getattr(curr_obj, curr_obj_name)
 
-        return {".".join(fqn_obj_names).replace(_CHECKPOINT_PREFIX, "")}
+        return {'.'.join(fqn_obj_names).replace(_CHECKPOINT_PREFIX, '')}
 
     def _verify_options_t2p3p0(
         model: nn.Module,
