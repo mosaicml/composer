2255209c24bfd51b41f24dea156abca653f6202e
Mihir Patel <mihir.v.patel7@gmail.com>
2024-04-30 15:48:07 -0400

    fix daily test (#3233)

--- tests/utils/object_store/test_gs_object_store.py
+++ tests/utils/object_store/test_gs_object_store.py
@@ -34,7 +34,7 @@ def test_gs_object_store_integration_json_auth(expected_use_gcs_sdk_val=True, cl
         save_folder='gs://mosaicml-internal-integration-testing/checkpoints/{run_name}',
         save_filename='test-model.pt',
         max_duration='1ba',
-        precision='bf16',
+        precision='amp_bf16',
     )
     run_name = trainer_save.state.run_name
     gcs_os = get_gcs_os_from_trainer(trainer_save)
@@ -51,7 +51,7 @@ def test_gs_object_store_integration_json_auth(expected_use_gcs_sdk_val=True, cl
         train_dataloader=train_dataloader,
         load_path=f'gs://mosaicml-internal-integration-testing/checkpoints/{run_name}/test-model.pt',
         max_duration='2ba',
-        precision='bf16',
+        precision='amp_bf16',
     )
     trainer_load.fit()
     trainer_load.close()
