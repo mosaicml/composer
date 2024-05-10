602a1626d4a25a0b99b290b7311dbbfbc5984c28
Ning Wang <ning.wang@databricks.com>
2024-05-10 11:45:41 -0700

    lint

--- composer/trainer/mosaic_fsdp_utils.py
+++ composer/trainer/mosaic_fsdp_utils.py
@@ -732,7 +732,7 @@ if version.parse(torch.__version__) >= version.parse('2.3.0') and version.parse(
     from torch.distributed.checkpoint.planner import SavePlan, WriteItem
     from torch.distributed.checkpoint.metadata import MetadataIndex, Metadata
 
-    def dedup_save_plans(all_plans: List[SavePlan]) -> List[SavePlan]:
+    def dedup_save_plans(all_plans: List[SavePlan]) -> List[SavePlan]:  # noqa: D103
         write_item_to_plan_indices: Dict[MetadataIndex, Set[int]] = defaultdict(set)
         write_item_idx_to_write_item: Dict[MetadataIndex, WriteItem] = {}
         for plan_idx, plan in enumerate(all_plans):
