f700f90db44e7690f0a6fc3588f24dd0d295c671
Jesse Chan <jesse.chan@databricks.com>
2024-05-14 14:26:24 -0700

    log image fix
    Signed-off-by: Jesse Chan <jesse.chan@databricks.com>

--- composer/loggers/mlflow_logger.py
+++ composer/loggers/mlflow_logger.py
@@ -491,8 +491,9 @@ class MLFlowLogger(LoggerDestination):
                 assert isinstance(self._run_id, str)
                 self._mlflow_client.log_image(
                     image=image,
-                    artifact_file=f'{name}_{step}_{im_ind}.png',
+                    key=f'{name}_{step}_{im_ind}',
                     run_id=self._run_id,
+                    step=step,
                 )
 
     def post_close(self):
