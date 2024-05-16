1975a5679057c24822a5bdc6a5e1802e898fdbcd
Evan Racah <ejracah@gmail.com>
2024-05-16 00:33:39 +0000

    pc

--- composer/checkpoint/__init__.py
+++ composer/checkpoint/__init__.py
@@ -1,5 +1,6 @@
 # Copyright 2024 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
+
 """Module for checkpointing API."""
 
 from composer.checkpoint.state_dict import get_model_state_dict
