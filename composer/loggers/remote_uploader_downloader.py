552958c312672a970350359238a0e43564fb1a80
Irene Dea <deaairene@gmail.com>
2024-05-20 10:54:54 -0700

    Allow overwrite on upload retry in remote uploader downloader (#3310)

--- composer/loggers/remote_uploader_downloader.py
+++ composer/loggers/remote_uploader_downloader.py
@@ -673,8 +673,8 @@ def _upload_worker(
 
         # defining as a function-in-function to use decorator notation with num_attempts as an argument
         @retry(ObjectStoreTransientError, num_attempts=num_attempts)
-        def upload_file():
-            if not overwrite:
+        def upload_file(retry_index: int = 0):
+            if retry_index == 0 and not overwrite:
                 try:
                     remote_backend.get_object_size(remote_file_name)
                 except FileNotFoundError:
