deed464ffb54622e6ea8c005fd0a2016d46ebb20
Jesse Chan <jesse.chan@databricks.com>
2024-05-22 15:57:38 -0400

    fixed log image tests
    Signed-off-by: Jesse Chan <jesse.chan@databricks.com>

--- tests/loggers/test_mlflow_logger.py
+++ tests/loggers/test_mlflow_logger.py
@@ -650,8 +650,9 @@ def test_mlflow_log_image_works(tmp_path, device):
     experiment_id = run.info.experiment_id
 
     run_file_path = mlflow_uri / Path(experiment_id) / Path(run_id)
-    im_dir = run_file_path / Path('artifacts')
-    assert len(os.listdir(im_dir)) == expected_num_ims
+    im_dir = run_file_path / Path('artifacts') / Path('images')
+    # 2 (compressed & uncompressed) per image, and two log images calls in ImageLogger
+    assert len(os.listdir(im_dir)) == expected_num_ims * 2 * 2 
 
 
 @device('cpu')
