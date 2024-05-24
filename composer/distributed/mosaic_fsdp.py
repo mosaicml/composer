a24916693b009a03b3ddebcc24e8c10ad328a63e
Your Name <you@example.com>
2024-05-24 15:02:21 +0000

    fix lint

--- composer/distributed/mosaic_fsdp.py
+++ composer/distributed/mosaic_fsdp.py
@@ -214,9 +214,9 @@ def _get_process_group(pg, process_group_cache=None):
     (
         current_group,
         _subgroups,
-    ) = distributed.distributed_c10d.new_subgroups_by_enumeration(
+    ) = distributed.distributed_c10d.new_subgroups_by_enumeration(  # type: ignore[reportGeneralTypeIssues]
         ranks_per_subgroup_list
-    )  # type: ignore reportGeneralTypeIssues
+    ) 
 
     if process_group_cache is not None:
         process_group_cache[ranks] = current_group
