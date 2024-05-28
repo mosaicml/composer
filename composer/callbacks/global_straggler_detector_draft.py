424b2bd3cdaa19b4e7fe0d3939b1cc249bdd08d2
JackZ-db <jack.zhang@databricks.com>
2024-05-28 11:08:54 -0700

    changed import statement to account for the fact that its an extra dependency

--- composer/callbacks/global_straggler_detector_draft.py
+++ composer/callbacks/global_straggler_detector_draft.py
@@ -1,5 +1,7 @@
 #from megatron_core.megatron.core.utils import StragglerDetector
-from megatron_core.megatron.core.utils import *
+#from megatron_core.megatron.core.utils import *
+from mosaicml.composer.megatron import *
+
 
 from composer.core import Callback, State, Event, Time
 from composer.utils import dist
