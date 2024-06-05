9c019fd69ef4eb5740bbeda343f332d9801ad524
Ning Wang <ning.wang@databricks.com>
2024-06-05 16:17:22 -0700

    a

--- make_test_ckpt.py
+++ make_test_ckpt.py
@@ -1,6 +1,5 @@
 from composer.models import ComposerClassifier
 import torch
-from utils import RandomClassificationDataset, SimpleMLP
 import itertools
 from composer.trainer import Trainer
 from torch.utils.data import DataLoader
@@ -11,6 +10,66 @@ from torchmetrics import Metric, MetricCollection
 from torchmetrics.classification import MulticlassAccuracy, MulticlassAveragePrecision, MulticlassROC
 
 s3_bucket = 'mosaicml-internal-checkpoints-test' #
+
+import torch
+from torch.utils.data import Dataset
+from composer.models import ComposerClassifier
+import contextlib
+import os
+import tempfile
+from pathlib import Path
+
+import torch
+from composer.core import Callback, State
+from composer.core.state import fsdp_state_dict_type_context, fsdp_get_optim_state_dict
+from composer.loggers import Logger
+from composer.loggers.remote_uploader_downloader import RemoteUploaderDownloader
+from composer.utils import (dist, format_name_with_dist_and_time, parse_uri,
+                            reproducibility)
+
+class RandomClassificationDataset(Dataset):
+    """Classification dataset drawn from a normal distribution.
+
+    Args:
+        shape (Sequence[int]): shape of features (default: (1, 1, 1))
+        size (int): number of samples (default: 100)
+        num_classes (int): number of classes (default: 2)
+    """
+
+    def __init__(self, shape = (1, 1, 1), size: int = 100, num_classes: int = 2):
+        self.size = size
+        self.shape = shape
+        self.num_classes = num_classes
+        self.x = None
+        self.y = None
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, index: int):
+        # Note: lazily generate data so it runs after Composer seeds everything, giving the same
+        # dataset across multiple calls when using the same seed.
+        if self.x is None:
+            self.x = torch.randn(self.size, *self.shape)
+        if self.y is None:
+            self.y = torch.randint(0, self.num_classes, size=(self.size,))
+        return self.x[index], self.y[index]
+
+
+
+class SimpleMLP(ComposerClassifier):
+    def __init__(self, num_features: int, num_classes: int = 4, 
+                 train_metrics = None,
+                 val_metrics = None):
+        net = torch.nn.Sequential(
+            torch.nn.Linear(num_features, num_features, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(num_features, num_classes, bias=False),
+        )
+        super().__init__(module=net, num_classes=num_classes,
+                         train_metrics=train_metrics, val_metrics=val_metrics)
+
+
 if __name__ == "__main__":
 
     (
