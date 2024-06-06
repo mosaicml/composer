3a6c185fbe0657fe7e57285aeab8487def9d7f6a
Evan Racah <ejracah@gmail.com>
2024-06-06 23:37:49 +0000

    add cpu sharded test

--- tests/checkpoint/test_save.py
+++ tests/checkpoint/test_save.py
@@ -58,11 +58,12 @@ def test_save_full_state_dict_to_disk_cpu(world_size: int, tmp_path: str, sharde
 
 @world_size(2)
 @pytest.mark.gpu
-def test_save_sharded_state_dict_to_disk(world_size: int, tmp_path: str):
+@pytest.mark.parametrize('tensor_type', ['sharded_tensor', 'dtensor'])
+def test_save_sharded_state_dict_to_disk(world_size: int, tmp_path: str, tensor_type: str):
     destination_file_path = os.path.join(tmp_path, str(uuid.uuid4())[:8])
     # Sync the path across all ranks
     destination_file_path = dist.all_gather_object(destination_file_path)[0]
-    model, _ = _init_model(use_fsdp=True, device='cuda')
+    model, _ = _init_model(use_fsdp=True, device='cuda', tensor_type=tensor_type)
 
     state_dict = get_model_state_dict(model, sharded_state_dict=True)
     loaded_in_state_dict = deepcopy(state_dict)
@@ -76,6 +77,19 @@ def test_save_sharded_state_dict_to_disk(world_size: int, tmp_path: str):
              checkpoint_id=load_path)
     deep_compare(state_dict, loaded_in_state_dict)
 
-# @world_size(2, 4)
-def test_save_sharded_state_dict_to_disk_cpu(world_size: int, tmp_path: str, use_cpu: bool):
-    pass
+@pytest.mark.filterwarnings('ignore:The passed') # Torch issues a warning for wrapping a CPU model in FSDP
+@world_size(2)
+def test_save_sharded_state_dict_to_disk_cpu(world_size: int, tmp_path: str):
+    destination_file_path = os.path.join(tmp_path, str(uuid.uuid4())[:8])
+    destination_file_path = dist.all_gather_object(destination_file_path)[0]
+    model, _ = _init_model(use_fsdp=True, device='cpu', sync_module_states=False, cpu_offload=True)
+    state_dict = get_model_state_dict(model, sharded_state_dict=True)
+    loaded_in_state_dict = deepcopy(state_dict)
+    path_saved = save_state_dict_to_disk(state_dict, destination_file_path=destination_file_path,
+                                         overwrite=True)
+    assert path_saved == f"{destination_file_path}/{_TORCH_DISTRIBUTED_CHECKPOINTS_FILENAME}"
+
+    load_path = str(Path(path_saved).parent)
+    DCP.load(state_dict=loaded_in_state_dict,
+             checkpoint_id=load_path)
+    deep_compare(state_dict, loaded_in_state_dict)
