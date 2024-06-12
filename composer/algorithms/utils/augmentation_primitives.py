329df18965722fdff02aca04c218a1be6910459e
Milo Cress <milo.cress@databricks.com>
2024-06-12 21:44:16 +0000

    versioning yada yada yada

--- composer/algorithms/utils/augmentation_primitives.py
+++ composer/algorithms/utils/augmentation_primitives.py
@@ -183,7 +183,7 @@ def shear_x(pil_img: Image.Image, level: float):
     level = _float_parameter(_sample_level(level), 0.3)
     if np.random.uniform() > 0.5:
         level = -level
-    return pil_img.transform(pil_img.size, Image.AFFINE, (1, level, 0, 0, 1, 0), resample=Image.BILINEAR)
+    return pil_img.transform(pil_img.size, Image.Transform.AFFINE, (1, level, 0, 0, 1, 0), resample=Image.Resampling.BILINEAR)
 
 
 def shear_y(pil_img: Image.Image, level: float):
@@ -197,7 +197,7 @@ def shear_y(pil_img: Image.Image, level: float):
     level = _float_parameter(_sample_level(level), 0.3)
     if np.random.uniform() > 0.5:
         level = -level
-    return pil_img.transform(pil_img.size, Image.AFFINE, (1, 0, 0, level, 1, 0), resample=Image.BILINEAR)
+    return pil_img.transform(pil_img.size, Image.Transform.AFFINE, (1, 0, 0, level, 1, 0), resample=Image.Resampling.BILINEAR)
 
 
 def translate_x(pil_img: Image.Image, level: float):
@@ -211,7 +211,7 @@ def translate_x(pil_img: Image.Image, level: float):
     level = _int_parameter(_sample_level(level), pil_img.size[0] / 3)
     if np.random.random() > 0.5:
         level = -level
-    return pil_img.transform(pil_img.size, Image.AFFINE, (1, 0, level, 0, 1, 0), resample=Image.BILINEAR)
+    return pil_img.transform(pil_img.size, Image.Transform.AFFINE, (1, 0, level, 0, 1, 0), resample=Image.Resampling.BILINEAR)
 
 
 def translate_y(pil_img: Image.Image, level: float):
@@ -225,7 +225,7 @@ def translate_y(pil_img: Image.Image, level: float):
     level = _int_parameter(_sample_level(level), pil_img.size[1] / 3)
     if np.random.random() > 0.5:
         level = -level
-    return pil_img.transform(pil_img.size, Image.AFFINE, (1, 0, 0, 0, 1, level), resample=Image.BILINEAR)
+    return pil_img.transform(pil_img.size, Image.Transform.AFFINE, (1, 0, 0, 0, 1, level), resample=Image.Resampling.BILINEAR)
 
 
 # The following augmentations overlap with corruptions in the ImageNet-C/CIFAR10-C test
