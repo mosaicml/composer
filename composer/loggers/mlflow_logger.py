1b24551dc10eb4cc846e404a568ab1e05a00bf44
Mihir Patel <mihir.v.patel7@gmail.com>
2024-06-18 18:13:43 -0400

    lint

--- composer/loggers/mlflow_logger.py
+++ composer/loggers/mlflow_logger.py
@@ -150,9 +150,12 @@ class MLFlowLogger(LoggerDestination):
                 )
             assert self.experiment_name is not None  # type hint
 
-            if os.getenv('DATABRICKS_TOKEN') is not None and not self.experiment_name.startswith(
-                ('/Users/', '/Shared/')
-            ):
+            if os.getenv(
+                'DATABRICKS_TOKEN',
+            ) is not None and not self.experiment_name.startswith((
+                '/Users/',
+                '/Shared/',
+            )):
                 try:
                     from databricks.sdk import WorkspaceClient
                 except ImportError as e:
