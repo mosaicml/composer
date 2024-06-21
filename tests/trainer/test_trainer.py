0903554a48a5153d6c7bae81420a551f3258f6a7
Joe Early <joe.early@helsing.ai>
2024-06-21 11:43:34 +0100

    Update unit test

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1250,27 +1250,29 @@ class TestTrainerInitOrFit:
         assert num_tokens_accum == num_tokens * 2
         assert batch_time_accum == datetime.timedelta(seconds=0.1 * (1 + 0))
 
-
-    # @pytest.mark.gpu
-    # @pytest.mark.world_size(2)
+    @pytest.mark.gpu
+    @pytest.mark.world_size(2)
     def test_rank_dependent_dataloader_lengths(
         self,
         model: ComposerModel,
         max_duration: Time[int],
     ):
         # Change rank 1 dataloader size to create different sized dataloaders on each rank
-        batch_size = 2
-        num_samples = 10
-        if dist.get_local_rank() == 1:
-            num_samples += 4
+        batch_size = 4
+        orig_num_samples = 16
+        rank_num_samples = orig_num_samples + 8 if dist.get_local_rank() == 1 else orig_num_samples
         # Create train and eval dataloaders (will have rank-dependent lengths)
-        train_dataset = RandomClassificationDataset(size=num_samples)
+        train_dataset = RandomClassificationDataset(size=rank_num_samples)
         train_dataloader = DataLoader(
-            dataset=train_dataset, batch_size=batch_size, sampler=dist.get_sampler(train_dataset)
+            dataset=train_dataset,
+            batch_size=batch_size,
+            sampler=dist.get_sampler(train_dataset),
         )
-        eval_dataset = RandomClassificationDataset(size=num_samples)
+        eval_dataset = RandomClassificationDataset(size=rank_num_samples)
         eval_dataloader = DataLoader(
-            dataset=eval_dataset, batch_size=batch_size, sampler=dist.get_sampler(eval_dataset)
+            dataset=eval_dataset,
+            batch_size=batch_size,
+            sampler=dist.get_sampler(eval_dataset),
         )
         # Fit (train + eval)
         trainer = Trainer(
@@ -1281,10 +1283,10 @@ class TestTrainerInitOrFit:
         )
         trainer.fit()
         # Check the correct number of samples and batches have been processed
-        assert trainer.state.timestamp.sample.value == num_samples
-        assert trainer.state.timestamp.batch.value == math.ceil(num_samples / batch_size)
-        assert trainer.state.eval_timestamp.sample.value == num_samples
-        assert trainer.state.eval_timestamp.batch.value == math.ceil(num_samples / batch_size)
+        assert trainer.state.timestamp.sample.value == orig_num_samples
+        assert trainer.state.timestamp.batch.value == orig_num_samples / batch_size / 2
+        assert trainer.state.eval_timestamp.sample.value == orig_num_samples
+        assert trainer.state.eval_timestamp.batch.value == orig_num_samples / batch_size / 2
 
 
 @world_size(1, 2)
