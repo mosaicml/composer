38c464cbfed599f159e495fdf19fb0879355a9ee
Evan Racah <ejracah@gmail.com>
2024-06-25 15:49:32 -0700

    load

--- composer/checkpoint/load.py
+++ composer/checkpoint/load.py
@@ -1,7 +1,7 @@
-from typing import List, Optional
+from typing import List, Optional, Sequence, Union
 from composer.core.state import State
 from composer.model.model import ComposerModel
-
+from composer.checkpoint.state_dict import _is_model_fsdp
 
 import torch
 # def load_checkpoint(
@@ -19,8 +19,8 @@ import torch
 def load_model_checkpoint(
     model: ComposerModel,
     load_path: str,
-    do_not_load_keys: Optional[List[str]]=None,
-    only_load_keys: Optional[List[str]]=None, 
+    include_keys: Optional[Union[str, Sequence[str]]] = None,
+    ignore_keys: Optional[Union[str, Sequence[str]]] = None, 
     sharded: bool=False
 ):
     """
@@ -29,10 +29,24 @@ def load_model_checkpoint(
     Args:
         model (ComposerModel): The model to load the checkpoint into.
         load_path (str): The path or uri to the checkpoint to load or symlink to the path/uri. If URI specified then files will be downloaded first.
-        load_ignore_keys (List[str]): The keys to not load from the model checkpoint. Note that if only_load_keys is specified, then this argument will be ignored.
-        only_load_keys (List[str]): The keys to only load from the model checkpoint. Note that if load_ignore_keys is specified, then this argument will be ignored.
+        include_keys (Optional[Union[str, Sequence[str]]]): The keys to include from the model checkpoint. Note that if ignore_keys is specified, then this argument will be ignored.
+        ignore_keys (Optional[Union[str, Sequence[str]]]): The keys to ignore from the model checkpoint. Note that if include_keys is specified, then this argument will be
         sharded (bool): If the checkpoint is sharded or not.
     """
+    is_model_sharded = _is_model_fsdp(model)
+    is_checkpoint_sharded =  _is_checkpoint_sharded(load_path)
+
+def _is_checkpoint_sharded(load_path: str) -> bool:
+    """
+    Check if the checkpoint at the specified path is sharded or not.
+
+    Args:
+        load_path (str): The path to the checkpoint to check.
+
+    Returns:
+        bool: True if the checkpoint is sharded, False otherwise.
+    """
+    pass
 
 def load_optim_checkpoint(
     optim: torch.optim.Optimizer,
