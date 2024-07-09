6dec8359374dc81b1569e21744c0942ff91f0e72
bigning <ning.wang@databricks.com>
2024-07-08 22:11:22 -0700

    [checkpoint v2] Download api (#3447)
    * a
    
    * a
    
    * fix lint and test
    
    * lint
    
    * comments
    
    * comment

--- composer/checkpoint/__init__.py
+++ composer/checkpoint/__init__.py
@@ -3,6 +3,7 @@
 
 """Module for checkpointing API."""
 
+from composer.checkpoint.download import download_monolithic_checkpoint
 from composer.checkpoint.state_dict import (
     get_metadata_state_dict,
     get_model_state_dict,
@@ -15,4 +16,5 @@ __all__ = [
     'get_optim_state_dict',
     'get_metadata_state_dict',
     'get_resumption_state_dict',
+    'download_monolithic_checkpoint',
 ]
