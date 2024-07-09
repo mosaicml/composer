a9ecbb89c518db92bec5a1190c7b211201732854
Chuck Tang <chuck.tang@databricks.com>
2024-07-09 16:51:05 -0700

    commit change

--- tests/trainer/test_trainer_eval.py
+++ tests/trainer/test_trainer_eval.py
@@ -94,8 +94,7 @@ def test_eval_with_nondivisible_dataset(world_size: int, size: int, batch_size: 
 
 from unittest.mock import call, patch
 
-@device('gpu')
-def test_fp8_autocast_called_with_enabled(device: str):
+def test_fp8_autocast_called_with_enabled():
     try:
         import transformer_engine.pytorch as te
     except ImportError:
