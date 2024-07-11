4eef8c9c5a96530e8ae81a007e71f6bac1a0f815
Chuck Tang <chuck.tang@databricks.com>
2024-07-10 17:34:16 -0700

    commit change

--- tests/trainer/test_trainer_eval.py
+++ tests/trainer/test_trainer_eval.py
@@ -116,12 +116,11 @@ def test_amp_fp8_eval_casts_to_bf16():
             sampler=dist.get_sampler(dataset),
         ),)
 
-    # Check that te.fp8_autocast was called with enabled=False and enabled=True in that order
-    expected_calls = [call(enabled=False), call(enabled=True)]
+    # Check that te.fp8_autocast was called with enabled=True and enabled=False in that order
+    expected_calls = [call(enabled=True), call(enabled=False)]
     # Compare only the first two calls in the actual calls list
     actual_calls = mock_fp8_autocast.call_args_list[:2]
     for expected_call, actual_call in zip(expected_calls, actual_calls):
-        print(expected_call.enabled, actual_call.enabled)
         assert expected_call.enabled == actual_call.enabled, 'fp8_autocast was not called with the expected arguments {} {}'.format(
             expected_call,
             actual_call,
