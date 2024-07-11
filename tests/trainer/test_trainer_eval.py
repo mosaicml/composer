9a88e388525535ef0a2e55c0b9dbd503e5479617
Chuck Tang <chuck.tang@databricks.com>
2024-07-10 18:06:36 -0700

    commit change

--- tests/trainer/test_trainer_eval.py
+++ tests/trainer/test_trainer_eval.py
@@ -100,7 +100,7 @@ from unittest.mock import call, patch
 def test_amp_fp8_eval_casts_to_bf16():
     # Check that we can import FP8 with TE. If not, skip this test.
     try:
-        import transformer_engine.pytorch as te
+        import transformer_engine
     except ImportError:
         pytest.skip('Precision amp_fp8 requires transformer-engine to be installed',)
 
