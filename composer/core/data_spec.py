c8e16ff8829303cf38e1f130d8b86dcd7b2a839c
jack-zhang_data <jack.zhang@databricks.com>
2024-07-25 09:41:01 -0700

    import transformers for smoke test

--- composer/core/data_spec.py
+++ composer/core/data_spec.py
@@ -13,6 +13,7 @@ import torch
 import torch.utils.data
 from torch.utils.data.distributed import DistributedSampler
 
+import transformers
 from transformers.tokenization_utils_base import BatchEncoding
 
 from composer.utils import dist, ensure_tuple
