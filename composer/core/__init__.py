a537c4c7c4c816b3fc6873b7db1ad6d33a060a6d
jack-zhang_data <jack.zhang@databricks.com>
2024-07-29 23:20:24 -0700

    fix circular imports

--- composer/core/__init__.py
+++ composer/core/__init__.py
@@ -10,6 +10,7 @@ Central parts of composer such as :class:`~.engine.Engine`, base class for criti
 
 from composer.core.algorithm import Algorithm
 from composer.core.callback import Callback
+from composer.core.state import State
 from composer.core.data_spec import DataSpec, ensure_data_spec
 from composer.core.engine import Engine, Trace
 from composer.core.evaluator import Evaluator, ensure_evaluator
@@ -17,7 +18,6 @@ from composer.core.event import Event
 from composer.core.passes import AlgorithmPass
 from composer.core.precision import Precision, get_precision_context
 from composer.core.serializable import Serializable
-from composer.core.state import State
 from composer.core.time import Time, Timestamp, TimeUnit, ensure_time
 from composer.core.types import JSON, Batch, Dataset, MemoryFormat, TrainerMode
 
