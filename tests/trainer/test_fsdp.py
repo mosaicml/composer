a193b76b2497e15af1a7d7e30c7365d965875cc2
jack-zhang_data <jack.zhang@databricks.com>
2024-07-30 19:03:32 -0700

    ignore runtime warning

--- tests/trainer/test_fsdp.py
+++ tests/trainer/test_fsdp.py
@@ -251,6 +251,7 @@ class SimpleMLPForTestingOOM(ComposerModel):
 
 @pytest.mark.gpu
 @pytest.mark.filterwarnings("ignore:`device_train_microbatch_size='auto'` may potentially fail with unexpected.*")
+@pytest.mark.filterwarnings("ignore:Automicrobatching changed the microbatch size from*")
 @world_size(2)
 def test_automicrobatching_fsdp(world_size: int):
     model = SimpleMLPForTestingOOM()
