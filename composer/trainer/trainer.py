11595f2a17460fed353def4d7f94735f260c3d8c
jack-zhang_data <jack.zhang@databricks.com>
2024-08-01 13:17:10 -0700

    post readd log

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -2905,8 +2905,8 @@ class Trainer:
                     # Readd sync hooks if they were previously turned off
                     if self.state.fsdp_enabled and len(self.automicrobatch_fsdp_hook_handles) == 0:
                         print("oom " + str(self.state.timestamp.batch))
-                        print(len(self.automicrobatch_fsdp_hook_handles))
                         self.automicrobatch_fsdp_hook_handles = _readd_fsdp_sync_hooks(self.fsdp_modules, sync_hook)
+                        print(len(self.automicrobatch_fsdp_hook_handles))
                     _adjust_device_train_microbatch_size(self.state)
                     self.num_consecutive_thrashes = 0
                     self.num_consecutive_non_OOM_batches = 0
