c4efc012877edee4bf64fc10a9c3e7dcfb4ad64a
Chuck Tang <chuck.tang@databricks.com>
2024-08-02 17:11:22 -0700

    commit change

--- tests/trainer/test_fsdp.py
+++ tests/trainer/test_fsdp.py
@@ -620,6 +620,27 @@ def test_fsdp_shard(world_size: int):
         max_duration='3ba',
     )
 
+@pytest.mark.gpu
+@world_size(2)
+def test_fsdp_invalid_config_throws_error(world_size: int):
+    model = SimpleModel()
+    model.fc1._fsdp_wrap = True  # pyright: ignore[reportGeneralTypeIssues]
+    model.fc2._fsdp_wrap = True  # pyright: ignore[reportGeneralTypeIssues]
+
+    expected_error = 'activation_cpu_offload=True is not supported with use_orig_params=False.'
+
+    with pytest.raises(ValueError, match=expected_error):
+        _ = Trainer(
+            model=model,
+            parallelism_config={
+                'fsdp': {
+                    'use_orig_params': False,
+                    'activation_cpu_offload': True,
+                },
+            },
+            max_duration='3ba',
+        )
+
 
 @pytest.mark.gpu
 @world_size(2)
