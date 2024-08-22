266288079f2f6b5298c47bc7398ae6f15cad843c
willgleich <22464726+willgleich@users.noreply.github.com>
2024-08-21 23:00:20 -0600

    Bump aws_ofi_nccl to 1.11.0 (#3569)

--- docker/generate_build_matrix.py
+++ docker/generate_build_matrix.py
@@ -237,7 +237,7 @@ def _main():
         if interconnect != 'EFA':
             entry['AWS_OFI_NCCL_VERSION'] = ''
         else:
-            entry['AWS_OFI_NCCL_VERSION'] = 'v1.9.1-aws'
+            entry['AWS_OFI_NCCL_VERSION'] = 'v1.11.0-aws'
 
         pytorch_entries.append(entry)
 
