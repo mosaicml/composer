249fe0c7214b2f60445619be2abbee413c41c45b
Irene Dea <deaairene@gmail.com>
2024-10-01 21:06:15 +0000

    Add event as a param

--- composer/callbacks/load_checkpoint.py
+++ composer/callbacks/load_checkpoint.py
@@ -2,32 +2,41 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Load a checkpoint."""
-from typing import Optional
+from typing import Optional, Union
 
 from composer.checkpoint.load import CheckpointLoadOptions, load_checkpoint
 from composer.core import Callback, State
+from composer.core.event import Event
 from composer.loggers import Logger
 
 
 class LoadCheckpoint(Callback):
-    """Callback that loads a checkpoint after other checkpoints have been loaded.
+    """Callback that loads a checkpoint at the specified event..
 
     Args:
         load_path: The path to the checkpoint to load.
         load_options: A dictionary of options to pass to the checkpoint loading function.
+        event: The event at which to load the checkpoint. Defaults to ``Event.BEFORE_LOAD``.
     """
 
-    def __init__(self, load_path: str, load_options: Optional[dict] = None):
+    def __init__(self, load_path: str, load_options: Optional[dict] = None, event: Union[str, Event] = Event.BEFORE_LOAD):
         super().__init__()
         self.load_path = load_path
         self.load_options = CheckpointLoadOptions(**(load_options or {}))
+        self.event = event if isinstance(event, Event) else Event[event.lower()]
 
-    def after_load(self, state: State, logger: Logger) -> None:
+    def run_event(self, event: Event, state: State, logger: Logger) -> None:
+        if event == self.event:
+            self._load(state, logger)
+        return super().run_event(event, state, logger)
+    
+    def _load(self, state: State, logger: Logger) -> None:
+        del logger  # unused
+
         load_checkpoint(
             load_path=self.load_path,
             load_options=self.load_options,
             state=state,
-            model_child_path=None,
-            optim_child_path=None,
+            model_child_path='',
+            optim_child_path='',
         )
-        return super().after_load(state, logger)
