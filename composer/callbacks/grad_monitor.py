b70bdc8d4c57b51a71b17d3dcece66f743438758
Dongqi Cai <dc912@aoraki.cl.cam.ac.uk>
2024-12-16 19:14:24 +0000

    store the averaged gradient clone in specific device

--- composer/callbacks/grad_monitor.py
+++ composer/callbacks/grad_monitor.py
@@ -40,19 +40,37 @@ class GradMonitor(Callback):
         self,
     )-> None:
         self.num_microbatches = 0
+        self.executed_steps = 0
 
-    def _extract_grads(self, state: State) -> None:
-        """Extracts gradients from the model and stores them in the state.
+    def _extract_grads(self, state: State, device: torch.device = torch.device('cpu')) -> None:
+        """Extracts gradients of each batch from the model
+        A running average of the gradients is stored in the state.
 
         Args:
             state (State): The state object.
+            device (torch.device, optional): The device to store the gradients. Defaults to CPU.
         """
+        
         group = list(state.model.parameters())
         grad_list = []
         for p in group:
             if p.grad is not None:
-                grad_list.append(p.grad)
-        state.grads = grad_list
+                grad_list.append(p.grad.detach().clone().to(device))
+        
+        # average the gradients
+        prev_grads = state.grads
+        if prev_grads:
+            aver_grad_list = [(prev_grads[i] * self.executed_steps + grad_list[i]) / (self.executed_steps + 1) for i in range(len(prev_grads))]
+        else: # the first batch, no need to average
+            aver_grad_list = grad_list
+        
+        self.executed_steps = self.executed_steps + 1
+
+        if self.executed_steps == state.local_steps: # averaged gradients will be sent to the cloud, so we can reset the counter
+            self.executed_steps = 0
+        
+        state.grads = aver_grad_list
+    
 
     def after_backward(self, state: State, logger: Logger) -> None:
         """Runs on ``Event.AFTER_BACKWARD`` in the function of _train_microbatch.
