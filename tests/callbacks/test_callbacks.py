5b797542af8a72986b3e789562dab0781e6f6605
Vincent Chen <v-chen_data@example.com>
2025-03-04 13:06:54 -0800

    precommit

--- tests/callbacks/test_callbacks.py
+++ tests/callbacks/test_callbacks.py
@@ -108,6 +108,7 @@ class TestCallbacks:
         # Add special handling for MLFlowLogger
         if cb_cls.__name__ == 'MLFlowLogger':
             import unittest.mock as mock
+
             import mlflow.utils.file_utils
 
             original_is_directory = mlflow.utils.file_utils.is_directory
@@ -146,6 +147,7 @@ class TestCallbacks:
         # Add special handling for MLFlowLogger
         if cb_cls.__name__ == 'MLFlowLogger':
             import unittest.mock as mock
+
             import mlflow.utils.file_utils
 
             original_is_directory = mlflow.utils.file_utils.is_directory
@@ -222,6 +224,7 @@ class TestCallbackTrains:
         # For MLFlowLogger, add a patch for the file_utils.is_directory function
         if cb_cls.__name__ == 'MLFlowLogger':
             import unittest.mock as mock
+
             import mlflow.utils.file_utils
 
             original_is_directory = mlflow.utils.file_utils.is_directory
