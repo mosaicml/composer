ae174eefadb31008af8efebf04c141570a0dcec9
Michael Shtelma <michael.shtelma@databricks.com>
2025-03-19 23:18:29 +0100

    Formatting

--- composer/callbacks/checkpoint_saver.py
+++ composer/callbacks/checkpoint_saver.py
@@ -277,7 +277,7 @@ class CheckpointSaver(Callback):  # noqa: D101
             root_temp_folder = None
             if os.environ.get('TMPDIR') is not None:
                 root_temp_folder = os.environ.get('TMPDIR')
-            elif pathlib.Path('/local_disk0').exists():  # Probably we are on MLR, so we have to use /local_disk0
+            elif os.path.exists('/local_disk0/'):  # Probably we are on MLR, so we have to use /local_disk0
                 root_temp_folder = '/local_disk0/temp'
             os.makedirs(root_temp_folder, exist_ok=True)
             local_folder = os.path.join(tempfile.mkdtemp(prefix=root_temp_folder), local_folder)
