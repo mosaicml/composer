004ef8dde20392fa3bb55201c120ea109deb0cf8
Rithwik Ediga Lakhamsani <rithwik.ediga@databricks.com>
2025-04-30 14:55:21 -0700

    it kinda works now

--- tests/trainer/test_activation_checkpointing.py
+++ tests/trainer/test_activation_checkpointing.py
@@ -5,11 +5,13 @@
 
 import pytest
 import torch
+import torch.nn as nn
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     _CHECKPOINT_WRAPPED_MODULE,
     ActivationWrapper,
     OffloadWrapper,
 )
+from torch.utils.checkpoint import _Holder
 
 from tests.common import (
     ComposerCounterModel,
@@ -19,7 +21,7 @@ from tests.common import (
 from tests.trainer.fsdp2_context import (
     fsdp2_context,
 )
-from tests.trainer.test_fsdp2 import create_trainer_with_model
+from tests.trainer.test_fsdp2 import create_trainer_with_model, parallelize_model, FSDP2Config
 
 
 @world_size(2)
@@ -241,3 +243,86 @@ def test_activation_checkpointing_cuda_memory_usage(world_size: int, type_of_che
 
     # Checkpointing and offloading should ideally use less memory than offloading alone
     assert both_mem <= offload_mem, 'Checkpointing and Offloading should use less memory than Offloading alone'
+
+
+@world_size(2)
+@fsdp2_context
+@pytest.mark.gpu
+@pytest.mark.parametrize('activation_checkpointing,activation_cpu_offload', [
+    (True, True),
+    (True, False),
+    (False, True),
+    (False, False),
+])
+def test_offloading_wrapper_works(world_size: int, activation_checkpointing: bool, activation_cpu_offload: bool):
+    """Test FSDP2 activation checkpointing CPU memory usage.
+    
+    We use the same methodology as pytorch (https://sourcegraph.com/github.com/pytorch/pytorch/-/blob/test/distributed/fsdp/test_checkpoint_wrapper.py?L341)
+    with some slight adjustments to support activation checkpointing wrapping as well.
+    """
+    del world_size
+
+    model = nn.Sequential(
+        nn.Linear(10, 10),
+        nn.Linear(10, 10),
+        nn.Linear(10, 10),
+    ).to('cuda')
+
+    if activation_checkpointing or activation_cpu_offload:
+        for module in model.children():
+            module._activation_checkpointing = True
+    
+    expected_device_type = "cpu" if activation_cpu_offload else "cuda"
+
+    # Patch saved_tensor_hooks to make the unpack keep the tensor on CPU for
+    # testing, otherwise the tensor access during the DFS will cause orig
+    # unpack to run, transferring the tensor back to GPU.
+    def patched_init(saved_tensor_hook_obj, pack_hook, _):
+        saved_tensor_hook_obj.pack_hook = pack_hook
+
+        def testing_cpu_offload_unpack_hook(packed):
+            # In cases where the checkpointing wrapper is used, the packed object is an instance of _Holder
+            # which is later used in the _checkpoint_hook function to recompute the tensor. In this test, since
+            # we are only validating the offloading, we can just return a zero tensor on the correct device to
+            # act as a no-op.
+            if isinstance(packed, _Holder):
+                return torch.zeros([1], device=torch.device(expected_device_type))
+            _, tensor = packed
+            return tensor
+
+        saved_tensor_hook_obj.unpack_hook = testing_cpu_offload_unpack_hook
+
+    orig_init = torch.autograd.graph.saved_tensors_hooks.__init__
+    torch.autograd.graph.saved_tensors_hooks.__init__ = patched_init
+
+    fsdp2_config = FSDP2Config(activation_cpu_offload=activation_cpu_offload, activation_checkpointing=activation_checkpointing)
+    parallelize_model(model=model, config=fsdp2_config)
+    inp = torch.randn(3, 10, device='cuda')
+    loss = model(inp).sum()
+
+    offload_verified = True 
+
+    def dfs(grad_fn):
+        for e in dir(grad_fn):
+            if not e.startswith("_saved_"):
+                continue
+
+            # the above unpack hook will be called when we do getattr(grad_fn, e) 
+            saved = getattr(grad_fn, e)
+            if isinstance(saved, torch.Tensor):
+                expected_device_type = "cpu" if activation_cpu_offload else "cuda"
+                if expected_device_type != saved.device.type:
+                    # If we encounter any tensor that's not on the expected device, we can immediately fail
+                    nonlocal offload_verified
+                    offload_verified = False
+
+        if hasattr(grad_fn, "next_functions"):
+            for next_grad_fn, _ in grad_fn.next_functions:
+                dfs(next_grad_fn)
+
+    dfs(loss.grad_fn)
+
+    assert offload_verified, "All autograd saved tensors should be offloaded to CPU"
+
+    torch.autograd.graph.saved_tensors_hooks.__init__ = orig_init
+    
