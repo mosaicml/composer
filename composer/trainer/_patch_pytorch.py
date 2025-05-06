bf313b05cfcf45362b22ae37c86a4fee420dcee2
Rithwik Ediga Lakhamsani <rithwik.ediga@databricks.com>
2025-05-06 11:44:10 -0700

    updating composer to support torch 2.7 and cuda 12.8

--- composer/trainer/_patch_pytorch.py
+++ composer/trainer/_patch_pytorch.py
@@ -121,6 +121,12 @@ def patch_pytorch():
         # No monkeypatches besides unshard (below)!
         pass
 
+    elif version.parse(torch.__version__) < version.parse('2.7.1'):
+        # Monkey patch for torch < 2.7.1 ie torch == 2.7.0
+
+        # No monkeypatches besides unshard (below)!
+        pass
+
 if version.parse(torch.__version__) >= version.parse('2.2.1') and version.parse(
         torch.__version__,) < version.parse('2.2.3'):
 
@@ -1060,7 +1066,7 @@ if version.parse(torch.__version__) >= version.parse('2.4.0') and version.parse(
 
 if version.parse(torch.__version__) >= version.parse('2.5.0') and version.parse(
         torch.__version__,
-) < version.parse('2.6.1'):
+) < version.parse('2.7.1'):
 
     # Save original FlatParamHandle.unshard to revert back to when dropping automicrobatching hooks
     from torch.distributed.fsdp._flat_param import FlatParamHandle
