81d4f8e00ad85840281adad31d52981892470704
Ethan Tang <ethan.tang@databricks.com>
2025-07-25 15:10:39 -0700

    reverted mlflow upgrade due to slowdowns (#3910)

--- tests/loggers/test_mlflow_logger.py
+++ tests/loggers/test_mlflow_logger.py
@@ -399,18 +399,19 @@ def test_mlflow_log_model(tmp_path, tiny_gpt2_model, tiny_gpt2_tokenizer):
             'model': tiny_gpt2_model,
             'tokenizer': tiny_gpt2_tokenizer,
         },
-        name='my_model',
+        artifact_path='my_model',
         task='llm/v1/completions',
     )
     test_mlflow_logger.post_close()
 
     run = _get_latest_mlflow_run(mlflow_exp_name, tracking_uri=mlflow_uri)
-    experiment_id = run.info.experiment_id
-    models_dir = mlflow_uri / Path(experiment_id) / 'models'
-    model_dir = next(models_dir.iterdir())
-    artifact_dir = model_dir / 'artifacts'
+    run_info = run.info
+    run_id = run_info.run_id
+    experiment_id = run_info.experiment_id
+    run_file_path = mlflow_uri / Path(experiment_id) / Path(run_id)
 
-    loaded_model = mlflow.transformers.load_model(artifact_dir, return_type='components')
+    model_directory = run_file_path / Path('artifacts') / Path('my_model')
+    loaded_model = mlflow.transformers.load_model(model_directory, return_type='components')
 
     # For some reason this is different, but its harmless. The actual attn implementation is the same
     loaded_model['model'].config._attn_implementation_autoset = False
